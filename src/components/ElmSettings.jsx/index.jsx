1b66dc1edb7d938207ef9578021d5147ebf0f982
Arif-Un <arifunctg@gmail.com>
2019-12-29 14:53:49 +0600

    converted into pure func component

--- src/components/ElmSettings.jsx/index.jsx
+++ src/components/ElmSettings.jsx/index.jsx
@@ -3,27 +3,34 @@ import React from 'react'
 import TextFieldSettings from './TextFieldSettings'
 import TextAreaSettings from './TextAreaSettings'
 import DateSettings from './DateSettings'
+import CheckBox from './CheckBox'
+import RadioBtn from './RadioBtn'
+import Select from './Select'
 
-export default class index extends React.Component {
-  renderSettings = (type) => {
+export default function index(props) {
+  const renderSettings = (type) => {
     switch (type) {
       case 'text-fld':
-        return <TextFieldSettings elm={this.props.elm} updateData={this.props.updateData} />
+        return <TextFieldSettings elm={props.elm} updateData={props.updateData} />
       case 'textarea':
-        return <TextAreaSettings elm={this.props.elm} updateData={this.props.updateData} />
+        return <TextAreaSettings elm={props.elm} updateData={props.updateData} />
       case 'date':
-        return <DateSettings elm={this.props.elm} updateData={this.props.updateData} />
+        return <DateSettings elm={props.elm} updateData={props.updateData} />
+      case 'ck':
+        return <CheckBox elm={props.elm} updateData={props.updateData} />
+      case 'rdo':
+        return <RadioBtn elm={props.elm} updateData={props.updateData} />
+      case 'select':
+        return <Select elm={props.elm} updateData={props.updateData} />
       default:
         return ''
     }
   }
 
-  render() {
-    return (
-      <div className="elm-settings">
-        <h4>Element Settings</h4>
-        {this.renderSettings(this.props.elm.type)}
-      </div>
-    )
-  }
+  return (
+    <div className="elm-settings">
+      <h4>Element Settings</h4>
+      {renderSettings(props.elm.type)}
+    </div>
+  )
 }
