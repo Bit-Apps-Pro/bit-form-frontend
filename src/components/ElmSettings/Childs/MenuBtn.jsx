89a524fd0a7475143621505d084076d13629a6c3
Bit-Code-Developer <developer@bitcode.pro>
2020-01-21 21:20:33 +0600

    From : Duplicate, Delete operation

--- src/components/ElmSettings/Childs/MenuBtn.jsx
+++ src/components/ElmSettings/Childs/MenuBtn.jsx
@@ -25,6 +25,12 @@ export default function MenuBtn(props) {
       console.log("Response", response)
     })
   }
+ const handleDuplicate = () => {
+    console.log(props.formID)
+    bitsFetch({id: props.formID},'bitapps_duplicate_aform').then(response=>{
+      console.log("Response", response)
+    })
+  }
   
   return (
     <div className="btcd-menu">
@@ -35,7 +41,7 @@ export default function MenuBtn(props) {
           {'  '}
           Edit
         </Link>
-        <button type="button" aria-label="actions">
+        <button type="button" aria-label="actions" onClick={handleDuplicate}>
           <span className="btcd-icn icn-copy" />
           {'  '}
           Duplicate
