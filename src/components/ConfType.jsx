242a83439d0e879387586ed9732cf1a21a9dfef1
Arif Uddin <arifunctg@gmail.com>
2020-03-18 17:57:08 +0600

    api moved to parent

--- src/components/ConfType.jsx
+++ src/components/ConfType.jsx
@@ -1,31 +1,15 @@
 /* eslint-disable no-undef */
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import ConfMsg from './ConfMsg'
 import RedirUrl from './RedirUrl'
 import WebHooks from './WebHooks'
-import bitsFetch from '../Utils/bitsFetch'
 
 export default function ConfType(props) {
   console.log('%c $render FormSettings', 'background:lightgreen;padding:3px;border-radius:5px;')
 
-  const { formSettings, setFormSettings } = props
+  const { formSettings, setFormSettings, formFields } = props
 
   const [pos, setPos] = useState(0)
-  const [formFields, setformFields] = useState(null)
-
-  useEffect(() => {
-    let mount = false
-    mount = true
-    bitsFetch({ id: props.formID }, 'bitapps_get_form_entry_count')
-      .then(res => {
-        if (res !== undefined && res.success) {
-          if (mount) {
-            setformFields(res.data.Labels)
-          }
-        }
-      })
-    return function cleanup() { mount = false }
-  }, [])
 
   return (
     <div className="btcd-f-c-t">
