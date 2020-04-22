0f31e16ff7aaac0428f5bfdd838cbf7602ac7620
Arif Uddin <arifunctg@gmail.com>
2020-04-22 19:43:13 +0600

    welcome created

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import greeting from '../resource/img/home.svg'
+
+export default function Welcome({ setModal }) {
+  return (
+    <div className="btcd-greeting">
+      <img src={greeting} alt="" />
+      <h2>Welcome to Bit Form</h2>
+      <div className="sub">
+        Thank you for install Bit Form.
+      </div>
+      <div>
+        Modern Form builder and database management  system
+        <br />
+        for Wordpress
+      </div>
+      <button onClick={() => setModal(true)} type="button" className="btn round btcd-btn-lg dp-blue">Create First From</button>
+    </div>
+  )
+}
