e7d98437f2f1aac4adeb1a42fa9fbe08beff623f
Arif Uddin <arifunctg@gmail.com>
2020-05-12 16:20:33 +0600

    responcive updated

--- src/pages/FormBuilder.jsx
+++ src/pages/FormBuilder.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { Container, Section, Bar } from 'react-simple-resizer'
 import GridLayout from '../components/GridLayout'
 import CompSettings from '../components/CompSettings/CompSettings'
@@ -7,16 +7,16 @@ import GridLayoutLoader from '../components/Loaders/GridLayoutLoader'
 
 function FormBuilder({ isLoading, newCounter, fields, setFields, subBtn, setSubBtn, lay, setLay, theme, setNewCounter, setFormName, formID, formType }) {
   const [tolbarSiz, setTolbarSiz] = useState(false)
-  const [gridWidth, setGridWidth] = useState(window.innerWidth - 480)
+  const [gridWidth, setGridWidth] = useState(window.innerWidth - 473)
   const [drgElm, setDrgElm] = useState(['', { h: 1, w: 1, i: '' }])
   const [elmSetting, setElmSetting] = useState({ id: null, data: { typ: '' } })
   const [newData, setNewData] = useState(null)
 
-  const notIE = !window.document.documentMode
-
   const conRef = React.createRef(null)
 
-  const setConSiz = () => {
+  const notIE = !window.document.documentMode
+
+  const setTolbar = useCallback(() => {
     const res = conRef.current.getResizer()
     if (res.getSectionSize(0) >= 160) {
       res.resizeSection(0, { toSize: 50 })
@@ -26,29 +26,70 @@ function FormBuilder({ isLoading, newCounter, fields, setFields, subBtn, setSubB
       setTolbarSiz(false)
     }
     conRef.current.applyResizer(res)
-  }
+  }, [conRef])
 
-  const updateFields = updatedElm => {
+  const updateFields = useCallback(updatedElm => {
     const tmp = { ...fields }
     // eslint-disable-next-line no-param-reassign
     fields[updatedElm.id] = updatedElm.data
     setFields(tmp)
-  }
+  }, [fields, setFields])
 
   const setElementSetting = elm => {
     setElmSetting(elm)
   }
 
-  const addNewData = ndata => {
+  const addNewData = useCallback(ndata => {
     setNewData(ndata)
-  }
+  }, [])
 
-  const setSubmitConfig = data => {
+  const setSubmitConfig = useCallback(data => {
     setSubBtn({ ...data })
-  }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [subBtn])
+
+  const onResize = useCallback(resizer => {
+    if (resizer.isBarActivated(1)) {
+      resizer.resizeSection(0, { toSize: resizer.getSectionSize(2) - 135 });
+    }
+  }, [])
+
+  const onResizeActivate = useCallback(() => {
+    document.querySelector('.tool-sec').style.transition = 'flex-grow 0ms'
+  }, [])
+
+  const afterResizing = useCallback(() => {
+    document.querySelector('.tool-sec').style.transition = 'flex-grow 500ms'
+  }, [])
+
+  const responsiveView = useCallback(view => {
+    const resizer = conRef.current.getResizer()
+    if (view === 'lg') {
+      resizer.resizeSection(0, { toSize: 165 });
+      resizer.resizeSection(2, { toSize: 300 });
+    } else if (view === 'md') {
+      const s0 = ((window.innerWidth - 473) / 2.5) - 165
+      const s2 = ((window.innerWidth - 473) / 2.5) - 300
+      resizer.resizeSection(0, { toSize: 165 + s0 });
+      resizer.resizeSection(2, { toSize: 300 + s2 });
+    } else if (view === 'sm') {
+      const s0 = ((window.innerWidth - 473) / 2) - 165
+      const s2 = ((window.innerWidth - 473) / 2) - 300
+      resizer.resizeSection(0, { toSize: 165 + s0 });
+      resizer.resizeSection(2, { toSize: 300 + s2 });
+    }
+    conRef.current.applyResizer(resizer)
+  }, [conRef])
 
   return (
-    <Container ref={conRef} className="btcd-bld-con" style={{ height: '100%' }}>
+    <Container
+      ref={conRef}
+      className="btcd-bld-con"
+      style={{ height: '100%' }}
+      beforeApplyResizer={onResize}
+      afterResizing={afterResizing}
+      onActivate={onResizeActivate}
+    >
       <Section
         className="tool-sec"
         defaultSize={165}
@@ -59,7 +100,7 @@ function FormBuilder({ isLoading, newCounter, fields, setFields, subBtn, setSubB
           setNewData={addNewData}
           className="tile"
           tolbarSiz={tolbarSiz}
-          setTolbarSiz={setConSiz}
+          setTolbar={setTolbar}
         />
       </Section>
       <Bar className="bar bar-l" />
@@ -69,7 +110,7 @@ function FormBuilder({ isLoading, newCounter, fields, setFields, subBtn, setSubB
         minSize={notIE && 320}
         defaultSize={gridWidth}
       >
-        {lay !== null && (
+        {lay.lg !== undefined && (
           <small
             style={{
               background: 'lightgray',
@@ -77,7 +118,7 @@ function FormBuilder({ isLoading, newCounter, fields, setFields, subBtn, setSubB
               display: 'none',
             }}
           >
-            {lay.map((item, i) => (
+            {lay.lg.map((item, i) => (
               <div
                 key={`k-${i + 10} `}
                 style={{
@@ -109,25 +150,32 @@ function FormBuilder({ isLoading, newCounter, fields, setFields, subBtn, setSubB
           </small>
         )}
         {!isLoading ? (
-          <GridLayout
-            theme={theme}
-            width={gridWidth}
-            draggedElm={drgElm}
-            setElmSetting={setElementSetting}
-            fields={fields}
-            newData={newData}
-            setNewData={setNewData}
-            formType={formType}
-            formID={formID}
-            setLay={setLay}
-            setFields={setFields}
-            setFormName={setFormName}
-            subBtn={subBtn}
-            isLoading={isLoading}
-            newCounter={newCounter}
-            setNewCounter={setNewCounter}
-            layout={lay}
-          />
+          <>
+            <div className="btcd-device-btn flx">
+              <button onClick={() => responsiveView('sm')} className="flx pos-rel tooltip phone" style={{ '--tooltip-txt': '"Phone View"' }} aria-label="responsive butoon" type="button"><span className="btcd-icn icn-phone_android" /></button>
+              <button onClick={() => responsiveView('md')} className="flx pos-rel tooltip tab" style={{ '--tooltip-txt': '"Tablet View"' }} aria-label="responsive butoon" type="button"><span className="btcd-icn icn-tablet_android" /></button>
+              <button onClick={() => responsiveView('lg')} className="flx pos-rel tooltip lap" style={{ '--tooltip-txt': '"Laptop View"' }} aria-label="responsive butoon" type="button"><span className="btcd-icn icn-laptop_mac" /></button>
+            </div>
+            <GridLayout
+              theme={theme}
+              width={gridWidth}
+              draggedElm={drgElm}
+              setElmSetting={setElementSetting}
+              fields={fields}
+              newData={newData}
+              setNewData={setNewData}
+              formType={formType}
+              formID={formID}
+              setLay={setLay}
+              setFields={setFields}
+              setFormName={setFormName}
+              subBtn={subBtn}
+              isLoading={isLoading}
+              newCounter={newCounter}
+              setNewCounter={setNewCounter}
+              layout={lay}
+            />
+          </>
         ) : <GridLayoutLoader />}
 
       </Section>
