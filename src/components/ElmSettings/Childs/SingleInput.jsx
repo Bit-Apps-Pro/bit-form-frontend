1c8bc4fc53496ceff5ba2c5af931459d790a9722
Arif Uddin <arifunctg@gmail.com>
2020-05-31 16:26:33 +0600

    style updated

--- src/components/ElmSettings/Childs/SingleInput.jsx
+++ src/components/ElmSettings/Childs/SingleInput.jsx
@@ -4,7 +4,7 @@ export default function SingleInput(props) {
   return (
     <div className={`mt-3 setting-inp ${props.className}`} style={{ ...(props.width && { width: props.width }) }}>
       <span>{props.title}</span>
-      <input type={props.inpType} onChange={props.action} value={props.value} placeholder={props.placeholder} />
+      <input className="btcd-paper-inp" type={props.inpType} onChange={props.action} value={props.value} placeholder={props.placeholder} />
     </div>
   )
 }
