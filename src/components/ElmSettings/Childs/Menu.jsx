69b47cea8610430375c40844a73b12667d0e32fc
Arif Uddin <arifunctg@gmail.com>
2020-06-03 18:28:58 +0600

    pop up problem solved

--- src/components/ElmSettings/Childs/Menu.jsx
+++ src/components/ElmSettings/Childs/Menu.jsx
@@ -25,7 +25,7 @@ export default function Menu(props) {
 
   return (
     <div className="btcd-menu">
-      <button ref={ref} onClick={handleMenu} className="icn-btn btcd-icn-lg tooltip" style={{ '--tooltip-txt': '"Column  Visibility"' }} aria-label="icon-btn" type="button">
+      <button ref={ref} onClick={handleMenu} className="icn-btn btcd-icn-lg tooltip" style={{ '--tooltip-txt': '"Column  Visibility"', '--left': '148%' }} aria-label="icon-btn" type="button">
         <span className={`btcd-icn ${props.icn}`} />
         {props.title}
       </button>
