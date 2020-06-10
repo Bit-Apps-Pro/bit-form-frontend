5e06a38ea631df560f05780674b239db5137370a
Arif Uddin <arifunctg@gmail.com>
2020-06-10 20:30:56 +0600

    dropdown updated

--- src/components/ElmSettings/Childs/DropDown.jsx
+++ src/components/ElmSettings/Childs/DropDown.jsx
@@ -1,9 +1,14 @@
-import React, { useEffect } from 'react'
-import SlimSelect from 'slim-select'
+import React from 'react'
+import MultiSelect from 'react-multiple-select-dropdown-lite'
+import 'react-multiple-select-dropdown-lite/dist/index.css'
 
 
-function DropDown(props) {
-  const s = React.useRef(null)
+function DropDown({ options, placeholder, action, className, isMultiple, value, addable, titleClassName, title,jsonValue }) {
+  if (!options || options === null || options.length === 0) {
+    // eslint-disable-next-line no-param-reassign
+    options = undefined
+  }
+  /* const s = React.useRef(null)
 
   const config = {
     select: s.current,
@@ -20,17 +25,26 @@ function DropDown(props) {
     config.select = s.current
     // eslint-disable-next-line no-new
     new SlimSelect(config)
-  }, [config])
+  }, [config]) */
 
   return (
-    <div className={`${props.titleClassName}`}>
-      <span>{props.title}</span>
-      <select value={props.value} ref={s} multiple={props.isMultiple} onChange={props.action} className={`btcd-app-slim ${props.className}`}>
+    <div className={`${titleClassName}`}>
+      <span>{title}</span>
+      <MultiSelect
+        width="100%"
+        defaultValue={value}
+        className={`${className}`}
+        onChange={action}
+        singleSelect={!isMultiple}
+        placeholder={placeholder}
+        jsonValue={jsonValue}
+        options={(options !== null && options !== false) && options}
+      />
+      {/* <select value={props.value} ref={s} multiple={props.isMultiple} onChange={props.action} className={`btcd-app-slim ${props.className}`}>
         {props.options !== null
           && props.options !== false
           && props.options.map(item => <option key={`key${item.name}`} value={item.value}>{item.name}</option>)}
-        {/* {props.children} */}
-      </select>
+      </select> */}
     </div>
   )
 }
