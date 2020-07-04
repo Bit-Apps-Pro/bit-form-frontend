2b0880f3ee832f557772ab69ab69ee4ebf7183d5
Arif Uddin <arifunctg@gmail.com>
2020-07-04 17:37:44 +0600

    cp updated

--- src/components/CompSettings/StyleCustomize/ChildComp/ColorPicker.jsx
+++ src/components/CompSettings/StyleCustomize/ChildComp/ColorPicker.jsx
@@ -10,12 +10,13 @@ export default function ColorPicker({ value, onChange }) {
     isComponentVisible,
     setIsComponentVisible,
   } = useComponentVisible(false)
-
   let isGradient = false
+
   let picrVal = { red: 0, green: 0, blue: 0, alpha: 0 }
 
-  if (value !== undefined && !value.includes('gradient')) {
-    const [r, g, b, a] = value.match(/\d|\W /g).join('').split(',')
+  if (value !== undefined && !value.match(/gradient/g)) {
+    const [r, g, b, a] = value.match(/\d\d\d|\d\d|\d/g)
+    // picrVal = { red: parseInt(r, 10), green: parseInt(g, 10), blue: parseInt(b, 10), alpha: parseInt(a, 10) }
     picrVal = { red: Number(r), green: Number(g), blue: Number(b), alpha: Number(a) }
   } else if (value !== undefined) {
     isGradient = true
@@ -23,10 +24,20 @@ export default function ColorPicker({ value, onChange }) {
 
   return (
     <div ref={ref} className="pos-rel">
+      <div
+        aria-label="color picker"
+        title="Color Picker"
+        onClick={() => setIsComponentVisible(!isComponentVisible)}
+        onKeyPress={() => setIsComponentVisible(!isComponentVisible)}
+        tabIndex="0"
+        role="button"
+        className="br-50 clr-pick"
+        style={{ background: value }}
+      />
       <CSSTransition
         in={isComponentVisible}
         timeout={150}
-        classNames="btc-pk"
+        classNames="btc-pk pos-rel"
         unmountOnExit
       >
         <div className="btc-pick">
@@ -37,16 +48,6 @@ export default function ColorPicker({ value, onChange }) {
           />
         </div>
       </CSSTransition>
-      <div
-        aria-label="color picker"
-        onClick={() => setIsComponentVisible(!isComponentVisible)}
-        onKeyPress={() => setIsComponentVisible(!isComponentVisible)}
-        tabIndex="0"
-        role="button"
-        className="br-50 clr-pick"
-        style={{ background: value }}
-      />
-
     </div>
   )
 }
