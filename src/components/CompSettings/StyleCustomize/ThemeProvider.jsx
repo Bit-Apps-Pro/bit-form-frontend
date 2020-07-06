fcc791e1c938ef13a340e96e0970f6c793aff937
Arif Uddin <arifunctg@gmail.com>
2020-07-06 11:30:04 +0600

    Create ThemeProvider.jsx

---
+++ src/components/CompSettings/StyleCustomize/ThemeProvider.jsx
@@ -0,0 +1,31 @@
+export const defaultTheme = {
+  '._frm': {
+    background: 'rgba(0, 255, 212, 1)',
+  },
+  '.fld-wrp': {
+    'min-height': 'inherit',
+    padding: '10px',
+    'box-sizing': 'border-box !important',
+    width: '100%',
+    cursor: 'default',
+    height: '100%',
+    background: 'rgba(37, 85, 122, 0.62)',
+  },
+
+  '.fld-wrp > * ': {
+    'box-sizing': 'border-box !important',
+  },
+
+  '@media only screen and (max-width: 600px)': {
+    '._frm': {
+      background: 'rgba(0, 215, 212, 1)',
+    },
+  },
+
+  '@media only screen and (max-width: 400px)': {
+    '._frm': {
+      background: 'rgba(0, 155, 212, 1)',
+    },
+  },
+}
+export const them = {}
