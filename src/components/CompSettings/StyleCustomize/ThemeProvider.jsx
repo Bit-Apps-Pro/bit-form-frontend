0c80b14b82894b36d0409c3d1a7ecbd7fd1cd63a
Arif Uddin <arifunctg@gmail.com>
2020-07-10 09:30:34 +0600

    Update ThemeProvider.jsx

--- src/components/CompSettings/StyleCustomize/ThemeProvider.jsx
+++ src/components/CompSettings/StyleCustomize/ThemeProvider.jsx
@@ -1,7 +1,8 @@
 export const defaultTheme = {
   '._frm': {
     overflow: 'hidden',
-    background: 'rgba(0, 255, 212, 1)',
+    // background: 'rgba(0, 255, 212, 1)',
+    background: 'linear-gradient(130deg,rgba(0, 0, 0, 1) 0%,rgba(178, 249, 255, 1) 0%,rgba(234, 251, 255, 1) 49.05271530151367%,rgba(239, 255, 234, 1) 100%)',
   },
   '.fld-wrp': {
     'min-height': 'inherit',
@@ -10,7 +11,7 @@ export const defaultTheme = {
     width: '100%',
     cursor: 'default',
     height: '100%',
-    background: 'rgba(37, 85, 122, 0.62)',
+    //  background: 'rgba(37, 85, 122, 0.62)',
   },
 
   '.fld-wrp > * ': {
