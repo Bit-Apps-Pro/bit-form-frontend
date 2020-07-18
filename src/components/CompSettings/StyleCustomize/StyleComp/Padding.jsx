b3afdc078f0b1d611779a8451dd5303abe561999
Arif Uddin <arifunctg@gmail.com>
2020-07-18 18:02:42 +0600

    new style comp

---
+++ src/components/CompSettings/StyleCustomize/StyleComp/Padding.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import StyleAccordion from '../ChildComp/StyleAccordion'
+import ResponsiveBtns from '../ChildComp/ResponsiveBtns'
+import Range from '../ChildComp/Range'
+
+export default function Padding({ style, cls, styleConfig, styleDispatch, brkPoint, setResponsiveView }) {
+  const padding = style?.[cls]?.['padding'] || '0px 0px 0px 0px'
+
+  const setPadding = value => {
+    styleDispatch({ apply: [{ cls, property: 'padding', delProp: false, value }], brkPoint })
+  }
+
+  return (
+    <StyleAccordion className="style-acc w-9" title="Padding">
+      {'responsive' in styleConfig && <ResponsiveBtns brkPoint={brkPoint} setResponsiveView={setResponsiveView} />}
+
+      <Range
+        info={[
+          { icn: 'd', lbl: 'Padding Top' },
+          { icn: 'd', lbl: 'Padding Right' },
+          { icn: 'd', lbl: 'Padding Bottom' },
+          { icn: 'd', lbl: 'Padding Left' },
+          { icn: <span className="btcd-icn icn-settings" />, lbl: 'All Side' },
+        ]}
+        className="btc-range"
+        unit="px"
+        maxRange={50}
+        value={padding}
+        onChange={setPadding}
+      />
+    </StyleAccordion>
+  )
+}
