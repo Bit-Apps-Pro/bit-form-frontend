390458bfa53583720553b8796f00495f51f572d6
Arif Uddin <arifunctg@gmail.com>
2020-07-18 18:02:04 +0600

    Update ColorPicker.jsx

--- src/components/CompSettings/StyleCustomize/ChildComp/ColorPicker.jsx
+++ src/components/CompSettings/StyleCustomize/ChildComp/ColorPicker.jsx
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react'
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useState } from 'react'
 import { ColorPicker as Picker } from 'react-color-gradient-picker';
 import 'react-color-gradient-picker/dist/index.css';
 import { CSSTransition } from 'react-transition-group';
 import useComponentVisible from './useComponentVisible';
 
-export default function ColorPicker({ value, onChange }) {
-  const {
-    ref,
-    isComponentVisible,
-    setIsComponentVisible,
-  } = useComponentVisible(false)
+ColorPicker.defaultProps = {
+  alwGradient: true,
+}
 
+export default function ColorPicker({ value, onChange, alwGradient }) {
+  const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(false)
+
+  const [gradient, setgradient] = useState(false)
   let isGradient = false
 
   let picrVal = { red: 0, green: 0, blue: 0, alpha: 0 }
@@ -20,30 +22,31 @@ export default function ColorPicker({ value, onChange }) {
     picrVal = { red: Number(r), green: Number(g), blue: Number(b), alpha: Number(a) }
   } else if (value !== undefined) {
     isGradient = true
-  }
-
-  const [gradient, setgradient] = useState(false)
-
-  let a = 'linear-gradient(113deg,rgba(0, 0, 0, 1) 0%,rgba(178, 249, 255, 1) 0%,rgba(234, 251, 255, 1) 49.05271530151367%,rgba(239, 255, 234, 1) 100%)'
-  a = a.split(',')
-  let i = 1
-  while (i < a.length - 1) {
-    let rgba = {}
-    console.log('sssssss', a[i], a[i + 1], a[i + 2], a[i + 3])
-    for (let j = i; j < i + 3; j += 1) {
-      // if(j){}
-      rgba.red = Number(a[j].replace(/\D/g, ''))
-      // const element = a[j]
-      console.log('sssss', Number(a[j].replace(/\D/g, '')))
-      if (j === i + 2) {
-        let t = a[j + 1].split(')')
-        // console.log('ssssssooooooooo', a[j + 1], parseInt(t[0], 10), parseInt(t[1], 10))
-      }
+    let i = 1
+    const str = value.split(',')
+    const color = {
+      degree: 0,
+      type: 'linear',
+      points: [],
     }
-    i += 4
-    console.log('sssss-------------')
+    if (str[0].match(/radial/g)) {
+      color.type = 'radial'
+      i = 0
+    }
+    for (; i < str.length - 1; i += 4) {
+      const rgba = {}
+      rgba.red = parseInt(str[i].replace(/\D/g, ''), 10)
+      rgba.green = parseInt(str[i + 1], 10)
+      rgba.blue = parseInt(str[i + 2], 10)
+      const [a, l] = str[i + 3].split(')')
+      rgba.alpha = parseInt(a, 10)
+      rgba.left = parseInt(l, 10)
+      color.points.push({ ...rgba })
+    }
+    color.degree = Number(str[0].replace(/\D/g, ''))
+    picrVal = color
   }
-  // console.log('ssssss', a)
+
 
   return (
     <div ref={ref} className="pos-rel">
@@ -55,7 +58,7 @@ export default function ColorPicker({ value, onChange }) {
         tabIndex="0"
         role="button"
         className="br-50 clr-pick"
-        style={{ background: value }}
+        style={{ background: value.replace('!important', '') }}
       />
       <CSSTransition
         in={isComponentVisible}
@@ -63,16 +66,19 @@ export default function ColorPicker({ value, onChange }) {
         classNames="btc-pk"
         unmountOnExit
         onEntered={() => isGradient && setgradient(true)}
+        onExit={() => setgradient(false)}
       >
         <div className="pos-rel">
           <div className="btc-pick">
             <div className="txt-center">
               <button onClick={(() => setgradient(false))} className={`btcd-btn-sm btn mr-1 ${gradient ? 'btcd-btn-o-blue' : 'blue'}`} type="button">Solid</button>
-              <button onClick={(() => setgradient(true))} className={`btcd-btn-sm btn ${gradient ? 'blue' : 'btcd-btn-o-blue'}`} type="button">Gradient</button>
+              {alwGradient && <button onClick={(() => setgradient(true))} className={`btcd-btn-sm btn ${gradient ? 'blue' : 'btcd-btn-o-blue'}`} type="button">Gradient</button>}
             </div>
             <Picker
               onChange={onChange}
-              color={picrVal}
+              onEndChange={onChange}
+              {...isGradient && { gradient: picrVal }}
+              {...!isGradient && { color: picrVal }}
               isGradient={gradient}
             />
           </div>
