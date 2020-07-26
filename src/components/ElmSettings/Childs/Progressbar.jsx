185ef975c9723fe4b6047bc90b9ffbd0e04220a1
Arif Uddin <arifunctg@gmail.com>
2020-07-26 16:42:15 +0600

    Update Progressbar.jsx

--- src/components/ElmSettings/Childs/Progressbar.jsx
+++ src/components/ElmSettings/Childs/Progressbar.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 
-export default function Progressbar(props) {
+export default function Progressbar({ value }) {
   return (
     <div className="flx" style={{ width: '100%' }}>
       <span style={{ width: 40 }}>
-        {props.value}
+        {value}
         %
       </span>
-
       <div className="btcd-prgrs-wrp">
-        <div className="btcd-prgrs" style={{ width: `${props.value}%` }} />
+        <div className="btcd-prgrs" style={{ width: `${value}%` }} />
       </div>
     </div>
   )
