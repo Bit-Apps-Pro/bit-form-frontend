c761e5e0dd66d4bf84b4a4a96630a40df937eafa
Arif Uddin <arifunctg@gmail.com>
2020-08-12 16:06:13 +0600

    Update MenuBtn.jsx

--- src/components/ElmSettings/Childs/MenuBtn.jsx
+++ src/components/ElmSettings/Childs/MenuBtn.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function MenuBtn(props) {
+
   const handleMenuClose = (e) => {
     const el = e.target
     setTimeout(() => {
