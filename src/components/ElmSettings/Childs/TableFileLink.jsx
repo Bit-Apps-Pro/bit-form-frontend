2fe05ba591de2326612140852d7b040b41fde742
Md Rubel Amin <mdrubelamin2@gmail.com>
2020-08-15 17:50:50 +0600

    response drawer bug fix

--- src/components/ElmSettings/Childs/TableFileLink.jsx
+++ src/components/ElmSettings/Childs/TableFileLink.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function TableFileLink(props) {
   return (
     <div className="btcd-t-link flx mr-2">
-      <div className="tooltip" style={{ '--tooltip-txt': `"${props.fname.substr(11)}"` }}>
+      <div className="tooltip" style={{ '--tooltip-txt': `"${props.fname}"`, whiteSpace: 'nowrap', width: `${props.width ? props.width : '80'}px` }}>
         <a
           href={`${props.link}`}
           rel="noopener noreferrer"
@@ -11,10 +11,10 @@ function TableFileLink(props) {
         >
           <span className="btcd-icn icn-file" />
           {' '}
-          {props.fname.substr(11)}
+          {props.fname}
         </a>
       </div>
-      <a target="_blank" href={`${props.link}&download`} download rel="noopener noreferrer" aria-label="Download" className="icn-btn icn-btn-sm"><span className="btcd-icn icn-file_download" /></a>
+      <a target="_blank" href={`${props.link}&download`} download rel="noopener noreferrer" aria-label="Download" className="icn-btn icn-btn-sm flx"><span className="btcd-icn icn-file_download" /></a>
     </div>
   )
 }
