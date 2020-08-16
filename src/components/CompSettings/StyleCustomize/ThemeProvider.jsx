06f2a8a4b91de236cbe2d7d05e9abced13a77f83
Arif Uddin <arifunctg@gmail.com>
2020-08-16 17:07:02 +0600

    Update ThemeProvider.jsx

--- src/components/CompSettings/StyleCustomize/ThemeProvider.jsx
+++ src/components/CompSettings/StyleCustomize/ThemeProvider.jsx
@@ -3,51 +3,54 @@ export const defaultTheme = {
     overflow: 'auto',
     'border-radius': '10px 10px 10px 10px',
     'background-size': '100% 100%',
-    'background-image': 'url(https://jooinn.com/images/abstract-background-278.jpg)',
   },
+  '._frm-g': {},
   '._frm': {
-    // 'backdrop-filter': 'blur(32px)',
-    'background-color': 'rgba(242, 246, 249, 0.59)',
-    'border-color': 'rgba(0, 0, 0, 1)',
-    'border-radius': '10px 10px 10px 10px',
-    'border-style': 'solid',
-    'border-width': '0px 0px 0px 0px',
+    'background-color': 'rgba(255, 255, 255, 1)',
     margin: '0px 0px 0px 0px',
     overflow: 'hidden',
-    padding: '50px 50px 50px 50px',
+    padding: '15px 15px 15px 15px',
   },
   '.fld-wrp': {
     padding: '10px 10px 10px 10px',
   },
-
   '.fld-lbl': {
     'font-size': '16px',
-    color: 'rgba(54, 54, 54,1)',
+    color: 'rgba(42, 49, 99, 1)!important',
   },
-
   'input.fld,textarea.fld': {
     'background-color': 'rgba(0, 0, 0, 0)',
-    'border-color': 'rgba(100, 138, 168, 1)!important',
-    'border-radius': '0px 0px 0px 0px!important',
+    'border-color': 'rgba(199, 212, 221, 1)!important',
+    'border-radius': '8px 8px 8px 8px!important',
     'border-style': 'solid!important',
-    'border-width': '0px 0px 1px 0px!important',
+    'border-width': '1px 1px 1px 1px!important',
     'font-size': '15px!important',
     color: 'rgba(0, 0, 0, 1)!important',
     margin: '5px 0 0 0!important',
-    padding: '8px 8px 8px 8px!important',
+    padding: '10px 8px 10px 8px!important',
   },
-
-  /*  'input.fld::placeholder,textarea.fld::placeholder': {
-     color: 'rgba(16, 64, 86, 1) !important',
-   }, */
   'input.fld:focus,textarea.fld:focus': {
-    'border-width': '0px 0px 2px 0px!important',
+    'box-shadow': '0px 0px 0px 3px rgba(151, 203, 252, 0.38) !important',
+    'border-color': 'rgba(29, 158, 249, 1)!important',
   },
-
   '.fld>.btcd-ck-wrp span:first-child': {
     color: 'rgba(0, 0, 0, 1) !important',
   },
-
+  'input.fld::placeholder,textarea.fld::placeholder': {
+    color: 'rgba(213, 212, 221, 1)!important',
+  },
+  'input.fld::-webkit-input-placeholder,textarea.fld::-webkit-input-placeholder': {
+    color: 'rgba(213, 212, 221, 1)!important',
+  },
+  'input.fld::-ms-input-placeholder,textarea.fld::-ms-input-placeholder': {
+    color: 'rgba(213, 212, 221, 1)!important',
+  },
+  'input.fld:-ms-input-placeholder,textarea.fld:-ms-input-placeholder': {
+    color: 'rgba(213, 212, 221, 1)!important',
+  },
+  'input.fld:hover,textarea.fld:hover': {
+    'border-color': 'rgba(29, 158, 249, 1)!important',
+  },
   '@media only screen and (max-width: 600px)': {},
   '@media only screen and (max-width: 400px)': {},
 }
