7de887883e7461b1578a2fb413e3d7d3825c910a
Arif Uddin <arifunctg@gmail.com>
2020-08-16 17:07:48 +0600

    encrypt function added

--- src/Utils/Helpers.js
+++ src/Utils/Helpers.js
@@ -65,3 +65,32 @@ export const multiAssign = (obj, assignArr) => {
     }
   }
 }
+
+const cipher = salt => {
+  const textToChars = text => text.split('').map(c => c.charCodeAt(0));
+  const byteHex = n => (`0${Number(n).toString(16)}`).substr(-2);
+  // eslint-disable-next-line no-bitwise
+  const applySaltToChar = code => textToChars(salt).reduce((a, b) => a ^ b, code);
+
+  return text => text
+    .split('')
+    .map(textToChars)
+    .map(applySaltToChar)
+    .map(byteHex)
+    .join('');
+};
+
+const decipher = salt => {
+  const textToChars = text => text.split('').map(c => c.charCodeAt(0));
+  // eslint-disable-next-line no-bitwise
+  const applySaltToChar = code => textToChars(salt).reduce((a, b) => (a ^ b), code);
+  return encoded => encoded
+    .match(/.{1,2}/g)
+    .map(hex => parseInt(hex, 16))
+    .map(applySaltToChar)
+    .map(charCode => String.fromCharCode(charCode))
+    .join('');
+}
+
+export const bitCipher = cipher('btcd');
+export const bitDecipher = decipher('btcd')
