c62aa3b9f369747db4be61a3e94213adb9c19628
Arif Uddin <arifunctg@gmail.com>
2020-09-10 12:16:20 +0600

    updated

--- src/components/ElmSettings/Childs/CopyText.jsx
+++ src/components/ElmSettings/Childs/CopyText.jsx
@@ -15,7 +15,7 @@ export default function CopyText({ value, setSnackbar, className }) {
   return (
     <div className={className}>
       <label htmlFor={value} className="flx">
-        <input value={value} readOnly />
+        <input className="w-10" value={value} readOnly />
         <button onClick={copyText} className="tooltip" style={{ '--tooltip-txt': '"Copy"' }} aria-label="Copy" type="button"><span className="btcd-icn icn-copy" /></button>
       </label>
     </div>
