9978854cbb7f39ecef46cd4f981c694e2fd3ff00
Arif Uddin <arifunctg@gmail.com>
2020-09-12 18:39:46 +0600

    Update Integrations.jsx

--- src/components/Integrations.jsx
+++ src/components/Integrations.jsx
@@ -113,6 +113,7 @@ function Integrations({ integrations, setIntegration, formFields, setProModal })
               title="Available Integrations"
               show={showMdl}
               setModal={setShowMdl}
+              style={{ width: 1000 }}
             >
               <div className="flx flx-wrp btcd-inte-wrp">
                 {integs.map((inte, i) => (
