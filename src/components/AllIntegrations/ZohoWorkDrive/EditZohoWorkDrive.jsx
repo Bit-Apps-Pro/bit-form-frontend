afcadafd4909e9e3759b92a36c8000d1ab23cd36
Md Rubel Amin <mdrubelamin2@gmail.com>
2020-09-28 13:36:30 +0600

    Update EditZohoWorkDrive.jsx

--- src/components/AllIntegrations/ZohoWorkDrive/EditZohoWorkDrive.jsx
+++ src/components/AllIntegrations/ZohoWorkDrive/EditZohoWorkDrive.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-param-reassign */
-import React, { useContext, useState } from 'react'
+import React, { useState } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
-import { FormSaveContext } from '../../../pages/FormDetails'
 import SnackMsg from '../../ElmSettings/Childs/SnackMsg'
 import { saveIntegConfig } from '../IntegrationHelpers/IntegrationHelpers'
 import IntegrationStepThree from '../IntegrationHelpers/IntegrationStepThree'
@@ -11,7 +10,6 @@ import ZohoWorkDriveIntegLayout from './ZohoWorkDriveIntegLayout'
 function EditZohoRecruit({ formFields, setIntegration, integrations, allIntegURL }) {
   const history = useHistory()
   const { id, formID } = useParams()
-  const saveForm = useContext(FormSaveContext)
 
   const [workDriveConf, setWorkDriveConf] = useState({ ...integrations[id] })
   const [isLoading, setisLoading] = useState(false)
@@ -20,7 +18,7 @@ function EditZohoRecruit({ formFields, setIntegration, integrations, allIntegURL
   console.log('workDriveConf', workDriveConf)
 
   const saveConfig = () => {
-    saveIntegConfig(integrations, setIntegration, allIntegURL, workDriveConf, history, saveForm, id, 1)
+    saveIntegConfig(integrations, setIntegration, allIntegURL, workDriveConf, history, id, 1)
   }
 
   return (
