677ada74ac69037779d3217873f0f9f43a6b3655
Md Rubel Amin <mdrubelamin2@gmail.com>
2020-09-28 13:33:52 +0600

    zohoAnalytics update

--- src/components/AllIntegrations/zohoanalytics/EditZohoAnalytics.jsx
+++ src/components/AllIntegrations/zohoanalytics/EditZohoAnalytics.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-param-reassign */
-import React, { useContext, useState } from 'react'
+import React, { useState } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
-import { FormSaveContext } from '../../../pages/FormDetails'
 import SnackMsg from '../../ElmSettings/Childs/SnackMsg'
 import { saveIntegConfig } from '../IntegrationHelpers/IntegrationHelpers'
 import IntegrationStepThree from '../IntegrationHelpers/IntegrationStepThree'
@@ -11,20 +10,10 @@ import ZohoAnalyticsIntegLayout from './ZohoAnalyticsIntegLayout'
 function EditZohoRecruit({ formFields, setIntegration, integrations, allIntegURL }) {
   const history = useHistory()
   const { id, formID } = useParams()
-  const saveForm = useContext(FormSaveContext)
 
   const [analyticsConf, setAnalyticsConf] = useState({ ...integrations[id] })
   const [isLoading, setisLoading] = useState(false)
   const [snack, setSnackbar] = useState({ show: false })
-  const [actionMdl, setActionMdl] = useState({ show: false })
-
-  const saveConfig = () => {
-    if (analyticsConf.actions?.update && analyticsConf.actions?.update.criteria === '' && actionMdl.show !== 'criteria') {
-      setActionMdl({ show: 'criteria' })
-      return
-    }
-    saveIntegConfig(integrations, setIntegration, allIntegURL, analyticsConf, history, saveForm, id, 1)
-  }
 
   return (
     <div style={{ width: 900 }}>
@@ -46,14 +35,11 @@ function EditZohoRecruit({ formFields, setIntegration, integrations, allIntegURL
         isLoading={isLoading}
         setisLoading={setisLoading}
         setSnackbar={setSnackbar}
-        actionMdl={actionMdl}
-        setActionMdl={setActionMdl}
-        action={saveConfig}
       />
 
       <IntegrationStepThree
         edit
-        saveConfig={saveConfig}
+        saveConfig={() => saveIntegConfig(integrations, setIntegration, allIntegURL, analyticsConf, history, id, 1)}
         disabled={analyticsConf.workspace === '' || analyticsConf.table === '' || analyticsConf.field_map.length < 1}
       />
       <br />
