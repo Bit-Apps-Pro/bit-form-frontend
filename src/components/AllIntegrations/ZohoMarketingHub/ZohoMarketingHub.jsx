971e8b0d53ac161d073c19de6cfbc86bc3a17dd8
Md Rubel Amin <mdrubelamin2@gmail.com>
2020-10-25 16:02:04 +0600

    zoho marketing hub

---
+++ src/components/AllIntegrations/ZohoMarketingHub/ZohoMarketingHub.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from 'react'
+import 'react-multiple-select-dropdown-lite/dist/index.css'
+import { useHistory, useParams } from 'react-router-dom'
+import SnackMsg from '../../ElmSettings/Childs/SnackMsg'
+import Steps from '../../ElmSettings/Childs/Steps'
+import { handleAuthorize, saveIntegConfig, setGrantTokenResponse } from '../IntegrationHelpers/IntegrationHelpers'
+import IntegrationStepOne from '../IntegrationHelpers/IntegrationStepOne'
+import IntegrationStepThree from '../IntegrationHelpers/IntegrationStepThree'
+import { checkMappedFields, handleInput, refreshLists } from './ZohoMarketingHubCommonFunc'
+import ZohoMarketingHubIntegLayout from './ZohoMarketingHubIntegLayout'
+
+function ZohoMarketingHub({ formFields, setIntegration, integrations, allIntegURL }) {
+  const history = useHistory()
+  const { formID } = useParams()
+  const [isAuthorized, setisAuthorized] = useState(false)
+  const [isLoading, setisLoading] = useState(false)
+  const [step, setstep] = useState(1)
+  const [error, setError] = useState({ dataCenter: '', clientId: '', clientSecret: '' })
+  const [snack, setSnackbar] = useState({ show: false })
+  const scopes = 'ZohoMarketingHub.lead.READ,ZohoMarketingHub.lead.CREATE,ZohoMarketingHub.lead.UPDATE'
+  const [marketingHubConf, setMarketingHubConf] = useState({
+    name: 'Zoho Marketing Hub API',
+    type: 'Zoho Marketing Hub',
+    clientId: process.env.NODE_ENV === 'development' ? '1000.PFZHKP6NP8HCCM90TDLLPDNUFCCTZX' : '',
+    clientSecret: process.env.NODE_ENV === 'development' ? 'da7c284de2969d24dda6a167bd4980d225d4a9233b' : '',
+    list: '',
+    field_map: [
+      { formField: '', zohoFormField: '' },
+    ],
+  })
+
+  useEffect(() => {
+    window.opener && setGrantTokenResponse('zohoMarketingHub')
+  }, [])
+
+  const nextPage = val => {
+    if (val === 3) {
+      if (!checkMappedFields(marketingHubConf)) {
+        setSnackbar({ show: true, msg: 'Please map mandatory fields' })
+        return
+      }
+
+      if (marketingHubConf.list !== '' && marketingHubConf.table !== '' && marketingHubConf.field_map.length > 0) {
+        setstep(val)
+      }
+    } else {
+      setstep(val)
+      if (val === 2 && !marketingHubConf.list) {
+        refreshLists(formID, marketingHubConf, setMarketingHubConf, setisLoading, setSnackbar)
+      }
+    }
+
+    document.querySelector('.btcd-s-wrp').scrollTop = 0
+  }
+
+  console.log('marketingHubConf', marketingHubConf);
+
+  return (
+    <div>
+      <SnackMsg snack={snack} setSnackbar={setSnackbar} />
+      <div className="txt-center w-9 mt-2"><Steps step={3} active={step} /></div>
+
+      {/* STEP 1 */}
+      <IntegrationStepOne
+        step={step}
+        confTmp={marketingHubConf}
+        handleInput={(e) => handleInput(e, formID, marketingHubConf, setMarketingHubConf, setisLoading, setSnackbar, true, error, setError)}
+        error={error}
+        setSnackbar={setSnackbar}
+        handleAuthorize={() => handleAuthorize('zohoMarketingHub', 'zmarketingHub', scopes, marketingHubConf, setMarketingHubConf, setError, setisAuthorized, setisLoading, setSnackbar)}
+        isLoading={isLoading}
+        isAuthorized={isAuthorized}
+        nextPage={nextPage}
+      />
+
+      {/* STEP 2 */}
+      <div className="btcd-stp-page" style={{ width: step === 2 && 900, height: step === 2 && `${100}%` }}>
+        <ZohoMarketingHubIntegLayout
+          formID={formID}
+          formFields={formFields}
+          handleInput={(e) => handleInput(e, formID, marketingHubConf, setMarketingHubConf, setisLoading, setSnackbar)}
+          marketingHubConf={marketingHubConf}
+          setMarketingHubConf={setMarketingHubConf}
+          isLoading={isLoading}
+          setisLoading={setisLoading}
+          setSnackbar={setSnackbar}
+        />
+
+        <button
+          onClick={() => nextPage(3)}
+          disabled={marketingHubConf.list === '' || marketingHubConf.table === '' || marketingHubConf.field_map.length < 1}
+          className="btn f-right btcd-btn-lg green sh-sm flx"
+          type="button"
+        >
+          Next &nbsp;
+          <div className="btcd-icn icn-arrow_back rev-icn d-in-b" />
+        </button>
+
+      </div>
+
+      {/* STEP 3 */}
+      <IntegrationStepThree
+        step={step}
+        saveConfig={() => saveIntegConfig(integrations, setIntegration, allIntegURL, marketingHubConf, history)}
+      />
+    </div>
+  )
+}
+
+export default ZohoMarketingHub
---
+++ src/components/AllIntegrations/ZohoMarketingHub/ZohoMarketingHubCommonFunc.js
@@ -0,0 +1,111 @@
+import bitsFetch from '../../../Utils/bitsFetch'
+
+export const handleInput = (e, formID, marketingHubConf, setMarketingHubConf, setisLoading, setSnackbar, isNew, error, setError) => {
+  let newConf = { ...marketingHubConf }
+  if (isNew) {
+    const rmError = { ...error }
+    rmError[e.target.name] = ''
+    setError({ ...rmError })
+  }
+  newConf[e.target.name] = e.target.value
+
+  switch (e.target.name) {
+    case 'list':
+      newConf = listChange(newConf, formID, setMarketingHubConf, setisLoading, setSnackbar)
+      break;
+    default:
+      break;
+  }
+  setMarketingHubConf({ ...newConf })
+}
+
+export const listChange = (marketingHubConf, formID, setMarketingHubConf, setisLoading, setSnackbar) => {
+  const newConf = { ...marketingHubConf }
+  newConf.field_map = [{ formField: '', zohoFormField: 'Contact Email' }]
+
+  if (!newConf?.default?.fields?.[newConf.list]) {
+    refreshContactFields(formID, newConf, setMarketingHubConf, setisLoading, setSnackbar)
+  }
+  return newConf
+}
+
+export const refreshLists = (formID, marketingHubConf, setMarketingHubConf, setisLoading, setSnackbar) => {
+  setisLoading(true)
+  const refreshListsRequestParams = {
+    formID,
+    id: marketingHubConf.id,
+    dataCenter: marketingHubConf.dataCenter,
+    clientId: marketingHubConf.clientId,
+    clientSecret: marketingHubConf.clientSecret,
+    tokenDetails: marketingHubConf.tokenDetails,
+  }
+  bitsFetch(refreshListsRequestParams, 'bitforms_zmarketingHub_refresh_lists')
+    .then(result => {
+      if (result && result.success) {
+        const newConf = { ...marketingHubConf }
+        if (result.data.lists) {
+          newConf.default = { ...newConf.default, lists: result.data.lists }
+        }
+        setSnackbar({ show: true, msg: 'Lists refreshed' })
+        setMarketingHubConf({ ...newConf })
+      } else if ((result && result.data && result.data.data) || (!result.success && typeof result.data === 'string')) {
+        setSnackbar({ show: true, msg: `Lists refresh failed Cause:${result.data.data || result.data}. please try again` })
+      } else {
+        setSnackbar({ show: true, msg: 'Lists refresh failed. please try again' })
+      }
+      setisLoading(false)
+    })
+    .catch(() => setisLoading(false))
+}
+
+export const refreshContactFields = (formID, marketingHubConf, setMarketingHubConf, setisLoading, setSnackbar) => {
+  const { list } = marketingHubConf
+  if (!list) {
+    return
+  }
+
+  setisLoading(true)
+  const refreshContactFieldsRequestParams = {
+    formID,
+    list,
+    dataCenter: marketingHubConf.dataCenter,
+    clientId: marketingHubConf.clientId,
+    clientSecret: marketingHubConf.clientSecret,
+    tokenDetails: marketingHubConf.tokenDetails,
+  }
+  bitsFetch(refreshContactFieldsRequestParams, 'bitforms_zmarketingHub_refresh_contact_fields')
+    .then(result => {
+      if (result && result.success) {
+        const newConf = { ...marketingHubConf }
+        if (result.data.fields) {
+          if (!newConf.default.fields) {
+            newConf.default.fields = {}
+          }
+
+          newConf.default.fields[list] = result.data
+
+          setSnackbar({ show: true, msg: 'Contact Fields refreshed' })
+        } else {
+          setSnackbar({ show: true, msg: "Zoho didn't provide fields names for this list" })
+        }
+
+        if (result.data.tokenDetails) {
+          newConf.tokenDetails = result.data.tokenDetails
+        }
+        setMarketingHubConf({ ...newConf })
+      } else {
+        setSnackbar({ show: true, msg: 'Contact Fields refresh failed. please try again' })
+      }
+      setisLoading(false)
+    })
+    .catch(() => setisLoading(false))
+}
+
+export const checkMappedFields = (marketingHubConf) => {
+  const mappedFields = marketingHubConf?.field_map ? marketingHubConf.field_map.filter(mappedField => (!mappedField.formField && mappedField.zohoFormField && marketingHubConf?.default?.fields?.[marketingHubConf.list]?.required.indexOf(mappedField.zohoFormField) !== -1)) : []
+  if (mappedFields.length > 0) {
+    return false
+  }
+
+  return true
+}
