4c74b35434ee9247b2d6d88ede3285002e89f143
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2020-12-28 15:17:36 +0600

    Create Import.jsx
    Import file added

---
+++ src/components/ExportImport/Import.jsx
@@ -0,0 +1,56 @@
+import { useRef, useState } from 'react'
+import Modal from '../Modal'
+import bitsFetch from '../../Utils/bitsFetch'
+import ImportStepOne from './ImportStepOne'
+import ImportStepTwo from './ImportStepTwo'
+
+export default function Import({ cols, formID, close, showImportMdl }) {
+  const columns = cols.filter((col) => col.Header !== '#' && typeof col.Header !== 'object')
+  const formName = bits.allForms.find(form => form.id === formID)?.form_name
+  const formRef = useRef(null)
+  const [fileResponses, setResponses] = useState([])
+  const [filecolumns, setFilieColumns] = useState([])
+  const [step, setStep] = useState(1)
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const formData = new FormData(formRef.current)
+    const file = document.getElementById('file').files
+    if (file.length === 0) {
+      alert("Please select a file")
+      return false
+    }
+    // eslint-disable-next-line default-case
+    switch (step) {
+      case 1:
+        const filedata = sessionStorage.getItem('file_data')
+        setResponses(filedata)
+        const headers = sessionStorage.getItem('file_header')
+        setFilieColumns(headers?.split(','))
+        setStep(2)
+    }
+  }
+  return (
+    <div>
+      <Modal lg show={showImportMdl} setModal={close} title={`Import Data for ${formName} [Step ${step} of 2]`}>
+        {step === 1 && (
+          <form
+            ref={formRef}
+            method="POST"
+            onSubmit={handleSubmit}
+          >
+            <ImportStepOne />
+          </form>
+        )}
+        {step === 2 && (
+          <form>
+            <ImportStepTwo
+              fileResponses={fileResponses}
+              fileColumns={filecolumns}
+              tableColumns={columns}
+            />
+          </form>
+        )}
+      </Modal>
+    </div>
+  )
+}
