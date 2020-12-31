e1ee5d3a4120b23e6ad4d3f72df2fe0b4c8a95db
Arif Uddin <arifunctg@gmail.com>
2020-12-31 11:53:37 +0600

    Create DropdownStyleEditors.jsx

---
+++ src/components/CompSettings/StyleCustomize/DropdownStyleEditors.jsx
@@ -0,0 +1,161 @@
+/* eslint-disable no-underscore-dangle */
+import { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import BorderIcn from '../../../Icons/BorderIcn';
+import ColorPicker from './ChildComp/ColorPicker';
+import Range from './ChildComp/Range';
+import StyleAccordion from './ChildComp/StyleAccordion';
+import StyleEditor from './StyleEditor';
+
+export default function DropdownStyleEditors({ editorLabel, style, styleDispatch, brkPoint, setResponsiveView, styleEditorConfig, formID }) {
+  const history = useHistory()
+  const goBackUrl = () => {
+    if (history.location.pathname.match(/style\/fl\/.+/g)) return history.location.pathname.replace(/style\/fl\/.+/g, 'style/fl')
+    return history.location.pathname.replace(/style\/.+/g, 'style')
+  }
+
+  const [controlAccordion, setcontrolAccordion] = useState(false)
+
+  const borderRadius = style?.[`.fld-${formID}.dpd`]?.['--border-radius'] || '5px 5px 5px 5px'
+  const fontSize = style?.[`.fld-${formID}.dpd`]?.['--font-size'] || '16px'
+  const lineHeight = style?.[`.fld-${formID}.dpd`]?.['--line-height'] || '1.4'
+  const minHeight = style?.[`.fld-${formID}.dpd`]?.['min-height'] || '40px'
+  const activeMenuBgColor = style?.[`.fld-${formID}.dpd`]?.['--active-menu-background'] || 'rgba(255, 255, 255, 1)'
+  const activeMenuRadius = style?.[`.fld-${formID}.dpd`]?.['--active-menu-radius'] || borderRadius
+
+  const setValueByProperty = (value, property, clas) => {
+    const cls = clas || `.fld-${formID}.dpd`
+    styleDispatch({ apply: [{ cls, property, delProp: false, value }], brkPoint })
+  }
+
+  return (
+    <div className="mt-2">
+      <Link to={`${goBackUrl()}`}>
+        <h4 className="w-9 m-a flx txt-dp">
+          <button className="icn-btn" type="button" aria-label="back btn"><span className="btcd-icn icn-arrow_back" /></button>
+          <div className="flx w-10">
+            <span>Back</span>
+            <div className="txt-center w-10 f-5">{editorLabel}</div>
+          </div>
+        </h4>
+        <div className="btcd-hr m-a" />
+        <div className="btcd-hr m-a" />
+      </Link>
+
+      <StyleAccordion
+        open={controlAccordion === 1}
+        onOpen={() => setcontrolAccordion(1)}
+        title={<span className="txt-blue" style={{ fontSize: 16 }}>Dropdown Input Style</span>}
+        className="style-acc"
+      >
+        <StyleEditor noBack compStyle={style} cls={`.fld-${formID}.dpd .msl`} styleDispatch={styleDispatch} brkPoint={brkPoint} setResponsiveView={setResponsiveView} styleConfig={styleEditorConfig.dropdown} formID={formID} />
+
+        <StyleAccordion title="Min Height" className="style-acc w-9">
+          <Range
+            info={[{ icn: <BorderIcn borderWidth="3px 1px 1px 1px" />, lbl: 'Border Top' }]}
+            className="btc-range"
+            unit="px"
+            master={false}
+            maxRange={80}
+            minRange={0}
+            value={minHeight}
+            onChange={(val) => setValueByProperty(val, 'min-height', `.fld-${formID}.dpd .msl`)}
+          />
+        </StyleAccordion>
+        <div className="btcd-hr m-a" />
+
+        <StyleAccordion title="Border Radius" className="style-acc w-9">
+          <Range
+            info={[
+              { icn: <BorderIcn borderWidth="3px 1px 1px 1px" />, lbl: 'Border Top' },
+              { icn: <BorderIcn borderWidth="1px 3px 1px 1px" />, lbl: 'Border Right' },
+              { icn: <BorderIcn borderWidth="1px 1px 3px 1px" />, lbl: 'Border Bottom' },
+              { icn: <BorderIcn borderWidth="1px 1px 1px 3px" />, lbl: 'Border Left' },
+              { icn: <BorderIcn borderWidth="3px 3px 3px 3px" />, lbl: 'All Side' },
+            ]}
+            className="btc-range"
+            unit="px"
+            maxRange={20}
+            minRange={0}
+            value={borderRadius}
+            onChange={(val) => setValueByProperty(val, '--border-radius')}
+          />
+        </StyleAccordion>
+        <div className="btcd-hr m-a" />
+
+        <StyleAccordion title="Font Size" className="style-acc w-9">
+          <Range
+            info={[
+              { icn: <BorderIcn borderWidth="3px 1px 1px 1px" />, lbl: 'Border Top' },
+            ]}
+            className="btc-range"
+            unit="px"
+            master={false}
+            maxRange={50}
+            minRange={0}
+            value={fontSize}
+            onChange={(val) => setValueByProperty(val, '--font-size')}
+          />
+        </StyleAccordion>
+        <div className="btcd-hr m-a" />
+
+        <StyleAccordion title="Line Height" className="style-acc w-9">
+          <Range
+            info={[
+              { icn: <BorderIcn borderWidth="3px 1px 1px 1px" />, lbl: 'Border Top' },
+            ]}
+            className="btc-range"
+            master={false}
+            maxRange={8}
+            minRange={0}
+            value={lineHeight}
+            onChange={(val) => setValueByProperty(val, '--line-height')}
+          />
+        </StyleAccordion>
+        <div className="btcd-hr m-a" />
+
+      </StyleAccordion>
+      <div className="btcd-hr m-a" />
+
+      <StyleAccordion open={controlAccordion === 2} onOpen={() => setcontrolAccordion(2)} title={<span className="txt-blue" style={{ fontSize: 16 }}>Dropdowns Active Menu</span>} className="style-acc">
+        <div className="flx flx-between w-9 p-2 m-a">
+          <b>Background Color</b>
+          <ColorPicker value={activeMenuBgColor} onChange={val => setValueByProperty(val.style, '--active-menu-background', `.fld-${formID}.dpd`)} />
+        </div>
+        <div className="btcd-hr m-a" />
+
+        <StyleAccordion title="Border Radius" className="style-acc w-9">
+          <Range
+            info={[
+              { icn: <BorderIcn borderWidth="3px 1px 1px 1px" />, lbl: 'Border Top' },
+              { icn: <BorderIcn borderWidth="1px 3px 1px 1px" />, lbl: 'Border Right' },
+              { icn: <BorderIcn borderWidth="1px 1px 3px 1px" />, lbl: 'Border Bottom' },
+              { icn: <BorderIcn borderWidth="1px 1px 1px 3px" />, lbl: 'Border Left' },
+              { icn: <BorderIcn borderWidth="3px 3px 3px 3px" />, lbl: 'All Side' },
+            ]}
+            className="btc-range"
+            unit="px"
+            maxRange={20}
+            minRange={0}
+            value={activeMenuRadius}
+            onChange={(val) => setValueByProperty(val, '--active-menu-radius', `.fld-${formID}.dpd`)}
+          />
+        </StyleAccordion>
+        <div className="btcd-hr m-a" />
+      </StyleAccordion>
+      <div className="btcd-hr m-a" />
+
+      <StyleAccordion open={controlAccordion === 3} onOpen={() => setcontrolAccordion(3)} title={<span className="txt-blue" style={{ fontSize: 16 }}>Dropdowns Options Style</span>} className="style-acc">
+        <StyleEditor noBack compStyle={style} cls={`.fld-${formID}.dpd .msl-option`} styleDispatch={styleDispatch} brkPoint={brkPoint} setResponsiveView={setResponsiveView} styleConfig={styleEditorConfig.dropdownOptions} formID={formID} />
+      </StyleAccordion>
+      <div className="btcd-hr m-a" />
+
+      <StyleAccordion open={controlAccordion === 4} onOpen={() => setcontrolAccordion(4)} title={<span className="txt-blue" style={{ fontSize: 16 }}>Dropdowns Chips Style</span>} className="style-acc">
+        <StyleEditor noBack compStyle={style} cls={`.fld-${formID}.dpd .msl-chip`} styleDispatch={styleDispatch} brkPoint={brkPoint} setResponsiveView={setResponsiveView} styleConfig={styleEditorConfig.dropdownChip} formID={formID} />
+        <StyleEditor title="Chip Delete Button" noBack compStyle={style} cls={`.fld-${formID}.dpd .msl-chip div[aria-label="delete-value"]`} styleDispatch={styleDispatch} brkPoint={brkPoint} setResponsiveView={setResponsiveView} styleConfig={styleEditorConfig.dropdownChipButton} formID={formID} />
+      </StyleAccordion>
+      <div className="btcd-hr m-a" />
+
+    </div>
+  )
+}
