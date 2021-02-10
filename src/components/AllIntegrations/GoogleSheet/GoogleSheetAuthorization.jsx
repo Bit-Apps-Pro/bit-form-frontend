f6ee1b60e89d6a4b2a622653bff4b0bbc17d5a98
mdabbas-cse <gmabbas44@gmail.com>
2021-02-10 19:24:21 +0600

    google sheet integ components

---
+++ src/components/AllIntegrations/GoogleSheet/GoogleSheetAuthorization.jsx
@@ -0,0 +1,47 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { __ } from '@wordpress/i18n'
+import CopyText from '../../ElmSettings/Childs/CopyText'
+import LoaderSm from '../../Loaders/LoaderSm'
+
+export default function GoogleSheetAuthorization({ step, confTmp, handleInput, error, setSnackbar, handleAuthorize, isLoading, isAuthorized, nextPage, children }) {
+  return (
+    <div className="btcd-stp-page" style={{ ...{ width: step === 1 && 900 }, ...{ height: step === 1 && `${100}%` } }}>
+      <div className="mt-3"><b>{__('Integration Name:', 'bitform')}</b></div>
+      <input className="btcd-paper-inp w-6 mt-1" onChange={handleInput} name="name" value={confTmp.name} type="text" placeholder={__('Integration Name...', 'bitform')} />
+
+      <div className="mt-3"><b>{__('Homepage URL:', 'bitform')}</b></div>
+      <CopyText value={`${window.location.origin}`} setSnackbar={setSnackbar} className="field-key-cpy w-6 ml-0" />
+
+      <div className="mt-3"><b>{__('Authorized Redirect URIs:', 'bitform')}</b></div>
+      <CopyText value={`${bits.googleRedirectURL}`} setSnackbar={setSnackbar} className="field-key-cpy w-6 ml-0" />
+
+      <small className="d-blk mt-5">
+        {__('To get Client ID and SECRET , Please Visit', 'bitform')}
+        {' '}
+        <a className="btcd-link" href="https://console.developers.google.com/apis/credentials" target="_blank" rel="noreferrer">{__('Google API Console', 'bitform')}</a>
+      </small>
+
+      <div className="mt-3"><b>{__('Client id:', 'bitform')}</b></div>
+      <input className="btcd-paper-inp w-6 mt-1" onChange={handleInput} name="clientId" value={confTmp.clientId} type="text" placeholder={__('Client id...', 'bitform')} />
+      <div style={{ color: 'red', fontSize: '15px' }}>{error.clientId}</div>
+
+      <div className="mt-3"><b>{__('Client secret:', 'bitform')}</b></div>
+      <input className="btcd-paper-inp w-6 mt-1" onChange={handleInput} name="clientSecret" value={confTmp.clientSecret} type="text" placeholder={__('Client secret...', 'bitform')} />
+      <div style={{ color: 'red', fontSize: '15px' }}>{error.clientSecret}</div>
+
+      {children}
+
+      <button onClick={handleAuthorize} className="btn btcd-btn-lg green sh-sm flx" type="button" disabled={isAuthorized}>
+        {isAuthorized ? __('Authorized ✔', 'bitform') : __('Authorize', 'bitform')}
+        {isLoading && <LoaderSm size="20" clr="#022217" className="ml-2" />}
+      </button>
+      <br />
+      <button onClick={() => nextPage(2)} className="btn f-right btcd-btn-lg green sh-sm flx" type="button" disabled={!isAuthorized}>
+        {__('Next', 'bitform')}
+        {' '}
+&nbsp;
+        <div className="btcd-icn icn-arrow_back rev-icn d-in-b" />
+      </button>
+    </div>
+  )
+}
---
+++ src/components/AllIntegrations/GoogleSheet/GoogleSheetCommonFunc.js
@@ -0,0 +1,181 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { __, sprintf } from '@wordpress/i18n'
+import bitsFetch from '../../../Utils/bitsFetch'
+import { deepCopy } from '../../../Utils/Helpers'
+
+export const handleInput = (e, sheetConf, setSheetConf, formID, setisLoading, setSnackbar, isNew, error, setError) => {
+  let newConf = { ...sheetConf }
+  if (isNew) {
+    const rmError = { ...error }
+    rmError[e.target.name] = ''
+    setError({ ...rmError })
+  }
+  newConf[e.target.name] = e.target.value
+  switch (e.target.name) {
+    case 'spreadsheetId':
+      newConf = spreadSheetChange(newConf, formID, setSheetConf, setisLoading, setSnackbar)
+      break;
+    case 'worksheetName':
+      newConf = worksheetChange(newConf, formID, setSheetConf, setisLoading, setSnackbar)
+      break;
+    default:
+      break;
+  }
+  setSheetConf({ ...newConf })
+}
+
+export const spreadSheetChange = (sheetConf, formID, setSheetConf, setisLoading, setSnackbar) => {
+  const newConf = deepCopy(sheetConf)
+  newConf.worksheetName = ''
+  newConf.headerRow = 'A1'
+  newConf.field_map = [{ formField: '', googleSheetField: '' }]
+
+  if (!newConf?.default?.worksheets?.[sheetConf.spreadsheetId]) {
+    refreshWorksheets(formID, newConf, setSheetConf, setisLoading, setSnackbar)
+  } else if (Object.keys(newConf?.default?.worksheets?.[sheetConf.spreadsheetId]).length === 1) {
+    newConf.worksheetName = newConf?.default?.worksheets?.[sheetConf.spreadsheetId][0].properties.title
+
+    if (!newConf?.default?.worksheets?.headers?.[newConf.worksheetName]) {
+      refreshWorksheetHeaders(formID, newConf, setSheetConf, setisLoading, setSnackbar)
+    }
+  }
+
+  return newConf
+}
+
+export const worksheetChange = (sheetConf, formID, setSheetConf, setisLoading, setSnackbar) => {
+  const newConf = { ...sheetConf }
+  newConf.headerRow = 'A1'
+  newConf.field_map = [{ formField: '', googleSheetField: '' }]
+
+  if (!newConf?.default?.worksheets?.headers?.[sheetConf.worksheetName]) {
+    refreshWorksheetHeaders(formID, newConf, setSheetConf, setisLoading, setSnackbar)
+  }
+
+  return newConf
+}
+
+export const refreshSpreadsheets = (formID, sheetConf, setSheetConf, setisLoading, setSnackbar) => {
+  setisLoading(true)
+  const refreshModulesRequestParams = {
+    formID,
+    id: sheetConf.id,
+    clientId: sheetConf.clientId,
+    clientSecret: sheetConf.clientSecret,
+    tokenDetails: sheetConf.tokenDetails,
+    ownerEmail: sheetConf.ownerEmail,
+  }
+  bitsFetch(refreshModulesRequestParams, 'bitforms_gsheet_refresh_spreadsheets')
+    .then(result => {
+      if (result && result.success) {
+        const newConf = { ...sheetConf }
+        if (!newConf.default) {
+          newConf.default = {}
+        }
+        if (result.data.spreadsheets) {
+          newConf.default.spreadsheets = result.data.spreadsheets
+        }
+        if (result.data.tokenDetails) {
+          newConf.tokenDetails = result.data.tokenDetails
+        }
+        setSnackbar({ show: true, msg: __('Spreadsheet refreshed', 'bitform') })
+        setSheetConf({ ...newConf })
+      } else if ((result && result.data && result.data.data) || (!result.success && typeof result.data === 'string')) {
+        setSnackbar({ show: true, msg: sprintf(__('Spreadsheet refresh failed Cause: %s. please try again', 'bitform'), result.data.data || result.data) })
+      } else {
+        setSnackbar({ show: true, msg: __('Spreadsheet refresh failed. please try again', 'bitform') })
+      }
+      setisLoading(false)
+    })
+    .catch(() => setisLoading(false))
+}
+
+export const refreshWorksheets = (formID, sheetConf, setSheetConf, setisLoading, setSnackbar) => {
+  const { spreadsheetId } = sheetConf
+  if (!spreadsheetId) {
+    return
+  }
+  setisLoading(true)
+  const refreshSpreadsheetsRequestParams = {
+    formID,
+    spreadsheetId,
+    clientId: sheetConf.clientId,
+    clientSecret: sheetConf.clientSecret,
+    tokenDetails: sheetConf.tokenDetails,
+  }
+  bitsFetch(refreshSpreadsheetsRequestParams, 'bitforms_gsheet_refresh_worksheets')
+    .then(result => {
+      if (result && result.success) {
+        const newConf = { ...sheetConf }
+        if (result.data.worksheets) {
+          if (!newConf.default.worksheets) {
+            newConf.default.worksheets = {}
+          }
+          newConf.default.worksheets[spreadsheetId] = result.data.worksheets
+        }
+
+        if (result.data.tokenDetails) {
+          newConf.tokenDetails = result.data.tokenDetails
+        }
+        setSnackbar({ show: true, msg: __('Worksheets refreshed', 'bitform') })
+        setSheetConf({ ...newConf })
+      } else {
+        setSnackbar({ show: true, msg: __('Worksheets refresh failed. please try again', 'bitform') })
+      }
+      setisLoading(false)
+    })
+    .catch(() => setisLoading(false))
+}
+
+export const refreshWorksheetHeaders = (formID, sheetConf, setSheetConf, setisLoading, setSnackbar) => {
+  const { spreadsheetId, worksheetName, header, headerRow } = sheetConf
+  if (!spreadsheetId && !worksheetName && !header && !headerRow) {
+    return
+  }
+
+  setisLoading(true)
+  const refreshWorksheetHeadersRequestParams = {
+    formID,
+    spreadsheetId,
+    worksheetName,
+    header,
+    headerRow,
+    clientId: sheetConf.clientId,
+    clientSecret: sheetConf.clientSecret,
+    tokenDetails: sheetConf.tokenDetails,
+  }
+  bitsFetch(refreshWorksheetHeadersRequestParams, 'bitforms_gsheet_refresh_worksheet_headers')
+    .then(result => {
+      if (result && result.success) {
+        const newConf = { ...sheetConf }
+        console.log('worksheed header', result.data.worksheet_headers.length)
+        if (result.data.worksheet_headers?.length > 0) {
+          if (!newConf.default.headers) {
+            newConf.default.headers = {}
+          }
+          if (!newConf.default.headers[spreadsheetId]) {
+            newConf.default.headers[spreadsheetId] = {}
+          }
+          if (!newConf.default.headers[spreadsheetId][worksheetName]) {
+            newConf.default.headers[spreadsheetId][worksheetName] = {}
+          }
+          newConf.default.headers[spreadsheetId][worksheetName][headerRow] = result.data.worksheet_headers
+          if (result.data.tokenDetails) {
+            newConf.tokenDetails = result.data.tokenDetails
+          }
+          setSnackbar({ show: true, msg: __('Worksheet Headers refreshed', 'bitform') })
+        } else {
+          setSnackbar({ show: true, msg: __('No Worksheet headers found. Try changing the header row number or try again', 'bitform') })
+        }
+
+        if (result.data.tokenDetails) {
+          newConf.tokenDetails = result.data.tokenDetails
+        }
+        setSheetConf({ ...newConf })
+      } else {
+        setSnackbar({ show: true, msg: __('Worksheet Headers refresh failed. please try again', 'bitform') })
+      }
+      setisLoading(false)
+    })
+    .catch(() => setisLoading(false))
+}
