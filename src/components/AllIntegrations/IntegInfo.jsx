18f4dc13fada54cc5bec497a215d4f76501a6a06
mdabbas-cse <gmabbas44@gmail.com>
2021-02-10 19:26:28 +0600

    google sheet info added

--- src/components/AllIntegrations/IntegInfo.jsx
+++ src/components/AllIntegrations/IntegInfo.jsx
@@ -13,9 +13,9 @@ export default function IntegInfo({ allIntegURL, integrations }) {
   let location = window.location.toString()
 
   const toReplaceInd = location.indexOf('/info')
-
   location = window.encodeURI(`${location.slice(0, toReplaceInd)}/new/${integrations[id].type}`)
 
+  // console.log(allIntegURL)
   return (
     <>
       <SnackMsg snack={snack} setSnackbar={setSnackbar} />
@@ -33,21 +33,25 @@ export default function IntegInfo({ allIntegURL, integrations }) {
       <div className="btcd-stp-page" style={{ width: 900, height: `${100}%` }}>
         <div className="mt-3"><b>{__('Integration Name:', 'bitform')}</b></div>
         <input className="btcd-paper-inp w-6 mt-1" name="name" defaultValue={integrations[id].name} type="text" placeholder={__('Integration Name...', 'bitform')} readOnly />
-
-        <div className="mt-3"><b>Data Center:</b></div>
-        <select name="dataCenter" defaultValue={integrations[id].dataCenter} className="btcd-paper-inp w-9 mt-1" disabled>
-          <option value="com">zoho.com</option>
-          <option value="eu">zoho.eu</option>
-          <option value="com.cn">zoho.com.cn</option>
-          <option value="in">zoho.in</option>
-          <option value="com.au">zoho.com.au</option>
-        </select>
-
+        {integrations[id].type !== 'Google Sheet' ? (
+          <>
+            <div className="mt-3"><b>Data Center:</b></div>
+            <select name="dataCenter" defaultValue={integrations[id].dataCenter} className="btcd-paper-inp w-9 mt-1" disabled>
+              <option value="com">zoho.com</option>
+              <option value="eu">zoho.eu</option>
+              <option value="com.cn">zoho.com.cn</option>
+              <option value="in">zoho.in</option>
+              <option value="com.au">zoho.com.au</option>
+            </select>
+          </>
+        ) : ''}
         <div className="mt-3"><b>Homepage URL:</b></div>
         <CopyText value={`${window.location.origin}`} setSnackbar={setSnackbar} className="field-key-cpy w-6 ml-0" readOnly />
 
         <div className="mt-3"><b>Authorized Redirect URIs:</b></div>
-        <CopyText value={`${location}/redirect`} setSnackbar={setSnackbar} className="field-key-cpy w-6 ml-0" readOnly />
+        {integrations[id].type !== 'Google Sheet' ?
+          (<CopyText value={`${location}/redirect`} setSnackbar={setSnackbar} className="field-key-cpy w-6 ml-0" readOnly />)
+          : (<CopyText value={bits.googleRedirectURL} setSnackbar={setSnackbar} className="field-key-cpy w-6 ml-0" readOnly />)}
 
         <div className="mt-3"><b>Client id:</b></div>
         <input className="btcd-paper-inp w-6 mt-1" name="clientId" defaultValue={integrations[id].clientId} type="text" placeholder="Client id..." readOnly />
