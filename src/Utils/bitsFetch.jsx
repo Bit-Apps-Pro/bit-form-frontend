cc88f926b8f50219d9f217acb851e3690279bd4e
mdabbas-cse <gmabbas44@gmail.com>
2021-02-14 12:06:46 +0600

    Update bitsFetch.jsx

--- src/Utils/bitsFetch.jsx
+++ src/Utils/bitsFetch.jsx
@@ -1,16 +1,26 @@
 /* eslint-disable no-undef */
 
 export default async function bitsFetch(data, action, contentType = null, queryParam = null) {
-  const uri = new URL(typeof bits === 'undefined' ? bitFromsFront.ajaxURL : bits.ajaxURL)
+  console.log('bitFromsFront', bitFromsFront)
+  const uri = new URL(typeof bits === 'undefined' ? bitFromsFront?.ajaxURL : bits.ajaxURL)
   uri.searchParams.append('action', action)
   uri.searchParams.append('_ajax_nonce', typeof bits === 'undefined' ? '' : bits.nonce)
+
+  // append query params in url
+  if (queryParam) {
+    for (const key in queryParam) {
+      if (key) {
+        uri.searchParams.append(key, queryParam[key])
+      }
+    }
+  }
 
   const response = await fetch(uri, {
     method: 'POST',
     headers: {
-      'Content-Type': contentType === null ? 'application/x-www-form-urlencoded' : contentType,
+      //  'Content-Type': contentType === null ? 'application/x-www-form-urlencoded' : contentType,
     },
-    body: JSON.stringify(data),
+    body: data instanceof FormData ? data : JSON.stringify(data),
   })
     .then(res => res.json())
 
