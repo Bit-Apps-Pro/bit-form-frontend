61360ea1b461bc34fb34bf1438c1d7bc907f03af
mdabbas-cse <gmabbas44@gmail.com>
2021-02-23 14:03:10 +0600

    mail chimp integration layout

--- src/components/AllIntegrations/MailChimp/MailChimpIntegLayout.jsx
+++ src/components/AllIntegrations/MailChimp/MailChimpIntegLayout.jsx
@@ -1,13 +1,22 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { __ } from '@wordpress/i18n'
 import Loader from '../../Loaders/Loader'
-import { addFieldMap } from '../IntegrationHelpers/MailChimpIntegrationHelpers'
+import { addAddressFieldMap, addFieldMap } from '../IntegrationHelpers/MailChimpIntegrationHelpers'
 import { refreshAudience } from './MailChimpCommonFunc'
 import MailChimpFieldMap from './MailChimpFieldMap'
+import AddressFieldMap from './AddressFieldMap'
 import MailChimpActions from './MailChimpActions'
 
-export default function MailChimpIntegLayout({ formID, formFields, handleInput, sheetConf, setSheetConf, isLoading, setisLoading, setSnackbar }) {
-  console.log('sheetConf', sheetConf)
+export default function MailChimpIntegLayout({ formID, formFields, handleInput, sheetConf, setSheetConf, isLoading, setisLoading, setSnackbar, a }) {
+  const address = [
+     { tag: 'addr1', name: 'Address 1', required: true },
+     { tag: 'addr1', name: 'Address 2', required: false },
+     { tag: 'city', name: 'City', required: true },
+     { tag: 'zip', name: 'Zip', required: true },
+     { tag: 'state', name: 'State', required: true },
+     { tag: 'country', name: 'Country', required: false },
+  ]
+
   return (
     <>
       <br />
@@ -60,9 +69,35 @@ export default function MailChimpIntegLayout({ formID, formFields, handleInput, 
             <div className="txt-center  mt-2" style={{ marginRight: 85 }}><button onClick={() => addFieldMap(sheetConf.field_map.length, sheetConf, setSheetConf)} className="icn-btn sh-sm" type="button">+</button></div>
             <br />
             <br />
+            {sheetConf.actions?.address && (
+              <>
+                <div className="mt-4">
+                  <b className="wdt-100">{__('Address Field Map', 'bitform')}</b>
+                </div>
+                <div className="btcd-hr mt-1" />
+                <div className="flx flx-around mt-2 mb-1">
+                  <div className="txt-dp"><b>{__('Form Address Fields', 'bitform')}</b></div>
+                  <div className="txt-dp"><b>{__('Mail Chimp Address Fields', 'bitform')}</b></div>
+                </div>
+                {sheetConf?.address_field?.map((itm, i) => (
+                  <AddressFieldMap
+                    key={`sheet-m-${i + 9}`}
+                    i={i}
+                    field={itm}
+                    sheetConf={sheetConf}
+                    formFields={formFields}
+                    setSheetConf={setSheetConf}
+                    addressField={address}
+                  />
+                ))}
+                <div className="txt-center  mt-2" style={{ marginRight: 85 }}><button onClick={() => addAddressFieldMap(sheetConf.address_field.length, sheetConf, setSheetConf)} className="icn-btn sh-sm" type="button">+</button></div>
+              </>
+            )}
+            <br />
+            <br />
           </>
         )}
-      {console.log('list id', sheetConf.listId)}
+
       {sheetConf.listId && (
         <>
           <div className="mt-4"><b className="wdt-100">{__('Actions', 'bitform')}</b></div>
@@ -71,6 +106,7 @@ export default function MailChimpIntegLayout({ formID, formFields, handleInput, 
             sheetConf={sheetConf}
             setSheetConf={setSheetConf}
             formFields={formFields}
+            address={address}
           />
         </>
       )}
