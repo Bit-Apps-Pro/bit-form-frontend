dfcbb51b85d6ee40294831266ab0a19caab71876
mdabbas-cse <gmabbas44@gmail.com>
2021-03-04 18:52:29 +0600

    add mailPoet integration

--- src/components/AllIntegrations/NewInteg.jsx
+++ src/components/AllIntegrations/NewInteg.jsx
@@ -17,6 +17,7 @@ import ZohoWorkDrive from './ZohoWorkDrive/ZohoWorkDrive'
 import GoogleSheet from './GoogleSheet/GoogleSheet'
 import MailChimp from './MailChimp/MailChimp'
 import Cpt from './Cpt/Cpt'
+import MailPoet from './MailPoet/MailPoet'
 
 function NewInteg({ allIntegURL, formFields, setIntegration, integrations }) {
   const { type } = useParams()
@@ -55,6 +56,8 @@ function NewInteg({ allIntegURL, formFields, setIntegration, integrations }) {
         return <MailChimp allIntegURL={allIntegURL} formFields={formFields} integrations={integrations} setIntegration={setIntegration} />
       case 'CPT':
         return <Cpt allIntegURL={allIntegURL} formFields={formFields} integrations={integrations} setIntegration={setIntegration} />
+      case 'Mail Poet':
+        return <MailPoet allIntegURL={allIntegURL} formFields={formFields} integrations={integrations} setIntegration={setIntegration} />
       default:
         break;
     }
