91e800e938edb1ff31ea7dbd603f4dc2b1aa7201
Arif Uddin <arifunctg@gmail.com>
2021-04-12 12:24:56 +0600

    Update Html.jsx

--- src/components/Fields/Html.jsx
+++ src/components/Fields/Html.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-props-no-spreading */
-import { useState, useRef, useEffect } from 'react'
 
 export default function HtmlField({ attr, onBlurHandler, resetFieldValue, formID }) {
   return (
-    <div className={`fld-wrp fld-wrp-${formID} drag ${attr.valid.hide ? 'btcd-hidden' : ''}`} btcd-fld="decisionbox" dangerouslySetInnerHTML={{ __html: attr.content || attr?.info?.content }} />
+    <div
+      className={`fld-wrp fld-wrp-${formID} drag ${attr.valid.hide ? 'btcd-hidden' : ''}`}
+      btcd-fld="decisionbox"
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: attr.content || attr?.info?.content }}
+    />
   )
 }
