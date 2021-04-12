4b30fc083e95bf77d3b31477389beb1669e07ae6
Arif Uddin <arifunctg@gmail.com>
2021-04-12 12:25:12 +0600

    added draggable

--- src/components/Fields/Button.jsx
+++ src/components/Fields/Button.jsx
@@ -3,7 +3,7 @@ import LoaderSm from '../Loaders/LoaderSm'
 /* eslint-disable react/jsx-props-no-spreading */
 export default function Button({ attr, buttonDisabled, handleReset, formID, data }) {
   return (
-    <div className={`fld-wrp fld-wrp-${formID}`} btcd-fld="submit">
+    <div className={`fld-wrp drag fld-wrp-${formID}`} btcd-fld="submit">
       <div className={`btcd-frm-sub ${attr.align === 'center' && 'j-c-c'} ${attr.align === 'right' && 'j-c-e'}`}>
         <button
           className={`btcd-sub-btn ${attr.btnTyp === 'reset' ? 'btcd-rst' : 'btcd-sub'} ${attr.btnSiz === 'md' && 'btcd-btn-md'} ${attr.fulW && 'ful-w'}`}
