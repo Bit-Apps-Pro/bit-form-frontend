6e4aa1e03005865cd9a59db89bdc3a1e271be079
Arif Uddin <arifunctg@gmail.com>
2021-04-12 12:23:58 +0600

    Create BtnIcn.jsx

---
+++ src/Icons/BtnIcn.jsx
@@ -0,0 +1,8 @@
+export default function BtnIcn({ size }) {
+  return (
+    <svg height={size} width={size} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+      <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M6.7 17.8A1.4 1.4 0 017 16a1.5 1.5 0 011.9-.1l2.8 2.6V7a1.6 1.6 0 011.5-1.3 1.6 1.6 0 011.5 1v5a1.6 1.6 0 013 .5 1.6 1.6 0 013 .5 1.4 1.4 0 012.8.2v8.8a5.7 5.7 0 01-1.8 3.2 5.6 5.6 0 01-2.7 1.4 5.7 5.7 0 01-4-.8z" />
+      <path fill="none" stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" d="M14.7 11.9v2M17.7 11.9v3M20.7 12.4v3.5M9.6 8.7a4 4 0 01-.5-2 4.2 4.2 0 018.3 0 4 4 0 01-.6 2.3" />
+    </svg>
+  )
+}
