6866653cbaa98e75b71465068fc196254b0b8489
Arif Uddin <arifunctg@gmail.com>
2021-04-17 18:03:41 +0600

    icon updated

--- src/Icons/BtnIcn.jsx
+++ src/Icons/BtnIcn.jsx
@@ -1,8 +1,8 @@
 export default function BtnIcn({ size }) {
   return (
-    <svg height={size} width={size} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
-      <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" d="M6.7 17.8A1.4 1.4 0 017 16a1.5 1.5 0 011.9-.1l2.8 2.6V7a1.6 1.6 0 011.5-1.3 1.6 1.6 0 011.5 1v5a1.6 1.6 0 013 .5 1.6 1.6 0 013 .5 1.4 1.4 0 012.8.2v8.8a5.7 5.7 0 01-1.8 3.2 5.6 5.6 0 01-2.7 1.4 5.7 5.7 0 01-4-.8z" />
-      <path fill="none" stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" d="M14.7 11.9v2M17.7 11.9v3M20.7 12.4v3.5M9.6 8.7a4 4 0 01-.5-2 4.2 4.2 0 018.3 0 4 4 0 01-.6 2.3" />
+    <svg width={size} height={size} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 215 215">
+      <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="12" d="M46.3 135a10.6 10.6 0 01-.6-15.1l.6-.6a11.4 11.4 0 0112.8-1.3 11 11 0 012.3 1.7l20.5 19V53.1A12.2 12.2 0 0188 42.4a11.9 11.9 0 015.5-1.4h.8A11.6 11.6 0 01105 52.7v34.5a11.8 11.8 0 0123.1 3.5 12 12 0 0122.7 4 11.1 11.1 0 0112.4-8.4 11 11 0 019 10.2v61a41.1 41.1 0 01-7.2 23.4 43.2 43.2 0 01-27.1 17.7 40.2 40.2 0 01-11.2.5 42 42 0 01-24.6-11.5z" />
+      <path fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="12px" strokeMiterlimit="10" d="M105 87.2V103M128.1 87.2v23.1M150.8 91.5v26.9M66.3 63.3A31.1 31.1 0 0162 48.5a31.8 31.8 0 0163.4-2.7 31.1 31.1 0 01-4.3 17.3" />
     </svg>
   )
 }
