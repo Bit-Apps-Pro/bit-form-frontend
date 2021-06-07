2412f44847048b1677c53a2c1529d94a42194b4c
Md Rubel Amin <mdrubelamin2@gmail.com>
2021-06-07 17:44:25 +0600

    decision box modal added

---
+++ src/components/CompSettings/CompSettingsUtils/DecisionBoxLabelModal.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { $fields, $selectedFieldId } from '../../../GlobalStates'
+import { deepCopy } from '../../../Utils/Helpers'
+import { __ } from '../../../Utils/i18nwrap'
+import Modal from '../../Utilities/Modal'
+import TinyMCE from '../../Utilities/TinyMCE'
+
+export default function DecisionBoxLabelModal({ labelModal, setLabelModal }) {
+  const fldKey = useRecoilValue($selectedFieldId)
+  const [fields, setFields] = useRecoilState($fields)
+  const fieldData = deepCopy(fields[fldKey])
+  const lbl = fieldData.lbl || fieldData?.info?.lbl
+  const [value, setValue] = useState(lbl)
+
+  useEffect(() => {
+    if (labelModal) setValue(lbl)
+  }, [labelModal])
+
+  const setLbl = val => {
+    const tmp = deepCopy(fieldData)
+    tmp.lbl = val
+
+    setFields(allFields => ({ ...allFields, ...{ [fldKey]: tmp } }))
+  }
+
+  const cancelModal = () => {
+    fieldData.lbl = value
+    setFields(allFields => ({ ...allFields, ...{ [fldKey]: fieldData } }))
+    setLabelModal(false)
+  }
+
+  return (
+    <Modal
+      md
+      show={labelModal}
+      setModal={cancelModal}
+      title={__('Edit Decision Box Label', 'bitform')}
+    >
+      <TinyMCE
+        id={fldKey}
+        value={lbl}
+        onChangeHandler={setLbl}
+      />
+      <div className="mt-2 f-right">
+        <button type="button" className="btn mr-2" onClick={cancelModal}>Cancel</button>
+        <button type="button" className="btn blue" onClick={() => setLabelModal(false)}>Save</button>
+      </div>
+    </Modal>
+  )
+}
