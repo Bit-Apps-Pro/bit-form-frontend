8a2279bd798dd22a2fcc7a5fa31a0152cc08f4b4
Arif Uddin <arifunctg@gmail.com>
2021-08-16 11:39:12 +0600

    webpack config and pacakge updated

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,9 @@ const WorkboxPlugin = require('workbox-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const safePostCssParser = require('postcss-safe-parser')
-const svgToMiniDataURI = require('mini-svg-data-uri')
-const RouteManifest = require('webpack-route-manifest')
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+// const svgToMiniDataURI = require('mini-svg-data-uri')
+// const RouteManifest = require('webpack-route-manifest')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 // const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 // const HtmlWebpackPlugin = require('html-webpack-plugin')
 // const autoprefixer = require('autoprefixer');
@@ -17,7 +17,7 @@ const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin'
 
 module.exports = (env, argv) => {
   const production = argv.mode !== 'development'
-
+  const { hot } = argv
   return {
     // devtool: production ? false : 'source-map',
     devtool: production ? false : 'eval',
@@ -40,16 +40,23 @@ module.exports = (env, argv) => {
       chunkFilename: production ? '[name].js?v=[contenthash:6]' : '[name].js',
       library: '_bitforms',
       libraryTarget: 'umd',
-      // publicPath: '/',
+      assetModuleFilename: production ? '../img/[name][contenthash:4].[ext]' : '../img/[name].[ext]',
+      // publicPath: 'http://localhost:3000/',
     },
     target: 'web',
-    devServer: {
-      hot: true,
-      liveReload: false,
-      port: 3000,
-      writeToDisk: true,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      disableHostCheck: true,
+    ...hot && {
+      devServer: {
+        // publicPath: 'http://localhost:3000',
+        hot: true,
+        liveReload: false,
+        port: 3000,
+        // writeToDisk: true,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        disableHostCheck: true,
+        // proxy: {
+        //   '*': 'http://localhost:3000',
+        // },
+      },
     },
     /*  performance: {
        hints: 'error',
@@ -115,7 +122,7 @@ module.exports = (env, argv) => {
       new CleanWebpackPlugin(),
       new webpack.DefinePlugin({ 'process.env': { NODE_ENV: production ? JSON.stringify('production') : JSON.stringify('development') } }),
       new MiniCssExtractPlugin({
-        filename: '../css/[name].css?v=[contenthash:6]',
+        filename: hot ? '[name].css' : '../css/[name].css?v=[contenthash:6]',
         ignoreOrder: true,
       }),
       new CopyPlugin({
@@ -277,29 +284,31 @@ module.exports = (env, argv) => {
         },
         {
           test: /\.svg$/i,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                options: { generator: (content) => svgToMiniDataURI(content.toString()) },
-                name: production ? '[name][contenthash:8].[ext]' : '[name].[ext]',
-                outputPath: '../img',
-              },
-            },
-          ],
+          type: 'asset/inline',
+          // use: [
+          //   {
+          //     loader: 'url-loader',
+          //     options: {
+          //       options: { generator: (content) => svgToMiniDataURI(content.toString()) },
+          //       name: production ? '[name][contenthash:8].[ext]' : '[name].[ext]',
+          //       outputPath: '../img',
+          //     },
+          //   },
+          // ],
         },
         {
           test: /\.(jpe?g|png|gif|ttf|woff|woff2|eot)$/i,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: production ? 3000 : 100,
-                name: production ? '[name][contenthash:8].[ext]' : '[name].[ext]',
-                outputPath: '../img',
-              },
-            },
-          ],
+          type: 'asset/inline',
+          // use: [
+          //   {
+          //     loader: 'url-loader',
+          //     options: {
+          //       limit: production ? 3000 : 100,
+          //       name: production ? '[name][contenthash:4].[ext]' : '[name].[ext]',
+          //       outputPath: '../img',
+          //     },
+          //   },
+          // ],
         },
       ],
     },
