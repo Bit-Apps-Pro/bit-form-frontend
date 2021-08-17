24b313905fb61f866acbfa85540df2fe024353ac
Md Rubel Amin <mdrubelamin2@gmail.com>
2021-08-17 16:46:16 +0600

    update: secondary update button added

--- src/components/AllIntegrations/IntegrationHelpers/WebHooksStepTwo.jsx
+++ src/components/AllIntegrations/IntegrationHelpers/WebHooksStepTwo.jsx
@@ -5,7 +5,7 @@ export default function WebHooksStepTwo({ step, saveConfig, edit, disabled }) {
     edit
       ? (
         <div className="txt-center w-9 mt-3">
-          <button onClick={saveConfig} className="btn btcd-btn-lg green sh-sm flx" type="button" disabled={disabled}>
+          <button id="secondary-update-btn" onClick={saveConfig} className="btn btcd-btn-lg green sh-sm flx" type="button" disabled={disabled}>
             {__('Save', 'bitform')}
           </button>
         </div>
@@ -13,7 +13,7 @@ export default function WebHooksStepTwo({ step, saveConfig, edit, disabled }) {
       : (
         <div className="txt-center" style={{ marginLeft: 210 }}>
           <h2 className="ml-3">{__('Successfully Integrated', 'bitform')}</h2>
-          <button onClick={saveConfig} className="btn btcd-btn-lg green sh-sm" type="button">
+          <button id="secondary-update-btn" onClick={saveConfig} className="btn btcd-btn-lg green sh-sm" type="button">
             {__('Finish & Save ', 'bitform')}
             ✔
           </button>
