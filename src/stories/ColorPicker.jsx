73449557760a2c29dbfd6bee076da910fb67db01
mdabbas-cse <gmabbas44@gmail.com>
2021-08-19 15:36:57 +0600

    updata color picker

--- src/stories/ColorPicker.jsx
+++ src/stories/ColorPicker.jsx
@@ -10,35 +10,171 @@ function ColorPicker() {
             <div className="title blue">
               Blue
             </div>
-            <div className="code b-10">
-              className: b-10
+            <div className="code b-11-50">
+              var name: --b-11-50
             </div>
-            <div className="code b-20">
-              className: b-20
+            <div className="code b-12-81">
+              var name: --b-12-81
             </div>
-            <div className="code b-30">
-              className: b-30
+            <div className="code b-13-52">
+              var name: --b-13-52
             </div>
-            <div className="code b-40">
-              className: b-40
+            <div className="code b-13-88">
+              var name: --b-13-88
+            </div>
+            <div className="code b-15-83">
+              var name: --b-15-83
+            </div>
+            <div className="code b-16-35">
+              var name: --b-16-35
+            </div>
+            <div className="code b-16-87">
+              var name: --b-16-87
+            </div>
+            <div className="code b-16-86">
+              var name: --b-16-86
+            </div>
+            <div className="code b-19-48">
+              var name: --b-19-48
+            </div>
+            <div className="code b-20-93">
+              var name: --b-20-93
+            </div>
+            <div className="code b-23-95">
+              var name: --b-23-95
+            </div>
+            <div className="code b-30-84">
+              var name: --b-30-84
+            </div>
+            <div className="code b-36">
+              var name: --b-36
+            </div>
+            <div className="code b-31-44-27">
+              var name: --b-31-44-27
+            </div>
+            <div className="code b-31-81">
+              var name: --b-31-81
+            </div>
+            <div className="code b-33-93">
+              var name: --b-33-93
+            </div>
+            <div className="code b-35-33">
+              var name: --b-35-33
+            </div>
+            <div className="code b-36-96">
+              var name: --b-36-96
+            </div>
+            <div className="code b-37-18">
+              var name: --b-37-18
+            </div>
+            <div className="code b-37-69">
+              var name: --b-37-69
+            </div>
+            <div className="code b-38-62-40">
+              var name: --b-38-62-40
+            </div>
+            <div className="code b-44-20">
+              var name: --b-44-20
+            </div>
+            <div className="code b-44-87">
+              var name: --b-44-87
+            </div>
+            <div className="code b-49-79">
+              var name: --b-49-79
+            </div>
+            <div className="code b-49-91">
+              var name: --b-49-91
+            </div>
+            <div className="code b-53-8">
+              var name: --b-53-8
+            </div>
+            <div className="code b-53-13">
+              var name: --b-53-13
+            </div>
+            <div className="code b-54-15-53">
+              var name: --b-54-15-53
+            </div>
+            <div className="code b-54-12">
+              var name: --b-54-12
+            </div>
+            <div className="code b-79-96">
+              var name: --b-79-96
             </div>
             <div className="code b-50">
-              className: b-50
+              var name: --b-50
             </div>
-            <div className="code b-60">
-              className: b-60
+            <div className="code b-50-22">
+              var name: --b-50-22
             </div>
-            <div className="code b-70">
-              className: b-70
+            <div className="code b-59">
+              var name: --b-59
             </div>
-            <div className="code b-80">
-              className: b-80
+            <div className="code b-50-95">
+              var name: --b-50-95
+            </div>
+            <div className="code b-54-31">
+              var name: --b-54-31
+            </div>
+            <div className="code b-63-18-67">
+              var name: --b-63-18-67
+            </div>
+            <div className="code b-72">
+              var name: --b-72
+            </div>
+            <div className="code b-73">
+              var name: --b-73
+            </div>
+            <div className="code b-82-11-5">
+              var name: --b-82-11-5
+            </div>
+            <div className="code b-86">
+              var name: --b-86
+            </div>
+            <div className="code b-70-12-24">
+              var name: --b-70-12-24
+            </div>
+            <div className="code b-58-82">
+              var name: --b-58-82
+            </div>
+            <div className="code b-35-86">
+              var name: --b-35-86
+            </div>
+            <div className="code b-38-89">
+              var name: --b-38-89
             </div>
             <div className="code b-90">
-              className: b-90
+              var name: --b-90
             </div>
-            <div className="code b-100">
-              className: b-100
+            <div className="code b-92-62">
+              var name: --b-92-62
+            </div>
+            <div className="code b-93-16-8">
+              var name: --b-93-16-8
+            </div>
+            <div className="code b-95-31-18">
+              var name: --b-95-31-18
+            </div>
+            <div className="code b-97">
+              var name: --b-97
+            </div>
+            <div className="code b-100-64-40">
+              var name: --b-100-64-40
+            </div>
+            <div className="code b-15-94">
+              var name: --b-15-94
+            </div>
+            <div className="code b-100-48-1">
+              var name: --b-100-48-1
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="colors ">
+            <div className="title blue">
+              Blue
+            </div>
+            <div className="code b-11-50">
+              var name: --b-11-50
             </div>
           </div>
         </div>
