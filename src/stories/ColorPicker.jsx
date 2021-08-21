72a2fe133648a70af3bd6e6c50e52f33a5b979d2
mdabbas-cse <gmabbas44@gmail.com>
2021-08-21 16:40:50 +0600

    sorting blue to white

--- src/stories/ColorPicker.jsx
+++ src/stories/ColorPicker.jsx
@@ -14,62 +14,59 @@ function ColorPicker() {
             <div className="title blue">
               Blue
             </div>
-            <div onClick={copyVar} className="code b-11-50">
-              var name: --b-11-50
+            <div onClick={copyVar} className="code b-53-8">
+              var name: --b-53-8
             </div>
-            <div onClick={copyVar} className="code b-12-81">
-              var name: --b-12-81
+            <div onClick={copyVar} className="code b-54-12">
+              var name: --b-54-12
             </div>
-            <div onClick={copyVar} className="code b-13-52">
-              var name: --b-13-52
+            <div onClick={copyVar} className="code b-53-13">
+              var name: --b-53-13
             </div>
-            <div onClick={copyVar} className="code b-13-88">
-              var name: --b-13-88
+            <div onClick={copyVar} className="code b-37-18">
+              var name: --b-37-18
             </div>
-            <div onClick={copyVar} className="code b-15-83">
-              var name: --b-15-83
+            <div onClick={copyVar} className="code b-44-20">
+              var name: --b-44-20
             </div>
-            <div onClick={copyVar} className="code b-16-35">
-              var name: --b-16-35
+            <div onClick={copyVar} className="code b-50-22">
+              var name: --b-50-22
             </div>
-            <div onClick={copyVar} className="code b-16-87">
-              var name: --b-16-87
-            </div>
-            <div onClick={copyVar} className="code b-16-86">
-              var name: --b-16-86
-            </div>
-            <div onClick={copyVar} className="code b-19-48">
-              var name: --b-19-48
-            </div>
-            <div onClick={copyVar} className="code b-20-93">
-              var name: --b-20-93
-            </div>
-            <div onClick={copyVar} className="code b-23-95">
-              var name: --b-23-95
-            </div>
-            <div onClick={copyVar} className="code b-30-84">
-              var name: --b-30-84
-            </div>
-            <div onClick={copyVar} className="code b-36">
-              var name: --b-36
-            </div>
-            <div onClick={copyVar} className="code b-31-44-27">
-              var name: --b-31-44-27
-            </div>
-            <div onClick={copyVar} className="code b-31-81">
-              var name: --b-31-81
-            </div>
-            <div onClick={copyVar} className="code b-33-93">
-              var name: --b-33-93
+            <div onClick={copyVar} className="code b-93-16-8">
+              var name: --b-93-16-8
             </div>
             <div onClick={copyVar} className="code b-35-33">
               var name: --b-35-33
             </div>
-            <div onClick={copyVar} className="code b-36-96">
-              var name: --b-36-96
+            <div onClick={copyVar} className="code b-54-31">
+              var name: --b-54-31
             </div>
-            <div onClick={copyVar} className="code b-37-18">
-              var name: --b-37-18
+            <div onClick={copyVar} className="code b-36">
+              var name: --b-36
+            </div>
+            <div onClick={copyVar} className="code b-50">
+              var name: --b-50
+            </div>
+            <div onClick={copyVar} className="code b-59">
+              var name: --b-59
+            </div>        
+            <div onClick={copyVar} className="code b-92-62">
+              var name: --b-92-62
+            </div>    
+            <div onClick={copyVar} className="code b-72">
+              var name: --b-72
+            </div>
+            <div onClick={copyVar} className="code b-73">
+              var name: --b-73
+            </div>
+            <div onClick={copyVar} className="code b-100-64-40">
+              var name: --b-100-64-40
+            </div>
+            <div onClick={copyVar} className="code b-86">
+              var name: --b-86
+            </div>
+            <div onClick={copyVar} className="code b-49-79">
+              var name: --b-49-79
             </div>
             <div onClick={copyVar} className="code b-37-69">
               var name: --b-37-69
@@ -77,62 +74,74 @@ function ColorPicker() {
             <div onClick={copyVar} className="code b-38-62-40">
               var name: --b-38-62-40
             </div>
-            <div onClick={copyVar} className="code b-44-20">
-              var name: --b-44-20
-            </div>
             <div onClick={copyVar} className="code b-44-87">
               var name: --b-44-87
             </div>
-            <div onClick={copyVar} className="code b-49-79">
-              var name: --b-49-79
-            </div>
-            <div onClick={copyVar} className="code b-49-91">
-              var name: --b-49-91
-            </div>
-            <div onClick={copyVar} className="code b-53-8">
-              var name: --b-53-8
-            </div>
-            <div onClick={copyVar} className="code b-53-13">
-              var name: --b-53-13
-            </div>
-            <div onClick={copyVar} className="code b-54-15-53">
-              var name: --b-54-15-53
-            </div>
-            <div onClick={copyVar} className="code b-54-12">
-              var name: --b-54-12
-            </div>
-            <div onClick={copyVar} className="code b-79-96">
-              var name: --b-79-96
-            </div>
-            <div onClick={copyVar} className="code b-50">
-              var name: --b-50
-            </div>
-            <div onClick={copyVar} className="code b-50-22">
-              var name: --b-50-22
-            </div>
-            <div onClick={copyVar} className="code b-59">
-              var name: --b-59
-            </div>
-            <div onClick={copyVar} className="code b-50-95">
-              var name: --b-50-95
-            </div>
-            <div onClick={copyVar} className="code b-54-31">
-              var name: --b-54-31
+            <div onClick={copyVar} className="code b-19-48">
+              var name: --b-19-48
             </div>
             <div onClick={copyVar} className="code b-63-18-67">
               var name: --b-63-18-67
             </div>
-            <div onClick={copyVar} className="code b-72">
-              var name: --b-72
+            <div onClick={copyVar} className="code b-13-52">
+              var name: --b-13-52
             </div>
-            <div onClick={copyVar} className="code b-73">
-              var name: --b-73
+            <div onClick={copyVar} className="code b-16-35">
+              var name: --b-16-35
+            </div>
+            <div onClick={copyVar} className="code b-11-50">
+              var name: --b-11-50
+            </div>
+            <div onClick={copyVar} className="code b-30-84">
+              var name: --b-30-84
+            </div>
+            <div onClick={copyVar} className="code b-31-44-27">
+              var name: --b-31-44-27
+            </div>
+            <div onClick={copyVar} className="code b-31-81">
+              var name: --b-31-81
+            </div>
+            <div onClick={copyVar} className="code b-12-81">
+              var name: --b-12-81
+            </div>
+            <div onClick={copyVar} className="code b-15-83">
+              var name: --b-15-83
+            </div>
+            <div onClick={copyVar} className="code b-13-88">
+              var name: --b-13-88
+            </div>
+            <div onClick={copyVar} className="code b-16-87">
+              var name: --b-16-87
+            </div>
+            <div onClick={copyVar} className="code b-16-86">
+              var name: --b-16-86
+            </div>
+            <div onClick={copyVar} className="code b-20-93">
+              var name: --b-20-93
+            </div>
+            <div onClick={copyVar} className="code b-23-95">
+              var name: --b-23-95
+            </div>
+            <div onClick={copyVar} className="code b-33-93">
+              var name: --b-33-93
+            </div>
+            <div onClick={copyVar} className="code b-36-96">
+              var name: --b-36-96
+            </div>
+            <div onClick={copyVar} className="code b-49-91">
+              var name: --b-49-91
+            </div>
+            <div onClick={copyVar} className="code b-54-15-53">
+              var name: --b-54-15-53
+            </div>
+            <div onClick={copyVar} className="code b-79-96">
+              var name: --b-79-96
+            </div>
+            <div onClick={copyVar} className="code b-50-95">
+              var name: --b-50-95
             </div>
             <div onClick={copyVar} className="code b-82-11-5">
               var name: --b-82-11-5
-            </div>
-            <div onClick={copyVar} className="code b-86">
-              var name: --b-86
             </div>
             <div onClick={copyVar} className="code b-70-12-24">
               var name: --b-70-12-24
@@ -149,20 +158,11 @@ function ColorPicker() {
             <div onClick={copyVar} className="code b-90">
               var name: --b-90
             </div>
-            <div onClick={copyVar} className="code b-92-62">
-              var name: --b-92-62
-            </div>
-            <div onClick={copyVar} className="code b-93-16-8">
-              var name: --b-93-16-8
-            </div>
             <div onClick={copyVar} className="code b-95-31-18">
               var name: --b-95-31-18
             </div>
             <div onClick={copyVar} className="code b-97">
               var name: --b-97
-            </div>
-            <div onClick={copyVar} className="code b-100-64-40">
-              var name: --b-100-64-40
             </div>
             <div onClick={copyVar} className="code b-15-94">
               var name: --b-15-94
