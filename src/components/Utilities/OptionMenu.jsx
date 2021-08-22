95f3c16269e71a4a5542798c2ae37c7063da4495
Arif Uddin <arifunctg@gmail.com>
2021-08-22 17:11:51 +0600

    MenuBtn replaced

--- src/components/Utilities/OptionMenu.jsx
+++ src/components/Utilities/OptionMenu.jsx
@@ -1,89 +1,28 @@
-import { useState } from "react"
-import "./OptionMenu.css"
-function OptionMenu() {
+import './OptionMenu.css'
+import useComponentVisible from '../CompSettings/StyleCustomize/ChildComp/useComponentVisible'
 
-  const [active, setActive] = useState(false)
-  const optionHandler = () => {
-    if(active) {
-      setActive(false)
-    } else {
-      setActive(true)
-    }
-  }
+export default function OptionMenu({ title, children, w = 200, h = 250 }) {
+  const { ref, isComponentVisible: isActive, setIsComponentVisible } = useComponentVisible(false)
+  const optionHandler = () => setIsComponentVisible(!isActive)
+
   return (
-    <div className="ballon" onClick={optionHandler}>
-      <button className={`btn ${active && ' btn-active'}`}>x</button>
-      <div className={`drop-down ${active && ' drop-down-show'}`}>
-        <div className="drop-down-title">Title</div>
-        <div className="item">
-          <span className="logo">
-            <svg
-              id="Layer_1"
-              data-name="Layer 1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              viewBox="0 0 30 30"
-            >
-              <path
-                className="cls-1"
-                d="M13.83,6.32H6.11a2.19,2.19,0,0,0-2.2,2.19v15.3A2.19,2.19,0,0,0,6.11,26H21.55a2.19,2.19,0,0,0,2.2-2.19V16.16"
-              />
-              <path
-                className="cls-1"
-                d="M22.1,4.68a2.35,2.35,0,0,1,3.31,0,2.31,2.31,0,0,1,0,3.28L14.93,18.35l-4.41,1.09,1.11-4.37Z"
-              />
+    <div ref={ref} className="dropdown-wrp">
+      <div className={`drop-down ${isActive && ' drop-down-show'}`} style={{ ...isActive && { width: w, height: h } }}>
+        <button onClick={optionHandler} type="button" className={`menu-btn ${isActive && ' btn-active'}`} aria-label="Toggle option menu">
+          <div aria-hidden="true" className={`hamburger ${isActive && 'open'}`}>
+            <svg width="20" height="20" viewBox="0 0 100 100">
+              <path className="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
+              <path className="line line2" d="M 20,50 H 80" />
+              <path className="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
             </svg>
-          </span>
-          <span className="title">Edit</span>
-        </div>
-        <div className="item">
-          <span className="logo">
-            <svg
-              id="Layer_1"
-              data-name="Layer 1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              viewBox="0 0 30 30"
-            >
-              <path
-                className="cls-1"
-                d="M13.83,6.32H6.11a2.19,2.19,0,0,0-2.2,2.19v15.3A2.19,2.19,0,0,0,6.11,26H21.55a2.19,2.19,0,0,0,2.2-2.19V16.16"
-              />
-              <path
-                className="cls-1"
-                d="M22.1,4.68a2.35,2.35,0,0,1,3.31,0,2.31,2.31,0,0,1,0,3.28L14.93,18.35l-4.41,1.09,1.11-4.37Z"
-              />
-            </svg>
-          </span>
-          <span className="title">Edit</span>
-        </div>
-        <div className="item">
-          <span className="logo">
-            <svg
-              id="Layer_1"
-              data-name="Layer 1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              viewBox="0 0 30 30"
-            >
-              <path
-                className="cls-1"
-                d="M13.83,6.32H6.11a2.19,2.19,0,0,0-2.2,2.19v15.3A2.19,2.19,0,0,0,6.11,26H21.55a2.19,2.19,0,0,0,2.2-2.19V16.16"
-              />
-              <path
-                className="cls-1"
-                d="M22.1,4.68a2.35,2.35,0,0,1,3.31,0,2.31,2.31,0,0,1,0,3.28L14.93,18.35l-4.41,1.09,1.11-4.37Z"
-              />
-            </svg>
-          </span>
-          <span className="title">Edit</span>
-        </div>
+          </div>
+        </button>
+        <div className="drop-down-title">{title}</div>
+        <ul className="items-wrp" aria-hidden={!isActive}>
+          {children}
+        </ul>
+
       </div>
     </div>
   )
 }
-
-export default OptionMenu
