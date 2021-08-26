77ac57f24dd8f7f2d9fa71b005b502454895f1b3
Arif Uddin <arifunctg@gmail.com>
2021-08-26 12:06:29 +0600

    Update FormBuilderHelper.js

--- src/Utils/FormBuilderHelper.js
+++ src/Utils/FormBuilderHelper.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-continue */
+/* eslint-disable no-param-reassign */
 import produce from 'immer'
+import { deepCopy } from './Helpers'
 import { __ } from './i18nwrap'
 
 /**
@@ -6,13 +9,11 @@ import { __ } from './i18nwrap'
  * @param {array} layoutArr layout array [{x:0,y:1,...}, {x:0,y:2,...}, ]
  * @returns {Array} sorted layout
  */
-export const sortLayoutByXY = (layoutArr) => produce(layoutArr, draftLayoutArr => draftLayoutArr.sort((first, second) => {
+export const sortLayoutByXY = (layoutArr) => layoutArr.sort((first, second) => {
   const n = first.y - second.y
-  if (n !== 0) {
-    return n
-  }
+  if (n !== 0) return n
   return first.x - second.x
-}))
+})
 
 /**
  * check specific space are blank in a col
@@ -88,8 +89,9 @@ export function delAllPrevKeys(obj, key) {
  * @param {number} fieldMinW minimum space ned in a row for field
  * @returns {Array} converted array of object
  */
-export function convertLayout(layout, tc, fieldMinW = 1) {
+export function convertLayout(lay, tc, fieldMinW = 1) {
   const newLayout = []
+  const layout = deepCopy(lay)
   const layoutYAxisCount = {} // key-> y , value-> used col arr
 
   function setOrUpdateXY(y, xFilledArr) {
@@ -227,4 +229,185 @@ export const checkFieldsExtraAttr = (field, allFields, paymentsIntegs = [], addi
   }
 
   return {}
+}
+
+function getStatics(layout) {
+  return layout.filter((l) => l.static)
+}
+
+function sortLayoutItemsByColRow(layout) {
+  return layout.slice(0).sort((a, b) => {
+    if (a.x > b.x || (a.x === b.x && a.y > b.y)) {
+      return 1
+    }
+    return -1
+  })
+}
+
+function bottom(layout) {
+  let max = 0
+  let bottomY
+  for (let i = 0, len = layout.length; i < len; i += 1) {
+    bottomY = layout[i].y + layout[i].h
+    if (bottomY > max) max = bottomY
+  }
+  return max
+}
+
+function cloneLayoutItem(layoutItem) {
+  return {
+    w: layoutItem.w,
+    h: layoutItem.h,
+    x: layoutItem.x,
+    y: layoutItem.y,
+    i: layoutItem.i,
+    minW: layoutItem.minW,
+    maxW: layoutItem.maxW,
+    minH: layoutItem.minH,
+    maxH: layoutItem.maxH,
+    moved: Boolean(layoutItem.moved),
+    static: Boolean(layoutItem.static),
+    // These can be null/undefined
+    isDraggable: layoutItem.isDraggable,
+    isResizable: layoutItem.isResizable,
+    resizeHandles: layoutItem.resizeHandles,
+    isBounded: layoutItem.isBounded,
+  }
+}
+
+const heightWidth = { x: 'w', y: 'h' }
+
+function resolveCompactionCollision(layout, item, moveToCoord, axis) {
+  const sizeProp = heightWidth[axis]
+  item[axis] += 1
+  const itemIndex = layout
+    .map((layoutItem) => layoutItem.i)
+    .indexOf(item.i)
+
+  // Go through each item we collide with.
+  for (let i = itemIndex + 1; i < layout.length; i += 1) {
+    const otherItem = layout[i]
+    // Ignore static items
+    if (otherItem.static) continue
+
+    // Optimization: we can break early if we know we're past this el
+    // We can do this b/c it's a sorted layout
+    if (otherItem.y > item.y + item.h) break
+
+    if (collides(item, otherItem)) {
+      resolveCompactionCollision(
+        layout,
+        otherItem,
+        moveToCoord + item[sizeProp],
+        axis,
+      )
+    }
+  }
+
+  item[axis] = moveToCoord
+}
+
+function collides(l1, l2) {
+  if (l1.i === l2.i) return false // same element
+  if (l1.x + l1.w <= l2.x) return false // l1 is left of l2
+  if (l1.x >= l2.x + l2.w) return false // l1 is right of l2
+  if (l1.y + l1.h <= l2.y) return false // l1 is above l2
+  if (l1.y >= l2.y + l2.h) return false // l1 is below l2
+  return true // boxes overlap
+}
+
+function getFirstCollision(layout, layoutItem) {
+  for (let i = 0, len = layout.length; i < len; i += 1) {
+    if (collides(layout[i], layoutItem)) return layout[i]
+  }
+}
+
+function compactItem(compareWith, l, compactType, cols, fullLayout) {
+  const compactV = compactType === 'vertical'
+  const compactH = compactType === 'horizontal'
+  if (compactV) {
+    // Bottom 'y' possible is the bottom of the layout.
+    // This allows you to do nice stuff like specify {y: Infinity}
+    // This is here because the layout must be sorted in order to get the correct bottom `y`.
+    l.y = Math.min(bottom(compareWith), l.y)
+    // Move the element up as far as it can go without colliding.
+    while (l.y > 0 && !getFirstCollision(compareWith, l)) {
+      l.y -= 1
+    }
+  } else if (compactH) {
+    // Move the element left as far as it can go without colliding.
+    while (l.x > 0 && !getFirstCollision(compareWith, l)) {
+      l.x -= 1
+    }
+  }
+
+  // Move it down, and keep moving it down if it's colliding.
+  let collides
+  while ((collides = getFirstCollision(compareWith, l))) {
+    if (compactH) {
+      resolveCompactionCollision(fullLayout, l, collides.x + collides.w, 'x')
+    } else {
+      resolveCompactionCollision(fullLayout, l, collides.y + collides.h, 'y')
+    }
+    // Since we can't grow without bounds horizontally, if we've overflown, let's move it down and try again.
+    if (compactH && l.x + l.w > cols) {
+      l.x = cols - l.w
+      l.y += 1
+    }
+  }
+
+  // Ensure that there are no negative positions
+  l.y = Math.max(l.y, 0)
+  l.x = Math.max(l.x, 0)
+
+  return l
+}
+
+function sortLayoutItemsByRowCol(layout) {
+  // Slice to clone array as sort modifies
+  return layout.slice(0).sort((a, b) => {
+    if (a.y > b.y || (a.y === b.y && a.x > b.x)) {
+      return 1
+    } if (a.y === b.y && a.x === b.x) {
+      // Without this, we can get different sort results in IE vs. Chrome/FF
+      return 0
+    }
+    return -1
+  })
+}
+
+function sortLayoutItems(layout, compactType) {
+  if (compactType === 'horizontal') return sortLayoutItemsByColRow(layout)
+  if (compactType === 'vertical') return sortLayoutItemsByRowCol(layout)
+  return layout
+}
+
+export function compact(layout, compactType, cols) {
+  // Statics go in the compareWith array right away so items flow around them.
+  const compareWith = getStatics(layout)
+  // We go through the items by row and column.
+  const sorted = sortLayoutItems(layout, compactType)
+  // Holding for new items.
+  const out = Array(layout.length)
+
+  for (let i = 0, len = sorted.length; i < len; i += 1) {
+    let l = cloneLayoutItem(sorted[i])
+
+    // Don't move static elements
+    if (!l.static) {
+      l = compactItem(compareWith, l, compactType, cols, sorted)
+
+      // Add to comparison array. We only collide with items before this one.
+      // Statics are already in this array.
+      compareWith.push(l)
+    }
+
+    // Add to output array to make sure they still come out in the right order.
+    out[layout.indexOf(sorted[i])] = l
+
+    // Clear moved flag, if it exists.
+    l.moved = false
+  }
+
+  return out
 }
