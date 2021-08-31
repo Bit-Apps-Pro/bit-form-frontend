6ad20ac42d7be396701e892f26191cfbfe05bc5f
mdabbas-cse <gmabbas44@gmail.com>
2021-08-31 14:42:47 +0600

    Update navbar.style.js

--- src/styles/navbar.style.js
+++ src/styles/navbar.style.js
@@ -24,7 +24,7 @@ const navbar = {
     w: 40,
     h: '70%',
     p: '5px 7px',
-    brl: '0.5px solid var(--white-0-61)',
+    brl: '0.5px solid var(--b-13-52)',
     brr: '0.5px solid var(--white-0-61)',
     my: 5,
   },
@@ -47,7 +47,41 @@ const navbar = {
       bg: 'var(--white)',
       clr: 'var(--b-54-12)',
     },
+  },
+  btcd_bld_lnk: {
+    mt: 2,
+    ml: '-15%',
+  },
 
+  btcd_bld_btn: { flx: 'center' },
+  cls_btn: {
+    clr: 'var(--white)',
+    fs: 22,
+    size: 30,
+    mr: 20,
+    mt: 8,
+    p: 0,
+    ta: 'center',
+    t: 'color 0.5s !important',
+
+    '&:hover': { clr: 'var(--red-100-61) !important' },
+  },
+
+  btn: {
+    bg: 'var(--b-50) !important',
+    ol: 'none',
+    b: 'none',
+    clr: 'var(--white-100) !important',
+    br: '10px !important',
+    h: 28,
+    td: 'none',
+    my: 5,
+    mx: 10,
+    py: 7,
+    px: 15,
+    fs: 14,
+
+    '&:disabled': { bg: 'var(--b-100-50-5) !important' },
   },
 }
 
