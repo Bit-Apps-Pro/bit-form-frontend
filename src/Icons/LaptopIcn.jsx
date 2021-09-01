8dfb48191caf01952556be0902c250c881ccd1b6
mdabbas-cse <gmabbas44@gmail.com>
2021-09-01 17:58:17 +0600

    Update LaptopIcn.jsx

--- src/Icons/LaptopIcn.jsx
+++ src/Icons/LaptopIcn.jsx
@@ -8,7 +8,7 @@ export default function LaptopIcn({ size, w, h, stroke = 2 }) {
     s.h = h
   }
   return (
-    <svg width={w} height={h} viewBox="0 0 30 30">
+    <svg width={s.w} height={s.h} viewBox="0 0 30 30">
       <rect className="svg-icn" strokeMiterlimit="10" strokeWidth={stroke} x="5.76" y="7.71" width="18.47" height="11.61" rx="1" />
       <line className="svg-icn" strokeMiterlimit="10" strokeWidth={stroke} x1="4" y1="22.29" x2="26" y2="22.29" />
     </svg>
