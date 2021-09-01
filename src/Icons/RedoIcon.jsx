86e0dbcd1a3840b829a3ec41f1757da1c9fbb4f9
mdabbas-cse <gmabbas44@gmail.com>
2021-09-01 12:26:03 +0600

    change name backIcon to redoIcon

--- src/Icons/RedoIcon.jsx
+++ src/Icons/RedoIcon.jsx
@@ -1,4 +1,4 @@
-export default function BackIcon({ size, stroke = 2 }) {
+export default function RedoIcon({ size, stroke = 2 }) {
   return (
     <svg width={size} height={size} viewBox="0 0 30 30">
       <path className="svg-icn" strokeWidth={stroke} strokeMiterlimit="10" d="M18.2,26H14.89a8.15,8.15,0,0,1-8.16-8.08h0A8.15,8.15,0,0,1,14.89,9.8h6.57" />
