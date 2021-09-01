9e07500849840e9ca6af78cc38e78d756ff68340
mdabbas-cse <gmabbas44@gmail.com>
2021-09-01 18:59:31 +0600

    Update LayerIcon.jsx

--- src/Icons/LayerIcon.jsx
+++ src/Icons/LayerIcon.jsx
@@ -1,6 +1,14 @@
-export default function LayerIcon({ size, stroke = 2.15 }) {
+export default function LayerIcon({ size, w, h, stroke = 2.15 }) {
+  const s = {}
+  if (size) {
+    s.w = size
+    s.h = size
+  } else {
+    s.w = w
+    s.h = h
+  }
   return (
-    <svg width={size} height={size} viewBox="0 0 30 30">
+    <svg width={s.w} height={s.h} viewBox="0 0 30 30">
       <path className="svg-icn" strokeWidth={stroke}
         d="M14.17,4.22,7.05,7.76a1.82,1.82,0,0,0,0,3.28l7.19,3.57a1.69,1.69,0,0,0,1.52,0L23,11a1.82,1.82,0,0,0,0-3.28L15.83,4.22A1.86,1.86,0,0,0,14.17,4.22Z" />
       <path className="svg-icn" strokeWidth={stroke} d="M4.86,20.58l9.38,5.21a1.69,1.69,0,0,0,1.52,0l9.38-5.21" />
