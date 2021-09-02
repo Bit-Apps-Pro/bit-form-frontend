a5466227a6c165a7f64eef9c9fbb5bcd092ad724
mdabbas-cse <gmabbas44@gmail.com>
2021-09-02 17:08:10 +0600

    add icon in story book

---
+++ src/stories/ShowIcon.jsx
@@ -0,0 +1,281 @@
+/* eslint-disable react/button-has-type */
+import AddIcon from '../Icons/AddIcon'
+import APIIcon from '../Icons/APIIcon'
+import BackIcn from '../Icons/BackIcn'
+import BdrDashIcn from '../Icons/BdrDashIcn'
+import BdrDottedIcn from '../Icons/BdrDottedIcn'
+import BdrDoubleIcn from '../Icons/BdrDoubleIcn'
+import BdrSolidIcn from '../Icons/BdrSolidIcn'
+import BlurIcn from '../Icons/BlurIcn'
+import BrushIcn from '../Icons/BrushIcn'
+import BtnIcn from '../Icons/BtnIcn'
+import CheckBoxIcn from '../Icons/CheckBoxIcn'
+import CloseIcn from '../Icons/CloseIcn'
+import CodeSnippetIcn from '../Icons/CodeSnippetIcn'
+import ColorIcn from '../Icons/ColorIcn'
+import ColorPickerIcn from '../Icons/ColorPickerIcn'
+import CopyIcn from '../Icons/CopyIcn'
+import CPTIcn from '../Icons/CPTIcn'
+import DateIcn from '../Icons/DateIcn'
+import DateTimeIcn from '../Icons/DateTimeIcn'
+import DecisionBoxIcn from '../Icons/DecisionBoxIcn'
+import DocIcn from '../Icons/DocIcn'
+import DownloadIcon from '../Icons/DownloadIcon'
+import DropDownIcn from '../Icons/DropDownIcn'
+import EditIcn from '../Icons/EditIcn'
+import EditIcon from '../Icons/EditIcon'
+import EllipsisIcon from '../Icons/EllipsisIcon'
+import FieldIcn from '../Icons/FieldIcn'
+import FileUploadIcn from '../Icons/FileUploadIcn'
+import FlagIcn from '../Icons/FlagIcn'
+import FormIcn from '../Icons/FormIcn'
+import GoogleAdIcn from '../Icons/GoogleAdIcn'
+import HeightIcn from '../Icons/HeightIcn'
+import HoneypotIcn from '../Icons/HoneypotIcn'
+import HWordinateIcn from '../Icons/HWordinateIcn'
+import ImageIcn from '../Icons/ImageIcn'
+import InsideIcn from '../Icons/InsideIcn'
+import ItemBlockIcn from '../Icons/ItemBlockIcn'
+import LaptopIcn from '../Icons/LaptopIcn'
+import LayerIcon from '../Icons/LayerIcon'
+import MailIcn from '../Icons/MailIcn'
+import MailOpenIcn from '../Icons/MailOpenIcn'
+import MobileIcon from '../Icons/MobileIcon'
+import MonthIcn from '../Icons/MonthIcn'
+import MoveIcn from '../Icons/MoveIcn'
+import NoneIcn from '../Icons/NoneIcn'
+import NumberIcn from '../Icons/NumberIcn'
+import PasswordIcn from '../Icons/PasswordIcn'
+import PaymentsIcn from '../Icons/PaymentsIcn'
+import PaypalIcn from '../Icons/PaypalIcn'
+import QuestionIcn from '../Icons/QuestionIcn'
+import RadioIcn from '../Icons/RadioIcn'
+import RazorPayIcn from '../Icons/RazorPayIcn'
+import ReCaptchaIcn from '../Icons/ReCaptchaIcn'
+import RedoIcon from '../Icons/RedoIcon'
+import Settings2 from '../Icons/Settings2'
+import SettingsIcn from '../Icons/SettingsIcn'
+import SpreadIcn from '../Icons/SpreadIcn'
+import TabletIcon from '../Icons/TabletIcon'
+import TextareaIcn from '../Icons/TextareaIcn'
+import TextIcn from '../Icons/TextIcn'
+import TimeIcn from '../Icons/TimeIcn'
+import TrashIcn from '../Icons/TrashIcn'
+import UndoIcon from '../Icons/UndoIcon'
+import UrlIcn from '../Icons/UrlIcn'
+import WeekIcn from '../Icons/WeekIcn'
+import XYordinateIcn from '../Icons/XYordinateIcn'
+import YoutubeIcn from '../Icons/YoutubeIcn'
+import MenuIcon from '../Icons/__MenuIcon'
+import './ShowIcon.css'
+
+export default function ShowIcon() {
+  return (
+    <div className="flx">
+      <button className="icon">
+        <MenuIcon size="30" />
+      </button>
+      <button className="icon">
+        <AddIcon size="30" />
+      </button>
+      <button className="icon">
+        <APIIcon size="30" />
+      </button>
+      <button className="icon">
+        <BackIcn size="30" />
+      </button>
+      <button className="icon">
+        <BdrDashIcn size="30" />
+      </button>
+      <button className="icon">
+        <BdrDottedIcn size="30" />
+      </button>
+      <button className="icon">
+        <BdrDoubleIcn size="30" />
+      </button>
+      <button className="icon">
+        <BdrSolidIcn size="30" />
+      </button>
+      <button className="icon">
+        <BlurIcn h="25" w="17" />
+      </button>
+      <button className="icon">
+        <BrushIcn height="30" />
+      </button>
+      <button className="icon">
+        <BtnIcn size="30" />
+      </button>
+      <button className="icon">
+        <CheckBoxIcn w="30" />
+      </button>
+      <button className="icon">
+        <CloseIcn size="30" />
+      </button>
+      <button className="icon">
+        <CodeSnippetIcn size="30" />
+      </button>
+      <button className="icon">
+        <ColorIcn w="30" h="20" />
+      </button>
+      <button className="icon">
+        <ColorPickerIcn w="30" />
+      </button>
+      <button className="icon">
+        <CopyIcn size="30" />
+      </button>
+      <button className="icon">
+        <CPTIcn size="30" />
+      </button>
+      <button className="icon">
+        <DateIcn w="30" />
+      </button>
+      <button className="icon">
+        <DateTimeIcn w="30" />
+      </button>
+      <button className="icon">
+        <DecisionBoxIcn size="30" />
+      </button>
+      <button className="icon">
+        <DocIcn size="30" />
+      </button>
+      <button className="icon">
+        <DownloadIcon size="30" />
+      </button>
+      <button className="icon">
+        <DropDownIcn w="30" />
+      </button>
+      <button className="icon">
+        <EditIcn size="30" />
+      </button>
+      <button className="icon">
+        <EditIcon size="30" />
+      </button>
+      <button className="icon">
+        <EllipsisIcon size="30" />
+      </button>
+      <button className="icon">
+        <FieldIcn w="30" />
+      </button>
+      <button className="icon">
+        <FileUploadIcn w="30" />
+      </button>
+      <button className="icon">
+        <FlagIcn size="30" />
+      </button>
+      <button className="icon">
+        <FormIcn w="30" />
+      </button>
+      <button className="icon">
+        <GoogleAdIcn size="30" />
+      </button>
+      <button className="icon">
+        <HeightIcn h="30" />
+      </button>
+      <button className="icon">
+        <HoneypotIcn h="30" w="20" />
+      </button>
+      <button className="icon">
+        <HWordinateIcn h="30" w="20" />
+      </button>
+      <button className="icon">
+        <ImageIcn w="20" />
+      </button>
+      <button className="icon">
+        <InsideIcn size="30" />
+      </button>
+      <button className="icon">
+        <ItemBlockIcn w="20" />
+      </button>
+      <button className="icon">
+        <LaptopIcn size="30" />
+      </button>
+      <button className="icon">
+        <LayerIcon size="30" />
+      </button>
+      <button className="icon">
+        <MailIcn size="30" />
+      </button>
+      <button className="icon">
+        <MailOpenIcn size="30" />
+      </button>
+      <button className="icon">
+        <MobileIcon size="30" />
+      </button>
+      <button className="icon">
+        <MonthIcn w="30" />
+      </button>
+      <button className="icon">
+        <MoveIcn size="30" />
+      </button>
+      <button className="icon">
+        <NoneIcn size="30" />
+      </button>
+      <button className="icon">
+        <NumberIcn w="30" />
+      </button>
+      <button className="icon">
+        <PasswordIcn size="30" />
+      </button>
+      <button className="icon">
+        <PaymentsIcn size="30" />
+      </button>
+      <button className="icon">
+        <PaypalIcn w="30" />
+      </button>
+      <button className="icon">
+        <QuestionIcn size="30" />
+      </button>
+      <button className="icon">
+        <RadioIcn size="30" />
+      </button>
+      <button className="icon">
+        <RazorPayIcn w="20" h="25" />
+      </button>
+      <button className="icon">
+        <ReCaptchaIcn size="30" />
+      </button>
+      <button className="icon">
+        <RedoIcon size="30" />
+      </button>
+      <button className="icon">
+        <Settings2 size="30" />
+      </button>
+      <button className="icon">
+        <SettingsIcn size="30" />
+      </button>
+      <button className="icon">
+        <SpreadIcn size="30" />
+      </button>
+      <button className="icon">
+        <TabletIcon size="30" />
+      </button>
+      <button className="icon">
+        <TextareaIcn size="30" />
+      </button>
+      <button className="icon">
+        <TextIcn size="30" />
+      </button>
+      <button className="icon">
+        <TimeIcn size="30" />
+      </button>
+      <button className="icon">
+        <TrashIcn size="30" />
+      </button>
+      <button className="icon">
+        <UndoIcon size="30" />
+      </button>
+      <button className="icon">
+        <UrlIcn w="30" />
+      </button>
+      <button className="icon">
+        <WeekIcn size="30" />
+      </button>
+      <button className="icon">
+        <XYordinateIcn size="30" />
+      </button>
+      <button className="icon">
+        <YoutubeIcn size="30" />
+      </button>
+    </div>
+  )
+}
