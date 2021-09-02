604e0c52df3e454fed48023167040c4626670685
mdabbas-cse <gmabbas44@gmail.com>
2021-09-02 11:20:56 +0600

    remove size control object

--- src/Icons/EllipsisIcon.jsx
+++ src/Icons/EllipsisIcon.jsx
@@ -1,14 +1,6 @@
-export default function EllipsisIcon({ size, w, h, stroke = 3 }) {
-  const s = {}
-  if (size) {
-    s.w = size
-    s.h = size
-  } else {
-    s.w = w
-    s.h = h
-  }
+export default function EllipsisIcon({ size, stroke = 3 }) {
   return (
-    <svg width={s.w} height={s.h} viewBox="0 0 30 30">
+    <svg width={size} height={size} viewBox="0 0 30 30">
       <line className="svg-icn" strokeWidth={stroke} x1="10.27" y1="15" x2="10.27" y2="15" />
       <line className="svg-icn" strokeWidth={stroke} x1="15" y1="15" x2="15" y2="15" />
       <line className="svg-icn" strokeWidth={stroke} x1="19.73" y1="15" x2="19.73" y2="15" />
