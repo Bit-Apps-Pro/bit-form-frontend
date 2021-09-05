86d763062afa6a4ced9dac33f6011d77ca98f689
Arif Uddin <arifunctg@gmail.com>
2021-09-05 17:30:38 +0600

    class renamced

--- src/styles/2.utilities.js
+++ src/styles/2.utilities.js
@@ -3,5 +3,22 @@ const ut = {
     display: 'grid',
     placeContent: 'center',
   },
+  icn_hover: {
+    pos: 'relative',
+    '::before': {
+      content: '""',
+      z: -1,
+      pos: 'absolute',
+      size: 0,
+      top: '50%',
+      left: '50%',
+      br: '50%',
+      tf: 'translate(-50%,-50%)',
+      t: '400ms border, opacity 300ms',
+      o: 0,
+      b: '0px solid var(--white-0-81-32)',
+    },
+    ':hover::before': { b: '30px solid var(--white-0-81-32)', o: 1 },
+  },
 }
 export default ut
