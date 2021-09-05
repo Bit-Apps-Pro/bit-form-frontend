98a888dc02a1b244291bd321f6ebebc31ff4b4fb
Arif Uddin <arifunctg@gmail.com>
2021-09-05 17:30:53 +0600

    Update EllipsisIcon.jsx

--- src/Icons/EllipsisIcon.jsx
+++ src/Icons/EllipsisIcon.jsx
@@ -1,4 +1,4 @@
-export default function EllipsisIcon({ size, stroke = 3 }) {
+export default function EllipsisIcon({ size, stroke = 2.5 }) {
   return (
     <svg width={size} height={size} viewBox="0 0 30 30">
       <line className="svg-icn" strokeWidth={stroke} x1="10.27" y1="15" x2="10.27" y2="15" />
