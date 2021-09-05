c636d36445e572c4a9f827cba96bc85c25d42212
Arif Uddin <arifunctg@gmail.com>
2021-09-05 17:30:15 +0600

    Create Tip.jsx

---
+++ src/components/Utilities/Tip.jsx
@@ -0,0 +1,31 @@
+import Tippy from '@tippyjs/react'
+import { animateFill } from 'tippy.js'
+import 'tippy.js/dist/tippy.css'
+// import 'tippy.js/themes/translucent.css'
+import 'tippy.js/themes/material.css'
+// import 'tippy.js/animations/scale.css'
+// import 'tippy.js/dist/svg-arrow.css'
+import 'tippy.js/dist/backdrop.css'
+import 'tippy.js/animations/shift-away.css'
+import { useFela } from 'react-fela'
+
+export default function Tip({ className, children, width = 'auto', msg = '', place = 'bottom' }) {
+  const { css } = useFela()
+  const style = { fw: 400 }
+  return (
+    <Tippy
+      animateFill
+      placement={place}
+      plugins={[animateFill]}
+      duration={150}
+      theme="material"
+      animation="shift-away"
+      interactive
+      maxWidth={width}
+      // arrow
+      content={<span className={css(style)}>{msg}</span>}
+    >
+      <span>{children}</span>
+    </Tippy>
+  )
+}
