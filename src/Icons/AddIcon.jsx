b637eecd68c25ece1b460debebd464baa944b6f7
Arif Uddin <arifunctg@gmail.com>
2021-09-05 12:26:55 +0600

    Update AddIcon.jsx

--- src/Icons/AddIcon.jsx
+++ src/Icons/AddIcon.jsx
@@ -1,17 +1,9 @@
-export default function AddIcon({ size, w, h, stroke = 2 }) {
-  const s = {}
-  if (size) {
-    s.w = size
-    s.h = size
-  } else {
-    s.w = w
-    s.h = h
-  }
+export default function AddIcon({ size, stroke = 2 }) {
   return (
-    <svg width={s.w} height={s.h} viewBox="0 0 30 30">
+    <svg width={size} height={size} viewBox="0 0 30 30">
       <path
         className="svg-icn"
-        strokeWidth="stroke"
+        strokeWidth={stroke}
         d="M6.4,4.16H23.6a2.43,2.43,0,0,1,2.45,2.41V23.43a2.43,2.43,0,0,1-2.45,2.41H6.4A2.43,2.43,0,0,1,4,23.43V6.57A2.43,2.43,0,0,1,6.4,4.16Z"
       />
       <line className="svg-icn" strokeWidth={stroke} x1="15" y1="10.18" x2="15" y2="19.82" />
