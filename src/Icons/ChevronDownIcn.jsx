29e559d7ea2a5f765758a957652e6375e691fd39
Arif Uddin <arifunctg@gmail.com>
2021-09-06 12:28:07 +0600

    new icon added

---
+++ src/Icons/ChevronDownIcn.jsx
@@ -0,0 +1,16 @@
+export default function ChevronDownIcn({ size, stroke = 2 }) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={stroke}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polyline points="6 9 12 15 18 9" />
+    </svg>
+  )
+}
