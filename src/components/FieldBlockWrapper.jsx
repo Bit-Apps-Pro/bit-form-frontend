56d08cf19038fe7d00dc7572e2a3ff41890ee601
Md Rubel Amin <mdrubelamin2@gmail.com>
2021-09-13 17:03:07 +0600

    new: field block context menu

--- src/components/FieldBlockWrapper.jsx
+++ src/components/FieldBlockWrapper.jsx
@@ -1,11 +1,23 @@
+import produce from 'immer'
 import { useContext } from 'react'
+import { useFela } from 'react-fela'
 import { useHistory } from 'react-router-dom'
+import { useSetRecoilState } from 'recoil'
 import { hideAll } from 'tippy.js'
+import { $fields, $updateBtn } from '../GlobalStates'
 import BrushIcn from '../Icons/BrushIcn'
+import CheckBoxIcn from '../Icons/CheckBoxIcn'
 import ChevronDownIcn from '../Icons/ChevronDownIcn'
+import ChevronRightIcon from '../Icons/ChevronRightIcon'
+import CopyIcn from '../Icons/CopyIcn'
 import EditIcn from '../Icons/EditIcn'
+import EyeOffIcon from '../Icons/EyeOffIcon'
+import LaptopIcn from '../Icons/LaptopIcn'
+import MobileIcon from '../Icons/MobileIcon'
 import MoveIcn from '../Icons/MoveIcn'
+import TabletIcon from '../Icons/TabletIcon'
 import TrashIcn from '../Icons/TrashIcn'
+import context from '../styles/fieldContextMenu.style'
 import { AppSettings } from '../Utils/AppSettingsContext'
 import { deepCopy } from '../Utils/Helpers'
 import { __ } from '../Utils/i18nwrap'
@@ -15,6 +27,9 @@ import Downmenu from './Utilities/Downmenu'
 export default function FieldBlockWrapper({ layoutItem, removeLayoutItem, cloneLayoutItem, fields, formID }) {
   const history = useHistory()
   const { reCaptchaV2 } = useContext(AppSettings)
+  const { css } = useFela()
+  const setFields = useSetRecoilState($fields)
+  const setUpdateBtn = useSetRecoilState($updateBtn)
 
   const navigateToFieldSettings = () => {
     history.replace(history.location.pathname.replace(/style\/.+|style/g, 'fs'))
@@ -34,6 +49,61 @@ export default function FieldBlockWrapper({ layoutItem, removeLayoutItem, cloneL
     }
     return <MapComponents isBuilder formID={formID} atts={componentProps} />
   }
+
+  const handleFieldHide = brkpnt => {
+    setFields(allFields => produce(allFields, draft => {
+      const fldData = draft[layoutItem.i]
+      if (!fldData.hidden) fldData.hidden = []
+      if (brkpnt === 'all' && fldData.hidden.length < 3) {
+        fldData.hidden = ['lg', 'md', 'sm']
+      } else if (brkpnt === 'all') {
+        fldData.hidden = []
+      } else if (fldData.hidden.includes(brkpnt)) {
+        fldData.hidden.splice(fldData.hidden.indexOf(brkpnt), 1)
+      } else {
+        fldData.hidden.push(brkpnt)
+      }
+      if (!fldData.hidden.length) delete fldData.hidden
+    }))
+
+    setUpdateBtn({ unsaved: true })
+  }
+
+  const checkIfHidden = brkpnt => {
+    const fldData = fields[layoutItem.i]
+    if (fldData?.hidden?.length === 3) return true
+    if (fldData?.hidden?.includes(brkpnt)) return true
+    return false
+  }
+
+  const DeleteButton = ({ placement, className, label }) => (
+    <Downmenu place={placement || 'top'}>
+      <button
+        data-close
+        type="button"
+        className={className}
+        unselectable="on"
+        draggable="false"
+        style={{ cursor: 'pointer' }}
+        title={__('Remove', 'bitform')}
+      >
+        <TrashIcn size="19" />
+        {label && <span>{label}</span>}
+      </button>
+      <div className="wdt-200">
+        <div className="mb-2 f-5">Are you sure ?</div>
+        <div className="f-12 mb-1 f-rob">After deleting this field, you will lose all previous responses of this field.</div>
+        <div className="f-12 mb-2 f-rob">
+          <b>Tip : </b>
+          You can hide this field instead.
+        </div>
+        <div className="flx flx-c">
+          <button onClick={() => hideAll()} className="tip-btn mr-2" type="button">Cancel</button>
+          <button onClick={() => removeLayoutItem(layoutItem.i)} className="tip-btn red-btn" type="button">Delete</button>
+        </div>
+      </div>
+    </Downmenu>
+  )
 
   return (
     <>
@@ -62,31 +132,7 @@ export default function FieldBlockWrapper({ layoutItem, removeLayoutItem, cloneL
         >
           <EditIcn size="20" />
         </button>
-        <Downmenu place="top">
-          <button
-            data-close
-            type="button"
-            className="g-c us-n no-drg blk-wrp-btn"
-            unselectable="on"
-            draggable="false"
-            style={{ cursor: 'pointer' }}
-            title={__('Remove', 'bitform')}
-          >
-            <TrashIcn size="19" />
-          </button>
-          <div className="wdt-200">
-            <div className="mb-2 f-5">Are you sure ?</div>
-            <div className="f-12 mb-1 f-rob">After deleting this field, you will lose all previous responses of this field.</div>
-            <div className="f-12 mb-2 f-rob">
-              <b>Tip : </b>
-              You can hide this field instead.
-            </div>
-            <div className="flx flx-c">
-              <button onClick={() => hideAll()} className="tip-btn mr-2" type="button">Cancel</button>
-              <button onClick={() => removeLayoutItem(layoutItem.i)} className="tip-btn red-btn" type="button">Delete</button>
-            </div>
-          </div>
-        </Downmenu>
+        <DeleteButton className="g-c us-n no-drg blk-wrp-btn" />
         <Downmenu>
           <button
             data-close
@@ -99,7 +145,80 @@ export default function FieldBlockWrapper({ layoutItem, removeLayoutItem, cloneL
           >
             <ChevronDownIcn size="19" />
           </button>
-          <button type="button" onClick={() => cloneLayoutItem(layoutItem.i)}>Clone Field</button>
+          <div className={css(context.menu)}>
+            <ul className={css(context.list)}>
+              <li className={css(context.item)}>
+                <button type="button" className={css(context.btn)} onClick={navigateToFieldSettings}>
+                  <EditIcn size="19" />
+                  <span>Settings</span>
+                </button>
+              </li>
+              <li className={css(context.item)}>
+                <button type="button" className={css(context.btn)} onClick={() => navigateToStyle(fields[layoutItem.i].typ)}>
+                  <BrushIcn height="18" width="14" />
+                  <span>Style</span>
+                </button>
+              </li>
+              <li className={css(context.item)}>
+                <button type="button" className={css(context.btn)} onClick={() => cloneLayoutItem(layoutItem.i)}>
+                  <CopyIcn size="19" />
+                  <span>Clone</span>
+                </button>
+              </li>
+              <li className={css(context.item)}>
+                <Downmenu place="right-start" arrow={false}>
+                  <button
+                    data-close
+                    type="button"
+                    className={`${css(context.btn)}}`}
+                    unselectable="on"
+                    draggable="false"
+                    style={{ cursor: 'pointer' }}
+                    title={__('More Options', 'bitform')}
+                  >
+                    <EyeOffIcon size="19" />
+                    <span>Hide</span>
+                    <ChevronRightIcon size="19" />
+                  </button>
+                  <div className={css(context.menu)}>
+                    <ul className={css(context.list)}>
+                      <li className={css(context.item)}>
+                        <button type="button" className={css(context.btn)} onClick={() => handleFieldHide('all')}>
+                          <ChevronDownIcn size="19" />
+                          <span>all</span>
+                          {checkIfHidden() && <CheckBoxIcn w="19" />}
+                        </button>
+                      </li>
+                      <li className={css(context.item)}>
+                        <button type="button" className={css(context.btn)} onClick={() => handleFieldHide('lg')}>
+                          <LaptopIcn size="19" />
+                          <span>large</span>
+                          {checkIfHidden('lg') && <CheckBoxIcn w="19" />}
+                        </button>
+                      </li>
+                      <li className={css(context.item)}>
+                        <button type="button" className={css(context.btn)} onClick={() => handleFieldHide('md')}>
+                          <TabletIcon size="19" />
+                          <span>medium</span>
+                          {checkIfHidden('md') && <CheckBoxIcn w="19" />}
+                        </button>
+                      </li>
+                      <li className={css(context.item)}>
+                        <button type="button" className={css(context.btn, context.checked)} onClick={() => handleFieldHide('sm')}>
+                          <MobileIcon size="19" />
+                          <span>small</span>
+                          {checkIfHidden('sm') && <CheckBoxIcn w="19" />}
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                </Downmenu>
+              </li>
+              <li className={css(context.item)}>
+                <DeleteButton placement="bottom" className={css(context.btn, context.delete)} label="Remove" />
+              </li>
+            </ul>
+          </div>
         </Downmenu>
       </div>
       <ComponentsByTheme />
