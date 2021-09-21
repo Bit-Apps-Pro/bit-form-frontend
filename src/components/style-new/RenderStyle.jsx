40862a22933f62e1399bfce947ab6cfe6eec24c3
Arif Uddin <arifunctg@gmail.com>
2021-09-21 15:16:43 +0600

    style object changed

--- src/components/style-new/RenderStyle.jsx
+++ src/components/style-new/RenderStyle.jsx
@@ -6,7 +6,7 @@ export default function RenderStyle({ styleClasses }) {
 
   for (let i = 0; i < fieldStyleArrLength; i += 1) {
     const [className, styleObj] = fieldStyleArr[i]
-    styleString += `.${className}{`
+    styleString += `${className}{`
 
     for (const prop in styleObj) {
       if (Object.hasOwnProperty.call(styleObj, prop)) {
--- src/components/style-new/defaultTheme.js
+++ src/components/style-new/defaultTheme.js
@@ -14,14 +14,14 @@ export default function defaultTheme(fk, type) {
     case 'color':
     case 'textarea':
       return {
-        [`${fk}-fw`]: {
+        [`.${fk}-fw`]: {
           background: 'Red',
           height: '100%',
           'text-align': 'start',
           width: '100%',
           padding: '10px',
         },
-        [`${fk}-lbl`]: {
+        [`.${fk}-lbl`]: {
           display: 'block',
           overflow: 'hidden',
           margin: 0,
@@ -30,8 +30,8 @@ export default function defaultTheme(fk, type) {
           color: 'rgba(42, 49, 99, 1)!important',
           'line-height': '1.4!important',
         },
-        [`${fk}-iw`]: { position: 'relative' },
-        [`${fk}-fld`]: {
+        [`.${fk}-iw`]: { position: 'relative' },
+        [`.${fk}-fld`]: {
           display: ' inline-block !important',
           direction: 'inherit !important',
           'max-width': '100% !important',
@@ -50,23 +50,23 @@ export default function defaultTheme(fk, type) {
           height: type === 'textarea' ? 'calc(100% - 30px)' : '40px',
           ...type === 'textarea' && { resize: 'vertical' },
         },
-        [`${fk}-fld:focus`]: {
+        [`.${fk}-fld:focus`]: {
           'box-shadow': '0px 0px 0px 3px rgba(151, 203, 252, 0.38) !important',
           'border-color': 'rgba(29, 158, 249, 1)!important',
         },
-        [`${fk}-fld:hover`]: { 'border-color': 'rgba(29, 158, 249, 1)!important' },
-        [`${fk}-fld::placeholder`]: { color: 'rgba(213, 212, 221, 1)!important' },
+        [`.${fk}-fld:hover`]: { 'border-color': 'rgba(29, 158, 249, 1)!important' },
+        [`.${fk}-fld::placeholder`]: { color: 'rgba(213, 212, 221, 1)!important' },
       }
     case 'check':
     case 'radio':
       return {
-        [`${fk}-fw`]: {
+        [`.${fk}-fw`]: {
           height: '100%',
           'text-align': 'start',
           width: '100%',
           padding: '10px',
         },
-        [`${fk}-lbl`]: {
+        [`.${fk}-lbl`]: {
           display: 'block',
           overflow: 'hidden',
           margin: 0,
@@ -76,41 +76,41 @@ export default function defaultTheme(fk, type) {
           'line-height': '1.4!important',
         },
         // checkbox container
-        [`${fk}-cc`]: {
+        [`.${fk}-cc`]: {
           display: 'flex',
           'flex-wrap': 'wrap',
           'margin-top': '8px',
         },
         // checkbox wrapper
-        [`${fk}-cw`]: { 'margin-right': '10px' },
+        [`.${fk}-cw`]: { 'margin-right': '10px' },
         // checkbox label
-        [`${fk}-cl`]: {
+        [`.${fk}-cl`]: {
           cursor: 'pointer',
           display: 'flex',
           'align-items': 'center',
         },
         // checkbox input
-        [`${fk}-ci`]: {
+        [`.${fk}-ci`]: {
           position: 'absolute',
           opacity: '0!important',
         },
-        [`${fk}-ci:checked ~ .${fk}-cl .${fk}-bx`]: {
+        [`.${fk}-ci:checked ~ .${fk}-cl .${fk}-bx`]: {
           background: 'blue',
           'border-color': 'blue',
         },
-        [`${fk}-ci:focus ~ .${fk}-cl .${fk}-bx`]: { 'box-shadow': '0 0 0 3px rgb(185, 213, 255)' },
-        [`${fk}-ci:active ~ .${fk}-cl .${fk}-bx`]: { transform: 'scale(0.9)' },
-        [`${fk}-ci:disabled ~ .${fk}-cl`]: {
+        [`.${fk}-ci:focus ~ .${fk}-cl .${fk}-bx`]: { 'box-shadow': '0 0 0 3px rgb(185, 213, 255)' },
+        [`.${fk}-ci:active ~ .${fk}-cl .${fk}-bx`]: { transform: 'scale(0.9)' },
+        [`.${fk}-ci:disabled ~ .${fk}-cl`]: {
           opacity: 0.6,
           'pointer-events': 'none',
           cursor: 'not-allowed',
         },
-        [`${fk}-ci:disabled ~ .${fk}-cl .${fk}-bx`]: {
+        [`.${fk}-ci:disabled ~ .${fk}-cl .${fk}-bx`]: {
           background: 'rgb(230, 230, 230)',
           'border-color': 'rgb(209, 209, 209)',
         },
-        [`${fk}-cw:hover .box`]: { 'border-color': 'blue' },
-        [`${fk}-bx`]: {
+        [`.${fk}-cw:hover .box`]: { 'border-color': 'blue' },
+        [`.${fk}-bx`]: {
           position: 'relative',
           height: '20px',
           width: '20px',
@@ -120,7 +120,7 @@ export default function defaultTheme(fk, type) {
           'aspect-ratio': '1 / 1',
           transition: 'all 0.2s',
         },
-        [`${fk}-bx::before`]: {
+        [`.${fk}-bx::before`]: {
           content: '""',
           position: 'absolute',
           left: '50%',
@@ -128,8 +128,8 @@ export default function defaultTheme(fk, type) {
           width: 0,
         },
         ...type === 'check' && {
-          [`${fk}-ck`]: { 'border-radius': '5px' },
-          [`${fk}-ck::before`]: {
+          [`.${fk}-ck`]: { 'border-radius': '5px' },
+          [`.${fk}-ck::before`]: {
             top: '40%',
             border: 'solid white',
             'border-width': 0,
@@ -137,7 +137,7 @@ export default function defaultTheme(fk, type) {
             filter: 'drop-shadow(1px 1px 1px gray)',
             transform: 'translate(-50%, -50%) rotate(45deg)',
           },
-          [`${fk}-ci:checked ~ .${fk}-cl .${fk}-ck::before`]: {
+          [`.${fk}-ci:checked ~ .${fk}-cl .${fk}-ck::before`]: {
             width: '3px',
             height: '8px',
             'border-width': '0 3px 3px 0',
@@ -145,8 +145,8 @@ export default function defaultTheme(fk, type) {
         },
 
         ...type === 'radio' && {
-          [`${fk}-rdo`]: { 'border-radius': '50%' },
-          [`${fk}-rdo::before`]: {
+          [`.${fk}-rdo`]: { 'border-radius': '50%' },
+          [`.${fk}-rdo::before`]: {
             'border-radius': '50%',
             transition: 'all 0.2s cubic-bezier(0.25, 0.1, 0.59, 1.82)',
             top: '50%',
@@ -154,7 +154,7 @@ export default function defaultTheme(fk, type) {
             'box-shadow': '0 1px 3px 0px grey',
             transform: 'translate(-50%, -50%)',
           },
-          [`${fk}-ci:checked ~ .${fk}-cl .${fk}-rdo::before`]: {
+          [`.${fk}-ci:checked ~ .${fk}-cl .${fk}-rdo::before`]: {
             width: '50%',
             height: '50%',
           },
