6a35c58511099c22c726954666bae324380d8d1b
Arif Uddin <arifunctg@gmail.com>
2021-09-22 16:36:51 +0600

    components splited

--- src/components/CompSettings/CompSettings.jsx
+++ src/components/CompSettings/CompSettings.jsx
@@ -47,50 +47,55 @@ import TextFieldSettings from './TextFieldSettings'
 import BtnIcn from '../../Icons/BtnIcn'
 import { $fields, $selectedFieldId } from '../../GlobalStates'
 import BackIcn from '../../Icons/BackIcn'
+import ChevronRightIcon from '../../Icons/ChevronRightIcon'
+import FieldLinkBtn from './FieldLinkButton'
 
 function CompSettings({ style, styleDispatch, brkPoint, setResponsiveView }) {
   const { path } = useRouteMatch()
   const { formType, formID } = useParams()
-  const [scrollTopShadow, setScrollTopShadow] = useState(false)
+  // const [scrollTopShadow, setScrollTopShadow] = useState(false)
 
-  const TabLink = ({ title, sub, icn, link }) => (
-    <NavLink to={`/form/builder/${formType}/${formID}/${link}`} activeClassName="s-t-l-active" className="btcd-s-tab-link active flx w-5 ">
-      {typeof icn === 'string' ? <span className={`btcd-icn icn-${icn} mr-2`} /> : icn}
-      <div className="d-in-b">
-        <div className="title">{title}</div>
-        <div className="sub">{sub}</div>
-      </div>
-    </NavLink>
-  )
+  // const TabLink = ({ title, sub, icn, link }) => (
+  //   <NavLink to={`/form/builder/${formType}/${formID}/${link}`} activeClassName="s-t-l-active" className="btcd-s-tab-link active flx w-5 ">
+  //     {typeof icn === 'string' ? <span className={`btcd-icn icn-${icn} mr-2`} /> : icn}
+  //     <div className="d-in-b">
+  //       <div className="title">{title}</div>
+  //       <div className="sub">{sub}</div>
+  //     </div>
+  //   </NavLink>
+  // )
 
-  const onSettingScroll = ({ target: { scrollTop } }) => {
-    scrollTop > 20 ? setScrollTopShadow(true) : setScrollTopShadow(false)
-  }
+  // const onSettingScroll = ({ target: { scrollTop } }) => {
+  //   scrollTop > 20 ? setScrollTopShadow(true) : setScrollTopShadow(false)
+  // }
 
   return (
     <div className="elm-settings">
-      <div className="elm-settings-title pos-rel flx" style={{ ...scrollTopShadow && { boxShadow: '0 0px 16px 2px #b0b7d8' } }}>
+      {/* <div className="elm-settings-title pos-rel flx" style={{ ...scrollTopShadow && { boxShadow: '0 0px 16px 2px #b0b7d8' } }}>
         <TabLink title={__('Field', 'bitform')} sub={__('Settings', 'bitform')} icn="settings" link="fs" />
         <TabLink title={__('Style', 'bitform')} sub={__('Customize', 'bitform')} icn={<i className="mr-2"><BrushIcn height="22" width="22" /></i>} link="style" />
-      </div>
-      <div className="btcd-hr" />
+      </div> */}
+      {/* <div className="btcd-hr" /> */}
       <div className="settings">
-        <Scrollbars onScroll={onSettingScroll} autoHide>
+        <Scrollbars
+          // onScroll={onSettingScroll}
+          autoHide
+        >
           <Switch>
             <Route path={`${path}/fs`}><RenderSettings /></Route>
 
             <Route exact path={`${path}/style`}>
               <Link to={`/form/builder/${formType}/${formID}/style/bg`}>
-                <FieldOptionBtn icn={<ImageIcn w="20" />} title={__('Background Customize', 'bitform')} />
+                <FieldLinkBtn icn={<ImageIcn w="20" />} title={__('Background Customize', 'bitform')} />
               </Link>
               <Link to={`/form/builder/${formType}/${formID}/style/f`}>
-                <FieldOptionBtn icn={<FormIcn w="20" />} title={__('Form Customize', 'bitform')} />
+                <FieldLinkBtn icn={<FormIcn w="20" />} title={__('Form Customize', 'bitform')} />
               </Link>
               <Link to={`/form/builder/${formType}/${formID}/style/fb`}>
-                <FieldOptionBtn icn={<ItemBlockIcn w="20" />} title={__('Field Block Customize', 'bitform')} />
+                <FieldLinkBtn icn={<ItemBlockIcn w="20" />} title={__('Field Block Customize', 'bitform')} />
               </Link>
               <Link to={`/form/builder/${formType}/${formID}/style/fl`}>
-                <FieldOptionBtn icn={<FieldIcn w="20" />} title={__('Field Customize', 'bitform')} />
+                <FieldLinkBtn icn={<FieldIcn w="20" />} title={__('Field Customize', 'bitform')} />
               </Link>
             </Route>
             <Route path={`${path}/style/bg`}>
@@ -115,16 +120,16 @@ function CompSettings({ style, styleDispatch, brkPoint, setResponsiveView }) {
                 </h4>
               </Link>
               <Link to={`/form/builder/${formType}/${formID}/style/fl/fld`}>
-                <FieldOptionBtn icn={<FieldIcn w="20" />} title={__('Field Style', 'bitform')} />
+                <FieldLinkBtn icn={<FieldIcn w="20" />} title={__('Field Style', 'bitform')} />
               </Link>
               <Link to={`/form/builder/${formType}/${formID}/style/fl/dpd`}>
-                <FieldOptionBtn icn={<DropDownIcn w="20" />} title="Dropdown Style" />
+                <FieldLinkBtn icn={<DropDownIcn w="20" />} title="Dropdown Style" />
               </Link>
               <Link to={`/form/builder/${formType}/${formID}/style/fl/ppl`}>
-                <FieldOptionBtn icn={<PaypalIcn w="20" />} title={__('Paypal Style', 'bitform')} />
+                <FieldLinkBtn icn={<PaypalIcn w="20" />} title={__('Paypal Style', 'bitform')} />
               </Link>
               <Link to={`/form/builder/${formType}/${formID}/style/fl/btn`}>
-                <FieldOptionBtn icn={<BtnIcn size="24" />} title="Button Style" />
+                <FieldLinkBtn icn={<BtnIcn size="24" />} title="Button Style" />
               </Link>
             </Route>
             <Route path={`${path}/style/fl/fld`}>
@@ -224,7 +229,7 @@ function FieldList() {
           if (typ === 'decision-box') {
             lbl = adminLbl
           }
-          return <FieldOptionBtn key={fldKey} icn={typ} title={lbl || adminLbl || typ} sub={fldKey} action={() => setSelectedFieldId(fldKey)} />
+          return <FieldLinkBtn key={fldKey} icn={typ} title={lbl || adminLbl || typ} sub={fldKey} action={() => setSelectedFieldId(fldKey)} />
         })}
       </div>
     )
@@ -235,91 +240,5 @@ function FieldList() {
       <FieldListSection title="Hidden Fields" filteredFields={hiddenFlds} />
       <FieldListSection title="All Fields" filteredFields={notHiddenFlds} />
     </>
-  )
-}
-
-const FieldIcon = icn => {
-  switch (icn) {
-    case 'text':
-      return <TextIcn size="23" />
-    case 'username':
-      return <UserIcn size="23" />
-    case 'textarea':
-      return <TextareaIcn size="23" />
-    case 'check':
-      return <CheckBoxIcn w="23" />
-    case 'radio':
-      return <RadioIcn size="23" />
-    case 'number':
-      return <NumberIcn w="23" />
-    case 'select':
-      return <DropDownIcn w="23" />
-    case 'password':
-      return <PasswordIcn size="23" />
-    case 'email':
-      return <MailIcn size="23" />
-    case 'url':
-      return <UrlIcn w="23" />
-    case 'file-up':
-      return <FileUploadIcn w="23" />
-    case 'date':
-      return <DateIcn w="23" />
-    case 'time':
-      return <TimeIcn size="23" />
-    case 'datetime-local':
-      return <DateTimeIcn w="23" />
-    case 'month':
-      return <MonthIcn w="23" />
-    case 'week':
-      return <WeekIcn size="23" />
-    case 'color':
-      return <ColorPickerIcn w="23" />
-    case 'recaptcha':
-      return <ReCaptchaIcn size="23" />
-    case 'decision-box':
-      return <DecisionBoxIcn size="23" />
-    case 'button':
-      return <BtnIcn size="26" />
-    case 'html':
-      return <CodeSnippetIcn size="23" />
-    case 'paypal':
-      return <PaypalIcn w="23" />
-    case 'razorpay':
-      return <RazorPayIcn w="17" h="23" />
-    default:
-      return false
-  }
-}
-
-function FieldOptionBtn({ icn, title, sub, action }) {
-  const extraProps = {}
-  if (action !== undefined) {
-    extraProps.role = 'button'
-    extraProps.tabIndex = 0
-    extraProps.onKeyPress = action
-    extraProps.onClick = action
-  }
-
-  return (
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <div className="btc-s-l mt-2" {...extraProps}>
-      <div className="flx flx-between ">
-        <div className="flx w-9">
-          <span className="lft-icn mr-2 btcd-icn-lg flx br-50">
-            {typeof icn === 'string' ? FieldIcon(icn) : icn}
-          </span>
-          <div className="w-nwrp o-h">
-            <div className="txt-o o-h mb-1">{title}</div>
-            {sub && (
-              <small>
-                {__('Key:', 'bitform')}
-                {` ${sub}`}
-              </small>
-            )}
-          </div>
-        </div>
-        <span className="btcd-icn icn-chevron-right btc-icn-md" />
-      </div>
-    </div>
   )
 }
---
+++ src/components/CompSettings/FieldLinkButton.jsx
@@ -0,0 +1,111 @@
+import BtnIcn from '../../Icons/BtnIcn'
+import CheckBoxIcn from '../../Icons/CheckBoxIcn'
+import ChevronRightIcon from '../../Icons/ChevronRightIcon'
+import CodeSnippetIcn from '../../Icons/CodeSnippetIcn'
+import ColorPickerIcn from '../../Icons/ColorPickerIcn'
+import DateIcn from '../../Icons/DateIcn'
+import DateTimeIcn from '../../Icons/DateTimeIcn'
+import DecisionBoxIcn from '../../Icons/DecisionBoxIcn'
+import DropDownIcn from '../../Icons/DropDownIcn'
+import FileUploadIcn from '../../Icons/FileUploadIcn'
+import MailIcn from '../../Icons/MailIcn'
+import MonthIcn from '../../Icons/MonthIcn'
+import NumberIcn from '../../Icons/NumberIcn'
+import PasswordIcn from '../../Icons/PasswordIcn'
+import PaypalIcn from '../../Icons/PaypalIcn'
+import RadioIcn from '../../Icons/RadioIcn'
+import RazorPayIcn from '../../Icons/RazorPayIcn'
+import ReCaptchaIcn from '../../Icons/ReCaptchaIcn'
+import TextareaIcn from '../../Icons/TextareaIcn'
+import TextIcn from '../../Icons/TextIcn'
+import TimeIcn from '../../Icons/TimeIcn'
+import UrlIcn from '../../Icons/UrlIcn'
+import UserIcn from '../../Icons/UserIcn'
+import WeekIcn from '../../Icons/WeekIcn'
+import { __ } from '../../Utils/i18nwrap'
+
+export default function FieldLinkBtn({ icn, title, sub, action }) {
+  const extraProps = {}
+  if (action !== undefined) {
+    extraProps.role = 'button'
+    extraProps.tabIndex = 0
+    extraProps.onKeyPress = action
+    extraProps.onClick = action
+  }
+
+  const FieldIcon = icon => {
+    switch (icon) {
+      case 'text':
+        return <TextIcn size="14" />
+      case 'username':
+        return <UserIcn size="14" />
+      case 'textarea':
+        return <TextareaIcn size="14" />
+      case 'check':
+        return <CheckBoxIcn w="14" />
+      case 'radio':
+        return <RadioIcn size="14" />
+      case 'number':
+        return <NumberIcn w="14" />
+      case 'select':
+        return <DropDownIcn w="14" />
+      case 'password':
+        return <PasswordIcn size="14" />
+      case 'email':
+        return <MailIcn size="14" />
+      case 'url':
+        return <UrlIcn w="14" />
+      case 'file-up':
+        return <FileUploadIcn w="14" />
+      case 'date':
+        return <DateIcn w="14" />
+      case 'time':
+        return <TimeIcn size="14" />
+      case 'datetime-local':
+        return <DateTimeIcn w="14" />
+      case 'month':
+        return <MonthIcn w="14" />
+      case 'week':
+        return <WeekIcn size="14" />
+      case 'color':
+        return <ColorPickerIcn w="14" />
+      case 'recaptcha':
+        return <ReCaptchaIcn size="14" />
+      case 'decision-box':
+        return <DecisionBoxIcn size="14" />
+      case 'button':
+        return <BtnIcn size="14" />
+      case 'html':
+        return <CodeSnippetIcn size="14" />
+      case 'paypal':
+        return <PaypalIcn w="14" />
+      case 'razorpay':
+        return <RazorPayIcn w="14" h="19" />
+      default:
+        return false
+    }
+  }
+
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <div className="btc-s-l mt-2" {...extraProps}>
+      <div className="flx flx-between ">
+        <div className="flx w-9">
+          <span className="lft-icn mr-2 flx br-50">
+            {typeof icn === 'string' ? FieldIcon(icn) : icn}
+          </span>
+          <div className="w-nwrp o-h">
+            <div className="txt-o o-h mb-1">{title}</div>
+            {sub && (
+              <small>
+                {__('Key:', 'bitform')}
+                {` ${sub}`}
+              </small>
+            )}
+          </div>
+        </div>
+        <ChevronRightIcon size={18} />
+      </div>
+    </div>
+  )
+}
