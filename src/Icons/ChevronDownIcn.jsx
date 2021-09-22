af714efc8af6296d4acce5e7c94f7a6bc67b2c29
Arif Uddin <arifunctg@gmail.com>
2021-09-22 16:37:29 +0600

    Update ChevronDownIcn.jsx

--- src/Icons/ChevronDownIcn.jsx
+++ src/Icons/ChevronDownIcn.jsx
@@ -9,7 +9,7 @@ export default function ChevronDownIcn({ rotate, size, stroke = 2, viewBox = '0 
       strokeWidth={stroke}
       strokeLinecap="round"
       strokeLinejoin="round"
-      style={rotate ? { transform: 'rotate(180deg)' } : { transform: 'rotate(0deg)' }}
+      style={{ transform: rotate ? 'rotate(180deg)' : 'rotate(0deg)' }}
     >
       <polyline points="6 9 12 15 18 9" />
     </svg>
