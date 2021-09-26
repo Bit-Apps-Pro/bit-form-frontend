3d2e26d411afc1c255b13fd42d42ca914e895422
mdabbas-cse <gmabbas44@gmail.com>
2021-09-26 18:43:25 +0600

    update style

--- src/components/Utilities/SingleInput.jsx
+++ src/components/Utilities/SingleInput.jsx
@@ -2,7 +2,7 @@ export default function SingleInput({ className, cls, width, title, inpType, act
   return (
     <div className={`mt-3 setting-inp ${className}`} style={{ ...(width && { width }) }}>
       <span>{title}</span>
-      <input className={`btcd-paper-inp ${cls}`} type={inpType} onChange={action} value={value} placeholder={placeholder} name={name} list={list} disabled={disabled} />
+      <input className={cls || 'btcd-paper-inp '} type={inpType} onChange={action} value={value} placeholder={placeholder} name={name} list={list} disabled={disabled} />
     </div>
   )
 }
