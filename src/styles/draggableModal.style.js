e5bf728125dfe860d673ab7938f0dde4e52552ec
Arif Uddin <arifunctg@gmail.com>
2021-09-26 13:01:41 +0600

    style fix

--- src/styles/draggableModal.style.js
+++ src/styles/draggableModal.style.js
@@ -1,13 +1,16 @@
 const draggableModalStyle = {
   container: {
     w: 250,
-    bc: '#fff',
+    bc: 'var(--white-100)',
     pn: 'absolute',
     tp: 10,
     zx: 9999,
-    p: 10,
+    px: 9,
+    pt: 0,
+    pb: 10,
     brs: 10,
-    b: '1px solid #ddd',
+    b: '1px solid var(--white-0-83)',
+    bs: '0 6px 6px 0 #00000012, 0 3px 23px 0 #e8e8e8',
   },
 
   title: {
@@ -23,13 +26,21 @@ const draggableModalStyle = {
     m: 0,
     b: 0,
     bc: 'transparent',
-    cr: '#9E9E9E',
+    cr: 'var(--white-0-0-32)',
     cur: 'pointer',
+    flx: 'center',
+    ':hover': { cr: 'var(--white-0-0-64)' },
   },
 
-  hr: { m: 10 },
+  hr: { mx: 10, mb: 10, mt: 5 },
 
   content: { px: 10 },
+  titleBar: {
+    h: 25,
+    pt: 17,
+    pb: 12,
+    flx: 'center',
+  },
 
 }
 
