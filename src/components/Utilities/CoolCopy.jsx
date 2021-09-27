64a9f1aefcd4e2e09bee725299b43beb70366aeb
mdabbas-cse <gmabbas44@gmail.com>
2021-09-27 13:24:58 +0600

    remove CoolCopy.style & add style add internal

--- src/components/Utilities/CoolCopy.jsx
+++ src/components/Utilities/CoolCopy.jsx
@@ -2,7 +2,6 @@ import { useRef } from 'react'
 import { useFela } from 'react-fela'
 import toast from 'react-hot-toast'
 import CopyIcn from '../../Icons/CopyIcn'
-import coolcopy from '../../styles/CoolCopy.style'
 import { __ } from '../../Utils/i18nwrap'
 import Tip from './Tip'
 
@@ -29,9 +28,9 @@ export default function CoolCopy({ className, cls, value, readOnly }) {
   }
   return (
     <div className={className}>
-      <label className={css(coolcopy.label)} htmlFor="text-copy">
-        <input ref={copyInput} className={`${css(coolcopy.input)} ${cls}`} id="text-copy" type="text" value={value} readOnly={readOnly} />
-        <button onClick={copyText} className={`${css(coolcopy.btn)}`} type="button" aria-label="Copy" style={{ '--tooltip-txt': '"Copy"' }}>
+      <label className={css(style.label)} htmlFor="text-copy">
+        <input ref={copyInput} className={`${css(style.input)} ${cls}`} id="text-copy" type="text" value={value} readOnly={readOnly} />
+        <button onClick={copyText} className={`${css(style.btn)}`} type="button" aria-label="Copy" style={{ '--tooltip-txt': '"Copy"' }}>
           <Tip msg="Copy">
             <CopyIcn w="20" />
           </Tip>
@@ -39,4 +38,29 @@ export default function CoolCopy({ className, cls, value, readOnly }) {
       </label>
     </div>
   )
+}
+
+const style = {
+  label: { pn: 'relative !important' },
+  input: {
+    bd: 'var(--white-0-95)',
+    oe: 'none',
+    b: '1px solid var(--b-79-96)',
+    brs: '10px !important',
+    p: '10px 30px 10px 10px',
+    w: 80,
+    fw: 600,
+  },
+  btn: {
+    pn: 'absolute !important',
+    b: 'none',
+    oe: 'none',
+    bd: 'none',
+    tp: '-2px',
+    rt: 2,
+    cur: 'pointer',
+    tn: 'color 0.2s',
+    cr: 'var(--b-59)',
+    '&:hover': { cr: 'var(--b-73)' },
+  },
 }
