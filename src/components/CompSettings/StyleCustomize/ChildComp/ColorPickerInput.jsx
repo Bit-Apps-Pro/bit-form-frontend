f239b46f938aae8bcac05aeceadf6246a81ae4c6
Md Rubel Amin <mdrubelamin2@gmail.com>
2021-09-27 14:47:44 +0600

    Update ColorPickerInput.jsx

--- src/components/CompSettings/StyleCustomize/ChildComp/ColorPickerInput.jsx
+++ src/components/CompSettings/StyleCustomize/ChildComp/ColorPickerInput.jsx
@@ -1,29 +1,26 @@
 import { useFela } from 'react-fela'
 import CloseIcn from '../../../../Icons/CloseIcn'
 import colorPickerInputStyle from '../../../../styles/colorPickerInput.style'
-import SimpleColorPicker from '../../../style-new/SimpleColorPicker'
 import Downmenu from '../../../Utilities/Downmenu'
 
 export default function ColorPickerInput({ title }) {
   const { css } = useFela()
 
   return (
-    <>
-      <div className="flx flx-between">
-        <span className={css(colorPickerInputStyle.title)}>{title}</span>
-        <div className={css(colorPickerInputStyle.inputcontainer)}>
-          <Downmenu>
-            <span className={css(colorPickerInputStyle.colorbox)}>&nbsp;</span>
-            <div>
-              color picker
-            </div>
-          </Downmenu>
-          <input className={css(colorPickerInputStyle.input)} value="#FF0000" type="text" />
-          <button type="button" className={css(colorPickerInputStyle.button)}>
-            <CloseIcn size={10} />
-          </button>
-        </div>
+    <div className="flx flx-between">
+      <span className={css(colorPickerInputStyle.title)}>{title}</span>
+      <div className={css(colorPickerInputStyle.inputcontainer)}>
+        <Downmenu>
+          <span className={css(colorPickerInputStyle.colorbox)}>&nbsp;</span>
+          <div>
+            color picker
+          </div>
+        </Downmenu>
+        <input className={css(colorPickerInputStyle.input)} value="#FF0000" type="text" />
+        <button type="button" className={css(colorPickerInputStyle.button)}>
+          <CloseIcn size={10} />
+        </button>
       </div>
-    </>
+    </div>
   )
 }
