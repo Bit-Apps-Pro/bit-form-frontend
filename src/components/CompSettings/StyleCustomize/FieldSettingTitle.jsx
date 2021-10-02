5716bce101a2f6a68e89cd24c2da610a0322d654
mdabbas-cse <gmabbas44@gmail.com>
2021-10-02 15:08:14 +0600

    remove style for global

--- src/components/CompSettings/StyleCustomize/FieldSettingTitle.jsx
+++ src/components/CompSettings/StyleCustomize/FieldSettingTitle.jsx
@@ -3,6 +3,7 @@ import ut from '../../../styles/2.utilities'
 import { __ } from '../../../Utils/i18nwrap'
 import CoolCopy from '../../Utilities/CoolCopy'
 import Back2FldBtn from '../Back2FldBtn'
+import style from '../../../styles/FieldSettingTitle.style'
 
 export default function FieldSettingTitle({ title, subtitle, fieldKey }) {
   const { css } = useFela()
@@ -26,52 +27,4 @@ export default function FieldSettingTitle({ title, subtitle, fieldKey }) {
       <hr className={css(style.divider)} />
     </>
   )
-}
-
-const style = {
-  section: {
-    flx: 'center-between',
-    mx: 10,
-    my: 5,
-    brs: 8,
-    fw: 600,
-  },
-  flxColumn: {
-    fd: 'column',
-    jc: 'start !important',
-    ai: 'self-start',
-  },
-  title: {
-    fs: 14,
-    fw: 600,
-  },
-  mainTitle: {
-    fs: 16,
-    fw: 700,
-  },
-  divider: {
-    my: 0,
-    mx: 10,
-    bb: '0.5px soild var(--white-0-83)',
-  },
-  subtitle: { fs: 12, fw: 500 },
-  btn: {
-    oe: 'none',
-    flx: 'center-between',
-    tn: 'background 0.2s',
-    // b: '1px solid var(--white-0-89)',
-    // bc: 'var(--white-0-97)',
-    bc: 'none',
-    px: 4,
-    brs: 8,
-    cur: 'pointer',
-    cr: 'var(--white-0-50)',
-    bd: 'none !important',
-    b: 'none',
-    pl: '0 !important',
-    ml: '-5px',
-    mb: 5,
-    fw: 400,
-    '&:hover': { bc: 'var(--white-0-97) !important' },
-  },
 }
