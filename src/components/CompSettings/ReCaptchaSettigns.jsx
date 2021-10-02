cc171e82c9cf043eb9bec477f9d97ce233cf794d
mdabbas-cse <gmabbas44@gmail.com>
2021-10-02 15:09:23 +0600

    remove style for global

--- src/components/CompSettings/ReCaptchaSettigns.jsx
+++ src/components/CompSettings/ReCaptchaSettigns.jsx
@@ -3,6 +3,7 @@ import { useFela } from 'react-fela'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { $fields, $selectedFieldId } from '../../GlobalStates'
 import ut from '../../styles/2.utilities'
+import style from '../../styles/FieldSettingTitle.style'
 import FieldStyle from '../../styles/FieldStyle.style'
 import { deepCopy } from '../../Utils/Helpers'
 import { __ } from '../../Utils/i18nwrap'
@@ -42,7 +43,7 @@ export default function ReCaptchaSettigns() {
         title="Theme"
         className={css(FieldStyle.fieldSection)}
       >
-        <select onChange={onInput} name="theme" value={fieldData.theme} className={css(style.input, ut.w10)}>
+        <select onChange={onInput} name="theme" value={fieldData.theme} className={css(FieldStyle.input, ut.w10)}>
           <option value="dark">{__('Dark', 'bitform')}</option>
           <option value="light">{__('Light', 'bitform')}</option>
         </select>
@@ -59,65 +60,4 @@ export default function ReCaptchaSettigns() {
       </div> */}
     </div>
   )
-}
-
-const style = {
-  section: {
-    flx: 'center-between',
-    mx: 10,
-    my: 5,
-    brs: 8,
-    fw: 600,
-  },
-  input: {
-    fs: 14,
-    fw: 400,
-    w: '96%',
-    bd: 'var(--b-79-96) !important',
-    oe: 'none !important',
-    mx: 'auto',
-    dy: 'block',
-    my: 10,
-    b: 'none !important',
-    brs: '8px !important',
-    ':focus': { bs: '0 0 0 2px var(--b-50) !important' },
-  },
-  flxColumn: {
-    fd: 'column',
-    jc: 'start !important',
-    ai: 'self-start',
-  },
-  title: {
-    fs: 14,
-    fw: 600,
-  },
-  mainTitle: {
-    fs: 16,
-    fw: 700,
-  },
-  divider: {
-    my: 0,
-    mx: 10,
-    bb: '0.5px soild var(--white-0-83)',
-  },
-  subtitle: { fs: 12, fw: 500 },
-  btn: {
-    oe: 'none',
-    flx: 'center-between',
-    tn: 'background 0.2s',
-    // b: '1px solid var(--white-0-89)',
-    // bc: 'var(--white-0-97)',
-    bc: 'none',
-    px: 4,
-    brs: 8,
-    cur: 'pointer',
-    cr: 'var(--white-0-50)',
-    bd: 'none !important',
-    b: 'none',
-    pl: '0 !important',
-    ml: '-5px',
-    mb: 5,
-    fw: 400,
-    '&:hover': { bc: 'var(--white-0-97) !important' },
-  },
 }
