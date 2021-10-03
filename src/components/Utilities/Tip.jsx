27941aa416bec93f3dccc7afcf89a37f0cdf00d7
Arif Uddin <arifunctg@gmail.com>
2021-10-03 12:36:33 +0600

    Update Tip.jsx

--- src/components/Utilities/Tip.jsx
+++ src/components/Utilities/Tip.jsx
@@ -3,28 +3,40 @@ import { animateFill } from 'tippy.js'
 import 'tippy.js/dist/tippy.css'
 // import 'tippy.js/themes/translucent.css'
 import 'tippy.js/themes/material.css'
+import 'tippy.js/themes/light-border.css'
+// import 'tippy.js/themes/light.css'
 // import 'tippy.js/animations/scale.css'
 // import 'tippy.js/dist/svg-arrow.css'
 import 'tippy.js/dist/backdrop.css'
 import 'tippy.js/animations/shift-away.css'
 import { useFela } from 'react-fela'
 
-export default function Tip({ className, children, width = 'auto', msg = '', place = 'bottom', trigger = 'mouseenter' }) {
+export default function Tip({ className,
+  children,
+  width = 'auto',
+  delay = 0,
+  msg = '',
+  theme = 'material',
+  place = 'bottom',
+  felaStyle = {},
+  whiteSpaceNowrap = false,
+  trigger = 'mouseenter' }) {
   const { css } = useFela()
-  const style = { fw: 400 }
+  const style = { fw: 400, ff: '"Roboto", sans-serif', fs: 12, letterSpacing: '.3px', ...whiteSpaceNowrap && { whiteSpace: 'nowrap' } }
   return (
     <Tippy
       trigger={trigger}
       animateFill
       placement={place}
-      plugins={[animateFill]}
+      plugins={theme === 'material' && [animateFill]}
       duration={150}
-      theme="material"
+      delay={delay}
+      theme={theme}
       animation="shift-away"
       interactive
       maxWidth={width}
-      // arrow
-      content={<span className={css(style)}>{msg}</span>}
+      arrow={false}
+      content={<span className={css([style, felaStyle])}>{msg}</span>}
     >
       <span>{children}</span>
     </Tippy>
