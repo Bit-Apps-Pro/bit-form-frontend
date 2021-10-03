ab655273d350cba4a145b58a961f79a4bbd457ca
Arif Uddin <arifunctg@gmail.com>
2021-10-03 15:50:06 +0600

    Update Toolbar.jsx

--- src/components/Toolbars/Toolbar.jsx
+++ src/components/Toolbars/Toolbar.jsx
@@ -45,7 +45,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Check Box', 'bitform'),
       keywords: 'Check Box, check box',
       icn: <CheckBoxIcn w="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', minH: 40 },
       elm: {
         typ: 'check',
         lbl: __('Check Boxs', 'bitform'),
@@ -62,7 +62,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Date', 'bitform'),
       keywords: 'Date, date',
       icn: <DateIcn w="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', maxH: 40, minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', maxH: 40, minH: 40 },
       elm: {
         typ: 'date',
         lbl: __('Date Input', 'bitform'),
@@ -74,7 +74,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Date-Time', 'bitform'),
       keywords: 'Date-Time, Date-time, Date, Time',
       icn: <DateTimeIcn w="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', maxH: 40, minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', maxH: 40, minH: 40 },
       elm: {
         typ: 'datetime-local',
         lbl: __('Date-Time Input', 'bitform'),
@@ -86,7 +86,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Dropdown', 'bitform'),
       keywords: 'Dropdown, dropdown, Menu, menu',
       icn: <DropDownIcn w="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', minH: 40 },
       elm: {
         typ: 'select',
         lbl: __('Drop-Down', 'bitform'),
@@ -105,7 +105,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Text', 'bitform'),
       keywords: 'Text, text',
       icn: <TextIcn size="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', maxH: 40, minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', maxH: 40, minH: 40 },
       elm: {
         typ: 'text',
         lbl: __('Text Field', 'bitform'),
@@ -118,7 +118,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('User Name', 'bitform'),
       keywords: 'User Name, Username, User name, username, user name, text',
       icn: <UserIcn size="22" />,
-      pos: { h: 40, w: 60, i: 'n_blk', maxH: 40, minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', maxH: 40, minH: 40 },
       elm: {
         typ: 'username',
         lbl: __('User Name', 'bitform'),
@@ -145,7 +145,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Radio Button', 'bitform'),
       keywords: 'Radio button, radio button, button',
       icn: <RadioIcn size="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', minH: 40 },
       elm: {
         typ: 'radio',
         lbl: __('Radio', 'bitform'),
@@ -163,7 +163,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Number', 'bitform'),
       keywords: 'Number, number',
       icn: <NumberIcn w="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', maxH: 40, minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', maxH: 40, minH: 40 },
       elm: {
         typ: 'number',
         lbl: __('Number Field', 'bitform'),
@@ -176,7 +176,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Password', 'bitform'),
       keywords: 'Password, password',
       icn: <PasswordIcn size="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', maxH: 40, minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', maxH: 40, minH: 40 },
       elm: {
         typ: 'password',
         lbl: __('Password Field', 'bitform'),
@@ -189,7 +189,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Email', 'bitform'),
       keywords: 'Email, email',
       icn: <MailIcn size="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', maxH: 40, minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', maxH: 40, minH: 40 },
       elm: {
         typ: 'email',
         lbl: __('Email Field', 'bitform'),
@@ -204,7 +204,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Time', 'bitform'),
       keywords: 'Time, time',
       icn: <TimeIcn size="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', maxH: 40, minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', maxH: 40, minH: 40 },
       elm: {
         typ: 'time',
         lbl: __('Time Input', 'bitform'),
@@ -217,7 +217,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Month', 'bitform'),
       keywords: 'Month, month, Date',
       icn: <MonthIcn w="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', maxH: 40, minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', maxH: 40, minH: 40 },
       elm: {
         typ: 'month',
         lbl: __('Month Input', 'bitform'),
@@ -229,7 +229,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Week', 'bitform'),
       keywords: 'Week, week, Date',
       icn: <WeekIcn size="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', maxH: 40, minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', maxH: 40, minH: 40 },
       elm: {
         typ: 'week',
         lbl: __('Week Input', 'bitform'),
@@ -241,7 +241,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Country', 'bitform'),
       keywords: 'Country, country',
       icn: <FlagIcn size="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', minH: 40 },
       elm: {
         typ: 'select',
         lbl: __('Select Country', 'bitform'),
@@ -254,7 +254,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('File Upload', 'bitform'),
       keywords: 'File Upload, file upload',
       icn: <FileUploadIcn w="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', minH: 40, minW: 20 },
+      pos: { h: 40, w: 60, i: 'shadow_block', minH: 40, minW: 20 },
       elm: {
         typ: 'file-up',
         lbl: __('File Upload', 'bitform'),
@@ -267,7 +267,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('URL', 'bitform'),
       keywords: 'URL, url',
       icn: <UrlIcn w="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', maxH: 40, minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', maxH: 40, minH: 40 },
       elm: {
         typ: 'url',
         attr: {
@@ -285,7 +285,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Color Picker', 'bitform'),
       keywords: 'Color Picker, color picker',
       icn: <ColorPickerIcn w="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', maxH: 40, minH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', maxH: 40, minH: 40 },
       elm: {
         typ: 'color',
         lbl: __('Color Picker', 'bitform'),
@@ -297,7 +297,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('reCaptcha v2', 'bitform'),
       keywords: 'ReCaptcha, reCaptcha, recaptcha, recaptcha v2',
       icn: <ReCaptchaIcn size="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', maxH: 40, minH: 40, minW: 20 },
+      pos: { h: 40, w: 60, i: 'shadow_block', maxH: 40, minH: 40, minW: 20 },
       elm: {
         typ: 'recaptcha',
         theme: 'light',
@@ -309,7 +309,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Decision Box', 'bitform'),
       keywords: 'Decision box, GDPR, gdpr',
       icn: <DecisionBoxIcn size="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', minH: 20 },
+      pos: { h: 40, w: 60, i: 'shadow_block', minH: 20 },
       elm: {
         typ: 'decision-box',
         adminLbl: __('Decision Box', 'bitform'),
@@ -326,7 +326,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: 'HTML',
       keywords: 'HTML, Html, html',
       icn: <CodeSnippetIcn size="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk' },
+      pos: { h: 40, w: 60, i: 'shadow_block' },
       elm: {
         typ: 'html',
         lbl: 'HTML Content',
@@ -338,7 +338,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: 'Button',
       keywords: 'Button, button',
       icn: <BtnIcn size="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', minH: 40, maxH: 40 },
+      pos: { h: 40, w: 60, i: 'shadow_block', minH: 40, maxH: 40 },
       elm: {
         typ: 'button',
         btnTyp: 'button',
@@ -355,7 +355,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Paypal', 'bitform'),
       keywords: 'Paypal, Payment, payment, paypal',
       icn: <PaypalIcn w="23" />,
-      pos: { h: 100, w: 60, i: 'n_blk', minH: 60, maxH: 140, minW: 20 },
+      pos: { h: 100, w: 60, i: 'shadow_block', minH: 60, maxH: 140, minW: 20 },
       elm: {
         typ: 'paypal',
         currency: 'USD',
@@ -373,7 +373,7 @@ function Toolbar({ tolbarSiz, setNewData, setTolbar }) {
       name: __('Razorpay', 'bitform'),
       keywords: 'Razorpay, razorpay, Payment, payment',
       icn: <RazorPayIcn w="17" h="23" />,
-      pos: { h: 40, w: 60, i: 'n_blk', minH: 40, maxH: 140, minW: 20 },
+      pos: { h: 40, w: 60, i: 'shadow_block', minH: 40, maxH: 140, minW: 20 },
       elm: {
         typ: 'razorpay',
         lbl: __('Razorpay', 'bitform'),
