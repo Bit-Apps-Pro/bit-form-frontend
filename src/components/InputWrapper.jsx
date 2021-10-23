0aff056e5b0e51a60f1483086a60db31185c851c
Arif Uddin <arifunctg@gmail.com>
2021-10-23 20:06:21 +0600

    Update InputWrapper.jsx

--- src/components/InputWrapper.jsx
+++ src/components/InputWrapper.jsx
@@ -1,15 +1,17 @@
 import { useRecoilValue } from 'recoil'
-import { $breakpoint, $fieldsDirection } from '../GlobalStates'
+import { $breakpoint, $fieldsDirection, $flags } from '../GlobalStates'
 
 export default function InputWrapper({ formID, fieldKey, fieldData, children, noLabel, isBuilder }) {
   const breakpoint = useRecoilValue($breakpoint)
   const fieldDirection = useRecoilValue($fieldsDirection)
+  const flags = useRecoilValue($flags)
   const isHidden = fieldData.hidden?.includes(breakpoint) || false
 
   const err = fieldData.error || ''
   return (
     <div
-      className={`${fieldKey}-fw  drag  ${isBuilder ? 'o-h' : ''} ${fieldData?.valid?.hide ? 'vis-n' : ''} ${isHidden ? 'fld-hide' : ''}`}
+      data-field-wrp
+      className={`${fieldKey}-fw  ${flags.styleMode ? '' : 'drag'} ${isBuilder ? 'o-h' : ''} ${fieldData?.valid?.hide ? 'vis-n' : ''} ${isHidden ? 'fld-hide' : ''}`}
       style={{ direction: fieldDirection }}
     >
       {(!noLabel && !fieldData?.valid?.hideLbl && 'lbl' in fieldData) && (
