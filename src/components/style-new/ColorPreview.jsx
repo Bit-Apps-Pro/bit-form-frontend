f2e11ed3afea877cb15344c13e44d951304a793d
Arif Uddin <arifunctg@gmail.com>
2021-11-13 15:23:54 +0600

    color preview box component added

---
+++ src/components/style-new/ColorPreview.jsx
@@ -0,0 +1,20 @@
+import { useFela } from 'react-fela'
+
+export default function ColorPreview({ w = 30, h = 30, bg, className }) {
+  const { css } = useFela()
+  return (
+    <div className={`${css(s.wrp)} ${className}`} style={{ height: h, width: w }}>
+      <div className={css(s.preview)} style={{ background: bg }} />
+    </div>
+  )
+}
+const s = {
+  wrp: {
+    dy: 'inline-block',
+    ow: 'hidden',
+    brs: 8,
+    b: '1px solid #dadada',
+    bi: 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAJUlEQVQYV2N89erVfwY0ICYmxoguxjgUFKI7GsTH5m4M3w1ChQC1/Ca8i2n1WgAAAABJRU5ErkJggg==)',
+  },
+  preview: { se: '100%' },
+}
