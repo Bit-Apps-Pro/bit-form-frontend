371c137b435a813c69f64d49db314ecf77c73ac3
Arif Uddin <arifunctg@gmail.com>
2021-11-14 15:46:53 +0600

    Update Icons.jsx

--- src/components/CompSettings/Icons.jsx
+++ src/components/CompSettings/Icons.jsx
@@ -9,19 +9,21 @@ import { useAsyncDebounce } from 'react-table'
 import ut from '../../styles/2.utilities'
 import StyleSegmentControl from '../Utilities/StyleSegmentControl'
 import bitsFetch from '../../Utils/bitsFetch'
-import TrashIcn from '../../Icons/TrashIcn'
+import CloseIcn from '../../Icons/CloseIcn'
 import app from '../../styles/app.style'
 import SearchIcon from '../../Icons/SearchIcon'
 import DownloadIcon from '../../Icons/DownloadIcon'
 import LoaderSm from '../Loaders/LoaderSm'
 import { $fields, $selectedFieldId } from '../../GlobalStates'
 import { deepCopy } from '../../Utils/Helpers'
+import Grow from './StyleCustomize/ChildComp/Grow'
+import FileUploadIcn from '../../Icons/FileUploadIcn'
 
 function Icons({ iconType, setModal }) {
   const fldKey = useRecoilValue($selectedFieldId)
   const [fields, setFields] = useRecoilState($fields)
   const fieldData = deepCopy(fields[fldKey])
-  const [controller, setController] = useState({ parent: 'Icons', child: '' })
+  const [controller, setController] = useState({ parent: 'Icons' })
   const [files, setFiles] = useState([])
   const [loading, setLoading] = useState(false)
   const [dnLoading, setDnLoading] = useState(false)
@@ -36,21 +38,22 @@ function Icons({ iconType, setModal }) {
   const ref = useRef()
 
   const iconPacks = [
-    { label: 'Font Awesome', value: 't=2_id_fontawesome', status: false },
-    { label: 'Bootstrap Icon', value: 't=2_id_bootstrapicons', status: false },
-    { label: 'Material Design Icon', value: 't=2_id_materialdesign-icons', status: false },
-    { label: 'Ion Icons', value: 't=2_id_ionicons', status: false },
-    { label: 'Octions', value: 't=2_id_octions', status: false },
-    { label: 'CSS.GG', value: 't=2_id_css.gg', status: false },
-    { label: 'Feather', value: 't=2_id_feather', status: false },
-    { label: 'Carbon Icons', value: 't=2_id_carbonicons', status: false },
-    { label: 'Typicons', value: 't=2_id_typicons', status: false },
-    { label: 'Vscode Icons', value: 't=2_id_vscodecodicons', status: false },
-    { label: 'Simple Icons', value: 't=2_id_simpleicons', status: false },
+    { label: 'Font Awesome', value: 't=2_id_fontawesome', id: 'font-awesome', status: false },
+    { label: 'Bootstrap', value: 't=2_id_bootstrapicons', id: 'bootstrap-icons', status: false },
+    { label: 'Material Design', value: 't=2_id_materialdesign-icons', id: 'material-design-icons', status: false },
+    { label: 'Ion', value: 't=2_id_ionicons', id: 'ionicons', status: false },
+    { label: 'Octions', value: 't=2_id_octions', id: 'octicons', status: false },
+    { label: 'CSS.GG', value: 't=2_id_css.gg', id: 'css.gg', status: false },
+    { label: 'Feather', value: 't=2_id_feather', id: 'feather', status: false },
+    { label: 'Carbon', value: 't=2_id_carbonicons', id: 'carbon-icons', status: false },
+    { label: 'Typicons', value: 't=2_id_typicons', id: 'typicons', status: false },
+    { label: 'Vscode', value: 't=2_id_vscodecodicons', id: 'vscode-codicons', status: false },
+    { label: 'Simple', value: 't=2_id_simpleicons', id: 'simple-icons', status: false },
   ]
   const [filter, setFilter] = useState(iconPacks || [])
 
   const existFilter = () => {
+    if (searchTerm) return 'nofilter=true'
     const exit = filter.find(item => item.status === true)
     let filterByIconPack = ''
     if (exit) {
@@ -66,10 +69,14 @@ function Icons({ iconType, setModal }) {
       .then(response => response.json())
       .then(data => {
         if (data) {
-          setIcons(data.results)
+          let icns = data.results
+          if (filter.find(term => term.status)) {
+            icns = icns?.filter(icn => filter.find(f => f.id === icn.id)?.status)
+          }
+          setIcons(icns)
           setTotal(data.total)
         }
-        ref.current.scrollToTop(0)
+        ref?.current?.scrollToTop(0)
         setSearchLoading(false)
       })
   }
@@ -104,8 +111,10 @@ function Icons({ iconType, setModal }) {
       wpMediaMdl.open()
     }
   }
+  console.log('first', { controller })
 
   useEffect(() => {
+    console.log('secnd', { controller })
     setPrefix('')
     setFiles([])
     setIcons([])
@@ -145,7 +154,14 @@ function Icons({ iconType, setModal }) {
       })
   }
 
-  const delIcon = (file) => {
+  const delIcon = file => {
+    setFiles(prv => prv.filter(fil => fil !== file))
+    const fileNameRegex = new RegExp(file, 'gi')
+    if (fieldData[iconType]?.match(fileNameRegex)) {
+      delete fieldData[iconType]
+      setFields(allFields => produce(allFields, draft => { draft[fldKey] = fieldData }))
+    }
+
     bitsFetch({ file }, 'bitforms_icon_remove')
       .then(res => {
         if (res !== undefined && res.success) {
@@ -190,7 +206,11 @@ function Icons({ iconType, setModal }) {
         .then(response => response.json())
         .then(data => {
           if (data) {
-            setIcons(data.results)
+            let icns = data.results
+            if (filter.find(term => term.status)) {
+              icns = icns?.filter(icn => filter.find(f => f.id === icn.id)?.status)
+            }
+            setIcons(icns)
             setTotal(data.total)
           }
         })
@@ -206,155 +226,184 @@ function Icons({ iconType, setModal }) {
   return (
     <div>
       <StyleSegmentControl
-        options={[{ label: 'Icons' }, { label: 'Upload icon' }, { label: 'Downloaded icons' }]}
+        options={[{ label: 'Icons', show: ['label'] }, { label: 'Upload icon' }, { label: 'Downloaded icons' }]}
         onChange={lbl => onTabChangeHandler(lbl, 'parent')}
         // defaultActive={controller.parent}
-        defaultActive="Icons"
+        // defaultActive={'Icons'}
+        // activeValue="Icons"
+        defaultItmWidth={220}
         wideTab
+        className={css(ut.mt1)}
       />
-      {controller.parent === 'Icons' && (
-        <>
+      <Grow open={controller.parent === 'Icons'}>
+        <div className={css(ut.mt2, ut.flxc, { flxp: 1, jc: 'center' })}>
+          {filter.map((iconPack, i) => (
+            <button title={iconPack.label} onClick={() => searchByIconPack(i, iconPack.value, iconPack.status)} type="button" className={`${css(s.chip, ut.mr2)} ${iconPack.status && css(s.active)}`}>
+              {iconPack.label}
+            </button>
 
-          <div className={css(ut.mt2)}>
-            {filter.map((iconPack, i) => (
-              <button title={iconPack.label} onClick={() => searchByIconPack(i, iconPack.value, iconPack.status)} type="button" className={`${css(app.btn, ut.mr2)} ${iconPack.status === true && css(preFixStyle.active)}`}>
-                {iconPack.label}
-              </button>
+          ))}
+        </div>
+        <div className={css(ut.flxc, s.searchBar, ut.mt2, ut.mb2)}>
+          <div className={css(s.fields_search)}>
+            <input
+              title="Search Icons"
+              aria-label="Search Icons"
+              placeholder="Search Icons by keywords one at a time. e.g., user or login or etc..."
+              id="search-icon"
+              type="search"
+              name="searchIcn"
+              value={searchTerm}
+              onChange={onSearchChange}
+              className={css(s.search_field)}
 
-            ))}
+            />
+            <span title="search" className={css(s.search_icn)}>
+              <SearchIcon size="20" />
+            </span>
           </div>
-          <div className={css(ut.flxc, preFixStyle.searchBar, ut.mt2, ut.mb2)}>
-            <div className={css(preFixStyle.fields_search)}>
-              <input
-                title="Search Icons"
-                aria-label="Search Icons"
-                placeholder="Search Icons..."
-                id="search-icon"
-                type="search"
-                name="searchIcn"
-                value={searchTerm}
-                onChange={onSearchChange}
-                className={css(preFixStyle.search_field)}
 
-              />
-              <span title="search" className={css(preFixStyle.search_icn)}>
-                <SearchIcon size="20" />
-              </span>
-            </div>
-
-            <div>
-              {searchLoading && <LoaderSm size={20} clr="#000" />}
-            </div>
-
+          <div>
+            {searchLoading && <LoaderSm size={20} clr="#13132b" />}
           </div>
-          <Scrollbars ref={ref} style={{ minHeight: '300px' }} onScroll={onScrollFetch}>
-            {loading && (
-              <div title="Loading...">
-                <div className={css(ut.mt2)} />
-                {Array(12).fill(1).map((itm, i) => (
-                  <div key={`loderfnt-${i * 2}`} title="Loading..." className={`${css({ w: 40, h: 40, brs: 8, lh: 2, my: 3, mx: 5, dy: 'inline-block' })} loader`} />
-                ))}
-              </div>
-            )}
-            <div className={css(ut.flxc, ut.mt2, preFixStyle.icon)}>
-              {icons.map((item) => (
-                <button type="button" title={`${item.name} (${item.id}) `} className={`${css(ut.btn, preFixStyle.icnBtn)} ${url + item.url === prefix && css(preFixStyle.active)}`} onClick={(e) => setPrefix(e.target.src)}>
-                  <img src={`${url}${item.url}`} onError={errHandle} alt={item.name} className={css(preFixStyle.img)} />
-                </button>
-              ))}
-            </div>
-            {scrollLoading && (
-              <div title="Loading...">
-                <div className={css(ut.mt2)} />
-                {Array(12).fill(1).map((itm, i) => (
-                  <div key={`loderfnt-${i * 2}`} title="Loading..." className={`${css({ w: 40, h: 40, brs: 8, lh: 2, my: 3, mx: 5, dy: 'inline-block' })} loader`} />
-                ))}
-              </div>
-            )}
-          </Scrollbars>
-          <button type="button" disabled={prefix === ''} className={css(app.btn, preFixStyle.btnPosition)} onClick={saveIcn}>
-            <DownloadIcon size="19" />
-            Download & save
-            {dnLoading && <LoaderSm size={20} clr="#000" className={ut.ml2} />}
-          </button>
 
-        </>
-      )}
-
-      {controller.parent === 'Upload icon' && (
-        <>
-          <button type="button" className={css(app.btn)} onClick={setWpMedia}>
-            Browse
-          </button>
-        </>
-      )}
-      {controller.parent === 'Downloaded icons' && (
-        <>
+        </div>
+        <Scrollbars ref={ref} style={{ minHeight: 350 }} onScroll={onScrollFetch}>
           {loading && (
             <div title="Loading...">
               <div className={css(ut.mt2)} />
-              {Array(13).fill(1).map((itm, i) => (
-                <div key={`loderfnt-${i * 2}`} title="Loading..." className={`${css({ w: 40, h: 40, brs: 8, lh: 2, my: 3, mx: 5, dy: 'inline-block' })} loader`} />
+              {Array(26).fill(1).map((itm, i) => (
+                <div key={`loderfnt-${i * 2}`} title="Loading..." className={`${css(s.loadingPlaceholder)} loader`} />
               ))}
             </div>
           )}
+          <div className={css(ut.flxc, ut.mt2, s.icon)}>
+            {icons.map((item) => (
+              <button
+                type="button"
+                title={`${item.name} (${item.id})`}
+                className={`${css(s.icnBtn)} ${url + item.url === prefix && css(s.active, s.activeIcn)}`}
+                onClick={(e) => setPrefix(e.target.src)}
+              >
+                <img src={`${url}${item.url}`} onError={errHandle} alt={item.name} className={css(s.img)} />
+              </button>
+            ))}
+          </div>
+          {scrollLoading && (
+            <div title="Loading...">
+              <div className={css(ut.mt2)} />
+              {Array(26).fill(1).map((itm, i) => (
+                <div key={`loderfnt-${i * 2}`} title="Loading..." className={`${css(s.loadingPlaceholder)} loader`} />
+              ))}
+            </div>
+          )}
+        </Scrollbars>
+        <button type="button" disabled={!prefix} className={css(s.saveBtn, s.btnPosition)} onClick={saveIcn}>
+          <span className={css(ut.mr1)}><DownloadIcon size="19" /></span>
+          Download & save
+          {dnLoading && <LoaderSm size={20} clr="#fff" className={ut.ml2} />}
+        </button>
+
+      </Grow>
+
+      <Grow open={controller.parent === 'Upload icon'}>
+        <button type="button" className={css(s.upBtn)} onClick={setWpMedia}>
+          <FileUploadIcn w="35" />
+          {' '}
+          Browse
+        </button>
+      </Grow>
+
+      <Grow open={controller.parent === 'Downloaded icons'}>
+        {loading && (
+          <div className={css({ h: 300 })}>
+            <div title="Loading..." className={css({ flxp: 'wrap', jc: 'center', flx: 1 })}>
+              {Array(22).fill(1).map((itm, i) => (
+                <div key={`loderfnt-${i * 2}`} title="Loading..." className={`${css(s.loadingPlaceholder)} loader`} />
+              ))}
+            </div>
+          </div>
+        )}
+        {!loading && (
           <Scrollbars ref={ref} style={{ minHeight: '300px' }}>
-            <div className={css(ut.flxc, ut.mt4, preFixStyle.icon)}>
-              {files.length > 0 && files.map((file) => (
-                <div className={`${css(ut.flxc, ut.mt2, preFixStyle.div)}`}>
-                  <button type="button" className={`${css(ut.btn, preFixStyle.action)} trash`} onClick={() => delIcon(file)}>
-                    <TrashIcn size={14} />
+            <div className={css(ut.flxc, ut.mt4, s.icon)}>
+              {files.length && files.map((file) => (
+                <div className={`${css(ut.flxc, ut.mt2, s.downloadedBtnWrapper)}`} data-file={file} style={{ display: 'inline-block' }}>
+                  <button type="button" className={`${css(s.delBtn)} trash`} title="Delete" onClick={() => delIcon(file)}>
+                    <CloseIcn size={10} />
                   </button>
-                  <button onClick={(e) => setPrefix(e.target.src)} type="button" title={file.name} className={`${css(ut.btn)} `}>
-                    <img src={`${bits.iconURL}/${file}`} alt="aa" width="40" height="30" className={`${`${bits.iconURL}/${file}` === prefix && css(preFixStyle.active)}`} />
+                  <button onClick={(e) => setPrefix(e.target.src)} type="button" title={file.name} className={`${css(s.icnBtn)} ${`${bits.iconURL}/${file}` === prefix && css(s.active)}`}>
+                    <img src={`${bits.iconURL}/${file}`} alt={`icon ${file}`} width="40" height="30" />
                   </button>
                 </div>
               ))}
             </div>
           </Scrollbars>
-          <button type="button" disabled={prefix === ''} className={css(app.btn, preFixStyle.btnPosition)} onClick={selectedSaveIcon}>
-            Save
-          </button>
-        </>
-      )}
+        )}
+        <button type="button" disabled={!prefix} className={css(app.btn, s.saveBtn)} onClick={selectedSaveIcon}>
+          Save
+        </button>
+      </Grow>
 
     </div>
   )
 }
 
-const preFixStyle = {
+const s = {
   scrollBar: { scrollBehavior: 'auto !important', '& *': { scrollBehavior: 'auto !important' } },
-  div: {
+  loadingPlaceholder: { w: 40, h: 40, brs: 8, lh: 2, my: 3, mx: 5, dy: 'inline-block' },
+  downloadedBtnWrapper: {
     pn: 'relative',
-    dy: 'inline-grid',
+    dy: 'inline-block',
     '&:hover > .trash':
       { flx: 'align-center' },
   },
-  icon: { dy: 'inline-block' },
-  action: {
-    dy: 'none',
+  delBtn: {
     pn: 'absolute',
-    rt: -6,
-    tp: -15,
+    bd: '#ffa1a1',
+    b: 'none',
+    brs: 20,
+    h: 20,
+    dy: 'none',
+    w: 20,
+    curp: 1,
+    rt: -3,
+    tp: -3,
+    ':hover': { bd: '#ff7d7d' },
   },
-  btnPosition: {
-    dy: 'flex',
-    ml: 'auto',
+  icon: { dy: 'inline-block' },
+  chip: {
+    bd: 'var(--b-100-48-1)',
+    b: 'none',
+    brs: 50,
+    p: '6px 16px',
+    cr: 'var(--b-37-18)',
+    m: '0px 7px 6px 0px',
+    curp: 1,
+    tn: 'background .3s',
+    ':hover': { bd: 'var(--b-90)' },
   },
-  active: { bd: 'red' },
+  active: {
+    bd: 'var(--b-50) !important',
+    cr: 'var(--white-100) !important',
+  },
+  activeIcn: { fr: 'invert(100%) sepia(0%) saturate(6403%) hue-rotate(223deg) brightness(126%) contrast(109%)' },
   icnBtn: {
     p: '4px',
-    w: 45,
+    mnw: 45,
     h: 45,
     dy: 'inline-grid',
     placeContent: 'center',
-
+    bd: 'transparent',
+    b: 'none',
+    brs: 8,
+    curp: 1,
+    ':hover': { bd: 'var(--white-0-95)' },
   },
   img: { h: 25 },
   searchBar: {
     pn: 'relative',
     zx: 99,
-
   },
   btnColor: { cr: 'var(--b-50)' },
   fields_search: {
@@ -364,10 +413,26 @@ const preFixStyle = {
     tn: 'width .2s',
     w: '100%',
   },
+  upBtn: {
+    b: '1px solid var(--b-38-89)',
+    w: '80%',
+    bd: 'var(--b-36-96)',
+    fs: 20,
+    mx: 'auto',
+    h: 100,
+    flx: 'center',
+    fd: 'column',
+    brs: 20,
+    mt: 10,
+    cr: 'var(--b-54-31)',
+    curp: 1,
+    ':hover': { bd: 'var(--b-50-95)' },
+    ':focus-visible': { focusShadow: 1 },
+  },
   search_icn: {
     pn: 'absolute',
     tp: '50%',
-    mx: 6,
+    mx: 9,
     lt: 0,
     tm: 'translateY(-50%)',
     cr: 'var(--white-0-75)',
@@ -376,10 +441,12 @@ const preFixStyle = {
   },
   search_field: {
     w: '100%',
+    fw: 500,
+    fs: '14px !important',
     oe: 'none',
     b: '1px solid var(--white-0-75) !important',
-    brs: '9px !important',
-    pl: '27px !important',
+    brs: '20px !important',
+    pl: '30px !important',
     pr: '5px !important',
     pb: '5px !important',
     pt: '5px !important',
@@ -404,18 +471,22 @@ const preFixStyle = {
       brs: 20,
     },
   },
-  sort_btn: {
-    w: 25,
-    h: 30,
-    cr: 'var(--white-0-50)',
-    p: 0,
+  saveBtn: {
     b: 'none',
-    bd: 'transparent',
+    bd: 'var(--b-50)',
     brs: 8,
+    fw: 500,
+    p: '5px 10px',
+    cr: 'var(--white-100)',
     flxc: 1,
-    curp: 1,
     flx: 'center',
+    mt: 5,
+    ml: 'auto',
+    mr: 5,
+    curp: 1,
+    tn: 'background .3s',
+    ':hover:not(:disabled)': { bd: 'var(--b-36)' },
+    ':disabled': { bd: 'var(--b-13-88)', cr: 'var(--b-16-35)', cur: 'not-allowed' },
   },
-
 }
 export default Icons
