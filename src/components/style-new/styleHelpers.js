afa0e2fd324ad298b8010f0b97a6ee6e5c0effdf
Arif Uddin <arifunctg@gmail.com>
2021-11-16 11:41:07 +0600

    new helper added

--- src/components/style-new/styleHelpers.js
+++ src/components/style-new/styleHelpers.js
@@ -68,3 +68,109 @@ export const unitConverterHelper = (unit, value, preUnit) => {
 
 export const getNumFromStr = (str = '') => str.match(/[-]?([0-9]*[.])?[0-9]+/gi)?.[0]
 export const getStrFromStr = (str = '') => str.match(/([A-z]|%)+/gi)?.[0]
+
+function getAbsoluteSize(el) {
+  const styles = window.getComputedStyle(el)
+  const marginTop = parseFloat(styles.marginTop)
+  const marginBottom = parseFloat(styles.marginBottom)
+  const marginY = marginTop + marginBottom
+
+  const marginLeft = parseFloat(styles.marginLeft)
+  const marginRight = parseFloat(styles.marginRight)
+  const marginX = marginLeft + marginRight
+
+  const borderTop = parseFloat(styles.borderTop)
+  const borderBottom = parseFloat(styles.borderBottom)
+  const borderY = borderBottom + borderTop
+
+  const borderLeft = parseFloat(styles.borderLeft)
+  const borderRight = parseFloat(styles.borderRight)
+  const borderX = borderRight + borderLeft
+
+  const paddingLeft = parseFloat(styles.paddingLeft)
+  const paddingRight = parseFloat(styles.paddingRight)
+  const paddingTop = parseFloat(styles.paddingTop)
+  const paddingBottom = parseFloat(styles.paddingBottom)
+  const height = Math.ceil(el.offsetHeight)
+  const width = Math.ceil(el.offsetWidth)
+
+  return {
+    height,
+    width,
+    borderBottom,
+    borderTop,
+    borderLeft,
+    borderRight,
+    marginBottom,
+    marginTop,
+    marginLeft,
+    marginRight,
+    marginX,
+    marginY,
+    borderX,
+    borderY,
+    paddingLeft,
+    paddingRight,
+    paddingTop,
+    paddingBottom,
+  }
+}
+
+/**
+ * @param {string} selector html query  selector
+ * @param {string} selector "element" | "margin" | "padding"
+*/
+export function highlightElm(selector, selectType = 'element') {
+  const elms = document.querySelectorAll(selector)
+  elms.forEach(elm => {
+    const marginDiv = document.createElement('div')
+    const paddingDiv = document.createElement('div')
+    const elementDiv = document.createElement('div')
+    const { height,
+      width,
+      marginX,
+      marginY,
+      marginRight,
+      marginBottom,
+      marginLeft,
+      marginTop,
+      paddingLeft,
+      paddingRight,
+      paddingTop,
+      paddingBottom } = getAbsoluteSize(elm)
+    marginDiv.style.width = `${width + marginX}px`
+    marginDiv.style.height = `${height + marginY}px`
+    marginDiv.style.top = `${elm.offsetTop - marginTop}px`
+    marginDiv.style.left = `${elm.offsetLeft - marginLeft}px`
+    marginDiv.classList.add('margin')
+
+    elementDiv.classList.add('element')
+    elementDiv.style.width = `${width}px`
+    elementDiv.style.height = `${height}px`
+    elementDiv.style.marginRight = `${marginRight}px`
+    elementDiv.style.marginLeft = `${marginLeft}px`
+    elementDiv.style.marginTop = `${marginTop}px`
+    elementDiv.style.marginBottom = `${marginBottom}px`
+
+    paddingDiv.classList.add('padding')
+    paddingDiv.style.width = `${width - paddingLeft - paddingRight}px`
+    paddingDiv.style.height = `${height - paddingTop - paddingBottom}px`
+    paddingDiv.style.marginRight = `${paddingRight}px`
+    paddingDiv.style.marginLeft = `${paddingLeft}px`
+    paddingDiv.style.marginTop = `${paddingTop}px`
+    paddingDiv.style.marginBottom = `${paddingBottom}px`
+
+    if (selectType.indexOf('element') < 0) {
+      elementDiv.style.background = 'transparent'
+    }
+    if (selectType.indexOf('margin') < 0) {
+      marginDiv.style.background = 'transparent'
+    }
+    if (selectType.indexOf('padding') < 0) {
+      paddingDiv.style.background = 'transparent'
+    }
+    marginDiv.appendChild(elementDiv)
+    elementDiv.appendChild(paddingDiv)
+    elementDiv.style.width = document.body.appendChild(marginDiv)
+  })
+}
