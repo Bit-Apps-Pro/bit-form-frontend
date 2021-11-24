31fe54f9846878fee54d8026ee90bc1a22747da3
mdabbas-cse <gmabbas44@gmail.com>
2021-11-24 11:48:29 +0600

    Update: style function

--- src/components/style-new/RenderThemeVarsAndFormCSS.jsx
+++ src/components/style-new/RenderThemeVarsAndFormCSS.jsx
@@ -1,23 +1,24 @@
 import { useParams } from 'react-router-dom'
 import { useRecoilValue } from 'recoil'
-import { $styles, $themeVars } from '../../GlobalStates'
+import { $colorScheme, $darkThemeColors, $highContrastThemeColors, $lightThemeColors, $styles, $themeVars } from '../../GlobalStates'
 import { json2CssStr } from './styleHelpers'
 
 export default function RenderThemeVarsAndFormCSS() {
   const { formID } = useParams()
   const styles = useRecoilValue($styles)
   const themeVars = useRecoilValue($themeVars)
-  let globalvars = '.layout-wrapper'
-  globalvars += json2CssStr(themeVars)
-
-  let fromStyle = `._frm-${formID}`
-  // eslint-disable-next-line no-underscore-dangle
-  fromStyle += json2CssStr(styles.form._frm)
+  const lightThemeColors = useRecoilValue($lightThemeColors)
+  const darkThemeColors = useRecoilValue($darkThemeColors)
+  const highContrastThemeColors = useRecoilValue($highContrastThemeColors)
+  const colorScheme = useRecoilValue($colorScheme)
 
   return (
     <>
-      <style>{globalvars}</style>
-      <style>{fromStyle}</style>
+      <style>{json2CssStr('.layout-wrapper', themeVars)}</style>
+      {colorScheme === 'light' && <style>{json2CssStr('.layout-wrapper', lightThemeColors)}</style>}
+      {colorScheme === 'dark' && <style>{json2CssStr('.layout-wrapper', darkThemeColors)}</style>}
+      {colorScheme === 'high-contrast' && <style>{json2CssStr('.layout-wrapper', highContrastThemeColors)}</style>}
+      <style>{json2CssStr(`._frm-${formID}`, styles.form._frm)}</style>
     </>
   )
 }
