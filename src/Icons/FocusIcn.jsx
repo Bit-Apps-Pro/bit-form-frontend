36bdbf5d291917d9f3c2814ed694aa81f9d0b13f
Arif Uddin <arifunctg@gmail.com>
2021-11-27 17:12:08 +0600

    Update FocusIcn.jsx

--- src/Icons/FocusIcn.jsx
+++ src/Icons/FocusIcn.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable max-len */
 export default function FocusIcn({ size, stroke = 2 }) {
-  const style = { marginBottom: '-3px' }
   return (
-    <svg style={style} width={size} height={size} viewBox="0 0 30 30">
+    <svg width={size} height={size} viewBox="0 0 30 30">
       <ellipse className="svg-icn" strokeWidth={stroke} cx="15" cy="15" rx="10.02" ry="9.9" />
       <ellipse className="svg-icn" strokeWidth={stroke} cx="15" cy="15" rx="3.24" ry="3.19" />
       <line className="svg-icn" strokeWidth={stroke} x1="15" y1="2.69" x2="15" y2="5.1" />
