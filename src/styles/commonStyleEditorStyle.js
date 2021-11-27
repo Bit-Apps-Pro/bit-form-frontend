908bc5bef6de946357b2f876a97c222151ab35c5
Arif Uddin <arifunctg@gmail.com>
2021-11-27 17:12:15 +0600

    Create commonStyleEditorStyle.js

---
+++ src/styles/commonStyleEditorStyle.js
@@ -0,0 +1,23 @@
+const sc = {
+  select: {
+    bc: 'var(--white-0-95) !important',
+    b: 'none',
+    brs: '10px !important',
+    fw: 500,
+    w: 110,
+    cr: 'var(--b-54-12) !important',
+    ':hover': {
+      bs: '0 0 0 1px var(--white-0-83) !important',
+      cr: 'var(--b-54-12)',
+    },
+    ':focus': { focusShadow: 1 },
+  },
+  resetBtn: {
+    w: 25,
+    h: 25,
+    flx: 'center',
+
+  }
+}
+
+export default sc
