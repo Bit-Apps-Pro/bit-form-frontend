356b389eb07453f099f956b52a69f95ed5ddb12b
Arif Uddin <arifunctg@gmail.com>
2021-12-01 11:39:38 +0600

    Update StyleLayers.jsx

--- src/components/LeftBars/StyleLayers.jsx
+++ src/components/LeftBars/StyleLayers.jsx
@@ -1,5 +1,6 @@
 import { useFela } from 'react-fela'
 import ut from '../../styles/2.utilities'
+import LayerAccordion from '../CompSettings/StyleCustomize/ChildComp/LayerAccordion'
 
 export default function StyleLayers() {
   const { css } = useFela()
@@ -8,7 +9,16 @@ export default function StyleLayers() {
       <h4 className={css(s.title)}>Layers & asdasd</h4>
       <div className={css(ut.divider)} />
 
-      
+      <LayerAccordion title="Texfield">
+        <LayerAccordion title="Container" offset={1}>
+          <LayerAccordion title="Label & Sub Label Container" offset={2}>
+            asdasdasdf asdf asdfasdfasdf
+          </LayerAccordion>
+        </LayerAccordion>
+      </LayerAccordion>
+      <LayerAccordion title="Checkbox">
+        asdasdasdf asdf asdfasdfasdf
+      </LayerAccordion>
     </div>
   )
 }
@@ -20,11 +30,14 @@ const s = {
     h: '100%',
     bs: '0 0 0 1px var(--b-44-87)',
     ow: 'hidden',
-    p: 10,
+    '& .toggle-icn': { oy: '0' },
+    ':hover': { '& .toggle-icn': { oy: '1' } },
   },
   title: {
     ff: '"Montserrat", sans-serif',
-    m: 0,
+    mt: 7,
     bs: 'none',
+    mb: 10,
+    mx: 8,
   },
 }
