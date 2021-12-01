0fb1aae3f3c27005eca01eae438767d91b0f2b07
Arif Uddin <arifunctg@gmail.com>
2021-12-01 11:39:33 +0600

    Update SortIcn.jsx

--- src/Icons/SortIcn.jsx
+++ src/Icons/SortIcn.jsx
@@ -1,6 +1,6 @@
-export default function SortIcn({ size = 5, style }) {
+export default function SortIcn({ size = 5, style, className }) {
   return (
-    <svg width={size} height={size} style={{ ...style }} viewBox="0 0 3 3" fill="currentColor">
+    <svg width={size} height={size} style={{ ...style }} className={className} viewBox="0 0 3 3" fill="currentColor">
       <path d="M1.5 0L2.79904 2.25H0.200962L1.5 0Z" fill="currentColor" />
     </svg>
   )
