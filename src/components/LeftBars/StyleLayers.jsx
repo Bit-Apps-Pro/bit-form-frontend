a6f59a6a5d7b095f2a416a2a2d293da0dd4e37f3
Arif Uddin <arifunctg@gmail.com>
2021-12-04 19:13:43 +0600

    Update StyleLayers.jsx

--- src/components/LeftBars/StyleLayers.jsx
+++ src/components/LeftBars/StyleLayers.jsx
@@ -1,51 +1,89 @@
 import { useFela } from 'react-fela'
 import { useHistory, useParams } from 'react-router-dom'
+import FocusIcn from '../../Icons/FocusIcn'
+import TweaksIcn from '../../Icons/TweaksIcn'
+// import SettingsIcn from '../../Icons/SettingsIcn'
 import ut from '../../styles/2.utilities'
+// import ut from '../../styles/2.utilities'
 import LayerAccordion from '../CompSettings/StyleCustomize/ChildComp/LayerAccordion'
+import { highlightElm, removeHightlight } from '../style-new/styleHelpers'
 
 export default function StyleLayers() {
   const { css } = useFela()
-  const { formType, formID } = useParams()
-  const history = useHistory()
-
-  const styleHandler = ({ target: { value: customstyle } }) => {
-    history.push(`/form/builder/${formType}/${formID}/theme-customize/${customstyle}`)
-  }
 
   return (
     <div className={css(s.con)}>
-      <h4 className={css(s.title)}>Layers & asdasd</h4>
-      <div className={css(ut.divider)} />
+      <h4 className={css(s.title)}>Elements & Layers</h4>
+      <div className={css(s.divider)} />
+      <h5 className={css(s.subtitle, ut.mt1, ut.fontH)}>Common Elements</h5>
+      <NavBtn route="theme-customization" label="Theme Quick Tweaks asdasdasdasd" icn={<TweaksIcn size={13} />} />
+      <NavBtn route="field-container" label="Field Blocks" />
+      <NavBtn route="label-subtitle-container" label="Label Containers" />
+      <NavBtn route="label" label="Labels" />
+      <NavBtn route="subtitle" label="Sub Labels" />
+      <NavBtn route="helper-text" label="Helper Texts" />
+      <NavBtn route="error-messages" label="Error Messages" />
+
+      <h5 className={css(s.subtitle, ut.fontH, { mt: 12 })}>Individual Elements</h5>
 
       <LayerAccordion title="Texfield">
-        <LayerAccordion title="Container" offset={1}>
-          <LayerAccordion title="Label & Sub Label Container" offset={2}>
-            asdasdasdf asdf asdfasdfasdf
-          </LayerAccordion>
-        </LayerAccordion>
+        <NavBtn route="error-messages" label="Quick Tweaks" offset="2.5" icn={<TweaksIcn size={13} />} />
+        <NavBtn route="error-messages" label="Field Container" offset="2.5" />
+        <NavBtn route="error-messages" label="Label Container" offset="2.5" />
+        <NavBtn route="error-messages" label="Label" offset="2.5" />
+        <NavBtn route="error-messages" label="Sub Label" offset="2.5" />
+        <NavBtn route="error-messages" label="Helper Text" offset="2.5" />
+        <NavBtn route="error-messages" label="Error Message" offset="2.5" />
       </LayerAccordion>
       <LayerAccordion title="Checkbox">
-        asdasdasdf asdf asdfasdfasdf
+        <NavBtn route="error-messages" label="Quick Tweaks" offset="2.5" titleClass={{ fw: 600 }} />
+        <NavBtn route="error-messages" label="Field Container" offset="2.5" />
+        <NavBtn route="error-messages" label="Label Container" offset="2.5" />
+        <NavBtn route="error-messages" label="Label" offset="2.5" />
+        <NavBtn route="error-messages" label="Sub Label" offset="2.5" />
+        <NavBtn route="error-messages" label="Helper Text" offset="2.5" />
+        <NavBtn route="error-messages" label="Error Message" offset="2.5" />
       </LayerAccordion>
-      <button type="button" value="theme-customization" onClick={styleHandler}>Form Theme Customization</button>
-      {' '}
-      <br />
-      <button type="button" value="field-container" onClick={styleHandler}>Field Container</button>
-      {' '}
-      <br />
-      <button type="button" value="label-subtitle-container" onClick={styleHandler}>Label Subtitle Container</button>
-      {' '}
-      <br />
-      <button type="button" value="label" onClick={styleHandler}>Label</button>
-      {' '}
-      <br />
-      <button type="button" value="subtitle" onClick={styleHandler}>Sub Title</button>
-      {' '}
-      <br />
-      <button type="button" value="helper-text" onClick={styleHandler}>Helper Text</button>
-      {' '}
-      <br />
-      <button type="button" value="error-messages" onClick={styleHandler}>Error Messages</button>
+
+    </div>
+  )
+}
+function NavBtn({ route, label, offset = 1, icn, highlightSelector }) {
+  const { formType, formID } = useParams()
+  const history = useHistory()
+  const styleHandler = () => {
+    history.push(`/form/builder/${formType}/${formID}/theme-customize/${route}`)
+  }
+  const { css } = useFela()
+  return (
+    <div
+      className={css(s.navBtn)}
+      style={{ paddingLeft: offset * 10 }}
+      type="button"
+      value={route}
+      onClick={styleHandler}
+      onKeyPress={styleHandler}
+      role="button"
+      tabIndex="0"
+    >
+      <div className={css(ut.flxc)}>
+        {icn && <span className={css(ut.flxc, { mr: 5 })}>{icn}</span>}
+        {label}
+      </div>
+      <div className={css(s.navActionBtn)} data-action-btn>
+        <div
+          onMouseOver={() => highlightElm('highlightSelector')}
+          onFocus={() => highlightElm('highlightSelector')}
+          onMouseLeave={() => removeHightlight()}
+          onBlur={() => removeHightlight()}
+          role="button"
+          tabIndex="0"
+          className={css(s.highlightBtn)}
+          title="Highlight Element in Builder"
+        >
+          <FocusIcn size={15} stroke="2.5" />
+        </div>
+      </div>
     </div>
   )
 }
@@ -66,5 +104,62 @@ const s = {
     bs: 'none',
     mb: 10,
     mx: 8,
+    wb: 'keep-all',
+    ws: 'nowrap',
   },
+  subtitle: {
+    mx: 8,
+    mb: 8,
+    fs: 14,
+    fw: 500,
+    cr: '#9a9fb9',
+    wb: 'keep-all',
+    ws: 'nowrap',
+  },
+  navBtn: {
+    w: '100%',
+    h: 26,
+    fs: 13,
+    ta: 'left',
+    p: 0,
+    pl: 13,
+    lh: 2,
+    cr: 'inherit',
+    flx: 'center-between',
+    wb: 'keep-all',
+    ws: 'nowrap',
+    pn: 'relative',
+    us: 1,
+    curp: 1,
+    ':focus-within': { '& div[data-action-btn]': { dy: 'flex' } },
+    ':focus-visible': {
+      focusShadow: 1,
+      '& div[data-action-btn]': { dy: 'flex' },
+    },
+    ':hover': {
+      bd: '#eeeff7',
+      '& div[data-action-btn]': { dy: 'flex' },
+    },
+  },
+  navActionBtn: {
+    h: '100%',
+    b: 'none',
+    pn: 'absolute',
+    bd: 'linear-gradient(90deg, transparent, #eeeff7 45%)',
+    rt: 0,
+    jc: 'center',
+    ai: 'center',
+    dy: 'none',
+    pl: 15,
+  },
+  highlightBtn: {
+    flx: 'center',
+    brs: 5,
+    h: 20,
+    w: 20,
+    curp: 1,
+    ':focus-visible': { focusShadow: 1 },
+    ':hover': { bd: '#d3d3d3' },
+  },
+  divider: { bb: '1px solid var(--white-0-83)' },
 }
