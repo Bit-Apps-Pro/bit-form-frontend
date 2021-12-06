b066be89e6cd16b156035ec4c816b56acfd98293
Arif Uddin <arifunctg@gmail.com>
2021-12-06 13:16:59 +0600

    reset functionality udpated

--- src/components/style-new/ResetStyle.jsx
+++ src/components/style-new/ResetStyle.jsx
@@ -1,31 +1,40 @@
 /* eslint-disable no-param-reassign */
 import produce from 'immer'
 import { useFela } from 'react-fela'
-import { useRecoilValue, useSetRecoilState } from 'recoil'
+import { useRecoilState, useRecoilValue, } from 'recoil'
 import StyleResetIcn from '../../Icons/StyleResetIcn'
 import sc from '../../styles/commonStyleEditorStyle'
 import { $tempStyles, $themeColors, $themeVars } from '../../GlobalStates'
 
-export default function ResetStyle({ themeVar, stateName, show }) {
-  const { themeColors, themeVars } = useRecoilValue($tempStyles)
-  const setThemeColor = useSetRecoilState($themeColors)
-  const setThemeVar = useSetRecoilState($themeVars)
+export default function ResetStyle({ objectKey, stateName }) {
+  const { themeColors: tmpThemeColors, themeVars: tmpThemeVars } = useRecoilValue($tempStyles)
+  const [themeColor, setThemeColor] = useRecoilState($themeColors)
+  const [themeVar, setThemeVar] = useRecoilState($themeVars)
   const { css } = useFela()
-
-  const resetValue = val => {
+  let show = false
+  if (stateName === 'themeVars') {
+    if (themeVar?.[objectKey] !== tmpThemeVars?.[objectKey]) {
+      show = true
+    }
+  } else if (stateName === 'themeColors') {
+    if (tmpThemeColors?.[objectKey] !== themeColor?.[objectKey]) {
+      show = true
+    }
+  }
+  const resetValue = () => {
     if (stateName === 'themeVars') {
-      if (!themeVars[val]) return
-      setThemeVar(prvStyle => produce(prvStyle, drft => { drft[val] = themeVars[val] }))
+      if (!tmpThemeVars[objectKey]) return
+      setThemeVar(prvStyle => produce(prvStyle, drft => { drft[objectKey] = tmpThemeVars[objectKey] }))
     }
     if (stateName === 'themeColors') {
-      if (!themeColors[val]) return
-      setThemeColor(prvStyle => produce(prvStyle, drft => { drft[val] = themeColors[val] }))
+      if (!tmpThemeColors[objectKey]) return
+      setThemeColor(prvStyle => produce(prvStyle, drft => { drft[objectKey] = tmpThemeColors[objectKey] }))
     }
   }
 
   const reset = () => {
-    if (Array.isArray(themeVar)) themeVar.forEach(v => resetValue(v))
-    else resetValue(themeVar)
+    if (Array.isArray(objectKey)) objectKey.forEach(v => resetValue(v))
+    else resetValue()
   }
 
   return (
