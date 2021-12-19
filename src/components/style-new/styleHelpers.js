db38cf3297289c2f318f7e67f71691cfb604e909
Arif Uddin <arifunctg@gmail.com>
2021-12-19 16:19:25 +0600

    Update styleHelpers.js

--- src/components/style-new/styleHelpers.js
+++ src/components/style-new/styleHelpers.js
@@ -205,43 +205,43 @@ export const commonStyle = (fk, type) => {
     case 'small-2':
       return {
         [`.${fk}-lbl`]: { 'font-size': '10px' },
-        [`.${fk}-st`]: { 'font-size': '9px' },
-        [`.${fk}-ht`]: { 'font-size': '9px' },
+        [`.${fk}-sub-titl`]: { 'font-size': '9px' },
+        [`.${fk}-hlp-txt`]: { 'font-size': '9px' },
         [`.${fk}-fld`]: { 'font-size': '10px', padding: '5px 2px !important', height: '10px' },
       }
     case 'small-1':
       return {
         [`.${fk}-lbl`]: { 'font-size': '12px' },
-        [`.${fk}-st`]: { 'font-size': '10px' },
-        [`.${fk}-ht`]: { 'font-size': '10px' },
+        [`.${fk}-sub-titl`]: { 'font-size': '10px' },
+        [`.${fk}-hlp-txt`]: { 'font-size': '10px' },
         [`.${fk}-fld`]: { 'font-size': '12px', padding: '6px 3px' },
       }
     case 'small':
       return {
         [`.${fk}-lbl`]: { 'font-size': '14px' },
-        [`.${fk}-st`]: { 'font-size': '12px' },
-        [`.${fk}-ht`]: { 'font-size': '12px' },
+        [`.${fk}-sub-titl`]: { 'font-size': '12px' },
+        [`.${fk}-hlp-txt`]: { 'font-size': '12px' },
         [`.${fk}-fld`]: { 'font-size': '14px', padding: '7px 4px' },
       }
     case 'medium':
       return {
         [`.${fk}-lbl`]: { 'font-size': '16px' },
-        [`.${fk}-st`]: { 'font-size': '11px' },
-        [`.${fk}-ht`]: { 'font-size': '11px' },
+        [`.${fk}-sub-titl`]: { 'font-size': '11px' },
+        [`.${fk}-hlp-txt`]: { 'font-size': '11px' },
         [`.${fk}-fld`]: { 'font-size': '16px', padding: '8px 5px' },
       }
     case 'large':
       return {
         [`.${fk}-lbl`]: { 'font-size': '18px' },
-        [`.${fk}-st`]: { 'font-size': '12px' },
-        [`.${fk}-ht`]: { 'font-size': '12px' },
+        [`.${fk}-sub-titl`]: { 'font-size': '12px' },
+        [`.${fk}-hlp-txt`]: { 'font-size': '12px' },
         [`.${fk}-fld`]: { 'font-size': '18px', padding: '9px 6px' },
       }
     case 'large-1':
       return {
         [`.${fk}-lbl`]: { 'font-size': '20px' },
-        [`.${fk}-st`]: { 'font-size': '14px' },
-        [`.${fk}-ht`]: { 'font-size': '14px' },
+        [`.${fk}-sub-titl`]: { 'font-size': '14px' },
+        [`.${fk}-hlp-txt`]: { 'font-size': '14px' },
         [`.${fk}-fld`]: { 'font-size': '20px', padding: '10px 7px' },
       }
     default:
