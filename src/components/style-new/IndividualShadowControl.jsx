cdcf88301359803576275ead904397d35b966098
mdabbas-cse <gmabbas44@gmail.com>
2021-12-20 19:49:30 +0600

    Create: Individual Shadow Control and Menu

---
+++ src/components/style-new/IndividualShadowControl.jsx
@@ -0,0 +1,144 @@
+import produce from 'immer'
+import { useFela } from 'react-fela'
+import { useRecoilState, useSetRecoilState } from 'recoil'
+import { $draggableModal, $styles, $themeColors, $themeVars } from '../../GlobalStates'
+import CloseIcn from '../../Icons/CloseIcn'
+import TrashIcn from '../../Icons/TrashIcn'
+import ut from '../../styles/2.utilities'
+import { assignNestedObj } from '../../Utils/FormBuilderHelper'
+import { __ } from '../../Utils/i18nwrap'
+import ColorPreview from './ColorPreview'
+import Important from './Important'
+import ResetStyle from './ResetStyle'
+import { showDraggableModal } from './styleHelpers'
+
+export default function IndividualShadowControl({ title,
+  stateObjName,
+  propertyPath,
+  subtitle,
+  value,
+  objectPaths,
+  modalType,
+  modalId,
+  deleteable,
+  delPropertyHandler,
+  allowImportant,
+  hslaPaths,
+  fldKey }) {
+  const { css } = useFela()
+  const setStyles = useSetRecoilState($styles)
+  const setThemeVars = useSetRecoilState($themeVars)
+  const setThemeColors = useSetRecoilState($themeColors)
+  const [draggableModal, setDraggableModal] = useRecoilState($draggableModal)
+
+  const clearHandler = () => {
+    switch (stateObjName) {
+      case 'themeColors':
+        setThemeColors(prvStyle => produce(prvStyle, drft => {
+          drft[`${propertyPath}`] = ''
+        }))
+        break
+      case 'themeVars':
+        setThemeVars(prvStyle => produce(prvStyle, drft => {
+          drft[`${propertyPath}`] = ''
+        }))
+        break
+      case 'styles':
+        setStyles(prvState => produce(prvState, drftStyles => {
+          assignNestedObj(drftStyles, propertyPath, '')
+        }))
+        break
+      default:
+        break
+    }
+  }
+
+  return (
+    <div className={css(ut.flxcb, ut.mt2, c.containerHover)}>
+      <div className={css(ut.flxc)}>
+        {deleteable && (
+          <button title="Delete Property" onClick={delPropertyHandler} className={`${css(c.delBtn)} delete-btn`} type="button">
+            <TrashIcn size="15" />
+          </button>
+        )}
+        <span className={css(ut.fw500)}>{__(title, 'bitform')}</span>
+      </div>
+      <div className={css(ut.flxc)}>
+        <ResetStyle objectKey={`--${modalType}`} stateObjName={stateObjName} objectPaths={objectPaths} propertyPath={propertyPath} />
+        {allowImportant && <Important stateObjName={stateObjName} propertyPath={propertyPath} />}
+        <div className={css(c.preview_wrp, draggableModal.id === modalId && c.active)}>
+          <button
+            onClick={e => showDraggableModal(e, setDraggableModal, { component: 'individual-shadow-control', subtitle, action: { type: modalType }, value, id: modalId, objectPaths, stateObjName, propertyPath, hslaPaths, fldKey })}
+            type="button"
+            className={css(c.pickrBtn)}
+          >
+            <ColorPreview bg={value?.replace(/!important/gi, '')} h={24} w={24} className={css(ut.mr2)} />
+            <span className={css(c.clrVal)}>{value?.replaceAll(/\(|var|\)/gi, '')}</span>
+          </button>
+          {value && (
+            <button title="Clear Value" onClick={clearHandler} className={css(c.clearBtn)} type="button" aria-label="Clear Color">
+              <CloseIcn size="12" />
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const c = {
+  containerHover: {
+    '&:hover .delete-btn': {
+      bd: 'var(--b-79-96)',
+      brs: '50%',
+      cr: 'var(--b-50)',
+      oy: 1,
+      tm: 'scale(1.1)',
+    },
+  },
+  preview_wrp: {
+    bd: 'var(--white-0-95)',
+    w: 130,
+    mnw: 130,
+    brs: 10,
+    p: 3,
+    flx: 'center-between',
+    ':hover': { bs: '0 0 0 1px var(--white-0-83)' },
+  },
+  clearBtn: {
+    brs: '50%',
+    p: 4,
+    w: 17,
+    h: 17,
+    b: 'none',
+    flx: 'center',
+    bd: 'transparent',
+    cr: 'var(--white-0-50)',
+    curp: 1,
+    ':hover': { cr: 'var(--black-0)', bd: '#d3d1d1' },
+  },
+  pickrBtn: {
+    b: 'none',
+    curp: 1,
+    flx: 'center',
+    bd: 'transparent',
+    p: 0,
+  },
+  delBtn: {
+    b: 'none',
+    p: 5,
+    bd: 'transparent',
+    mr: 5,
+    oy: 0,
+    tn: '.2s all',
+    curp: 1,
+  },
+  clrVal: {
+    w: 73,
+    ws: 'nowrap',
+    textOverflow: 'ellipsis',
+    ow: 'hidden',
+  },
+  active: { focusShadow: 1 },
+
+}
---
+++ src/components/style-new/IndividualShadowControlMenu.jsx
@@ -0,0 +1,242 @@
+/* eslint-disable no-case-declarations */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable no-param-reassign */
+import ColorPicker from '@atomik-color/component'
+import { str2Color } from '@atomik-color/core'
+import produce from 'immer'
+import { memo, useEffect, useState } from 'react'
+import { useFela } from 'react-fela'
+import { useRecoilState } from 'recoil'
+import { $styles, $themeColors, $themeVars } from '../../GlobalStates'
+import ut from '../../styles/2.utilities'
+import boxSizeControlStyle from '../../styles/boxSizeControl.style'
+import { assignNestedObj } from '../../Utils/FormBuilderHelper'
+import Grow from '../CompSettings/StyleCustomize/ChildComp/Grow'
+import StyleSegmentControl from '../Utilities/StyleSegmentControl'
+import { hsva2hsla } from './colorHelpers'
+import ColorPreview from './ColorPreview'
+import { getStyleValueFromObjectPath } from './styleHelpers'
+
+// stateObjName string ex: themeColor
+// objPath string ex: a->b->
+// hslaPaths object ex: {h: 'a->b->',s: 'a->s->l'}
+// canSetVariable bool
+//
+// fields->id->classes->className->prop
+
+// <SimpleColorsPicker
+//  stateName="themevar"
+//  propertyPath="fields->id->classes->className->prop"
+//  hsla={{h:'--gfh', s: ''}}
+// />
+function IndividualShadowControlMenu({ stateObjName,
+  propertyPath,
+  hslaPaths,
+  canSetVariable,
+  id,
+  fldKey }) {
+  const { css } = useFela()
+  const [themeVars, setThemeVars] = useRecoilState($themeVars)
+  const [color, setColor] = useState()
+  const isColorVar = typeof color === 'string'
+  const [controller, setController] = useState(isColorVar ? 'Var' : 'Custom')
+  const [themeColors, setThemeColors] = useRecoilState($themeColors)
+  const [styles, setStyles] = useRecoilState($styles)
+  const options = [
+    { label: 'Custom', icn: 'Custom color', show: ['icn'], tip: 'Custom color' },
+    { label: 'Var', icn: 'Variables', show: ['icn'], tip: 'Variable color' },
+  ]
+
+  const { '--global-bg-color': themeBgColor,
+    '--global-fld-bdr-clr': themeFldBdrClr,
+    '--global-fld-bg-color': themeFldBgColor,
+    '--global-font-color': themeFontColor,
+    '--global-accent-color': themePrimaryColor } = themeColors
+
+  useEffect(() => {
+    switch (stateObjName) {
+      case 'themeColors':
+        const themeColorsVal = getStyleValueFromObjectPath(themeColors, propertyPath)
+        setColor(str2Color(themeColorsVal))
+        break
+
+      case 'themeVars':
+        const themeVarColor = getStyleValueFromObjectPath(themeVars, propertyPath)
+        setColor(str2Color(themeVarColor))
+        break
+
+      case 'styles':
+        const styleColor = getStyleValueFromObjectPath(styles, propertyPath)
+        // let c = styleColor
+        // if (styleColor.match(/var/gi)?.[0 === 'var']) {
+        //   const varClr = styleColor.replaceAll(/\(|var|,.*|\)/gi, '')
+        //   c = themeVars[varClr] ? themeVars[varClr] : $themeColors[varClr]
+        // }
+        // setColor(str2Color(c))
+        break
+      case 'field-accent-color':
+        const accColor = getStyleValueFromObjectPath(themeColors, propertyPath)
+        setColor(str2Color(accColor))
+
+      // eslint-disable-next-line no-fallthrough
+      default:
+        break
+    }
+  }, [id])
+
+  const handleColor = (_h, _s, _v, _a) => {
+    const [h, s, l, a, hslaStr] = hsva2hsla(_h, _s, _v, _a)
+    switch (stateObjName) {
+      case 'themeColors':
+        setThemeColors(prvState => produce(prvState, drftThmClr => {
+          drftThmClr[propertyPath] = hslaStr
+          if (hslaPaths) {
+            if ('h' in hslaPaths) { drftThmClr[hslaPaths.h] = h }
+            if ('s' in hslaPaths) { drftThmClr[hslaPaths.s] = s }
+            if ('l' in hslaPaths) { drftThmClr[hslaPaths.l] = l }
+            if ('a' in hslaPaths) { drftThmClr[hslaPaths.a] = a }
+          }
+        }))
+        break
+      case 'themeVars':
+        setThemeVars(prvState => produce(prvState, drftThmVar => {
+          drftThmVar[propertyPath] = hslaStr
+          if (hslaPaths) {
+            if ('h' in hslaPaths) { drftThmVar[hslaPaths.h] = h }
+            if ('s' in hslaPaths) { drftThmVar[hslaPaths.s] = s }
+            if ('l' in hslaPaths) { drftThmVar[hslaPaths.l] = l }
+            if ('a' in hslaPaths) { drftThmVar[hslaPaths.a] = a }
+          }
+        }))
+        break
+      case 'styles':
+        setStyles(prvState => produce(prvState, drftStyles => {
+          let hslaColor = hslaStr
+          const value = getStyleValueFromObjectPath(drftStyles, propertyPath)
+          const checkExistImportant = value?.match(/!important/gi)?.[0]
+          if (checkExistImportant) hslaColor = `${hslaColor} !important`
+          assignNestedObj(drftStyles, propertyPath, hslaColor)
+        }))
+        break
+      case 'field-accent-color':
+        setStyles(prvState => produce(prvState, drftStyles => {
+          const sc = `0 0 0 3px hsla(${h}, ${s}, ${l}, 0.30)!important`
+          drftStyles.fields[fldKey].classes[`.${fldKey}-fld:focus`]['border-color'] = `${hslaStr}!important`
+          drftStyles.fields[fldKey].classes[`.${fldKey}-fld:focus`]['box-shadow'] = sc
+          drftStyles.fields[fldKey].classes[`.${fldKey}-fld:hover`]['border-color'] = `${hslaStr}!important`
+        }))
+
+      // eslint-disable-next-line no-fallthrough
+      default:
+        break
+    }
+  }
+
+  const setColorState = (colorObj) => {
+    setColor(colorObj)
+    handleColor(colorObj.h, colorObj.s, colorObj.v, colorObj.a)
+  }
+
+  return (
+    <div className={css(c.preview_wrp)}>
+      {canSetVariable ? (
+        <>
+          <div className={css(boxSizeControlStyle.titlecontainer, c.mb)}>
+            <StyleSegmentControl
+              square
+              noShadow
+              defaultActive="Var"
+              options={options}
+              size={60}
+              component="button"
+              onChange={lbl => setController(lbl)}
+              show={['icn']}
+              variant="lightgray"
+              activeValue={controller}
+              width="100%"
+              wideTab
+            />
+          </div>
+
+          <Grow open={controller === 'Var'}>
+            <div className={css(c.varClr)}>
+              <button className={`${css(c.clrItem)} ${css(color === '--global-bg-color' ? c.active : null)}`} type="button" onClick={() => setColorState('--global-bg-color')}>
+                <ColorPreview bg={themeBgColor} className={css(ut.mr2)} />
+                <span>Background Color</span>
+              </button>
+              <button className={css(c.clrItem, color === '--global-accent-color' ? c.active : null)} type="button" onClick={() => setColorState('--global-accent-color')}>
+                <ColorPreview bg={themePrimaryColor} className={css(ut.mr2)} />
+                <span>Background Accent Color</span>
+              </button>
+              <button className={css(c.clrItem, color === '--global-font-color' ? c.active : null)} type="button" onClick={() => setColorState('--global-font-color')}>
+                <ColorPreview bg={themeFontColor} className={css(ut.mr2)} />
+                <span>Font Color</span>
+              </button>
+              <button className={css(c.clrItem, color === '--global-fld-bdr-clr' ? c.active : null)} type="button" onClick={() => setColorState('--global-fld-bdr-clr')}>
+                <ColorPreview bg={themeFldBdrClr} className={css(ut.mr2)} />
+                <span>Field Border Color</span>
+              </button>
+              <button className={css(c.clrItem, color === '--global-fld-bg-color' ? c.active : null)} type="button" onClick={() => setColorState('--global-fld-bg-color')}>
+                <ColorPreview bg={themeFldBgColor} className={css(ut.mr2)} />
+                <span>Field Background Color</span>
+              </button>
+            </div>
+          </Grow>
+
+          <Grow open={controller === 'Custom'}>
+            <ColorPicker showParams showPreview onChange={setColorState} value={color} />
+          </Grow>
+        </>
+      ) : (
+        <ColorPicker showParams showPreview onChange={setColorState} value={color} />
+      )}
+    </div>
+  )
+}
+
+export default memo(IndividualShadowControlMenu)
+
+const c = {
+  preview_wrp: {
+    '& div[role="group"]': { p: 4, b: 0 },
+    '& input': {
+      brs: 8,
+      b: '1px solid lightgray',
+      p: '3px 8px',
+      mnh: '10px !important',
+      fs: 12,
+      mb: 3,
+      bs: 'none',
+      ':focus': { focusShadow: 1, b: '1px solid var(--b-50)' },
+    },
+    '& .common-module_transBackground__2AOKu': {
+      brs: 8,
+      ow: 'hidden',
+    },
+  },
+  color: {
+    w: 30,
+    h: 30,
+    brs: 8,
+    mr: 10,
+  },
+  bggrid: { bi: 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAJUlEQVQYV2N89erVfwY0ICYmxoguxjgUFKI7GsTH5m4M3w1ChQC1/Ca8i2n1WgAAAABJRU5ErkJggg==)' },
+  varClr: { my: 5 },
+  active: { bcr: 'var(--b-50) !important', bd: '#f3f8ff' },
+  clrItem: {
+    dy: 'block',
+    flx: 'align-center',
+    bd: 'transparent',
+    b: '2px solid var(--white-0-93)',
+    p: 3,
+    mb: 8,
+    brs: 10,
+    fs: 12,
+    cur: 'pointer',
+    w: '100%',
+    tn: 'background .3s',
+    ':hover': { bd: 'var(--white-0-97)' },
+  },
+  m: { mr: 15 },
+  mb: { mb: 5 },
+}
