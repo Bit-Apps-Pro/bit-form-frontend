1a3f883cf6ff760d1b19672404b31a7a94b5f4c0
Arif Uddin <arifunctg@gmail.com>
2021-12-28 16:19:08 +0600

    Create NewStyleEditorConfig.js

---
+++ src/components/style-new/NewStyleEditorConfig.js
@@ -0,0 +1,20 @@
+const editorConfig = {
+  formWrapper: {
+    states: ['hover'],
+    properties: {
+      background: {},
+      color: {},
+      padding: {},
+      margin: {},
+      border: {},
+      'box-shadow': {},
+      transition: {},
+    },
+  },
+  defaultProps: {
+    margin: '0px',
+    padding: '0px',
+    border: '',
+  }
+}
+export default editorConfig
