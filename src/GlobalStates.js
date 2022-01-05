e2ce6b159a2a09f957bd1b6eeeaa522814d56b1e
Md Rubel Amin <mdrubelamin2@gmail.com>
2022-01-05 13:19:13 +0600

    themevars & styles responsive global state

--- src/GlobalStates.js
+++ src/GlobalStates.js
@@ -86,8 +86,61 @@ export const $darkThemeColors = atom({
   default: {},
 })
 
-export const $themeVars = atom({
-  key: '$themeVars',
+// export const $themeVars = atom({
+//   key: '$themeVars',
+//   default: {
+//     '--g-bdr-rad': '11px', // border radius
+//     '--g-bdr-width': '1px', // border width
+//     '--dir': 'ltr', // direaction
+//     '--inp-wrp-width': '',
+//     '--lbl-al': '', // label align
+//     '--fld-p': '', // field padding
+//     '--fld-m': '', // field margin
+//     '--fld-fs': '1rem', // field font size
+
+//     '--fld-wrp-dis': '', // field wrapper display
+//     '--fld-wrp-fdir': '', // field wrapper flex direction
+//     '--fld-wrp-m': '', // field wrapper margin
+//     '--fld-wrp-p': '10px', // field wrapper paddin
+//     '--fld-wrp-bdr-width': '', // field wrapper border width
+//     '--fld-wrp-bdr-rad': '', // field wrapper border radius
+
+//     '--lbl-wrp-sa': '',
+//     '--lbl-wrp-width': '', // label wrapper  width
+//     '--lbl-wrp-m': '', // label wrapper for margin
+//     '--lbl-wrp-p': '', // label wrapper for padding
+//     '--lbl-wrp-bdr-width': '', // label wrapper border width
+//     '--lbl-wrp-bdr-rad': '', // label wrapper border radius
+
+//     '--fld-lbl-m': '', // field label margin
+//     '--fld-lbl-p': '', // field label padding
+//     '--fld-lbl-fs': '1rem', // field label font size
+//     '--fld-lbl-bdr-width': '', // field label border width
+//     '--fld-lbl-bdr-rad': '', // field label border radius
+
+//     '--sub-titl-m': '', // subtitle margin
+//     '--sub-titl-p': '', // subtitle padding
+//     '--sub-titl-al': '', // subtitle align
+//     '--sub-titl-fs': '12px', // subtitle font size
+//     '--sub-titl-bdr-width': '', // subtitle border width
+//     '--sub-titl-bdr-rad': '', // subtitle border radius
+
+//     '--hlp-txt-m': '', // helper text margin
+//     '--hlp-txt-p': '', // hepler text padding
+//     '--hlp-txt-fs': '12px', // hepler text font size
+//     '--hlp-txt-al': '', // helper text align
+//     '--hlp-txt-bdr-width': '', // helper text border width
+//     '--hlp-txt-bdr-rad': '', // helper text border radius
+
+//     '--err-m': '', // error messages margin
+//     '--err-p': '', // error messages padding
+//     '--err-bdr-width': '1px', // error message border width
+//     '--err-bdr-rad': '8px', // error message border radius
+//   },
+// })
+
+const $themeVarsLg = atom({
+  key: '$themeVarsLg',
   default: {
     '--g-bdr-rad': '11px', // border radius
     '--g-bdr-width': '1px', // border width
@@ -138,6 +191,8 @@ export const $themeVars = atom({
     '--err-bdr-rad': '8px', // error message border radius
   },
 })
+export const $themeVarsMd = atom({ key: '$themeVarsMd', default: {} })
+export const $themeVarsSm = atom({ key: '$themeVarsSm', default: {} })
 
 export const $tempStyles = atom({
   key: '$tempStyles',
@@ -149,8 +204,8 @@ export const $tempStyles = atom({
   },
 })
 
-export const $styles = atom({
-  key: '$styles',
+export const $stylesLg = atom({
+  key: '$stylesLg',
   default: {
     theme: 'bitformDefault',
     form: {
@@ -166,6 +221,8 @@ export const $styles = atom({
     fields: {},
   },
 })
+export const $stylesMd = atom({ key: '$stylesMd', default: {} })
+export const $stylesSm = atom({ key: '$stylesSm', default: {} })
 
 // selectors
 export const $fieldsArr = selector({ key: '$fieldsArr', get: ({ get }) => makeFieldsArrByLabel(get($fields), get($fieldLabels)), dangerouslyAllowMutability: true })
@@ -199,5 +256,37 @@ export const $themeColors = selector({
     const colorScheme = get($colorScheme)
     if (colorScheme === 'light') set($lightThemeColors, newColors)
     if (colorScheme === 'dark') set($darkThemeColors, newColors)
+  },
+})
+
+export const $themeVars = selector({
+  key: '$themeVars',
+  get: ({ get }) => {
+    const breakpoint = get($breakpoint)
+    if (breakpoint === 'md') return merge(get($themeVarsLg), get($themeVarsMd))
+    if (breakpoint === 'sm') return merge(get($themeVarsLg), get($themeVarsSm))
+    return get($themeVarsLg)
+  },
+  set: ({ set, get }, newThemeVars) => {
+    const breakpoint = get($breakpoint)
+    if (breakpoint === 'md') set($themeVarsMd, newThemeVars)
+    else if (breakpoint === 'sm') set($themeVarsSm, newThemeVars)
+    else set($themeVarsLg, newThemeVars)
+  },
+})
+
+export const $styles = selector({
+  key: '$styles',
+  get: ({ get }) => {
+    const breakpoint = get($breakpoint)
+    if (breakpoint === 'md') return merge(get($stylesLg), get($stylesMd))
+    if (breakpoint === 'sm') return merge(get($stylesLg), get($stylesSm))
+    return get($stylesLg)
+  },
+  set: ({ set, get }, newStyles) => {
+    const breakpoint = get($breakpoint)
+    if (breakpoint === 'md') set($stylesMd, newStyles)
+    else if (breakpoint === 'sm') set($stylesSm, newStyles)
+    else set($stylesLg, newStyles)
   },
 })
