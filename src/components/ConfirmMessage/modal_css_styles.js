021b91b7b4065d99005327a1dc49779d7d604725
ShahriarH26 <87649717+ShahriarH26@users.noreply.github.com>
2022-01-05 16:24:22 +0600

    Confirm Message Preview Component and Style Added

---
+++ src/components/ConfirmMessage/modal_css_styles.js
@@ -0,0 +1,104 @@
+import fade from './Animations/fade'
+import scale from './Animations/scale'
+import modalSlide from './Animations/modal-slide'
+import { arrayToStyleStringGenarator } from '../style-new/styleHelpers'
+
+export default function modalCssStyles(formID, position, animation, width, background, borderWidth, borderType, borderColor, borderRadius, boxShadow, closeBackground, closeHover, closeIconColor, closeIconHover) {
+  const boxShadowString = arrayToStyleStringGenarator(boxShadow)
+
+  return {
+    [`.msg-container-${formID}`]: {
+      display: 'flex',
+      'justify-content': 'center',
+      'align-items': 'center',
+      position: 'fixed',
+      'z-index': 999,
+      left: 0,
+      top: 0,
+      width: '100vw',
+      height: '100vh',
+      visibility: 'hidden',
+      opacity: 0,
+      transition: 'opacity 100ms, visibility 0s',
+    },
+
+    [`.msg-container-${formID}.active`]: {
+      opacity: 1,
+      visibility: 'visible',
+    },
+
+    [`.msg-container-${formID}.deactive`]: {
+      opacity: 0,
+      'transition-delay': '400ms',
+      visibility: 'hidden',
+    },
+
+    [`.msg-background-${formID}`]: {
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      'justify-content': 'center',
+      'align-items': 'center',
+      background: 'rgba(0, 0, 0, 0.4)',
+    },
+
+    [`.msg-content-${formID}`]: {
+      background,
+      padding: '20px',
+      'border-width': borderWidth,
+      'border-style': borderType,
+      'border-color': borderColor,
+      'border-radius': borderRadius,
+      width,
+      margin: 'auto',
+      position: 'relative',
+      'box-shadow': boxShadowString,
+
+      ...animation === 'fade' && fade(position).msgContent,
+      ...animation === 'scale' && scale(position).msgContent,
+      ...animation === 'slide-down' && modalSlide(position, 'down').msgContent,
+      ...animation === 'slide-up' && modalSlide(position, 'up').msgContent,
+
+    },
+
+    [`.active .msg-content-${formID}`]: {
+      ...animation === 'fade' && fade(position).activeMsgContent,
+      ...animation === 'scale' && scale(position).activeMsgContent,
+      ...animation === 'slide-down' && modalSlide(position, 'down').activeMsgContent,
+      ...animation === 'slide-up' && modalSlide(position, 'up').activeMsgContent,
+
+    },
+    [`.deactive .msg-content-${formID}`]: {
+      ...animation === 'fade' && fade(position).deactiveMsgContent,
+      ...animation === 'scale' && scale(position).deactiveMsgContent,
+      ...animation === 'slide-down' && modalSlide(position, 'down').deactiveMsgContent,
+      ...animation === 'slide-up' && modalSlide(position, 'up').deactiveMsgContent,
+    },
+
+    [`.close-${formID}`]: {
+      color: closeIconColor,
+      background: closeBackground,
+      position: 'absolute',
+      right: '7px',
+      top: '7px',
+      height: '25px',
+      width: '25px',
+      border: 'none',
+      'border-radius': '50%',
+      padding: 0,
+      display: 'grid',
+      'place-content': 'center',
+      cursor: 'pointer',
+    },
+
+    [`.close-${formID}:hover`]: { color: closeIconHover, bd: closeHover },
+
+    [`.close-${formID}:focus`]: { color: '#000' },
+
+    [`.close-icn-${formID}`]: {
+      width: '15px',
+      height: '15px',
+      'stroke-width': 2,
+    },
+  }
+}
---
+++ src/components/ConfirmMessage/snackbar_css_styles.js
@@ -0,0 +1,101 @@
+import { arrayToStyleStringGenarator } from '../style-new/styleHelpers'
+import snackbarFade from './Animations/snackbar-fade'
+import snackbarScale from './Animations/snackbar-scale'
+import snackbarSlide from './Animations/snackbar-slide'
+
+export default function snackbarCssStyles(formID, position, animation, width, background, borderWidth, borderType, borderColor, borderRadius, boxShadow, closeBackground, closeHover, closeIconColor, closeIconHover) {
+  const boxShadowString = arrayToStyleStringGenarator(boxShadow)
+  return {
+    [`.msg-container-${formID}`]: {
+      display: 'flex',
+      'justify-content': 'center',
+      'align-items': 'center',
+      position: 'fixed',
+      'z-index': 999,
+      width,
+      height: 'auto',
+      visibility: 'hidden',
+      opacity: 0,
+      transition: 'opacity 100ms, visibility 0s',
+
+      ...animation === 'fade' && snackbarFade(position).msgContainer,
+      ...animation === 'scale' && snackbarScale(position).msgContainer,
+      ...animation === 'slide-down' && snackbarSlide(position, 'down').msgContainer,
+      ...animation === 'slide-up' && snackbarSlide(position, 'up').msgContainer,
+      ...animation === 'slide-left' && snackbarSlide(position, 'left').msgContainer,
+      ...animation === 'slide-right' && snackbarSlide(position, 'right').msgContainer,
+    },
+
+    [`.msg-container-${formID}.active`]: {
+      opacity: 1,
+      visibility: 'visible',
+      ...animation === 'fade' && snackbarFade(position).activeMsgContainer,
+      ...animation === 'scale' && snackbarScale(position).activeMsgContainer,
+      ...animation === 'slide-down' && snackbarSlide(position, 'down').activeMsgContainer,
+      ...animation === 'slide-up' && snackbarSlide(position, 'up').activeMsgContainer,
+      ...animation === 'slide-left' && snackbarSlide(position, 'left').activeMsgContainer,
+      ...animation === 'slide-right' && snackbarSlide(position, 'right').activeMsgContainer,
+    },
+
+    [`.msg-container-${formID}.deactive`]: {
+      opacity: 0,
+      visibility: 'hidden',
+      ...animation === 'fade' && snackbarFade(position).deactiveMsgContainer,
+      ...animation === 'scale' && snackbarScale(position).deactiveMsgContainer,
+      ...animation === 'slide-down' && snackbarSlide(position, 'down').deactiveMsgContainer,
+      ...animation === 'slide-up' && snackbarSlide(position, 'up').deactiveMsgContainer,
+      ...animation === 'slide-left' && snackbarSlide(position, 'left').deactiveMsgContainer,
+      ...animation === 'slide-right' && snackbarSlide(position, 'right').deactiveMsgContainer,
+    },
+
+    [`.msg-background-${formID}`]: {
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      'justify-content': 'center',
+      'align-items': 'center',
+      background: 'rgba(0, 0, 0, 0.0)',
+    },
+
+    [`.msg-content-${formID}`]: {
+      background,
+      padding: '5px 35px 5px 20px',
+      'border-width': borderWidth,
+      'border-style': borderType,
+      'border-color': borderColor,
+      'border-radius': borderRadius,
+      width: '100%',
+      margin: 'auto',
+      position: 'relative',
+      'word-break': 'break-all',
+      'box-shadow': boxShadowString,
+    },
+
+    [`.close-${formID}`]: {
+      color: closeIconColor,
+      background: closeBackground,
+      position: 'absolute',
+      right: '7px',
+      top: '50%',
+      transform: 'translateY(-50%)',
+      height: '25px',
+      width: '25px',
+      border: 'none',
+      'border-radius': '50%',
+      padding: 0,
+      display: 'grid',
+      'place-content': 'center',
+      cursor: 'pointer',
+    },
+
+    [`.close-${formID}:hover`]: { color: closeIconHover, bd: closeHover },
+
+    [`.close-${formID}:focus`]: { color: closeIconHover },
+
+    [`.close-icn-${formID}`]: {
+      width: '15px',
+      height: '15px',
+      'stroke-width': 2,
+    },
+  }
+}
