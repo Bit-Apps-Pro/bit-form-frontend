abbd4d9c43db765155e44bb728616b447fac1d7d
Md Rubel Amin <mdrubelamin2@gmail.com>
2022-01-18 15:53:14 +0600

    update: eslint config ignore parameter reassign (d|D)raft

--- .eslintrc.js
+++ .eslintrc.js
@@ -73,6 +73,6 @@ module.exports = {
     'no-use-before-define': 'off',
     allowShortCircuit: 0,
     'react/function-component-definition': 0,
-    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsForRegex: ['draft'] }],
+    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsForRegex: ['(d|D)raft'] }],
   },
 }
