a04d012357277e955d495b8cc58243b1a503c0c5
ShahriarH26 <mdrobiulhasan2620@gmail.com>
2022-01-19 15:35:02 +0600

    New TxtAlignJustifyIcn Added

---
+++ src/Icons/TxtAlignJustifyIcn.jsx
@@ -0,0 +1,9 @@
+export default function TxtAlignJustifyIcn({ size = 30, stroke = 2 }) {
+  return (
+    <svg width={size} height={size} viewBox="0 0 30 30">
+      <line className="svg-icn" strokeWidth={stroke} strokeMiterlimit="10" x1="2" y1="6.81" x2="28" y2="6.81" />
+      <line className="svg-icn" strokeWidth={stroke} strokeMiterlimit="10" x1="2" y1="15" x2="28" y2="15" />
+      <line className="svg-icn" strokeWidth={stroke} strokeMiterlimit="10" x1="2" y1="23.19" x2="28" y2="23.19" />
+    </svg>
+  )
+}
