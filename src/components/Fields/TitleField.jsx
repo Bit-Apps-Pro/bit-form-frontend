7c3be82241747dc1fa82365027e1f708e9780858
Md RH Maruf <mdrobiulhasan2620@gmail.com>
2022-02-03 16:07:06 +0600

    Title Field Comoponent modified and field setting  changed

--- src/components/Fields/TitleField.jsx
+++ src/components/Fields/TitleField.jsx
@@ -1,30 +1,121 @@
-import { useEffect } from 'react'
-import { selectInGrid } from '../../Utils/globalHelpers'
+import { renderDOMObjectFromHTMLStr } from '../../Utils/Helpers'
 import RenderStyle from '../style-new/RenderStyle'
 
 function TitleField({ fieldKey, attr, styleClasses }) {
-  const { titleImg, title, subTitle, titleTag, subTitleTag } = attr
+  const { logo, logoHide, title, subtitle, titleHide, subtitleHide, titleTag, subTitleTag } = attr
+  const fieldData = attr
+  // useEffect(() => {
+  //   const txtWrp = selectInGrid(`#${fieldKey}-txt-wrp`)
+  //   const titleHeading = document.createElement(titleTag || 'h1')
+  //   const subTitleSpan = document.createElement(subTitleTag || 'span')
+  //   titleHeading.className = `${fieldKey}-title`
+  //   titleHeading.textContent = title
+  //   txtWrp.appendChild(titleHeading)
 
-  useEffect(() => {
-    const txtWrp = selectInGrid(`#${fieldKey}-txt-wrp`)
-    const titleHeading = document.createElement(titleTag || 'h1')
-    const subTitleSpan = document.createElement(subTitleTag || 'span')
-    titleHeading.className = `${fieldKey}-title`
-    titleHeading.textContent = title
-    txtWrp.appendChild(titleHeading)
+  //   subTitleSpan.className = `${fieldKey}-sub-titl`
+  //   subTitleSpan.textContent = subtitle
+  //   txtWrp.appendChild(subTitleSpan)
+  // }, [titleTag || subTitleTag])
 
-    subTitleSpan.className = `${fieldKey}-sub-title`
-    subTitleSpan.textContent = subTitle
-    txtWrp.appendChild(subTitleSpan)
-  }, [titleTag || subTitleTag])
+  const titleGenerator = (tag, text, cls, preIcn, sufIcn) => {
+    switch (tag) {
+      case 'h1':
+        return (
+          <h1 className={`${fieldKey}${cls}`}>
+            {preIcn}
+            {renderDOMObjectFromHTMLStr(text)}
+            {sufIcn}
+          </h1>
+        )
+      case 'h2':
+        return (
+          <h2 className={`${fieldKey}${cls}`}>
+            {preIcn}
+            {renderDOMObjectFromHTMLStr(text)}
+            {sufIcn}
+          </h2>
+        )
+      case 'h3':
+        return (
+          <h3 className={`${fieldKey}${cls}`}>
+            {preIcn}
+            {renderDOMObjectFromHTMLStr(text)}
+            {sufIcn}
+          </h3>
+        )
+      case 'h4':
+        return (
+          <h4 className={`${fieldKey}${cls}`}>
+            {preIcn}
+            {renderDOMObjectFromHTMLStr(text)}
+            {sufIcn}
+          </h4>
+        )
+      case 'h5':
+        return (
+          <h5 className={`${fieldKey}${cls}`}>
+            {preIcn}
+            {renderDOMObjectFromHTMLStr(text)}
+            {sufIcn}
+          </h5>
+        )
+      case 'h6':
+        return (
+          <h6 className={`${fieldKey}${cls}`}>
+            {preIcn}
+            {renderDOMObjectFromHTMLStr(text)}
+            {sufIcn}
+          </h6>
+        )
+      case 'div':
+        return (
+          <div className={`${fieldKey}${cls}`}>
+            {preIcn}
+            {renderDOMObjectFromHTMLStr(text)}
+            {sufIcn}
+          </div>
+        )
+      case 'span':
+        return (
+          <span className={`${fieldKey}${cls}`}>
+            {preIcn}
+            {renderDOMObjectFromHTMLStr(text)}
+            {sufIcn}
+          </span>
+        )
+      case 'p':
+        return (
+          <p className={`${fieldKey}${cls}`}>
+            {preIcn}
+            {renderDOMObjectFromHTMLStr(text)}
+            {sufIcn}
+          </p>
+        )
+      default:
+        return (
+          <h1 className={`${fieldKey}${cls}`}>
+            {preIcn}
+            {renderDOMObjectFromHTMLStr(text)}
+            {sufIcn}
+          </h1>
+        )
+    }
+  }
+
+  const titlePreIcn = fieldData.titlePreIcn ? <img data-dev-title-pre-i={fieldKey} className={`${fieldKey}-title-pre-i`} src={fieldData.titlePreIcn} alt="" /> : ''
+  const titleSufIcn = fieldData.titleSufIcn ? <img data-dev-title-suf-i={fieldKey} className={`${fieldKey}-title-suf-i`} src={fieldData.titleSufIcn} alt="" /> : ''
+  const subTlePreIcn = fieldData.subTlePreIcn ? <img data-dev-sub-titl-pre-i={fieldKey} className={`${fieldKey}-sub-titl-pre-i`} src={fieldData.subTlePreIcn} alt="" /> : ''
+  const subTleSufIcn = fieldData.subTleSufIcn ? <img data-dev-sub-titl-suf-i={fieldKey} className={`${fieldKey}-sub-titl-suf-i`} src={fieldData.subTleSufIcn} alt="" /> : ''
 
   return (
     <>
       <RenderStyle styleClasses={styleClasses} />
-      <div className="drag">
-        <div className={`${fieldKey}-wrp`}>
-          <img className={`${fieldKey}-logo`} src={titleImg} alt="img" width="40" height="40" />
-          <div className={`${fieldKey}-txt-wrp`} id={`${fieldKey}-txt-wrp`} />
+      <div className={`${fieldKey}-fld-wrp drag`}>
+        {logoHide && <img className={`${fieldKey}-logo`} src={logo} alt="img" width="40" height="40" />}
+        <div className="title-wrp">
+          {titleHide && titleGenerator(titleTag, title, '-title', titlePreIcn, titleSufIcn)}
+
+          {subtitleHide && titleGenerator(subTitleTag, subtitle, '-sub-titl', subTlePreIcn, subTleSufIcn)}
         </div>
       </div>
     </>
