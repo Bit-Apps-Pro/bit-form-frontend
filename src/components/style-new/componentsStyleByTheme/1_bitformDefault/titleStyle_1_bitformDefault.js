2c1a02a903365762982fd83961503c90ae838654
Md RH Maruf <mdrobiulhasan2620@gmail.com>
2022-02-03 16:15:25 +0600

    Style class & Css Property added

--- src/components/style-new/componentsStyleByTheme/1_bitformDefault/titleStyle_1_bitformDefault.js
+++ src/components/style-new/componentsStyleByTheme/1_bitformDefault/titleStyle_1_bitformDefault.js
@@ -1,16 +1,34 @@
 /* eslint-disable camelcase */
 export default function titleStyle_1_bitformDefault({ fk, type, direction }) {
   return {
-    [`.${fk}-wrp`]: {
+    [`.${fk}-fld-wrp`]: {
       display: 'flex',
       'justify-content': 'center',
       'flex-direction': 'column',
       'align-items': 'flex-start',
       'background-image': '',
     },
-    [`.${fk}-main`]: { },
-    [`.${fk}-title`]: { margin: 0 },
-    [`.${fk}-sub-title`]: { margin: 0 },
-    [`.${fk}-logo`]: { },
+    [`.${fk}-title`]: { margin: '0px' },
+    [`.${fk}-sub-titl`]: { margin: '0px' },
+    [`.${fk}-logo`]: {
+      width: '50px',
+      height: '50px',
+    },
+    [`.${fk}-title-pre-i`]: {
+      width: '20px',
+      height: '20px',
+    },
+    [`.${fk}-title-suf-i`]: {
+      width: '20px',
+      height: '20px',
+    },
+    [`.${fk}-sub-titl-pre-i`]: {
+      width: '20px',
+      height: '20px',
+    },
+    [`.${fk}-sub-titl-suf-i`]: {
+      width: '20px',
+      height: '20px',
+    },
   }
 }
