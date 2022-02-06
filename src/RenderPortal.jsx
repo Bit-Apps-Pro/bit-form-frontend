62c0722813f9ddedd56f3728102244aba691ae57
Md Rubel Amin <mdrubelamin2@gmail.com>
2022-02-06 19:30:34 +0600

    feat: undo/redo shortcut added in renderportal component

--- src/RenderPortal.jsx
+++ src/RenderPortal.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-expressions */
 import { useState } from 'react'
 import { createPortal } from 'react-dom'
+import { handleUndoRedoShortcut } from './components/FormBuilderHistory'
 import { searchKey } from './components/style-new/styleHelpers'
 
 // eslint-disable-next-line import/prefer-default-export
@@ -8,7 +9,11 @@ export const RenderPortal = ({ children, title, ...props }) => {
   const [contentRef, setContentRef] = useState(null)
   const mountNode = contentRef?.contentWindow?.document?.body
 
-  contentRef && contentRef.contentWindow.addEventListener('keydown', searchKey)
+  if (contentRef) {
+    contentRef.contentWindow.addEventListener('keydown', searchKey)
+    contentRef.contentWindow.addEventListener('keydown', handleUndoRedoShortcut)
+  }
+
   return (
     <iframe title={title} {...props} ref={setContentRef}>
       {mountNode && createPortal(children, mountNode)}
