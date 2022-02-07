97d7b8ba2c951a517577e33772dcf6781fab32fe
mdabbas-cse <gmabbas44@gmail.com>
2022-02-07 13:56:18 +0600

    Add: Icon height & weight control in element and layers

--- src/components/style-new/IcnCustomizer.jsx
+++ src/components/style-new/IcnCustomizer.jsx
@@ -1,21 +1,52 @@
+/* eslint-disable no-param-reassign */
+import produce from 'immer'
 import { useFela } from 'react-fela'
-import { useRecoilValue } from 'recoil'
+import { useRecoilState, useRecoilValue } from 'recoil'
 import { $themeColors } from '../../GlobalStates/ThemeColorsState'
+import { $themeVars } from '../../GlobalStates/ThemeVarsState'
 import ut from '../../styles/2.utilities'
 import { __ } from '../../Utils/i18nwrap'
+import SizeControl from '../CompSettings/StyleCustomize/ChildComp/SizeControl'
 import BorderControl from './BorderControl'
 import ResetStyle from './ResetStyle'
 import ShadowControl from './ShadowControl'
 import SpacingControl from './SpacingControl'
+import { getNumFromStr, getStrFromStr, unitConverter } from './styleHelpers'
 import ThemeStylePropertyBlock from './ThemeStylePropertyBlock'
 
 export default function IcnCustomizer({ elementKey }) {
   const { css } = useFela()
   const themeColors = useRecoilValue($themeColors)
+  const [themeVars, setThemeVars] = useRecoilState($themeVars)
+  const weightVar = `--${elementKey}-w`
+  const heightVar = `--${elementKey}-h`
+
+  const icnValue = (varName) => getNumFromStr(themeVars[varName])
+  const icnUnit = (varName) => getStrFromStr(themeVars[varName])
+
+  const updateState = (varName, value = '') => {
+    setThemeVars(prvStyle => produce(prvStyle, drftStyle => {
+      drftStyle[varName] = value
+    }))
+  }
+  const unitHandler = (unit, value, name) => {
+    const preUnit = getStrFromStr(themeVars[name])
+    const convetVal = unitConverter(unit, value, preUnit)
+    const val = `${convetVal}${unit}`
+    updateState(name, val)
+  }
+
+  const icnSizeHandler = ({ v, u }, prop) => {
+    const val = `${v}${u}`
+    updateState(prop, val)
+  }
 
   const preIconSpacingObj = {
     object: 'themeVars',
-    paths: { margin: `--${elementKey}-m`, padding: `--${elementKey}-p` },
+    paths: {
+      margin: `--${elementKey}-m`,
+      padding: `--${elementKey}-p`,
+    },
   }
   const preIcnShObj = {
     object: 'themeColors',
@@ -24,7 +55,11 @@ export default function IcnCustomizer({ elementKey }) {
   const preIcnBdrObj = {
     object: 'themeVars',
     borderObjName: 'themeColors',
-    paths: { border: `--${elementKey}-bdr`, borderWidth: `--${elementKey}-bdr-width`, borderRadius: `--${elementKey}-bdr-rad` },
+    paths: {
+      border: `--${elementKey}-bdr`,
+      borderWidth: `--${elementKey}-bdr-width`,
+      borderRadius: `--${elementKey}-bdr-rad`,
+    },
   }
 
   return (
@@ -63,6 +98,38 @@ export default function IcnCustomizer({ elementKey }) {
             value={themeColors[`--${elementKey}-bdr`]}
             objectPaths={preIcnBdrObj}
             id="pre-i-control"
+          />
+        </div>
+      </ThemeStylePropertyBlock>
+      <ThemeStylePropertyBlock label="Width">
+        <div className={css(ut.flxc)}>
+          <ResetStyle
+            propertyPath={weightVar}
+            stateObjName="themeVars"
+          />
+          <SizeControl
+            width="128px"
+            value={Number(icnValue(weightVar))}
+            unit={icnUnit(weightVar)}
+            inputHandler={({ value, unit }) => icnSizeHandler({ v: value, u: unit }, weightVar)}
+            sizeHandler={({ unitKey, unitValue }) => unitHandler(unitKey, unitValue, weightVar)}
+            options={['px', 'em', 'rem', '%']}
+          />
+        </div>
+      </ThemeStylePropertyBlock>
+      <ThemeStylePropertyBlock label="Height">
+        <div className={css(ut.flxc)}>
+          <ResetStyle
+            propertyPath={heightVar}
+            stateObjName="themeVars"
+          />
+          <SizeControl
+            width="128px"
+            value={Number(icnValue(heightVar))}
+            unit={icnUnit(heightVar)}
+            inputHandler={({ value, unit }) => icnSizeHandler({ v: value, u: unit }, heightVar)}
+            sizeHandler={({ unitKey, unitValue }) => unitHandler(unitKey, unitValue, heightVar)}
+            options={['px', 'em', 'rem', '%']}
           />
         </div>
       </ThemeStylePropertyBlock>
