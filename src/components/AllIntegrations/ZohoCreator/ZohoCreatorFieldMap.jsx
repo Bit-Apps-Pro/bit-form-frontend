56ed789f240ac38fb7ded6d47179e570e7f89b8a
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2022-03-05 17:36:04 +0600

    Smartcodes field added all integration in form field

--- src/components/AllIntegrations/ZohoCreator/ZohoCreatorFieldMap.jsx
+++ src/components/AllIntegrations/ZohoCreator/ZohoCreatorFieldMap.jsx
@@ -1,5 +1,8 @@
+import { useRecoilValue } from 'recoil'
+import { $bits } from '../../../GlobalStates'
 import TrashIcn from '../../../Icons/TrashIcn'
 import { __ } from '../../../Utils/i18nwrap'
+import { SmartTagField } from '../../../Utils/StaticData/SmartTagField'
 import MtInput from '../../Utilities/MtInput'
 import { addFieldMap, delFieldMap, handleCustomValue, handleFieldMapping } from '../IntegrationHelpers/IntegrationHelpers'
 
@@ -10,6 +13,8 @@ export default function ZohoCreatorFieldMap({ i, formFields, uploadFields, field
   if (uploadFields) isNotRequired = field.zohoFormField === '' || creatorConf.default?.fields?.[applicationId]?.[formId]?.requiredFileUploadFields?.indexOf(field.zohoFormField) === -1
   else isNotRequired = field.zohoFormField === '' || creatorConf.default?.fields?.[applicationId]?.[formId]?.required?.indexOf(field.zohoFormField) === -1
 
+  const bits = useRecoilValue($bits)
+  const { isPro } = bits
   return (
     <div
       className="flx mt-2 mr-1"
@@ -17,10 +22,19 @@ export default function ZohoCreatorFieldMap({ i, formFields, uploadFields, field
       <div className="flx integ-fld-wrp">
         <select className="btcd-paper-inp mr-2" name="formField" value={field.formField || ''} onChange={(ev) => handleFieldMapping(ev, i, creatorConf, setCreatorConf, uploadFields)}>
           <option value="">{__('Select Field', 'bitform')}</option>
-          {
-            uploadFields ? formFields.map(f => f.type === 'file-up' && <option key={`ff-zhcrm-${f.key}`} value={f.key}>{f.name}</option>) : formFields.map(f => f.type !== 'file-up' && <option key={`ff-zhcrm-${f.key}`} value={f.key}>{f.name}</option>)
-          }
+          <optgroup label="Form Fields">
+            {
+              uploadFields ? formFields.map(f => f.type === 'file-up' && <option key={`ff-zhcrm-${f.key}`} value={f.key}>{f.name}</option>) : formFields.map(f => f.type !== 'file-up' && <option key={`ff-zhcrm-${f.key}`} value={f.key}>{f.name}</option>)
+            }
+          </optgroup>
           <option value="custom">{__('Custom...', 'bitform')}</option>
+          <optgroup label={`General Smart Codes ${isPro ? '' : '(PRO)'}`}>
+            {isPro && SmartTagField?.map(f => (
+              <option key={`ff-rm-${f.name}`} value={f.name}>
+                {f.label}
+              </option>
+            ))}
+          </optgroup>
         </select>
 
         {field.formField === 'custom' && <MtInput onChange={ev => handleCustomValue(ev, i, creatorConf, setCreatorConf)} label={__('Custom Value', 'bitform')} className="mr-2" type="text" value={field.customValue} placeholder={__('Custom Value', 'bitform')} />}
