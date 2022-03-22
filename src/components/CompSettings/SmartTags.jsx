ce37d49a9d9bfc2835309b9803b6aa3735eb99ec
mdrubelamin2 <mdrubelamin2@gmail.com>
2022-03-22 19:26:03 +0600

    feat: smart tags list updated in title field

--- src/components/CompSettings/SmartTags.jsx
+++ src/components/CompSettings/SmartTags.jsx
@@ -7,6 +7,7 @@ import { useParams } from 'react-router-dom'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { $fields, $fieldsArr } from '../../GlobalStates/GlobalStates'
 import { deepCopy } from '../../Utils/Helpers'
+import { SmartTagField } from '../../Utils/StaticData/SmartTagField'
 
 function SmartTags({ fieldName }) {
   const { fieldKey: fldKey } = useParams()
@@ -19,50 +20,33 @@ function SmartTags({ fieldName }) {
     setFields(allFields => produce(allFields, draft => { draft[fldKey] = fieldData }))
   }
 
-  const others = [
-    { label: 'Administrator Email', value: 'bf_admin_email' },
-    { label: 'Date(mm/dd/yyy)', value: 'bf_date.m/d/Y' },
-    { label: 'Date(dd/mm/yyy)', value: 'bf_date.d/m/Y' },
-    { label: 'Embedded Post/Page ID', value: 'bf_emabedded_page_id' },
-    { label: 'Embedded Post/Page Title', value: 'bf_emabedded_page_title' },
-    { label: 'Embedded URL', value: 'bf_embedded_url' },
-    { label: 'Form Name', value: 'bf_form_name' },
-    { label: 'Form ID', value: 'bf_form_id' },
-    { label: 'Referer URL', value: 'bf_http_referer_url' },
-    { label: 'User IP Address', value: 'bf_ip_address' },
-    { label: 'User ID', value: 'bf_user_id' },
-    { label: 'Author Display Name', value: 'bf_user_display_name' },
-    { label: 'Author Email', value: 'bf_user_display_name' },
-    { label: 'Author First Name', value: 'bf_user_first_name' },
-    { label: 'Author Last Name', value: 'bf_user_last_name' },
-    { label: 'User Browser Client', value: 'bf_user_browser_client' },
-    { label: 'User Operating System', value: 'bf_user_operating_system' },
-    { label: 'Site URL', value: 'bf_site_url' },
-    { label: 'Site Title', value: 'bf_site_title' },
-  ]
+  const fieldsList = formFields.filter(f => !f.type.match(/^(file-up|recaptcha|title)$/))
 
   return (
     <div className={css(style.main)}>
-      <ul>
-        <h4 style={{ margin: 0 }}>Form Fields</h4>
-        {formFields !== null && formFields.map(f => !f.type.match(/^(file-up|recaptcha|title)$/)
-          && (
-            <li>
-              <button
-                type="button"
-                className={`${css(style.button)} btnHover`}
-                title={f.name}
-                onClick={() => addField(f.key)}
-              >
-                {f.name}
-              </button>
-            </li>
-          ))}
-      </ul>
-      <ul>
+      {!!fieldsList.length && (
+        <ul className={css(style.ul)}>
+          <h4 style={{ margin: 0 }}>Form Fields</h4>
+          {formFields !== null && formFields.map(f => !f.type.match(/^(file-up|recaptcha|title)$/)
+            && (
+              <li className={css(style.li)}>
+                <button
+                  type="button"
+                  className={`${css(style.button)} btnHover`}
+                  title={f.name}
+                  onClick={() => addField(f.key)}
+                >
+                  {f.name}
+                </button>
+              </li>
+            ))}
+        </ul>
+      )}
+
+      <ul className={css(style.ul)}>
         <h4 style={{ margin: 0 }}>Smart Tags</h4>
-        {others !== null && others.map(f => (
-          <li>
+        {SmartTagField.map(f => (
+          <li className={css(style.li)}>
             <button
               type="button"
               className={`${css(style.button)} btnHover`}
@@ -82,7 +66,14 @@ const style = {
     h: 300,
     ow: 'scroll',
   },
+  ul: { mt: 10 },
+  li: {
+    mb: 0,
+    mt: 5,
+    ml: 5,
+  },
   button: {
+    fw: 'normal',
     dy: 'block',
     w: '100%',
     ta: 'left',
