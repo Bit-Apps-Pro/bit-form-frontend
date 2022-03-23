2ef12b2bd77956017570cc37318859301d9b73d1
mdabbas-cse <gmabbas44@gmail.com>
2022-03-23 19:18:31 +0600

    Update: Change markup and styling Razorpay field

--- src/components/Fields/Razorpay/RazorpayField.jsx
+++ src/components/Fields/Razorpay/RazorpayField.jsx
@@ -69,7 +69,17 @@ export default function RazorpayField({ fieldKey, formID, attr, isBuilder, style
       >
         <div className="bf-form">
           <div ref={razorpayElemntRef} className={`${fieldKey}-razorpay-wrp`}>
-            <button type="button" className={`${fieldKey}-razorpay-btn`}>Pay with Razorpay</button>
+            {/* <button type="button" className={`${fieldKey}-razorpay-btn`}>Pay with Razorpay</button> */}
+            <button type="button" className={`${fieldKey}-razorpay-btn`}>
+              <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7.077 6.476l-.988 3.569 5.65-3.589-3.695 13.54 3.752.004 5.457-20L7.077 6.476z" fill="#fff" />
+                <path d="M1.455 14.308L0 20h7.202L10.149 8.42l-8.694 5.887z" fill="#fff" />
+              </svg>
+              <div className={`${fieldKey}-btn-text`}>
+                <span className={`${fieldKey}-btn-title`}>Pay Now</span>
+                <span className={`${fieldKey}-btn-sub-title`}>Secured by Razorpay</span>
+              </div>
+            </button>
           </div>
         </div>
       </InputWrapper>
