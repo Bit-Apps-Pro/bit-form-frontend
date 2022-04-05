12e76c7801aedfda4d4f394257c2303fd3bee80d
mdrubelamin2 <mdrubelamin2@gmail.com>
2022-04-05 15:52:28 +0600

    update: style layer highlight selector fix

--- src/components/LeftBars/StyleLayers.jsx
+++ src/components/LeftBars/StyleLayers.jsx
@@ -35,8 +35,8 @@ export default function StyleLayers() {
         <div className={css(s.scrollDiv)}>
           <h5 className={css(s.subtitle, ut.mt1, ut.fontH)}>Common Elements</h5>
           <NavBtn route="quick-tweaks" label={<span className={css({ fw: 500 })}>Theme Quick Tweaks</span>} icn={<TweaksIcn size={13} />} />
-          <NavBtn route="form-wrappers" label="Form Wrappers" highlightSelector="[data-dev-fld-wrp]" offset="3" />
-          <NavBtn route="form-containers" label="Form Containers" highlightSelector="[data-dev-fld-wrp]" offset="3" />
+          <NavBtn route="form-wrappers" label="Form Wrapper" highlightSelector="[data-dev-frm-bg]" offset="3" />
+          <NavBtn route="form-containers" label="Form Container" highlightSelector="[data-dev-frm-wrp]" offset="3" />
           <NavBtn route="field-containers" label="Field Containers" highlightSelector="[data-dev-fld-wrp]" offset="3" />
           <NavBtn route="label-containers" label="Label Containers" offset="3" highlightSelector="[data-dev-lbl-wrp]" />
 
