b805bdbf48c0ef352915f46fc8b163b51174b340
mdrubelamin2 <mdrubelamin2@gmail.com>
2022-04-10 13:15:38 +0600

    update: recaptcha onload recalculate fix [temprory]

--- src/components/Fields/ReCaptchaV2/ReCaptchaV2.jsx
+++ src/components/Fields/ReCaptchaV2/ReCaptchaV2.jsx
@@ -1,52 +1,78 @@
 import { useEffect, useRef } from 'react'
-import { useRecoilValue, useSetRecoilState } from 'recoil'
-import { $bits, $breakpoint, $builderHookStates, $fields, $flags } from '../../../GlobalStates/GlobalStates'
+import { useRecoilValue } from 'recoil'
+import { $bits, $breakpoint, $fields, $flags } from '../../../GlobalStates/GlobalStates'
+import { reCalculateFieldHeights } from '../../../Utils/FormBuilderHelper'
 import { loadScript, selectInGrid } from '../../../Utils/globalHelpers'
 import RenderStyle from '../../style-new/RenderStyle'
 import RecaptchaField from './re-captchaV2'
 
+export const onLoadCallback = () => {
+  console.log('on load complete')
+}
+
 export default function ReCaptchaV2({ fieldKey, formId, styleClasses }) {
   const bits = useRecoilValue($bits)
   const recaptchaWrapElmRef = useRef(null)
+  const grecaptchaElm = useRef(null)
   const recaptchaFieldRef = useRef(null)
   const fields = useRecoilValue($fields)
   const fieldData = fields[fieldKey]
   const breakpoint = useRecoilValue($breakpoint)
   const { styleMode } = useRecoilValue($flags)
-  const setBuilderHookState = useSetRecoilState($builderHookStates)
   const isHidden = fieldData.hidden?.includes(breakpoint) || false
+  const recaptchaId = useRef(null)
 
-  const setBuilderFldWrpHeight = () => {
-    setBuilderHookState(olds => ({ ...olds, reCalculateSpecificFldHeight: { fieldKey, counter: olds.reCalculateSpecificFldHeight.counter + 1 } }))
+  function onloadCallback() {
+    const iframeWindow = document.getElementById('bit-grid-layout').contentWindow
+    const recaptcha = iframeWindow.grecaptcha
+    recaptchaId.current = recaptcha.render(grecaptchaElm.current, {
+      sitekey: bits?.allFormSettings?.gReCaptcha?.siteKey,
+      theme: fieldData.theme,
+    })
+    reCalculateFieldHeights(fieldKey)
   }
 
   useEffect(() => {
     if (!recaptchaWrapElmRef?.current) {
       recaptchaWrapElmRef.current = selectInGrid(`.${fieldKey}-recaptcha-wrp`)
     }
-    // const fldConstructor = recaptchaFieldRef.current
+    const fldConstructor = recaptchaFieldRef.current
     const fldElm = recaptchaWrapElmRef.current
-    // if (fldConstructor && fldElm && 'destroy' in fldConstructor) {
-    //   fldConstructor.destroy()
-    // }
+    if (fldConstructor && fldElm && 'destroy' in fldConstructor) {
+      fldConstructor.destroy()
+    }
+
+    const iframeWindow = document.getElementById('bit-grid-layout').contentWindow
+    if (recaptchaId.current !== null) {
+      iframeWindow.grecaptcha.reset(recaptchaId.current)
+    }
+
+    iframeWindow.onloadCallback = onloadCallback
+
+    const src = 'https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit'
+    const srcData = {
+      src,
+      integrity: '',
+      id: 'bf-recaptcha-script',
+      scriptInGrid: true,
+      attr: {
+        async: true,
+        defer: true,
+      }
+    }
+
+    loadScript(srcData)
+
 
     const { theme, size } = fieldData.config
     const configOptions = {
       theme,
       size,
     }
-    recaptchaFieldRef.current = new RecaptchaField(fldElm, configOptions)
-    const src = 'https://www.google.com/recaptcha/api.js'
-    const srcData = {
-      src,
-      integrity: '',
-      id: 'bf-recaptcha-script',
-      scriptInGrid: true,
-    }
 
-    loadScript(srcData).then(() => {
-      setBuilderFldWrpHeight()
-    })
+    recaptchaFieldRef.current = new RecaptchaField(fldElm, configOptions)
+
+
   }, [fieldData])
 
   return (
@@ -54,7 +80,7 @@ export default function ReCaptchaV2({ fieldKey, formId, styleClasses }) {
       <RenderStyle styleClasses={styleClasses} />
       <div data-dev-fld-wrp={fieldKey} className={`${fieldKey}-recaptcha-container ${fieldKey}-fld-wrp ${styleMode ? '' : 'drag'} ${isHidden ? 'fld-hide' : ''}`}>
         <div className={`${fieldKey}-recaptcha-wrp`} ref={recaptchaWrapElmRef}>
-          <div className="g-recaptcha" data-sitekey={bits?.allFormSettings?.gReCaptcha?.siteKey} />
+          <div className="g-recaptcha" ref={grecaptchaElm} />
         </div>
       </div>
       {/* <script src="https://www.google.com/recaptcha/api.js" /> */}
