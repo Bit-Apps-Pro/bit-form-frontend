836842d7af433f9701e66d191246784d3a17c1d6
mdrubelamin2 <mdrubelamin2@gmail.com>
2022-04-11 13:13:05 +0600

    fix: optiontoolbar rightpanel default activate issue solve

--- src/components/OptionToolBar.jsx
+++ src/components/OptionToolBar.jsx
@@ -89,6 +89,15 @@ export default function OptionToolBar({ setResponsiveView, setShowToolbar, showT
     setFlags(prvFlags => ({ ...prvFlags, inspectMode: !prvFlags.inspectMode }))
   }
 
+  const handleRightPanelDefaultActive = () => {
+    if (rightBar.match(/fields-list|field-settings/)) {
+      return 'fld-settings'
+    }
+    if (rightBar.match(/themes|style|field-theme-customize|theme-customize/)) {
+      return 'theme-customize'
+    }
+  }
+
   return (
     <div className={css(OptionToolBarStyle.optionToolBar)}>
       <div className={css(OptionToolBarStyle.form_section)}>
@@ -184,7 +193,7 @@ export default function OptionToolBar({ setResponsiveView, setShowToolbar, showT
               width={180}
               show={['icn']}
               tipPlace="bottom"
-              defaultActive={rightBar.match(/themes|theme-customize/gi) && 'theme-customize'}
+              defaultActive={handleRightPanelDefaultActive()}
               options={[
                 { icn: <EditIcn size="19" />, label: 'fld-settings', tip: 'Field Settings' },
                 { icn: <BrushIcn size="15" />, label: 'theme-customize', tip: 'Theme Customization' },
