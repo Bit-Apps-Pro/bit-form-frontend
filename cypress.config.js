10a6743aa0dc59b8b7db0891bf572e181473982e
Md RH Maruf <mdrobiulhasan2620@gmail.com>
2022-06-06 11:41:18 +0600

    Modify: Cypress config file change

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,11 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   waitForAnimations: true,
   animationDistanceThreshold: 50,
-  env: { formNo: 13 },
+  env: {
+    formNo: 13,
+    wpUserName: 'maruf',
+    wpPassword: '123',
+  },
   viewportWidth: 1366,
   viewportHeight: 786,
   defaultCommandTimeout: 5000,
@@ -11,5 +15,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://bitform.mr/wp-admin/admin.php?page=bitform#',
+    experimentalSessionAndOrigin: false,
   },
 })
