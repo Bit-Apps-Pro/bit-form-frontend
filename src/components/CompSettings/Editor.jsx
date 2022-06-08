8d08e3b76b98a645eb7637b648b2bcbd31a35f9d
mdrubelamin2 <mdrubelamin2@gmail.com>
2022-06-08 18:11:41 +0600

    new: predefined snippets list added for code editor

--- src/components/CompSettings/Editor.jsx
+++ src/components/CompSettings/Editor.jsx
@@ -1,39 +1,84 @@
 /* eslint-disable react/jsx-no-useless-fragment */
-import { useState } from 'react'
-import { useFela } from 'react-fela'
-import AceEditor from 'react-ace'
-import StyleSegmentControl from '../Utilities/StyleSegmentControl'
-import Grow from './StyleCustomize/ChildComp/Grow'
-import SingleToggle from '../Utilities/SingleToggle'
-import ut from '../../styles/2.utilities'
-import { __ } from '../../Utils/i18nwrap'
-
-import 'ace-builds/src-min-noconflict/ext-language_tools'
-import 'ace-builds/src-min-noconflict/mode-javascript'
-import 'ace-builds/src-min-noconflict/snippets/javascript'
+import 'ace-builds'
+// modes
 import 'ace-builds/src-min-noconflict/mode-css'
+import 'ace-builds/src-min-noconflict/mode-javascript'
+// snippets
 import 'ace-builds/src-min-noconflict/snippets/css'
+import 'ace-builds/src-min-noconflict/snippets/javascript'
+// themes
 import 'ace-builds/src-min-noconflict/theme-tomorrow'
 import 'ace-builds/src-min-noconflict/theme-twilight'
+// extensions
+import 'ace-builds/src-min-noconflict/ext-language_tools'
+import 'ace-builds/src-noconflict/ext-beautify'
+import 'ace-builds/webpack-resolver'
+import { useRef, useState } from 'react'
+import AceEditor from 'react-ace'
+import { useFela } from 'react-fela'
+import BdrDottedIcn from '../../Icons/BdrDottedIcn'
+import ut from '../../styles/2.utilities'
+import { __ } from '../../Utils/i18nwrap'
+import { jsPredefinedCodeList } from '../../Utils/StaticData/predefinedCodeList'
+import CheckBoxMini from '../Utilities/CheckBoxMini'
+import Downmenu from '../Utilities/Downmenu'
+import ListGroup from '../Utilities/ListGroup'
+import SimpleDropdown from '../Utilities/SimpleDropdown'
+import StyleSegmentControl from '../Utilities/StyleSegmentControl'
+import Grow from './StyleCustomize/ChildComp/Grow'
 
 function Editor() {
   const { css } = useFela()
-  const [editorTab, setEditorTab] = useState('JS')
+  const [editorTab, setEditorTab] = useState('JavaScript')
   const [tab, setTab] = useState('Custom Code')
-  const [theme, setTheme] = useState(localStorage.getItem('bf-editor-theme') || 'twilight')
+  const [theme, setTheme] = useState(localStorage.getItem('bf-editor-theme') || 'tomorrow')
   const [jsCode, setJsCode] = useState('')
   const [cssCode, setCssCode] = useState('')
   const [enableEditor, setEnableEditor] = useState(localStorage.getItem('bf-enable-editor') || 'on')
+  const codeEditorRef = useRef(null)
 
   const options = {
+    autoScrollEditorIntoView: true,
     enableBasicAutocompletion: true,
     enableLiveAutocompletion: true,
     enableSnippets: true,
     showLineNumbers: true,
     tabSize: 2,
+    animatedScroll: true,
+    showFoldWidgets: true,
+    displayIndentGuides: true,
+    enableEmmet: true,
+    enableMultiselect: true,
+    highlightSelectedWord: true,
+    fontSize: 16,
+    useSoftTabs: true,
+    showPrintMargin: true,
+    showGutter: true,
+    highlightActiveLine: true,
+    wrapEnabled: false,
   }
 
-  const themeSetLocalStorage = ({ target: { value } }) => {
+  const [editorOptions, setEditorOptions] = useState(options)
+
+  const rTabs = (str, spaceToRemove = 8) => str.trim().replace(new RegExp(`^ {${spaceToRemove}}`, 'gm'), '')
+
+  const handlePredefinedCode = val => {
+    // trim the val from beginning and end
+    const jsCode = val
+    // put the jsCode into the editor where the cursor is & store it in the state
+    const editor = codeEditorRef.current.editor
+    editor.session.insert(editor.getCursorPosition(), jsCode)
+    setJsCode(editor.getValue())
+
+    // go to end of line if scroll to end is available
+    if (codeEditorRef.current.editor.renderer.scrollBarV.scrollTop !== codeEditorRef.current.editor.renderer.scrollBarV.maxScrollTop) {
+      codeEditorRef.current.editor.gotoLine(codeEditorRef.current.editor.session.getLength() + 1)
+    }
+  }
+
+  console.log('jsocode', jsCode);
+
+  const themeSetLocalStorage = value => {
     localStorage.setItem('bf-editor-theme', value)
     setTheme(value)
   }
@@ -48,6 +93,16 @@ function Editor() {
       localStorage.setItem('bf-enable-editor', 'off')
     }
   }
+
+  const saveCode = e => {
+    e.preventDefault()
+
+  }
+
+  const themesList = [
+    { label: 'Light Theme', value: 'tomorrow' },
+    { label: 'Dark Theme', value: 'twilight' },
+  ]
 
   return (
     <div>
@@ -64,50 +119,48 @@ function Editor() {
         <>
           <div className={css({ flx: 'between' })}>
             <StyleSegmentControl
-              className={css(ut.w5)}
-              options={[{ label: 'JS' }, { label: 'CSS' }]}
+              className={css(ut.w5, ut.mb2)}
+              options={[{ label: 'JavaScript' }, { label: 'CSS' }]}
               onChange={lbl => setEditorTab(lbl)}
-              defaultActive="JS"
+              defaultActive="JavaScript"
               actionValue={editorTab}
               wideTab
             />
-            <div className={css(ut.flxc, ut.w10, style.editorBtn)}>
-              {enableEditor === 'on' && (
-                <>
-                  <span className={css(ut.mr1)}>{__(' Editor Theme', 'bitform')}</span>
-                  <div className={css(style.theme)}>
-                    <select className={css(style.select)} onChange={themeSetLocalStorage} value={theme}>
-                      <option>Theme Select</option>
-                      <option value="twilight">Twilight</option>
-                      <option value="tomorrow">Tomorrow</option>
-                    </select>
-                  </div>
-                </>
-              )}
-              <span className={css(ut.mr1, ut.ml2)}>{__(' Enable Editor', 'bitform')}</span>
-              <SingleToggle
-                isChecked={enableEditor === 'on'}
-                action={editorHandler}
-              />
+            <div className={css(ut.flxc)}>
+              {/* <span>add snippets</span> */}
+              <Downmenu place='bottom-end'>
+                <button
+                  data-testid="titl-mor-opt-btn"
+                  data-close
+                  type="button"
+                  className={css(style.btn)}
+                  unselectable="on"
+                  draggable="false"
+                  style={{ cursor: 'pointer' }}
+                  title={__('Snippets', 'bitform')}
+                >
+                  <BdrDottedIcn size="16" />
+                </button>
+                <ListGroup options={jsPredefinedCodeList} action={handlePredefinedCode} />
+              </Downmenu>
             </div>
           </div>
 
-          <Grow open={editorTab === 'JS'}>
+          <Grow open={editorTab === 'JavaScript'}>
             {enableEditor === 'on' ? (
               <AceEditor
+                ref={codeEditorRef}
                 height="330px"
                 width="99%"
-                placeholder=""
+                placeholder="Write your code here..."
                 mode="javascript"
                 theme={theme}
                 onChange={(newValue) => setJsCode(newValue)}
-                fontSize={14}
-                showPrintMargin
-                showGutter
-                highlightActiveLine
-                className={css(style.editor)}
                 value={jsCode}
-                setOptions={options}
+                onLoad={(editor) => {
+                  editor.session.$worker.send('changeOptions', [{ asi: true }])
+                }}
+                setOptions={editorOptions}
               />
             ) : (
               <div>
@@ -131,12 +184,8 @@ function Editor() {
                 theme={theme}
                 className={css(style.editor)}
                 onChange={(newValue) => setCssCode(newValue)}
-                fontSize={14}
-                showPrintMargin
-                showGutter
-                highlightActiveLine
                 value={cssCode}
-                setOptions={options}
+                setOptions={editorOptions}
               />
             ) : (
               <div>
@@ -151,8 +200,17 @@ function Editor() {
           </Grow>
         </>
       </Grow>
-      <div className={css(ut.flxb, ut.mt1, { jc: 'end' })}>
-        <button type="button" className={css(ut.btn, style.saveBtn)}>Save</button>
+      <div className={css(ut.flxb, ut.mt1, { jc: 'between' })}>
+        <div className={css(ut.flxc, ut.w10, style.editorBtn)}>
+          <CheckBoxMini title="Editor Mode" checked={enableEditor === 'on'} onChange={editorHandler} />
+          {enableEditor === 'on' && (
+            <>
+              <SimpleDropdown options={themesList} placeholder="Theme" onChange={themeSetLocalStorage} value={theme} cls={css(ut.ml2)} w={130} />
+              <CheckBoxMini className={css(ut.ml4)} title="Word Wrap" checked={editorOptions.wrap} onChange={() => setEditorOptions(oldOptions => ({ ...oldOptions, wrap: !oldOptions.wrap }))} />
+            </>
+          )}
+        </div>
+        <button onClick={saveCode} type="button" className={css(ut.btn, style.saveBtn)}>Save</button>
       </div>
     </div>
   )
@@ -160,9 +218,13 @@ function Editor() {
 
 const style = {
   editor: {
-    brs: 8,
-    mt: 6,
     w: '99%',
+  },
+  btn: {
+    b: 0,
+    brs: 5,
+    curp: 1,
+    flx: 'center-between',
   },
   theme: { dy: 'flex', jc: 'flex-end' },
   select: {
@@ -186,16 +248,16 @@ const style = {
     ':focus': { bs: '0 0 0 2px var(--b-50) !important' },
   },
   editorBtn: {
-    jc: 'end',
     fs: 12,
     pr: 5,
   },
   saveBtn: {
     bc: 'var(--b-50)',
     brs: 8,
-    fs: 12,
+    fs: 13,
+    fw: 800,
     px: 15,
-    py: 5,
+    py: 8,
     mr: 8,
     cr: 'var(--white-100)',
     ':hover': { bd: 'var(--b-36)' },
