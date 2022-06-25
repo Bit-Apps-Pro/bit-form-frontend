7da693657494fb4e4dd36260e00c86bf40f18d49
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2022-06-25 10:17:00 +0600

    :hammer:  Refactor code

--- src/components/Report/helper.jsx
+++ src/components/Report/helper.jsx
@@ -83,8 +83,8 @@ export const getLogicOptionByFieldType = (type, fields, fieldKey) => {
       title: '',
       type: 'group',
       childs: [
-        { label: __('Equal'), value: '=' },
-        { label: __('Not equal'), value: '!=' },
+        { label: __('Equal'), value: 'equal' },
+        { label: __('Not equal'), value: 'not_equal' },
         { label: __('Empty'), value: 'empty' },
         { label: __('Not empty'), value: 'not_empty' },
       ],
@@ -177,56 +177,3 @@ export const stringToArray = (type, val) => {
   return val
 }
 
-{ /* <MtSelect
-        label="Logic"
-        value={logicValue || ''}
-        onChange={e => changeLogic(e.target.value, lgcInd, subLgcInd, subSubLgcInd)}
-        className="w-5"
-      >
-        <option value="">{__('Select One')}</option>
-        <option value="=">{__('Equal')}</option>
-        <option value="!=">{__('Not Equal')}</option>
-        <option value="empty">{__('Is Empty')}</option>
-        <option value="not_empty">{__('Is Not Empty')}</option>
-        {!type.match(/^(number|date|time|datetime|month|week)$/) && <option value="contain">{__('Contain')}</option>}
-
-        {((fldType === 'select' && fields?.[fieldKey]?.mul) || fldType === 'check') && <option value="contain_all">{__('Contain All')}</option>}
-        {!type.match(/^(number|date|time|datetime|month|week)$/) && <option value="not_contain">{__('Not Contain')}</option>}
-        {type.match(/^(number|date|time|datetime|month|week)$/) && <option value=">">{__('Greater Than')}</option>}
-        {type.match(/^(number|date|time|datetime|month|week)$/) && <option value="<">{__('Less Than')}</option>}
-        {type.match(/^(number|date|time|datetime|month|week)$/) && <option value=">=">{__('Greater Than or Equal')}</option>}
-        {type.match(/^(number|date|time|datetime|month|week)$/) && <option value="<=">{__('Less Than or Equal')}</option>}
-        {!type.match(/^(number|color|url|password|email|date|time|datetime|month|week)$/) && <option value="start_with">{__('Start With')}</option>}
-        {!type.match(/^(number|color|url|password|email|date|time|datetime|month|week)$/) && <option value="end_with">{__('End With')}</option>}
-        {!type.match(/^(number|color|url|password|email|date|time|datetime|month|week)$/) && <option value="not_start_with">{__('Not Start With')}</option>}
-        {!type.match(/^(number|color|url|password|email|date|time|datetime|month|week)$/) && <option value="not_end_with">{__('Not End With')}</option>}
-
-        {type.match(/^(date|time|datetime)$/) && (
-          <>
-            <optgroup label={__('Days')}>
-              <option value="today">{__('Today')}</option>
-              <option value="till_today">{__('Till Today')}</option>
-              <option value="tomorrow">{__('Tomorrow')}</option>
-              <option value="till_tommorrow">{__('Till Tomorrow')}</option>
-              <option value="yesterday">{__('Yesterday')}</option>
-              <option value="till_yesterday">{__('Till Yesterday')}</option>
-            </optgroup>
-            <optgroup label={__('Month')}>
-              <option value="current_month">{__('Current Month')}</option>
-              <option value="next_month">{__('Next Month')}</option>
-              <option value="last_month">{__('Last Month')}</option>
-            </optgroup>
-            <optgroup label={__('Week')}>
-              <option value="current_week">{__('Current Week')}</option>
-              <option value="next_week">{__('Next Week')}</option>
-              <option value="last_week">{__('Last Week')}</option>
-            </optgroup>
-            <optgroup label={__('Year')}>
-              <option value="current_year">{__('Current Year')}</option>
-              <option value="next_year">{__('Next Year')}</option>
-              <option value="last_year">{__('Last Year')}</option>
-            </optgroup>
-          </>
-        )}
-
-      </MtSelect> */ }
