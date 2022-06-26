3af57b8d46d727e2e9261a21bdf6b1de8a122fc0
mdrubelamin2 <mdrubelamin2@gmail.com>
2022-06-26 17:27:27 +0600

    feat: singletoggle2 name & id props

--- src/components/Utilities/SingleToggle2.jsx
+++ src/components/Utilities/SingleToggle2.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-export default function SingleToggle2({ className, title, action, disabled, checked }) {
+export default function SingleToggle2({ className, name, title, action, disabled, checked }) {
   const onChange = (e) => {
     if (!disabled) {
       action(e)
@@ -10,7 +10,7 @@ export default function SingleToggle2({ className, title, action, disabled, chec
       <span>{title}</span>
       <label className="btcd-label">
         <div className="btcd-toggle">
-          <input aria-label={title} onChange={onChange} className="btcd-toggle-state" type="checkbox" name="check" value="check" checked={checked} />
+          <input aria-label={title} onChange={onChange} className="btcd-toggle-state" type="checkbox" name={name} id={name} value="check" checked={checked} />
           <div className="btcd-toggle-inner">
             <div className="btcd-indicator" />
           </div>
