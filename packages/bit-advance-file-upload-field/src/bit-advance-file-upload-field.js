177e8b349ff1f3aa3810d2af372bf08486a9b121
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2022-08-14 12:40:21 +0600

    updated advance file upload js

--- packages/bit-advance-file-upload-field/src/bit-advance-file-upload-field.js
+++ packages/bit-advance-file-upload-field/src/bit-advance-file-upload-field.js
@@ -37,46 +37,74 @@ export default class BitAdvanceFileUpload {
     this.#ajaxURL = this.#config.ajaxURL
     this.#nonce = this.#config.nonce
     this.#uploadFileToServer = this.#config.uploadFileToServer
+    this.files = []
+    this.uploaded_files = []
+    this.on_select_upload = false
 
     this.init()
   }
 
   init() {
     const plugins = []
-    if (this.#config.allowFileSizeValidation) plugins.push(FilePondPluginFileValidateSize)
-    if (this.#config.allowFileTypeValidation) plugins.push(FilePondPluginFileValidateType)
-    if (this.#config.allowImageCrop) plugins.push(FilePondPluginImageCrop)
-    if (this.#config.allowImagePreview) plugins.push(FilePondPluginImagePreview)
-    if (this.#config.allowImageResize) plugins.push(FilePondPluginImageResize)
-    if (this.#config.allowImageTransform) plugins.push(FilePondPluginImageTransform)
-    if (this.#config.allowImageValidateSize) plugins.push(FilePondPluginImageValidateSize)
-    if (this.#config.allowPreview) plugins.push(FilePondPluginMediaPreview)
+    if (this.#configSetting.allowFileSizeValidation) {
+      plugins.push(bit_filepond_plugin_file_validate_size)
+    }
+    if (this.#configSetting.allowFileTypeValidation) {
+      plugins.push(bit_filepond_plugin_file_validate_type)
+    }
+    if (this.#configSetting.allowImageCrop) {
+      plugins.push(bit_filepond_plugin_image_crop)
+    }
+    if (this.#configSetting.allowImagePreview) {
+      plugins.push(bit_filepond_plugin_image_preview)
+    }
+    if (this.#configSetting.allowImageResize) {
+      plugins.push(bit_filepond_plugin_image_resize)
+    }
+    if (this.#configSetting.allowImageTransform) {
+      plugins.push(bit_filepond_plugin_image_transform)
+    }
+    if (this.#configSetting.allowImageValidateSize) {
+      plugins.push(bit_filepond_plugin_image_validate_size)
+    }
+    if (this.#configSetting.allowPreview) {
+      plugins.push(bit_filepond_plugin_media_preview)
+    }
 
     registerPlugin(...plugins)
 
     this.#filePondRef = create(this.#configSetting)
     this.#fieldUploadWrapper.appendChild(this.#filePondRef.element)
-    if (this.#config.onFileUpdate) this.#filePondRef.on('updatefiles', this.#config.onFileUpdate)
+    if (this.#config.onFileUpdate) {
+      this.#filePondRef.on('updatefiles', this.#config.onFileUpdate)
+    }
 
     // TODO - config e uploadFileToServer jodi true ashe, tahole upload hobe, nahole hobena
     if (this.#uploadFileToServer) {
       const uri = new URL(this.#ajaxURL)
-      uri.searchParams.append('action', 'bitforms_file_store')
+      this.on_select_upload = this.#configSetting.instantUpload
       uri.searchParams.append('_ajax_nonce', this.#nonce)
 
-      // TODO - config e uploadFileToServer jodi true ashe, tahole upload hobe, nahole hobena
-      const removeFile = new URL(this.#ajaxURL)
-      removeFile.searchParams.append('action', 'bitforms_file_remove')
-      removeFile.searchParams.append('_ajax_nonce', this.#nonce)
-      removeFile.searchParams.append('form_id', this.#formID)
-
-      setOptions({
+      this.#filePondRef.setOptions({
         // TODO - config e uploadFileToServer jodi true ashe, tahole upload hobe, nahole hobena
         server: {
-          process: (fieldName, file, metadata, load, error, progress, abort) => {
+          process: (
+            fieldName,
+            file,
+            metadata,
+            load,
+            error,
+            progress,
+            abort,
+          ) => {
             const formData = new FormData()
+
+            if (uri.searchParams.has('action')) {
+              uri.searchParams.delete('action')
+            }
+            uri.searchParams.append('action', 'bitforms_file_upload')
+
             formData.append(`${fieldName}`, file, file.name)
-            formData.append('form_id', this.#formID)
             const request = new XMLHttpRequest()
             request.open('POST', uri.href)
             request.upload.onprogress = (e) => {
@@ -85,7 +113,8 @@ export default class BitAdvanceFileUpload {
             request.onload = () => {
               if (request.status >= 200 && request.status < 300) {
                 const response = JSON.parse(request.responseText)
-                load(response.data)
+                this.uploaded_files.push(response?.data?.file_name)
+                load(response?.data?.file_name)
               } else {
                 error('oh no')
               }
@@ -100,13 +129,49 @@ export default class BitAdvanceFileUpload {
               },
             }
           },
-          revert: removeFile.href,
+          revert: (uniqueFileId, load, error) => {
+            const existFileId = this.uploaded_files.find(
+              (file) => file === uniqueFileId,
+            )
+            if (existFileId) {
+              const fileIndex = this.uploaded_files.indexOf(uniqueFileId)
+              this.uploaded_files.splice(fileIndex, 1)
+              if (uri.searchParams.has('action')) {
+                uri.searchParams.delete('action')
+              }
+              uri.searchParams.append('action', 'bitforms_file_delete')
+
+              const xhr = new XMLHttpRequest()
+              xhr.open('DELETE', uri.href)
+              xhr.onload = () => {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                  load()
+                } else {
+                  error()
+                }
+              }
+              xhr.send()
+              return {
+                abort: () => {
+                  xhr.abort()
+                },
+              }
+            }
+          },
         },
       })
+
+      this.#filePondRef.on('updatefiles', () => {
+        this.files = []
+        this.files = this.#filePondRef.getFiles()
+      })
     }
+
+    // this.#filePondRef.destroy(this.#filePondRef.element)
   }
 
   reset() {
     this.init()
+    this.#filePondRef.destroy(this.#filePondRef.element)
   }
 }
