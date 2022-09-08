0272f7ba37edc62967869c53d81a9fd079eaa3fe
Arif Uddin <arifunctg@gmail.com>
2022-09-08 14:33:15 +0600

    style : remove global var from divider style & set individual style

--- src/components/style-new/componentsStyleByTheme/1_bitformDefault/dividerStyle_1_bitformDefault.js
+++ src/components/style-new/componentsStyleByTheme/1_bitformDefault/dividerStyle_1_bitformDefault.js
@@ -5,19 +5,14 @@ export default function dividerStyle_1_bitformDefault({ fk, breakpoint, colorSch
       [`.${fk}-fld-wrp`]: {
         display: 'flex',
         height: '100%',
-        // 'justify-content': 'center', // unused css
-        // width: '100%', // unused css
-        // padding: '10px 0px', // unused css
         'align-items': 'center',
       },
       [`.${fk}-divider`]: {
-        margin: '0px 10px 0px 10px',
+        margin: '15px 10px 15px 10px',
         width: '100%',
-        // border: '',
-        'border-bottom': 'var(--global-fld-bdr)',
-        'border-color': 'var(--global-fld-bdr-clr)',
+        'border-bottom': 'solid',
+        'border-color': 'hsla(0, 0%, 67%, 100)',
         'border-width': '1px !important',
-        // 'border-color': 'hsla(0, 0%, 62%, 100%)',
       },
     }
   }
