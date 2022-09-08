d54019d8ede4efb5368ceb5db9b88ce141e28b4a
Arif Uddin <arifunctg@gmail.com>
2022-09-08 17:00:49 +0600

    icon : update

--- src/Icons/ColorPickerIcn.jsx
+++ src/Icons/ColorPickerIcn.jsx
@@ -1,8 +1,10 @@
 export default function ColorPickerIcn({ w }) {
   return (
-    <svg width={w} xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 215 215">
-      <rect width="140.1" height="140.1" x="37.5" y="39.6" fill="none" stroke="currentColor" strokeMiterlimit="10" strokeWidth="16" rx="30" transform="rotate(-45 107.5 109.6)" />
-      <path fill="currentColor" d="M107.5 62.7l3.4 4.7a236.9 236.9 0 0121.6 36.8 38 38 0 013.7 15.5 26.9 26.9 0 01-20.1 26.8 29 29 0 01-29.6-7.4c-6.3-6.2-8.3-14-7.6-22.5.6-6 3.2-11.5 5.8-16.8a244.7 244.7 0 0122.8-37z" />
+    <svg width={w} height={w} viewBox="0 0 24 24">
+      <g fill="currentColor">
+        <path d="M20.385 2.879a3 3 0 0 0-4.243 0L14.02 5l-.707-.708a1 1 0 1 0-1.414 1.415l5.657 5.656A1 1 0 0 0 18.97 9.95l-.707-.707l2.122-2.122a3 3 0 0 0 0-4.242Z" />
+        <path fillRule="evenodd" d="M11.93 7.091L4.152 14.87a3.001 3.001 0 0 0-.587 3.415L2 19.85l1.414 1.415l1.565-1.566a3.001 3.001 0 0 0 3.415-.586l7.778-7.778L11.93 7.09Zm1.414 4.243L11.93 9.92l-6.364 6.364a1 1 0 0 0 1.414 1.414l6.364-6.364Z" clipRule="evenodd" />
+      </g>
     </svg>
   )
 }
