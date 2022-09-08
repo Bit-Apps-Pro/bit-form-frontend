dda2ba5e91562327f14aae1ee359508a54202cd4
Arif Uddin <arifunctg@gmail.com>
2022-09-08 15:21:53 +0600

    icon : btn icon updated and new icon added

---
+++ src/Icons/InspectIcn.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable max-len */
+export default function InspectIcn({ size }) {
+  return (
+    <svg width={size} height={size} viewBox="0 0 24 24">
+      <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
+        <path d="M21 11V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h6" />
+        <path d="m12 12l4 10l1.7-4.3L22 16Z" />
+      </g>
+    </svg>
+  )
+}
