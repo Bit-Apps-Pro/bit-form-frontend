426609b203e7582e191a0df8d61f5c540de87be7
Md RH Maruf <mdrobiulhasan2620@gmail.com>
2022-09-12 12:51:48 +0600

    Some Required property added

--- src/components/Utilities/MtInput.jsx
+++ src/components/Utilities/MtInput.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
-function MtInput({ label, onChange, value, disabled, type, textarea, className }) {
+function MtInput({ label, onChange, onKeyUp, onClick, value, disabled, type, textarea, className }) {
   return (
     <label className={`btcd-mt-inp ${className}`}>
-      {!textarea && <input type={type === undefined ? 'text' : type} onChange={onChange} placeholder=" " disabled={disabled} value={value} />}
-      {textarea && <textarea type={type === undefined ? 'text' : type} onChange={onChange} placeholder=" " disabled={disabled} value={value} />}
+      {!textarea && <input type={type === undefined ? 'text' : type} onChange={onChange} onClick={onClick} onKeyUp={onKeyUp} placeholder=" " disabled={disabled} value={value} />}
+      {textarea && <textarea type={type === undefined ? 'text' : type} onChange={onChange} onClick={onClick} onKeyUp={onKeyUp} placeholder=" " disabled={disabled} value={value} />}
       <span>{label}</span>
     </label>
   )
