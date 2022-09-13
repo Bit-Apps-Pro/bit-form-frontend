346d2dfe971b631797c1f754aaa4206326c67b04
Arif Uddin <arifunctg@gmail.com>
2022-09-13 16:18:13 +0600

    Update InfoIcn.jsx

--- src/Icons/InfoIcn.jsx
+++ src/Icons/InfoIcn.jsx
@@ -1,34 +1,5 @@
 export default function InfoIcn({ size = 30, stroke = 2 }) {
   return (
-    <svg width={size} height={size} viewBox="0 0 30 30">
-      <ellipse
-        className="svg-icn"
-        strokeWidth={stroke}
-        strokeMiterlimit="10"
-        cx="15"
-        cy="15"
-        rx="11"
-        ry="11.09"
-      />
-      <ellipse
-        className="svg-icn"
-        strokeWidth={2}
-        strokeMiterlimit="10"
-        cx="15"
-        cy="11"
-        rx="1"
-        ry="1"
-      />
-      <line
-        className="svg-icn"
-        strokeWidth={stroke - 0.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        x1="15"
-        y1="18"
-        x2="15"
-        y2="21.72"
-      />
-    </svg>
+    <svg width={size} height={size} viewBox="0 0 24 24"><path fill="currentColor" d="M11 9h2V7h-2m1 13c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m0-18A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2m-1 15h2v-6h-2v6Z" /></svg>
   )
 }
