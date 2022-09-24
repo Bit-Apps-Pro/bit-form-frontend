e491b26f1aaf19743a133c9e15083fdd29ad86df
Arif Uddin <arifunctg@gmail.com>
2022-09-24 17:43:03 +0600

    refactor : move style in root component

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,8 @@
 /* eslint-disable react/jsx-filename-extension */
 // import 'react-app-polyfill/ie11'
 // import 'react-app-polyfill/stable'
-
+import './resource/sass/app.scss'
+import './resource/sass/global.scss'
 import { createRenderer } from 'fela'
 import customProperty from 'fela-plugin-custom-property'
 import ReactDOM from 'react-dom/client'
