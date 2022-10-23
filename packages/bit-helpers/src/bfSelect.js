72396c7bbc0beca2993a744af173a9dc5a905001
Md RH Maruf <mdrobiulhasan2620@gmail.com>
2022-10-23 12:38:50 +0600

    Refactor: select function name replace to 'bfSelect'

---
+++ packages/bit-helpers/src/bfSelect.js
@@ -0,0 +1,3 @@
+export default function bfSelect(selector, elm) {
+  return (elm || document).querySelector(selector)
+}
--- packages/bit-helpers/src/select.js
+++
@@ -1,3 +0,0 @@
-export default function select(selector) {
-  return document.querySelector(selector)
-}
