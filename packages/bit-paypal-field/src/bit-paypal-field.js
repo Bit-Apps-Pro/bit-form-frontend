d10dfc267598ca2218d4cd95a128c80fce261d60
Md RH Maruf <mdrobiulhasan2620@gmail.com>
2022-10-31 14:56:19 +0600

    Modify: Submit Form Data on payment click

--- packages/bit-paypal-field/src/bit-paypal-field.js
+++ packages/bit-paypal-field/src/bit-paypal-field.js
@@ -1,7 +1,9 @@
-const formSelector = '.bf-form'
-
 export default class BitPayPalField {
   #paypalWrpSelector = ''
+
+  #formSelector = ''
+
+  #responseData = null
 
   #config = {
     namespace: 'paypal',
@@ -22,6 +24,7 @@ export default class BitPayPalField {
     Object.assign(this.#config, config)
 
     this.#paypalWrpSelector = selector
+    this.#formSelector = `#form-${this.#getContentId()}`
 
     this.init()
   }
@@ -42,7 +45,7 @@ export default class BitPayPalField {
       btnProps.createOrder = (data, actions) => this.#createOrderHandler(data, actions)
     }
     btnProps.onClick = () => this.#handleOnClick(this.#getContentId())
-    btnProps.onApprove = (data, actions) => this.#onApproveHanlder(data, actions)
+    btnProps.onApprove = (data, actions) => this.#onApproveHandler(data, actions)
 
     if (this.#config.onInit) {
       btnProps.onInit = this.#config.onInit
@@ -63,12 +66,113 @@ export default class BitPayPalField {
   }
 
   #handleOnClick(contentId) {
-    return isFormValidatedWithoutError(contentId, handleFormValidationErrorMessages)
-      .then(() => true)
-      .catch(() => false)
+    const form = bfSelect(this.#formSelector)
+    if (
+      typeof validateForm !== 'undefined'
+      && !validateForm({ form: contentId })
+    ) {
+      const validationEvent = new CustomEvent('bf-form-validation-error', {
+        detail: { formId: contentId, fieldId: '', error: '' },
+      })
+      form.dispatchEvent(validationEvent)
+      return false
+    }
+    let update = false
+    let formData = new FormData(form)
+    if (this.#getEntryId()) {
+      update = true
+      formData.append('entryID', this.#getEntryId())
+    }
+    const props = window.bf_globals[contentId]
+
+    if (typeof advancedFileHandle !== 'undefined') {
+      formData = advancedFileHandle(props, formData)
+    }
+    if (props.GCLID) {
+      formData.set('GCLID', props.GCLID)
+    }
+
+    const hidden = []
+    Object.entries(props?.fields || {}).forEach((fld) => {
+      if (fld[1]?.valid?.hide) {
+        hidden.push(fld[0])
+      }
+    })
+
+    if (hidden.length) {
+      formData.append('hidden_fields', hidden)
+    }
+    if (props?.gRecaptchaVersion === 'v3' && props?.gRecaptchaSiteKey) {
+      grecaptcha.ready(() => {
+        grecaptcha
+          .execute(props.gRecaptchaSiteKey, { action: 'submit' })
+          .then((token) => {
+            formData.append('g-recaptcha-response', token)
+            const submitResp = this.#bfSubmitFetch(props?.ajaxURL, formData, update)
+            return this.#submitResponse(submitResp, contentId, formData)
+          })
+      })
+    } else {
+      const submitResp = this.#bfSubmitFetch(props?.ajaxURL, formData, update)
+      return this.#submitResponse(submitResp, contentId, formData)
+    }
   }
 
-  #onApproveHanlder(_, actions) {
+  #setEntryId(id) {
+    localStorage.setItem('bf-entry-id', id)
+  }
+
+  #getEntryId() {
+    return localStorage.getItem('bf-entry-id')
+  }
+
+  #bfSubmitFetch(ajaxURL, formData, update) {
+    const uri = new URL(ajaxURL)
+    uri.searchParams.append('action', update ? 'bitforms_entry_update' : 'bitforms_submit_form')
+    return fetch(uri, {
+      method: 'POST',
+      body: formData,
+    })
+  }
+
+  #submitResponse(resp, contentId, formData) {
+    resp
+      .then(
+        (response) => new Promise((resolve, reject) => {
+          if (response.staus > 400) {
+            const errorEvent = new CustomEvent('bf-form-submit-error', {
+              detail: { formId: contentId, errors: result.data },
+            })
+            bfSelect(`#form-${contentId}`).dispatchEvent(errorEvent)
+            response.staus === 500
+              ? reject(new Error('Mayebe Internal Server Error'))
+              : reject(response.json())
+          } else resolve(response.json())
+        }),
+      )
+      .then((result) => {
+        const successEvent = new CustomEvent('bf-form-submit-success', {
+          detail: { formId: contentId, entryId: result.entryId, formData },
+        })
+        bfSelect(`#form-${contentId}`).dispatchEvent(successEvent)
+        this.#responseData = result.data
+        if (result !== undefined && result.success) {
+          this.#setEntryId(result.data.entry_id)
+          return true
+        }
+        const errorEvent = new CustomEvent('bf-form-submit-error', {
+          detail: { formId: contentId, errors: result.data },
+        })
+        bfSelect(`#form-${contentId}`).dispatchEvent(errorEvent)
+        return false
+      })
+      .catch((error) => {
+        const err = error?.message ? error.message : 'Unknown Error'
+        return false
+      })
+  }
+
+  #onApproveHandler(_, actions) {
     const formParent = document.getElementById(`${this.#getContentId()}`)
     formParent.classList.add('pos-rel', 'form-loading')
     const order = this.#isSubscription() ? actions.subscription.get() : actions.order.capture()
@@ -76,31 +180,18 @@ export default class BitPayPalField {
       const form = document.getElementById(`form-${this.#getContentId()}`)
       const formID = this.#getContentId()?.split('_')[1]
       if (typeof form !== 'undefined' && form !== null) {
-        const input = document.createElement('input')
-        input.setAttribute('type', 'hidden')
-        input.setAttribute('name', this.#config.fieldKey)
-        input.setAttribute('id', 'paypalfield')
-        input.setAttribute('value', result.id)
-        form.appendChild(input)
-        let submitBtn = form.querySelector('button[type="submit"]')
-        if (!submitBtn) {
-          submitBtn = document.createElement('input')
-          submitBtn.setAttribute('type', 'submit')
-          submitBtn.style.display = 'none'
-          form.appendChild(submitBtn)
-        }
-        submitBtn.click()
         const paymentParams = {
           formID,
           transactionID: result.id,
           payment_name: 'paypal',
           payment_type: this.#isSubscription() ? 'subscription' : 'order',
           payment_response: result,
+          entry_id: this.#getEntryId(),
+          fieldKey: this.#config.namespace,
         }
-        // bitsFetchFront(paymentParams, 'bitforms_payment_insert')
-        //   .then(() => formParent.classList.remove('pos-rel', 'form-loading'))
-        const uri = new URL(bf_globals[this.#getContentId()]?.ajaxURL)
-        uri.searchParams.append('_ajax_nonce', bf_globals[this.#getContentId()]?.nonce)
+        const props = bf_globals[this.#getContentId()]
+        const uri = new URL(props?.ajaxURL)
+        uri.searchParams.append('_ajax_nonce', props?.nonce)
         uri.searchParams.append('action', 'bitforms_payment_insert')
         const submitResp = fetch(
           uri,
@@ -111,9 +202,80 @@ export default class BitPayPalField {
         )
         submitResp.then(() => {
           formParent.classList.remove('pos-rel', 'form-loading')
+          this.#responseRedirect()
+          this.#handleReset(this.#getContentId())
         })
       }
     })
+  }
+
+  #handleReset(contentId, customHook = false) {
+    if (customHook) {
+      const resetEvent = new CustomEvent('bf-form-reset', {
+        detail: { formId: contentId },
+      })
+      bfSelect(`#form-${contentId}`).dispatchEvent(resetEvent)
+    }
+
+    const props = window.bf_globals[contentId]
+    bfSelect(`#form-${contentId}`).reset()
+    localStorage.setItem('bf-entry-id', '')
+    typeof customFieldsReset !== 'undefined' && customFieldsReset(props)
+
+    if (props.gRecaptchaSiteKey && props.gRecaptchaVersion === 'v2') {
+      resetCaptcha()
+    }
+  }
+
+  #responseRedirect() {
+    const responsedRedirectPage = this.#responseData.redirectPage
+    let hitCron = null
+    let newNonce = ''
+    if (this.#responseData.cron) {
+      hitCron = this.#responseData.cron
+    }
+    if (this.#responseData.cronNotOk) {
+      hitCron = this.#responseData.cronNotOk
+    }
+    if (this.#responseData.new_nonce) {
+      newNonce = this.#responseData.new_nonce
+    }
+
+    this.#triggerIntegration(hitCron, newNonce, this.#getContentId())
+    if (responsedRedirectPage) {
+      const timer = setTimeout(() => {
+        window.location = decodeURI(responsedRedirectPage)
+        if (timer) {
+          clearTimeout(timer)
+        }
+      }, 1000)
+    }
+  }
+
+  #triggerIntegration(hitCron, newNonce, contentId) {
+    const props = window.bf_globals[contentId]
+    if (hitCron) {
+      if (typeof hitCron === 'string') {
+        const uri = new URL(hitCron)
+        if (uri.protocol !== window.location.protocol) {
+          uri.protocol = window.location.protocol
+        }
+        fetch(uri)
+      } else {
+        const uri = new URL(props.ajaxURL)
+        uri.searchParams.append('action', 'bitforms_trigger_workflow')
+        const data = {
+          cronNotOk: hitCron,
+          token: newNonce || props.nonce,
+          id: props.appID,
+        }
+        fetch(uri, {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: { 'Content-Type': 'application/json' },
+        }).then((response) => response.json())
+      }
+    }
   }
 
   #select(selector, elm) {
@@ -121,10 +283,10 @@ export default class BitPayPalField {
   }
 
   #getDynamicValue(fldName) {
-    let elm = this.#select(`[name="${fldName}"]`, formSelector)
+    let elm = this.#select(`[name="${fldName}"]`, this.#formSelector)
     if (elm) {
       if (elm.type === 'radio') {
-        elm = this.#select(`[name="${fldName}"]:checked`, formSelector)
+        elm = this.#select(`[name="${fldName}"]:checked`, this.#formSelector)
       }
 
       return elm.value || ''
--- packages/bit-razorpay-field/src/bit-razorpay-field.js
+++ packages/bit-razorpay-field/src/bit-razorpay-field.js
@@ -18,6 +18,8 @@ export default class BitRazorpayField {
 
   #document = null
 
+  #responseData = null
+
   #window = null
 
   constructor(selector, config) {
@@ -63,30 +65,17 @@ export default class BitRazorpayField {
     const form = document.getElementById(`form-${this.#config.contentId}`)
     const formID = this.#config.contentId?.split('_')[1]
     if (typeof form !== 'undefined' && form !== null) {
-      const input = document.createElement('input')
-      input.setAttribute('type', 'hidden')
-      input.setAttribute('name', this.#config.fieldKey)
-      input.setAttribute('id', 'razorpayfield')
-      input.setAttribute('value', response.razorpay_payment_id)
-      form.appendChild(input)
-      let submitBtn = form.querySelector('button[type="submit"]')
-      if (!submitBtn) {
-        submitBtn = document.createElement('input')
-        submitBtn.setAttribute('type', 'submit')
-        submitBtn.style.display = 'none'
-        form.appendChild(submitBtn)
-      }
-      submitBtn.click()
       const paymentParams = {
         formID,
         fieldKey: this.#config.fieldKey,
         transactionID: response.razorpay_payment_id,
         payment_type: this.#config.payType === 'subscription' ? 'subscription' : 'order',
+        entry_id: this.#getEntryId(),
       }
-      // bitsFetchFront(paymentParams, 'bitforms_save_razorpay_details')
-      //   .then(() => formParent.classList.remove('pos-rel', 'form-loading'))
-      const uri = new URL(bf_globals[this.#config.contentId]?.ajaxURL)
-      uri.searchParams.append('_ajax_nonce', bf_globals[this.#config.contentId]?.nonce)
+
+      const props = bf_globals[this.#config.contentId]
+      const uri = new URL(props?.ajaxURL)
+      uri.searchParams.append('_ajax_nonce', props?.nonce)
       uri.searchParams.append('action', 'bitforms_save_razorpay_details')
       const submitResp = fetch(
         uri,
@@ -97,13 +86,87 @@ export default class BitRazorpayField {
       )
       submitResp.then(() => {
         formParent.classList.remove('pos-rel', 'form-loading')
+        this.#responseRedirect()
+        this.#handleReset(this.#config.contentId)
       })
     }
   }
 
-  #displayRazorpay = () => {
-    const { currency, amount, amountType, amountFld, name, description, theme, prefill, modal, notes } = this.#config.options
+  #handleReset(contentId, customHook = false) {
+    if (customHook) {
+      const resetEvent = new CustomEvent('bf-form-reset', {
+        detail: { formId: contentId },
+      })
+      bfSelect(`#form-${contentId}`).dispatchEvent(resetEvent)
+    }
+
+    const props = window.bf_globals[contentId]
+    bfSelect(`#form-${contentId}`).reset()
+    localStorage.setItem('bf-entry-id', '')
+    typeof customFieldsReset !== 'undefined' && customFieldsReset(props)
+
+    if (props.gRecaptchaSiteKey && props.gRecaptchaVersion === 'v2') {
+      resetCaptcha()
+    }
+  }
+
+  #responseRedirect() {
+    const responsedRedirectPage = this.#responseData.redirectPage
+    let hitCron = null
+    let newNonce = ''
+    if (this.#responseData.cron) {
+      hitCron = this.#responseData.cron
+    }
+    if (this.#responseData.cronNotOk) {
+      hitCron = this.#responseData.cronNotOk
+    }
+    if (this.#responseData.new_nonce) {
+      newNonce = this.#responseData.new_nonce
+    }
+
+    this.#triggerIntegration(hitCron, newNonce, this.#config.contentId)
+    if (responsedRedirectPage) {
+      const timer = setTimeout(() => {
+        window.location = decodeURI(responsedRedirectPage)
+        if (timer) {
+          clearTimeout(timer)
+        }
+      }, 1000)
+    }
+  }
+
+  #triggerIntegration(hitCron, newNonce, contentId) {
+    const props = window.bf_globals[contentId]
+    if (hitCron) {
+      if (typeof hitCron === 'string') {
+        const uri = new URL(hitCron)
+        if (uri.protocol !== window.location.protocol) {
+          uri.protocol = window.location.protocol
+        }
+        fetch(uri)
+      } else {
+        const uri = new URL(props.ajaxURL)
+        uri.searchParams.append('action', 'bitforms_trigger_workflow')
+        const data = {
+          cronNotOk: hitCron,
+          token: newNonce || props.nonce,
+          id: props.appID,
+        }
+        fetch(uri, {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: { 'Content-Type': 'application/json' },
+        }).then((response) => response.json())
+      }
+    }
+  }
+
+  async #displayRazorpay() {
+    const {
+      currency, amount, amountType, amountFld, name, description, theme, prefill, modal, notes,
+    } = this.#config.options
     const { confirm_close } = modal
+    const { contentId } = this.#config
 
     const totalAmount = Number(amountType === 'fixed' ? amount : this.#getDynamicValue(amountFld)) * 100
 
@@ -128,12 +191,118 @@ export default class BitRazorpayField {
       handler: async response => this.#onPaymentSuccess(response),
     }
 
-    isFormValidatedWithoutError(this.#config.contentId, handleFormValidationErrorMessages)
-      .then(() => {
+    const form = bfSelect(`#form-${contentId}`)
+    if (
+      typeof validateForm !== 'undefined'
+      && !validateForm({ form: contentId })
+    ) {
+      const validationEvent = new CustomEvent('bf-form-validation-error', {
+        detail: { formId: contentId, fieldId: '', error: '' },
+      })
+      form.dispatchEvent(validationEvent)
+      return false
+    }
+    let update = false
+    let formData = new FormData(form)
+    if (this.#getEntryId()) {
+      update = true
+      formData.append('entryID', this.#getEntryId())
+    }
+    const props = window.bf_globals[contentId]
+
+    if (typeof advancedFileHandle !== 'undefined') {
+      formData = advancedFileHandle(props, formData)
+    }
+    if (props.GCLID) {
+      formData.set('GCLID', props.GCLID)
+    }
+
+    const hidden = []
+    Object.entries(props?.fields || {}).forEach((fld) => {
+      if (fld[1]?.valid?.hide) {
+        hidden.push(fld[0])
+      }
+    })
+
+    if (hidden.length) {
+      formData.append('hidden_fields', hidden)
+    }
+    if (props?.gRecaptchaVersion === 'v3' && props?.gRecaptchaSiteKey) {
+      grecaptcha.ready(() => {
+        grecaptcha
+          .execute(props.gRecaptchaSiteKey, { action: 'submit' })
+          .then(async (token) => {
+            formData.append('g-recaptcha-response', token)
+            const submitResp = this.#bfSubmitFetch(props?.ajaxURL, formData, update)
+            if (await this.#submitResponse(submitResp, contentId, formData)) {
+              const paymentObject = new this.#window.Razorpay(options)
+              paymentObject.open()
+            }
+          })
+      })
+    } else {
+      const submitResp = this.#bfSubmitFetch(props?.ajaxURL, formData, update)
+      if (await this.#submitResponse(submitResp, contentId, formData)) {
         const paymentObject = new this.#window.Razorpay(options)
         paymentObject.open()
-      })
-      .catch((err) => false)
+      }
+    }
+  }
+
+  #submitResponse(resp, contentId, formData) {
+    return new Promise(resolve => {
+      resp
+        .then(
+          (response) => new Promise((resolve2, reject) => {
+            if (response.staus > 400) {
+              const errorEvent = new CustomEvent('bf-form-submit-error', {
+                detail: { formId: contentId, errors: response.data },
+              })
+              bfSelect(`#form-${contentId}`).dispatchEvent(errorEvent)
+              response.staus === 500
+                ? reject(new Error('Mayebe Internal Server Error'))
+                : reject(response.json())
+            } else resolve2(response.json())
+          }),
+        )
+        .then((result) => {
+          const successEvent = new CustomEvent('bf-form-submit-success', {
+            detail: { formId: contentId, entryId: result.entryId, formData },
+          })
+          bfSelect(`#form-${contentId}`).dispatchEvent(successEvent)
+          this.#responseData = result.data
+          if (result !== undefined && result.success) {
+            this.#setEntryId(result.data.entry_id)
+            resolve(true)
+          }
+          const errorEvent = new CustomEvent('bf-form-submit-error', {
+            detail: { formId: contentId, errors: result.data },
+          })
+          bfSelect(`#form-${contentId}`).dispatchEvent(errorEvent)
+          resolve(false)
+        })
+        .catch((error) => {
+          const err = error?.message ? error.message : 'Unknown Error'
+          resolve(false)
+        })
+    })
+  }
+
+  #setEntryId(id) {
+    localStorage.setItem('bf-entry-id', id)
+  }
+
+  #getEntryId() {
+    return localStorage.getItem('bf-entry-id')
+  }
+
+  #bfSubmitFetch(ajaxURL, formData, update) {
+    const uri = new URL(ajaxURL)
+    uri.searchParams.append('action', update ? 'bitforms_entry_update' : 'bitforms_submit_form')
+    return fetch(uri, {
+      method: 'POST',
+      body: formData,
+    })
   }
 
   destroy() {
