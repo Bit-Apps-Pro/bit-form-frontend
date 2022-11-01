21020ca02e407b962b65627b4e4f2e8f46f7a078
Md Rubel Amin <mdrubelamin2@gmail.com>
2022-11-01 15:46:36 +0600

    fix: bit-frontend packages

--- packages/bit-frontend/src/bitform-init.js
+++ packages/bit-frontend/src/bitform-init.js
@@ -1,12 +1,3 @@
 if (!window.bf_globals) window.bf_globals = {}
 
-const allContentids = window?.bf_globals
-if (allContentids) {
-  Object.keys(allContentids).forEach((contentId) => {
-
-  })
-}
-
-if (!window.bf_globals.$FormIdentifier) window.bf_globals.$FormIdentifier = {}
-window.bf_globals.$FormIdentifier = { ...window.bf_globals.$FormIdentifier, ...$frontArr }
 
