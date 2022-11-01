03755ae2adaf0828f3451c616f06134cf0b11eeb
Md Rubel Amin <mdrubelamin2@gmail.com>
2022-11-01 16:38:42 +0600

    remove: unnecessary function call to reduce file size

--- packages/bit-frontend/src/validate-focus.js
+++ packages/bit-frontend/src/validate-focus.js
@@ -1,17 +1,14 @@
 const allContentids = window?.bf_globals
-allContentids
-  && Object.keys(allContentids).forEach((contentId) => {
+if (allContentids) {
+  Object.keys(allContentids).forEach((contentId) => {
     const props = window.bf_globals[contentId]
-    addEventToFields(contentId, props)
-  })
-
-const addEventToFields = (contentId, props) => {
-  const form = document.getElementById(`form-${contentId}`)
-  Object.values(props.fields).forEach(fldData => {
-    const fldName = fldData.typ === 'check' ? `${fldData.fieldName}[]` : fldData.fieldName
-    const onaction = ['check', 'radio'].includes(fldData.typ) ? 'input' : 'blur'
-    form.querySelectorAll(`[name = '${fldName}']`).forEach(elm => {
-      elm.addEventListener(onaction, e => typeof validateForm !== 'undefined' && validateForm({ input: e.target }))
+    const form = document.getElementById(`form-${contentId}`)
+    Object.values(props.fields).forEach(fldData => {
+      const fldName = fldData.typ === 'check' ? `${fldData.fieldName}[]` : fldData.fieldName
+      const onaction = ['check', 'radio'].includes(fldData.typ) ? 'input' : 'blur'
+      form.querySelectorAll(`[name = '${fldName}']`).forEach(elm => {
+        elm.addEventListener(onaction, e => typeof validateForm !== 'undefined' && validateForm({ input: e.target }))
+      })
     })
   })
 }
