ce456c4d2732815d35ba96db6d4233ab7f8f0886
Md Rubel Amin <mdrubelamin2@gmail.com>
2022-11-01 16:42:24 +0600

    fix: bf_globals not ready during initial load

--- packages/bit-frontend/src/validate-focus.js
+++ packages/bit-frontend/src/validate-focus.js
@@ -1,14 +1,16 @@
-const allContentids = window?.bf_globals
-if (allContentids) {
-  Object.keys(allContentids).forEach((contentId) => {
-    const props = window.bf_globals[contentId]
-    const form = document.getElementById(`form-${contentId}`)
-    Object.values(props.fields).forEach(fldData => {
-      const fldName = fldData.typ === 'check' ? `${fldData.fieldName}[]` : fldData.fieldName
-      const onaction = ['check', 'radio'].includes(fldData.typ) ? 'input' : 'blur'
-      form.querySelectorAll(`[name = '${fldName}']`).forEach(elm => {
-        elm.addEventListener(onaction, e => typeof validateForm !== 'undefined' && validateForm({ input: e.target }))
+document.addEventListener('DOMContentLoaded', () => {
+  const allContentids = window?.bf_globals
+  if (allContentids) {
+    Object.keys(allContentids).forEach((contentId) => {
+      const props = window.bf_globals[contentId]
+      const form = document.getElementById(`form-${contentId}`)
+      Object.values(props.fields).forEach(fldData => {
+        const fldName = fldData.typ === 'check' ? `${fldData.fieldName}[]` : fldData.fieldName
+        const onaction = ['check', 'radio'].includes(fldData.typ) ? 'input' : 'blur'
+        form.querySelectorAll(`[name = '${fldName}']`).forEach(elm => {
+          elm.addEventListener(onaction, e => typeof validateForm !== 'undefined' && validateForm({ input: e.target }))
+        })
       })
     })
-  })
-}
+  }
+})
