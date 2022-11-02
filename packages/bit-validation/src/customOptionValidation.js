b829b58e0fbaa3ecd8b3ae107e2e8d18788eb538
Md RH Maruf <mdrobiulhasan2620@gmail.com>
2022-11-02 15:03:12 +0600

    Modify: Other Option Required validation

--- packages/bit-validation/src/customOptionValidation.js
+++ packages/bit-validation/src/customOptionValidation.js
@@ -1,5 +1,5 @@
 const customOptionValidation = (fldKey, fldData) => {
   const optElm = document.querySelector(`[data-oopt="${fldKey}"]`)
-  return (optElm?.checked && optElm.value === '') ? 'otherOptReq' : ''
+  return (optElm?.checked && optElm.value === '' && fldData.valid.otherOptReq) ? 'otherOptReq' : ''
 }
 export default customOptionValidation
