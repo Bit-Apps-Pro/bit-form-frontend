683844cfe4e07179608825ec5aa26e58e1ea9627
Arif Uddin <arifunctg@gmail.com>
2022-12-03 17:01:27 +0600

    fix: portal render in firefox

--- src/RenderPortal.jsx
+++ src/RenderPortal.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { createPortal } from 'react-dom'
 import { handleUndoRedoShortcut } from './components/FormBuilderHistory'
 import { searchKey } from './components/style-new/styleHelpers'
-import { isFirefox } from './Utils/Helpers'
+// import { isFirefox } from './Utils/Helpers'
 
 // eslint-disable-next-line import/prefer-default-export
 export const RenderPortal = ({ children, title, ...props }) => {
@@ -12,13 +12,13 @@ export const RenderPortal = ({ children, title, ...props }) => {
   const mountNode = contentRef?.contentDocument?.body
 
   if (contentRef) {
-    contentRef.contentWindow.addEventListener('keydown', searchKey)
-    contentRef.contentWindow.addEventListener('keydown', handleUndoRedoShortcut)
+    contentRef?.contentWindow?.addEventListener('keydown', searchKey)
+    contentRef?.contentWindow?.addEventListener('keydown', handleUndoRedoShortcut)
   }
 
-  const hasDoctype = (dom) => (dom.doctype !== null)
+  const hasDoctype = (dom) => (dom?.doctype !== null)
 
-  if (contentRef && !hasDoctype(contentRef.contentDocument)) {
+  if (contentRef && !hasDoctype(contentRef?.contentDocument)) {
     const newDoctype = document.implementation.createDocumentType('html', '', '')
     contentRef.contentDocument.insertBefore(newDoctype, contentRef.contentDocument.childNodes[0])
   }
@@ -27,8 +27,9 @@ export const RenderPortal = ({ children, title, ...props }) => {
     <iframe
       title={title}
       {...props}
-      {...isFirefox() && { onLoad: e => setContentRef(e.target) }}
-      {...!isFirefox() && { ref: setContentRef }}
+      // {...isFirefox() && { onLoad: e => setContentRef(e.target) }}
+      // {...isFirefox() && { ref: setContentRef }}
+      ref={setContentRef}
     >
       {mountNode && hasDoctype(contentRef.contentDocument) && createPortal(children, mountNode)}
     </iframe>
