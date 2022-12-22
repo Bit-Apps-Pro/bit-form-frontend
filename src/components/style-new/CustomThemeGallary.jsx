dd707592bd8f04539183cddf7611285955021d3f
Md Rubel Amin <mdrubelamin2@gmail.com>
2022-12-22 18:08:11 +0600

    fix: recalculate field heights on theme change

--- src/components/style-new/CustomThemeGallary.jsx
+++ src/components/style-new/CustomThemeGallary.jsx
@@ -16,6 +16,7 @@ import atlassianTheme from './themes/atlassianTheme/3_atlassianTheme'
 import themes from './themes/themeList'
 import individual from './themes/individual/individual'
 import materialTheme from './themes/material/2_material'
+import { reCalculateFldHeights } from '../../Utils/FormBuilderHelper'
 
 export default function CustomThemeGallary({ fldKey }) {
   const { css } = useFela()
@@ -56,6 +57,7 @@ export default function CustomThemeGallary({ fldKey }) {
       const type = prvStyle.fields[fk].fieldType
       drftStyle.fields[fk] = getStyle(themeSlug, fk, type)
     }))
+    reCalculateFldHeights(fldKey)
   }
 
   const checkActiveTheme = (themeSlug) => {
