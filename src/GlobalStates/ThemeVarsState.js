e6a91872d1468a6a84a3b9c5d63547646b618fcb
Md Rubel Amin <mdrubelamin2@gmail.com>
2022-12-24 11:59:28 +0600

    typo in state

--- src/GlobalStates/ThemeVarsState.js
+++ src/GlobalStates/ThemeVarsState.js
@@ -6,7 +6,7 @@ import { $breakpoint, $colorScheme } from './GlobalStates'
 
 export const $themeVarsLgLight = atom({
   key: '$themeVarsLgLight',
-  default: {}, O
+  default: {},
   effects: [({ onSet }) => {
     onSet((newThemeVarsLgLight, _, isReset) => {
       if (isReset) return
