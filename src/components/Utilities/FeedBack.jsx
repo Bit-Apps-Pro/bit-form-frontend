2f034357eccfc392d60602de0ef48486009652fe
mdabbas-cse <gmabbas44@gmail.com>
2022-12-29 20:24:02 +0600

    update: feedback collect

--- src/components/Utilities/FeedBack.jsx
+++ src/components/Utilities/FeedBack.jsx
@@ -1,36 +1,115 @@
+import { useState } from 'react'
 import { useFela } from 'react-fela'
 import WarningIcn from '../../Icons/WarningIcn'
 import ut from '../../styles/2.utilities'
+import { __ } from '../../Utils/i18nwrap'
 import Btn from './Btn'
 
 export default function FeedBack({ className }) {
   const { css } = useFela()
 
+  const [feedback, setFeedback] = useState({
+    option: 'issue',
+    body: '',
+  })
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target
+    setFeedback({
+      ...feedback,
+      [name]: value,
+    })
+  }
+
+  const sendFeedback = () => {
+    console.log(feedback)
+    // fetch('https://webhook.site/4c1b6854-a522-463d-af1d-5f4b7e10d26e', {
+    //   method: 'POST', // or 'PUT'
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(feedback),
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log('Success:', data)
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error:', error)
+    //   })
+  }
+
+  const msg = {
+    issue: "I'm having an issue with...",
+    idea: "I'd like to suggest...",
+    other: "Anything else you'd like to share?",
+  }
+
   return (
     <div className={className}>
       <div className={css(feedbackStyle.flx)}>
-        <label htmlFor="issue" className={css(feedbackStyle.lbl)}>
-          <input id="issue" type="radio" name="issue" checked />
+        <label
+          htmlFor="issue"
+          className={css(feedbackStyle.lbl, feedback.option === 'issue' && feedbackStyle.active)}
+        >
+          <input
+            id="issue"
+            type="radio"
+            name="option"
+            onChange={onChangeHandler}
+            value="issue"
+          />
           <WarningIcn size="15" />
-          <span className={css(feedbackStyle.title)}>Issue</span>
+          <span className={css(feedbackStyle.title)}>{__('Issue')}</span>
         </label>
 
-        <label htmlFor="idea" className={css(feedbackStyle.lbl)}>
-          <input id="idea" type="radio" name="idea" />
+        <label
+          htmlFor="idea"
+          className={css(feedbackStyle.lbl, feedback.option === 'idea' && feedbackStyle.active)}
+        >
+          <input
+            id="idea"
+            type="radio"
+            name="option"
+            value="idea"
+            onChange={onChangeHandler}
+          />
           <WarningIcn size="15" />
-          <span className={css(feedbackStyle.title)}>Idea</span>
+          <span className={css(feedbackStyle.title)}>{__('Idea')}</span>
         </label>
 
-        <label htmlFor="other" className={css(feedbackStyle.lbl)}>
-          <input id="other" type="radio" name="other" />
+        <label
+          htmlFor="other"
+          className={css(feedbackStyle.lbl, feedback.option === 'other' && feedbackStyle.active)}
+        >
+          <input
+            id="other"
+            type="radio"
+            name="option"
+            value="other"
+            onChange={onChangeHandler}
+          />
           <WarningIcn size="15" />
-          <span className={css(feedbackStyle.title)}>Other</span>
+          <span className={css(feedbackStyle.title)}>{__('Other')}</span>
         </label>
       </div>
       <div className={css(feedbackStyle.bodyContain)}>
-        <textarea className={css(feedbackStyle.body)} rows="5" />
+        <textarea
+          placeholder={msg[feedback.option]}
+          name="body"
+          onChange={onChangeHandler}
+          className={css(feedbackStyle.body)}
+          rows="5"
+        />
       </div>
-      <Btn size="sm" width="100%" className={css(ut.mt1)}>Send</Btn>
+      <Btn
+        onClick={sendFeedback}
+        size="sm"
+        width="100%"
+        className={css(ut.mt1)}
+      >
+        {__('Send')}
+      </Btn>
     </div>
   )
 }
@@ -46,6 +125,7 @@ const feedbackStyle = {
     bd: '#EFF2F7',
     p: '5px 10px',
     tn: 'background 0.2s',
+    // b: '1px solid var(--blue)',
     '>input[type="radio"]': {
       dy: 'none',
     },
@@ -53,10 +133,11 @@ const feedbackStyle = {
       bd: 'var(--b-79-96)',
       cr: 'var(--blue)',
     },
-    'input:checked + *': {
-      bd: 'var(--b-79-96)',
-      cr: 'var(--blue)',
-    },
+  },
+  active: {
+    bd: 'var(--b-79-96)',
+    cr: 'var(--blue)',
+    focusShadow: '',
   },
   title: {
     ml: 5,
