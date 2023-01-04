3af5b5e16f91f46d8abb9e5593fb67183c69e32d
mdabbas-cse <gmabbas44@gmail.com>
2023-01-04 14:43:18 +0600

    update: show active theme name in field quick tweaks.

--- src/components/style-new/ThemeControl.jsx
+++ src/components/style-new/ThemeControl.jsx
@@ -1,16 +1,26 @@
 import { useFela } from 'react-fela'
-import { useSetRecoilState } from 'recoil'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
 import { $draggableModal } from '../../GlobalStates/GlobalStates'
+import { $styles } from '../../GlobalStates/StylesState'
 import { showDraggableModal } from './styleHelpers'
 
 export default function ThemeControl({ fldKey }) {
   const setDraggableModal = useSetRecoilState($draggableModal)
   const { css } = useFela()
+  const styles = useRecoilValue($styles)
+  const theme = styles.fields[fldKey]?.theme
 
   const handleSpacingMenu = (e) => showDraggableModal(e, setDraggableModal, { component: 'theme-control', width: 600, fldKey })
   return (
-    <button className={css(cls.delBtn)} onClick={handleSpacingMenu} type="button">Choose theme</button>
+    <button className={css(cls.delBtn)} onClick={handleSpacingMenu} type="button">{themeNames[theme]}</button>
   )
+}
+const themeNames = {
+  bitformDefault: 'Bitform Default',
+  atlassian: 'Atlassian',
+  material: 'Material',
+  noStyle: 'No Style',
+  individual: 'Individual',
 }
 
 const cls = {
