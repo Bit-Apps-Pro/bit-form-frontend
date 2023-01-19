0e6f010f37b63032bdd7b34ad977988f7a09f8df
mdabbas-cse <gmabbas44@gmail.com>
2023-01-19 17:39:06 +0600

    fix: zoho sheet integration authorization issue

--- src/components/AllIntegrations/ZohoSheet/ZohoSheetCommonFunc.js
+++ src/components/AllIntegrations/ZohoSheet/ZohoSheetCommonFunc.js
@@ -182,7 +182,7 @@ export const refreshWorksheetHeaders = (formID, sheetConf, setSheetConf, setisLo
 export const setGrantTokenResponse = (integ) => {
   const grantTokenResponse = {}
   const authWindowLocation = window.location.href
-  const queryParams = authWindowLocation.replace(`${window.opener.location.href}/redirect`, '').split('&')
+  const queryParams = authWindowLocation.replace(`${window.opener.location.href}?redirect`, '').split('&')
   if (queryParams) {
     queryParams.forEach(element => {
       const gtKeyValue = element.split('=')
@@ -208,7 +208,7 @@ export const handleAuthorize = (confTmp, setConf, setError, setisAuthorized, set
   const bits = getRecoil($bits)
   setisLoading(true)
   const scopes = 'ZohoSheet.dataAPI.READ,ZohoSheet.dataAPI.UPDATE'
-  const apiEndpoint = `https://accounts.zoho.${confTmp.dataCenter}/oauth/v2/auth?scope=${scopes}&response_type=code&client_id=${confTmp.clientId}&prompt=Consent&access_type=offline&state=${encodeURIComponent(window.location.href)}/redirect?redirect_uri=${encodeURIComponent(bits.zohoRedirectURL)}`
+  const apiEndpoint = `https://accounts.zoho.${confTmp.dataCenter}/oauth/v2/auth?scope=${scopes}&response_type=code&client_id=${confTmp.clientId}&prompt=Consent&access_type=offline&state=${encodeURIComponent(window.location.href)}?redirect&redirect_uri=${encodeURIComponent(bits.zohoRedirectURL)}`
   const authWindow = window.open(apiEndpoint, 'zohoSheet', 'width=400,height=609,toolbar=off')
   const popupURLCheckTimer = setInterval(() => {
     if (authWindow.closed) {
@@ -235,11 +235,13 @@ export const handleAuthorize = (confTmp, setConf, setError, setisAuthorized, set
 }
 
 const tokenHelper = (grantToken, confTmp, setConf, setisAuthorized, setisLoading, setSnackbar) => {
+  const bits = getRecoil($bits)
   const tokenRequestParams = { ...grantToken }
   tokenRequestParams.dataCenter = confTmp.dataCenter
   tokenRequestParams.clientId = confTmp.clientId
   tokenRequestParams.clientSecret = confTmp.clientSecret
-  tokenRequestParams.redirectURI = `${encodeURIComponent(window.location.href)}/redirect`
+  // tokenRequestParams.redirectURI = `${encodeURIComponent(window.location.href)}/redirect`
+  tokenRequestParams.redirectURI = bits.zohoRedirectURL
   bitsFetch(tokenRequestParams, 'bitforms_zsheet_generate_token')
     .then(result => result)
     .then(result => {
