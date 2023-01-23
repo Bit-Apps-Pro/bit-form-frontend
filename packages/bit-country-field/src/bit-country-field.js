2a974d622ee0133bb61a6d51beeee79bf4ad53f3
Md Rubel Amin <mdrubelamin2@gmail.com>
2023-01-23 18:47:13 +0600

    fix: country field val key miss

--- packages/bit-country-field/src/bit-country-field.js
+++ packages/bit-country-field/src/bit-country-field.js
@@ -306,7 +306,7 @@ export default class BitCountryField {
   }
 
   #handleInputValueChange(oldVal, newVal) {
-    const searchedOption = this.#initialOptions.find(option => (option.val === newVal || option.lbl === newVal))
+    const searchedOption = this.#initialOptions.find(option => (option.i === newVal || option.lbl === newVal))
     if (searchedOption && oldVal !== newVal) {
       this.setSelectedCountryItem(searchedOption.i)
     }
