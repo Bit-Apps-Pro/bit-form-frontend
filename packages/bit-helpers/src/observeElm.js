9f47fd8cad466e1853fa67af5e8e821671266a30
Md Rubel Amin <mdrubelamin2@gmail.com>
2023-01-23 18:46:38 +0600

    new: observerElm by mutationObserver

--- packages/bit-helpers/src/observeElm.js
+++ packages/bit-helpers/src/observeElm.js
@@ -1,24 +1,20 @@
 export default function observeElm(element, property, callback, delay = 0) {
-  const elementPrototype = Object.getPrototypeOf(element)
-  if (Object.prototype.hasOwnProperty.call(elementPrototype, property)) {
-    const descriptor = Object.getOwnPropertyDescriptor(
-      elementPrototype,
-      property,
-    )
-    Object.defineProperty(element, property, {
-      configurable: true,
-      get(...args) {
-        return descriptor.get.apply(this, args)
-      },
-      set(...args) {
-        const oldValue = this[property]
-        descriptor.set.apply(this, args)
-        const newValue = this[property]
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.attributeName === property) {
+        const { oldValue } = mutation
+        const newValue = mutation.target[property]
         if (typeof callback === 'function') {
-          setTimeout(callback.bind(this, oldValue, newValue), delay)
+          setTimeout(callback.call(this, oldValue, newValue), delay)
         }
-        return newValue
-      },
+      }
     })
+  })
+  const config = {
+    attributes: true,
+    attributeOldValue: true,
+    attributeFilter: [property],
   }
+  observer.observe(element, config)
+  return observer
 }
