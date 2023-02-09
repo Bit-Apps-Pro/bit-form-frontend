423b4c846b36aa363d21001b7ae4933e1ac249be
Md Rubel Amin <mdrubelamin2@gmail.com>
2023-02-09 15:34:59 +0600

    feat: migration progress count

--- src/components/MigrationModal.jsx
+++ src/components/MigrationModal.jsx
@@ -1,15 +1,18 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRecoilValue } from 'recoil'
 import { $bits } from '../GlobalStates/GlobalStates'
 import bitsFetch from '../Utils/bitsFetch'
-import { generateAndSaveAtomicCss, generateUpdateFormData, setFormReponseDataToStates, setStyleRelatedStates } from '../Utils/Helpers'
+import { generateUpdateFormData, setFormReponseDataToStates, setStyleRelatedStates } from '../Utils/Helpers'
 import { __ } from '../Utils/i18nwrap'
 import Loader from './Loaders/Loader'
 import themeProvider from './style-new/themes/themeProvider'
 import Modal from './Utilities/Modal'
+import Progressbar from './Utilities/Progressbar'
 
 export default function MigrationModal() {
   const bits = useRecoilValue($bits)
+  const [migratingCount, setMigratingCount] = useState(0)
+  const [totalForms, setTotalForms] = useState(0)
 
   useEffect(() => {
     if (bits.isMigratingToV2) {
@@ -20,6 +23,7 @@ export default function MigrationModal() {
       bitsFetch({}, 'bitforms_get_all_form_contents')
         .then((res) => {
           if (res.success && res.data.length) {
+            setTotalForms(res.data.length)
             const updateFormPromises = []
             res.data.forEach(formData => {
               const { id: formID } = formData
@@ -28,7 +32,8 @@ export default function MigrationModal() {
               setFormReponseDataToStates(formData)
               setStyleRelatedStates({ themeVars, themeColors, styles })
               const migratedFormData = generateUpdateFormData(formID)
-              updateFormPromises.push(bitsFetch(migratedFormData, 'bitforms_update_form'))
+              const updateFormPromise = bitsFetch(migratedFormData, 'bitforms_update_form').then(() => { setMigratingCount(prevCount => prevCount + 1) })
+              updateFormPromises.push(updateFormPromise)
             })
             Promise.all(updateFormPromises)
               .then(() => {
@@ -49,12 +54,19 @@ export default function MigrationModal() {
       })
   }
 
+  const getMigratedPercentage = () => {
+    if (migratingCount === 0) return 0
+    return Math.round((migratingCount / totalForms) * 100)
+  }
+
   return (
-    <Modal sm show={bits.isMigratingToV2} showCloseBtn={false}>
+    <Modal sm show showCloseBtn={false}>
       <div className="flx flx-col flx-center">
         <h3 className="mb-0">{__('Migrating to V2')}</h3>
         <p className="mb-2">{__('Please wait while we migrate your data to V2')}</p>
-        <Loader />
+        {/* <Loader /> */}
+        <div className="flx flx-center" style={{ width: '70%' }}><Progressbar value={getMigratedPercentage()} /></div>
+        <p className="m-0">{`Migrated ${migratingCount} out of ${totalForms} forms`}</p>
         <p className="m-0 mt-2">{__('This may take a few minutes...')}</p>
         <p className="mt-0 mb-2">{__('Meanwhile do not close this window.')}</p>
         <button type="button" onClick={backToV1}>Back to v1</button>
