adc9c54ce82cd1cc5ffb9576c83ec04100e9cbcc
Md RH Maruf <mdrobiulhasan2620@gmail.com>
2023-03-13 17:57:41 +0600

    Remove: 'fela-plugin-multiple-selectors' library

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,19 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-undef */
-import './resource/sass/app.scss'
-import './resource/sass/global.scss'
 import { createRenderer } from 'fela'
 import customProperty from 'fela-plugin-custom-property'
 import ReactDOM from 'react-dom/client'
 import { RendererProvider } from 'react-fela'
 import { RecoilRoot } from 'recoil'
 import RecoilNexus from 'recoil-nexus'
-import multipleSelectors from 'fela-plugin-multiple-selectors'
+import App from './App'
+import './resource/sass/app.scss'
+import './resource/sass/global.scss'
 import customProperties from './styles/1.customProperties'
 import AppSettingsProvider from './Utils/AppSettingsContext'
-import App from './App'
 
 const renderer = createRenderer({
   plugins: [
-    multipleSelectors(),
     customProperty(customProperties),
   ],
   filterClassName: cls => cls.indexOf('cp') !== -1,
