1e0809810c23b6b9475723c4e98b9c294cedbb4a
Md Rubel Amin <mdrubelamin2@gmail.com>
2023-05-04 18:11:53 +0600

    chore: section style added for atlassian and no themes

--- src/components/style-new/themes/0_noStyle/sectionStyle_0_noStyle.js
+++ src/components/style-new/themes/0_noStyle/sectionStyle_0_noStyle.js
@@ -1,11 +1,11 @@
 import inputWrapperClasses from '../common/inputWrapperClasses'
 
 /* eslint-disable camelcase */
-export default function sectionStyle_1_bitformDefault({ fk, type, breakpoint, colorScheme }) {
+export default function sectionStyle_0_noStyle({ fk, type, breakpoint, colorScheme }) {
   if (breakpoint === 'lg' && colorScheme === 'light') {
     return {
       ...inputWrapperClasses(fk),
-      [`.${fk}-inp-fld-wrp`]: { display: 'grid', border: '1px solid #333', padding: '3px' },
+      [`.${fk}-inp-fld-wrp`]: { display: 'grid' },
     }
   }
   return {}
--- src/components/style-new/themes/2_atlassian/2_atlassian.js
+++ src/components/style-new/themes/2_atlassian/2_atlassian.js
@@ -22,6 +22,7 @@ import textStyle_2_atlassian from './textStyle_2_atlassian'
 import titleStyle_2_atlassian from './titleStyle_2_atlassian'
 import { defaultDarkThemeColors, defaultFont, defaultLgLightform, defaultLgLightThemeVars, defaultLightThemeColors } from '../1_bitformDefault/1_bitformDefault'
 import { msgDefaultConfig } from '../../../../Utils/StaticData/form-templates/defaultConfirmation'
+import sectionStyle_2_bitformDefault from './sectionStyle_2_atlassian'
 
 export default function atlassianTheme({
   type, fieldKey: fk, direction, fieldsArr, breakpoint = 'lg', colorScheme = 'light', formId, textOptions = {}, buttonOptions = {},
@@ -103,6 +104,8 @@ export default function atlassianTheme({
       return paypal({ type, fk, breakpoint, colorScheme })
     case 'razorpay':
       return razorpay({ type, fk, breakpoint, colorScheme })
+    case 'section':
+      return section({ type, fk, breakpoint, colorScheme })
     default:
       fieldsArr?.map(([fieldKey, fieldData]) => {
         lgLightFieldStyles[fieldKey] = atlassianTheme({ fieldKey, type: fieldData.typ, breakpoint: 'lg', colorScheme: 'light', textOptions: { fldPrefix: !!fieldData.prefixIcn, fldSuffix: !!fieldData.suffixIcn }, buttonOptions: { align: fieldData.align, txtAlign: fieldData.txtAlign, btnTyp: fieldData.btnTyp, fulW: fieldData.fulW } })
@@ -218,6 +221,19 @@ const text = ({ type, fk, breakpoint, colorScheme, fldPrefix, fldSuffix }) => {
       overrideGlobalTheme: [],
       fieldSize: 'medium',
       classes: textStyle_2_atlassian({ fk, type, breakpoint, colorScheme, fldPrefix, fldSuffix }),
+    }
+  }
+  return {}
+}
+
+const section = ({ type, fk, breakpoint, colorScheme }) => {
+  if (breakpoint === 'lg' && colorScheme === 'light') {
+    return {
+      theme: 'atlassian',
+      fieldType: type,
+      overrideGlobalTheme: [],
+      fieldSize: 'medium',
+      classes: sectionStyle_2_bitformDefault({ fk, type, breakpoint, colorScheme }),
     }
   }
   return {}
