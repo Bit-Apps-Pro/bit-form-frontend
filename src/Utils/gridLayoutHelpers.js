c170c604704cce4f34920ad383e12fee881e2b49
Md Rubel Amin <mdrubelamin2@gmail.com>
2023-05-04 18:10:42 +0600

    fix: immer default produce import deprycated in latest version

--- src/Utils/gridLayoutHelpers.js
+++ src/Utils/gridLayoutHelpers.js
@@ -1,4 +1,4 @@
-import produce from 'immer'
+import { produce } from 'immer'
 import { getRecoil, setRecoil } from 'recoil-nexus'
 import {
   $breakpoint,
--- src/components/Fields/SectionField.jsx
+++ src/components/Fields/SectionField.jsx
@@ -1,4 +1,4 @@
-import produce from 'immer'
+import { produce } from 'immer'
 import { Suspense, useRef } from 'react'
 import { default as ReactGridLayout } from 'react-grid-layout'
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
