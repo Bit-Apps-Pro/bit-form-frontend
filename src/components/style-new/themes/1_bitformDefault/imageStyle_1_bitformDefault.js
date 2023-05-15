f0b3c42c16897e8e04e3e54a54a36a6be8bee12e
Md RH Maruf <mdrobiulhasan2620@gmail.com>
2023-05-15 11:10:57 +0600

    Refactor: 'max-width' property set to field wrap of image field

--- src/components/style-new/themes/1_bitformDefault/imageStyle_1_bitformDefault.js
+++ src/components/style-new/themes/1_bitformDefault/imageStyle_1_bitformDefault.js
@@ -3,7 +3,7 @@ export default function imageStyle_1_bitformDefault({ fk, breakpoint, colorSchem
   if (breakpoint === 'lg' && colorScheme === 'light') {
     return {
       [`.${fk}-fld-wrp`]:
-        { height: '100%', width: '100%' },
+        { height: '100%', width: '100%', 'max-width': '100%' },
       [`.${fk}-img`]: {
         width: '100%',
         height: '100% !important',
