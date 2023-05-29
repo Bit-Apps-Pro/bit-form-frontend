0fb2bd2861cbe76472a294ef51ca12d4fc66a42e
Md RH Maruf <mdrobiulhasan2620@gmail.com>
2023-05-29 15:21:36 +0600

    misc changes

--- src/Icons/SectionIcon.jsx
+++ src/Icons/SectionIcon.jsx
@@ -1,4 +1,4 @@
-export default function SectionIcon({ size = 14, strokeWidth = 10 }) {
+export default function SectionIcon({ size = 14 }) {
   return (
     <svg width={size} height={size} viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect x="1" y="7" width="32" height="20" rx="1" stroke="currentColor" strokeWidth="2" />
