c8b66471835c2af6ffe57989ca03a5fbc0c89277
Md Rubel Amin <mdrubelamin2@gmail.com>
2023-07-13 17:30:37 +0600

    feat: sure triggers integ

---
+++ src/components/AllIntegrations/SureTriggers/SureTriggers.jsx
@@ -0,0 +1,58 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useState } from 'react'
+import 'react-multiple-select-dropdown-lite/dist/index.css'
+import { useNavigate, useParams } from 'react-router-dom'
+import tutorialLinks from '../../../Utils/StaticData/tutorialLinks'
+import SnackMsg from '../../Utilities/SnackMsg'
+import Steps from '../../Utilities/Steps'
+import TutorialLink from '../../Utilities/TutorialLink'
+import { saveIntegConfig } from '../IntegrationHelpers/IntegrationHelpers'
+import WebHooksIntegration from '../IntegrationHelpers/WebHooksIntegration'
+import WebHooksStepTwo from '../IntegrationHelpers/WebHooksStepTwo'
+
+function SureTriggers({ formFields, setIntegration, integrations, allIntegURL }) {
+  const history = useNavigate()
+  const { formID } = useParams()
+  const [step, setstep] = useState(1)
+  const [snack, setSnackbar] = useState({ show: false })
+  const [sureTriggers, setSureTriggers] = useState({
+    name: 'SureTriggers Web Hooks',
+    type: 'SureTriggers',
+    method: 'POST',
+    url: '',
+  })
+
+  return (
+    <div>
+      <SnackMsg snack={snack} setSnackbar={setSnackbar} />
+      <div className="txt-center w-9 mt-2 cal-width"><Steps step={2} active={step} /></div>
+      <TutorialLink
+        title={tutorialLinks.sureTriggers.title}
+        youTubeLink={tutorialLinks.sureTriggers.link}
+      />
+      {/* STEP 1 */}
+      <div className="btcd-stp-page" style={{ ...{ width: step === 1 && 900 }, ...{ height: step === 1 && `${100}%` } }}>
+        <WebHooksIntegration
+          formID={formID}
+          formFields={formFields}
+          webHooks={sureTriggers}
+          setWebHooks={setSureTriggers}
+          step={step}
+          setstep={setstep}
+          setSnackbar={setSnackbar}
+          create
+        />
+      </div>
+
+      {/* STEP 2 */}
+      <div className="btcd-stp-page" style={{ width: step === 2 && 900, minHeight: step === 2 && `${900}px` }}>
+        <WebHooksStepTwo
+          step={step}
+          saveConfig={() => saveIntegConfig(integrations, setIntegration, allIntegURL, sureTriggers, history)}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default SureTriggers
