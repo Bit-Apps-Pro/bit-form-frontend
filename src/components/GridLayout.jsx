bd9f8f1a55025bce9b30810cec7d468e3e5d9afe
Md RH Maruf <mdrobiulhasan2620@gmail.com>
2023-09-06 15:24:11 +0600

    Refactor: Reduce top margin of grid wrapper

--- src/components/GridLayout.jsx
+++ src/components/GridLayout.jsx
@@ -636,7 +636,7 @@ function GridLayout({ newData, setNewData, style: v1Styles, gridWidth, setAlertM
       {styleMode && <RenderGridLayoutStyle />}
 
       <Scrollbars autoHide style={{ overflowX: 'hidden' }}>
-        <div id={`f-${formID}`} style={{ padding: BUILDER_PADDING.all, margin: '23px 13px 400px 0', border: '1px solid lightblue' }} className={draggingField && breakpoint === 'lg' ? 'isDragging' : ''}>
+        <div id={`f-${formID}`} style={{ padding: BUILDER_PADDING.all, margin: '2px 13px 400px 0', border: '1px solid lightblue' }} className={draggingField && breakpoint === 'lg' ? 'isDragging' : ''}>
           <div className={`_frm-bg-b${formID}`} data-dev-_frm-bg={formID}>
             <div className={`_frm-b${formID}`} data-dev-_frm={formID}>
               <StepContainer className={`step-continer-${formID}`}>
