1dd1b5ea2862cfe84bd05571bac5d040b068696b
Md RH Maruf <mdrobiulhasan2620@gmail.com>
2023-09-06 11:38:08 +0600

    Refactor: formInfo Property add to bf_globals
    when multi step form active

--- packages/bit-multi-step-form/src/bit-multi-step-form.js
+++ packages/bit-multi-step-form/src/bit-multi-step-form.js
@@ -4,11 +4,12 @@ export default function initMultiStepForm(formContentId = null) {
   const contentIds = formContentId ? [formContentId] : Object.keys(window?.bf_globals || {})
   contentIds.forEach(contentId => {
     const props = window.bf_globals[contentId]
-    if (props.multiStepForm) {
+    const multiStepSettings = props.formInfo?.multiStepSettings
+    if (multiStepSettings) {
       const { formId } = props
       const form = bfSelect(`#form-${contentId}`)
       const container = bfSelect(`._frm-b${formId}-stp-cntnr`, form)
-      props.inits.multi_step_form = new BitMultiStepForm(container, { contentId })
+      props.inits.multi_step_form = new BitMultiStepForm(container, { contentId, ...multiStepSettings })
     }
   })
 }
