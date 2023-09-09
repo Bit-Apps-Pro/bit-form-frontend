cf51215df6a7a2cc482e30a135e363ae188e03de
Md RH Maruf <mdrobiulhasan2620@gmail.com>
2023-09-09 17:55:55 +0600

    Fix: Default value set to parameter

--- src/components/style-new/themes/2_atlassian/multiStepStyle_2_atlassian.js
+++ src/components/style-new/themes/2_atlassian/multiStepStyle_2_atlassian.js
@@ -1,8 +1,7 @@
 import multiStepStyle_1_bitformDefault from '../1_bitformDefault/multiStepStyle_1_bitformDefaullt'
 
 // eslint-disable-next-line camelcase
-export default function multiStepeStyle_2_atlassian({ formId, breakpoint, direction, colorScheme }) {
-  console.log('multiStepeStyle_2_atlassian', colorScheme, breakpoint)
+export default function multiStepeStyle_2_atlassian({ formId, breakpoint = 'lg', direction, colorScheme = 'light' }) {
   if (breakpoint === 'lg' && colorScheme === 'light') {
     return multiStepStyle_1_bitformDefault({ formId, breakpoint, direction, colorScheme })
   }
