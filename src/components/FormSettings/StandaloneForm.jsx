c4d178b30c2e7553b3ceeac2e95bdbddbc200c0f
Md Rubel Amin <mdrubelamin2@gmail.com>
2023-10-18 12:44:11 +0600

    tmp

--- src/components/FormSettings/StandaloneForm.jsx
+++ src/components/FormSettings/StandaloneForm.jsx
@@ -1,18 +1,23 @@
 import { useAtom, useAtomValue, useSetAtom } from 'jotai'
 import { create } from 'mutative'
+import { useRef } from 'react'
 import { useFela } from 'react-fela'
 import { $bits, $formId, $formInfo, $updateBtn } from '../../GlobalStates/GlobalStates'
-import { IS_PRO } from '../../Utils/Helpers'
+import { IS_PRO, isObject } from '../../Utils/Helpers'
 import tutorialLinks from '../../Utils/StaticData/tutorialLinks'
+import { copyToClipboard } from '../../Utils/globalHelpers'
 import { __ } from '../../Utils/i18nwrap'
+import ut from '../../styles/2.utilities'
+import SizeControl from '../CompSettings/StyleCustomize/ChildComp/SizeControl'
 import CopyText from '../Utilities/CopyText'
 import SingleToggle2 from '../Utilities/SingleToggle2'
-import { assignNestedObj } from '../style-new/styleHelpers'
-import { RenderPortal } from '../../RenderPortal'
-import ColorPicker from '../CompSettings/StyleCustomize/ChildComp/ColorPicker'
-import ut from '../../styles/2.utilities'
+import { assignNestedObj, jsObjtoCssStr } from '../style-new/styleHelpers'
+import BorderControlUtil from '../style-new/util-components/BorderControlUtil'
+import BoxSizingUtil from '../style-new/util-components/BoxSizingUtil'
+import ColorPickerUtil from '../style-new/util-components/ColorPickerUtil'
 
 export default function StandaloneForm() {
+  const textareaRef = useRef(null)
   const [formInfo, setFormInfo] = useAtom($formInfo)
   const { standaloneSettings = {} } = formInfo
   const setUpdateBtn = useSetAtom($updateBtn)
@@ -29,6 +34,22 @@ export default function StandaloneForm() {
     }))
 
     setUpdateBtn(prevState => ({ ...prevState, unsaved: true }))
+  }
+
+  const handleStyles = (path, val) => {
+    if (!IS_PRO) return true
+    const statePath = `standaloneSettings->${path}`
+    setFormInfo(oldConf => create(oldConf, draftConf => {
+      if (isObject(val)) {
+        Object.keys(val).forEach(k => {
+          assignNestedObj(draftConf, `${statePath}->${k}`, val[k])
+        })
+      } else assignNestedObj(draftConf, statePath, val)
+      if (draftConf.standaloneSettings?.styles) {
+        const stylescss = jsObjtoCssStr(draftConf.standaloneSettings.styles)
+        console.log({ stylescss })
+      }
+    }))
   }
 
   console.log({ formInfo })
@@ -84,43 +105,90 @@ export default function StandaloneForm() {
               className="flx"
             />
             <label htmlFor="disable_loggin_user">
-              {__('Save draft only for logged in users')}
+              {__('Visible only for logged in users')}
             </label>
           </div>
-          <div className="d-flx">
-            <div className="w-3">
+          <div className="w-5">
+            <div>
+              <h4>Share via Direct URL</h4>
+              <CopyText
+                value={standaloneUrl}
+                className="field-key-cpy w-12 ml-0"
+                readOnly
+              />
+            </div>
+            <div>
+              <h4>Embed via HTML Code</h4>
+              <textarea ref={textareaRef} rows={5} readOnly className={css(ut.w10, st.embed)} onClick={() => copyToClipboard({ ref: textareaRef })}>
+                {`<iframe id="bit-form" width="100%" height="500px" style="min-height: 500px; width: 100%" frameborder="0" src="${standaloneUrl}&embedded=1"></iframe>`}
+              </textarea>
+            </div>
+            <div className={css(ut.w5)}>
+              <h4>Styling</h4>
               <div>
-                <h4>Share via Direct URL</h4>
-                <CopyText
-                  value={standaloneUrl}
-                  className="field-key-cpy w-12 ml-0"
-                  readOnly
-                />
-              </div>
-              <div>
-                <h4>Embed via HTML Code</h4>
-                <textarea className="w-10" rows={5} readOnly>
-                  {`<iframe id="bit-form" width="100%" height="500px" style="min-height: 500px; width: 100%" frameborder="0" src="${standaloneUrl}&embedded=1"></iframe>`}
-                </textarea>
-              </div>
-              <div>
-                <h4>Styling</h4>
-                <div>
-                  <h5>Body</h5>
-                  <div className={css(ut.flxc, ut.g10)}>
-                    <p>Background Color</p>
-                    <ColorPicker value="#ddd" />
-                  </div>
+                <h5>Body</h5>
+                <div className={css(st.prop)}>
+                  <p>Background</p>
+                  <ColorPickerUtil value={standaloneSettings?.styles?.['.standalone-form-container'] || ''} onChangeHandler={val => handleStyles('styles->.standalone-form-container', val)} colorProp="background-color" />
                 </div>
               </div>
-            </div>
-            <div className="w-6">
-              <h4>Preview</h4>
-              <iframe title="bit-form" id="bit-form" width="100%" height="500px" style={{ minHeight: '500px', width: '100%' }} frameBorder="0" src={`${standaloneUrl}&embedded=1`} />
+              <div className={css(ut.flxClm)}>
+                <h5>Wrapper</h5>
+                <div className={css(st.prop)}>
+                  <p>Height</p>
+                  <SizeControl
+                    // className={css(style.select)}
+                    width={150}
+                    max={1000}
+                    // inputHandler={handleFormWidth}
+                    // sizeHandler={({ unitKey, unitValue }) => handleFormWidth({ value: unitValue, unit: unitKey })}
+                    // value={(formWidth && getNumFromStr(formWidth)) || ''}
+                    // unit={(formWidth && getStrFromStr(formWidth)) || 'px'}
+                    sliderWidth="40%"
+                    actualValue="auto"
+                  />
+                  {/* <ColorPickerUtil value={standaloneSettings?.styles?.wrapper?.background || ''} onChangeHandler={val => handleChanges('styles->wrapper->background', val)} /> */}
+                </div>
+                <div className={css(st.prop)}>
+                  <p>Width</p>
+                  <SizeControl
+                    // className={css(style.select)}
+                    width={150}
+                    max={1000}
+                    // inputHandler={handleFormWidth}
+                    // sizeHandler={({ unitKey, unitValue }) => handleFormWidth({ value: unitValue, unit: unitKey })}
+                    // value={(formWidth && getNumFromStr(formWidth)) || ''}
+                    // unit={(formWidth && getStrFromStr(formWidth)) || 'px'}
+                    sliderWidth="40%"
+                    actualValue="auto"
+                  />
+                </div>
+                <div className={css(st.prop)}>
+                  <p>Background</p>
+                  <ColorPickerUtil value={standaloneSettings?.styles?.['.standalone-form-wrapper'] || ''} onChangeHandler={val => handleStyles('styles->.standalone-form-wrapper', val)} colorProp="background-color" />
+                </div>
+                <div className={css(st.prop)}>
+                  <p>Border</p>
+                  <BorderControlUtil value={standaloneSettings?.styles?.['.standalone-form-wrapper']?.border} onChangeHandler={val => handleChanges('styles->.standalone-form-wrapper->border', val)} />
+                </div>
+                <div className={css(st.prop)}>
+                  <p>Padding</p>
+                  <BoxSizingUtil value={standaloneSettings?.styles?.['.standalone-form-wrapper']?.padding} onChangeHandler={val => handleChanges('styles->.standalone-form-wrapper->padding', val)} />
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   )
+}
+
+const st = {
+  embed: {
+    curp: 1,
+  },
+  prop: {
+    flx: 'between',
+  },
 }
