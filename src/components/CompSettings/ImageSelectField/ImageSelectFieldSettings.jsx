6dcb915f78e924f332bfea033d5b48900164f9aa
Md Rubel Amin <mdrubelamin2@gmail.com>
2023-11-05 11:32:19 +0600

    chore: max & minimum options for image select

--- src/components/CompSettings/ImageSelectField/ImageSelectFieldSettings.jsx
+++ src/components/CompSettings/ImageSelectField/ImageSelectFieldSettings.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable no-param-reassign */
-import { useAtom } from 'jotai'
+import { useAtom, useSetAtom } from 'jotai'
 import { create } from 'mutative'
-import { memo, useEffect, useState } from 'react'
+import { memo, useState } from 'react'
 import { useFela } from 'react-fela'
 import { useParams } from 'react-router-dom'
-import { $fields } from '../../../GlobalStates/GlobalStates'
+import { $fields, $updateBtn } from '../../../GlobalStates/GlobalStates'
 import { $styles } from '../../../GlobalStates/StylesState'
 import AddIcon from '../../../Icons/AddIcon'
 import BottomLefArrowIcn from '../../../Icons/BottomLefArrowIcn'
@@ -14,14 +14,16 @@ import CloseIcn from '../../../Icons/CloseIcn'
 import TopLeftArrowIcn from '../../../Icons/TopLeftArrowIcn'
 import TopRightArrowIcn from '../../../Icons/TopRightArrowIcn'
 import { addToBuilderHistory, deleteNestedObj, reCalculateFldHeights, setRequired } from '../../../Utils/FormBuilderHelper'
-import { deepCopy } from '../../../Utils/Helpers'
+import { IS_PRO, deepCopy } from '../../../Utils/Helpers'
 import tippyHelperMsg from '../../../Utils/StaticData/tippyHelperMsg'
 import { isDev } from '../../../Utils/config'
+import { mergeNestedObj } from '../../../Utils/globalHelpers'
 import { __ } from '../../../Utils/i18nwrap'
 import FieldStyle from '../../../styles/FieldStyle.style'
 import Btn from '../../Utilities/Btn'
 import Modal from '../../Utilities/Modal'
 import StyleSegmentControl from '../../Utilities/StyleSegmentControl'
+import { assignNestedObj } from '../../style-new/styleHelpers'
 import AdminLabelSettings from '../CompSettingsUtils/AdminLabelSettings'
 import FieldHideSettings from '../CompSettingsUtils/FieldHideSettings'
 import FieldLabelSettings from '../CompSettingsUtils/FieldLabelSettings'
@@ -35,10 +37,12 @@ import SimpleAccordion from '../StyleCustomize/ChildComp/SimpleAccordion'
 import FieldSettingTitle from '../StyleCustomize/FieldSettingTitle'
 import SizeAndPosition from '../StyleCustomize/StyleComponents/SizeAndPosition'
 import AllowMultipleImage from './AllowMultipleImage'
-import tickPosition from './tickPosition'
-import { mergeNestedObj } from '../../../Utils/globalHelpers'
-import { assignNestedObj } from '../../style-new/styleHelpers'
 import OptionLableShowHide from './OptionLableShowHide'
+import tickPosition from './tickPosition'
+import ErrorMessageSettings from '../CompSettingsUtils/ErrorMessageSettings'
+import SingleToggle from '../../Utilities/SingleToggle'
+import MinimumOptionSettings from '../CompSettingsUtils/MinimumOptionSettings'
+import MaximumOptionSettings from '../CompSettingsUtils/MaximumOptionSettings'
 
 function ImageSelectFieldSettings() {
   const { css } = useFela()
@@ -46,6 +50,7 @@ function ImageSelectFieldSettings() {
   const [fields, setFields] = useAtom($fields)
   const fieldData = deepCopy(fields[fldKey])
   const options = deepCopy(fields[fldKey].opt)
+  const setUpdateBtn = useSetAtom($updateBtn)
   const adminLabel = fieldData.adminLbl || ''
   const { itemSize } = fieldData
 
@@ -77,7 +82,9 @@ function ImageSelectFieldSettings() {
       type: 'option_list_modify',
       state: { fields: allFields, fldKey },
     })
-    reCalculateFldHeights(fldKey)
+    setTimeout(() => {
+      reCalculateFldHeights(fldKey)
+    }, 100)
   }
 
   function setItemSize({ target: { value } }) {
@@ -132,8 +139,15 @@ function ImageSelectFieldSettings() {
       type: 'tick_position_update',
       state: { fields: allFields, fldKey },
     })
-    // reCalculateFldHeights(fldKey)
+    reCalculateFldHeights(fldKey)
   }
+
+  const handleSettings = (path, val) => {
+    setFields(oldFlds => create(oldFlds, draftFlds => {
+      assignNestedObj(draftFlds[fldKey], path, val)
+    }))
+  }
+
   return (
     <div className="">
       <FieldSettingTitle
@@ -174,13 +188,27 @@ function ImageSelectFieldSettings() {
 
       <FieldSettingsDivider />
 
+      {
+        fieldData.inpType === 'checkbox' && (
+          <>
+            <MinimumOptionSettings tip="Set minimum number to be selected for checkbox option" />
+
+            <FieldSettingsDivider />
+
+            <MaximumOptionSettings tip="Set maximum number to be selected for checkbox option" />
+
+            <FieldSettingsDivider />
+          </>
+        )
+      }
+
       <SimpleAccordion
         id="opt-clm-stng"
         title={__('Item Size')}
         className={css(FieldStyle.fieldSection)}
         isPro
         proProperty="optionColumn"
-        tip={__('Specify your item size.')}
+        tip={__("Specify item's minimum size (in pixels)")}
       >
         <div className={css(FieldStyle.placeholder)}>
           <input
