d2644fc10102b0a6114b4ba0cfdbf0840b406621
mdabbas-cse <gmabbas44@gmail.com>
2023-11-19 18:18:47 +0600

    new: add turnstile captcha preview

--- src/components/AllCaptcha/Turnstile.jsx
+++ src/components/AllCaptcha/Turnstile.jsx
@@ -1,19 +1,23 @@
 import { useAtom, useAtomValue } from 'jotai'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useFela } from 'react-fela'
 import { Link } from 'react-router-dom'
 import { $turnstile } from '../../GlobalStates/AppSettingsStates'
 import { $bits } from '../../GlobalStates/GlobalStates'
-import { __ } from '../../Utils/i18nwrap'
-import SnackMsg from '../Utilities/SnackMsg'
-import app from '../../styles/app.style'
-import CopyText from '../Utilities/CopyText'
-import LoaderSm from '../Loaders/LoaderSm'
 import { deepCopy } from '../../Utils/Helpers'
 import bitsFetch from '../../Utils/bitsFetch'
+import { loadScript, removeScript } from '../../Utils/globalHelpers'
+import { __ } from '../../Utils/i18nwrap'
+import app from '../../styles/app.style'
+import LoaderSm from '../Loaders/LoaderSm'
+import CopyText from '../Utilities/CopyText'
+import SnackMsg from '../Utilities/SnackMsg'
 
 export default function Turnstile() {
   const { css } = useFela()
+  const turnstileWrapElmRef = useRef(null)
+  const turnstileResetIntervalRef = useRef(null)
+  const turnstileCaptchaId = useRef(null)
 
   const [turnstile, setTurnstile] = useAtom($turnstile)
   const bits = useAtomValue($bits)
@@ -50,6 +54,53 @@ export default function Turnstile() {
       })
       .catch(() => setLoading(false))
   }
+
+  // useEffect(() => {
+  //   window.onloadTurnstileCallback = onloadTurnstileCallback
+
+  //   const src = 'https://challenges.cloudflare.com/turnstile/v0/api.js?onload=onloadTurnstileCallback'
+  //   const srcData = {
+  //     src,
+  //     integrity: '',
+  //     id: 'bf-turnstile-script',
+  //     async: true,
+  //     defer: true,
+  //   }
+  //   if (turnstile?.siteKey) {
+  //     loadScript(srcData)
+  //   }
+
+  //   return () => {
+  //     clearInterval(turnstileResetIntervalRef.current)
+  //     removeScript(srcData.id)
+  //   }
+  // }, [turnstile?.siteKey])
+  // function onloadTurnstileCallback() {
+  //   let turnstileCaptchaElm = document.querySelector('.turnstile-captcha-preview')
+  //   if (turnstileCaptchaId.current !== null) {
+  //     turnstileCaptchaElm?.remove()
+  //     window.turnstile?.reset(turnstileCaptchaId.current)
+  //     turnstileCaptchaElm = document.createElement('div')
+  //     turnstileCaptchaElm.classList.add('turnstile-captcha-preview')
+  //     turnstileWrapElmRef.current.appendChild(turnstileCaptchaElm)
+  //   }
+
+  //   turnstileCaptchaId.current = window.turnstile.render(turnstileCaptchaElm, {
+  //     sitekey: turnstile?.siteKey || '1x00000000000000000000AA',
+  //     theme: 'auto',
+  //     size: 'normal',
+  //   })
+
+  //   if (turnstileResetIntervalRef.current) {
+  //     clearInterval(turnstileResetIntervalRef.current)
+  //   }
+  //   turnstileResetIntervalRef.current = setInterval(() => {
+  //     window.turnstile.reset(turnstileCaptchaId.current)
+  //   }, 30000)
+  // }
+  // useEffect(() => {
+  //   if (window.turnstile) onloadTurnstileCallback()
+  // }, [turnstile])
 
   return (
     <div>
@@ -122,6 +173,12 @@ export default function Turnstile() {
               {__('Cloudflare Turnstile reCAPTCHA Admin')}
             </a>
           </p>
+        </div>
+        <div className="mt-2">
+          <span>{__('Preview')}</span>
+          <div className="turnstile-wrp" ref={turnstileWrapElmRef}>
+            <div className="turnstile-captcha-preview" />
+          </div>
         </div>
         <button
           onClick={() => saveCaptcha()}
